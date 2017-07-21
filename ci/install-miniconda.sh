196d32d250525b2cb55f397eb111fbe86ac5f5ad
Leonardo Uieda <leouieda@gmail.com>
2017-07-20 16:29:38 -1000

    Don't need to set conda prefix

--- ci/install-miniconda.sh
+++ ci/install-miniconda.sh
@@ -7,14 +7,16 @@ MINICONDA_URL="http://repo.continuum.io/miniconda"
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     MINICONDA_FILE=Miniconda3-latest-MacOSX-x86_64.sh
-    export PATH=$CONDA_PREFIX/bin:$PATH
+    CONDA_PREFIX=/Users/travis/miniconda3
 else
     MINICONDA_FILE=Miniconda3-latest-Linux-x86_64.sh
-    export PATH=$CONDA_PREFIX/bin:$PATH
+    CONDA_PREFIX=/home/travis/miniconda3
 fi
 wget $MINICONDA_URL/$MINICONDA_FILE -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b
+
+export PATH=$CONDA_PREFIX/bin:$PATH
 
 # Workaround for https://github.com/travis-ci/travis-ci/issues/6522
 # Turn off exit on failure.
