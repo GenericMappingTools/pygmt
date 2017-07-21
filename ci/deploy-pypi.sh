bac1158a7b86f02daaaa6c3bba6da89ea4b78bd0
Leonardo Uieda <leouieda@gmail.com>
2017-07-21 11:24:30 -1000

    Use deploy section on travis config for PyPI

--- ci/deploy-pypi.sh
+++ ci/deploy-pypi.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 #
-# Push the built HTML pages to the gh-pages branch.
-# Only runs if this is not a pull request and is on the master branch.
-#
-# Based on
-# http://sleepycoders.blogspot.com.au/2013/03/sharing-travis-ci-generated-files.html
-# and https://github.com/richfitz/wood
+# Package and upload to PyPI
 
 # To return a failure if any commands inside fail
 set -e
 
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] &&
-   [[ ! -z "$TRAVIS_TAG" ]] &&
-   [[ "$TRAVIS_BRANCH" == "$TRAVIS_TAG" ]];
-then
-    echo ""
-    echo "Building packages for version ${TRAVIS_TAG}"
-    echo ""
-    # Build source distributions and wheels
-    python setup.py sdist bdist_wheel
+echo ""
+echo "Building source package and wheels for ${TRAVIS_TAG}"
+echo ""
+# Build source distributions and wheels
+python setup.py sdist bdist_wheel
 
-    # Upload to PyPI. Credentials are set using env variables.
-    twine upload --skip-existing dist/*
-else
-    echo "Not deploying to PyPI."
-    echo "  PR: $TRAVIS_PULL_REQUEST"
-    echo "  branch: $TRAVIS_BRANCH"
-    echo "  tag: $TRAVIS_TAG"
-fi
+echo ""
+echo "Packages built:"
+ls dist
+
+echo ""
+echo "Deploy to PyPI using twine."
+echo ""
+# Upload to PyPI. Credentials are set using the TWINCE_PASSWORD and
+# TWINE_USERNAME env variables.
+twine upload --skip-existing dist/*
 
 # Workaround for https://github.com/travis-ci/travis-ci/issues/6522
 # Turn off exit on failure.
