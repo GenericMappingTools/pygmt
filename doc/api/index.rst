af3cb25b379f668ea89aeff3f8538431ffae698c
Leonardo Uieda <leouieda@gmail.com>
2018-02-13 18:47:38 -1000

    Add datasets pkg + func to load tut_quakes data
    Start the gmt.datasets package with a function to load the tut_quakes
    dataset from the GMT tutorials. Use it in the first-steps tutorial and
    include the package in the API reference.
    Needs tests.

--- doc/api/index.rst
+++ doc/api/index.rst
@@ -4,6 +4,7 @@ API Reference
 =============
 
 .. include:: gmt.rst
+.. include:: gmt.datasets.rst
 .. include:: gmt.exceptions.rst
 .. include:: gmt.clib.rst
 .. include:: gmt.helpers.rst
---
+++ gmt/datasets/tutorial.py
@@ -0,0 +1,33 @@
+"""
+Functions to load sample data from the GMT tutorials.
+"""
+import os
+
+import pandas as pd
+
+from .. import which
+
+
+def load_japan_quakes():
+    """
+    Load a table of earthquakes around Japan as a pandas.Dataframe.
+
+    Data is from the NOAA NGDC database. This is the ``@tut_quakes.ngdc``
+    dataset used in the GMT tutorials.
+
+    The data are downloaded to a cache directory (usually ``~/.gmt/cache``) the
+    first time you invoke this function. Afterwards, it will load the data from
+    the cache. So you'll need an internet connection the first time around.
+
+    Returns
+    -------
+    data :  pandas.Dataframe
+        The data table. Columns are year, month, day, latitude, longitude,
+        depth (in km), and magnitude of the earthquakes.
+
+    """
+    fname = which('@tut_quakes.ngdc', download='c')
+    data = pd.read_table(fname, header=1, sep='\s+')
+    data.columns = ['year', 'month', 'day', 'latitude', 'longitude',
+                    'depth_km', 'magnitude']
+    return data
