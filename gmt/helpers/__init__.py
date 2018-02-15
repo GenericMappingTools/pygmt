27088061943fc3989e72511258bc8040b0068096
Leonardo Uieda <leouieda@gmail.com>
2018-02-14 21:32:04 -1000

    Add preview option using NASA WorldWind Web (#132)
    Use the [WorldWind Web](https://worldwind.arc.nasa.gov/web/) javascript 
    virtual globe to display a KML preview of the figure in the notebook.
    The preview files (KML and PNG) need to be in the same directory as the
    notebook for things to work. The /tmp dir is outside of the range of the
    notebook server. So they need to be deleted separately.
    Include an option to not delete the preview files so that the globe
    works when the notebook is converted to HTML (for the docs and
    nbviewer).
    Need to include require.js in order to load the javascript library in
    the documentation the same way as is done in the notebook.
    Not extensively tested but the notebook works.

--- gmt/helpers/__init__.py
+++ gmt/helpers/__init__.py
@@ -5,3 +5,4 @@ from .decorators import fmt_docstring, use_alias, kwargs_to_strings
 from .tempfile import GMTTempFile, unique_name
 from .utils import data_kind, dummy_context, build_arg_string, \
     is_nonstr_iter, launch_external_viewer
+from .worldwind import worldwind_show_kml
