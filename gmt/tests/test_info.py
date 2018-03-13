d3708cf24085d322d9b5d3fe07cf29e8d9393d4e
Dongdong Tian <seisman.info@gmail.com>
2018-03-13 22:04:25 +0800

    fix tests

--- gmt/tests/test_info.py
+++ gmt/tests/test_info.py
@@ -12,7 +12,7 @@ data_fname = os.path.join(TEST_DATA_DIR, 'points.txt')
 def test_info():
     "Make sure info works"
     output = info(fname=data_fname)
-    assert output == '{}: N = 20 <11.5309/61.7074> <-2.9289/7.8648> <0.1412/0.9338>\n'.format(data_fname)
+    assert output == '''{}: N = 20 <11.5309/61.7074> <-2.9289/7.8648> <0.1412/0.9338>\n'''.format(data_fname)
 
 
 def test_info_c():
@@ -23,14 +23,14 @@ def test_info_c():
 
 def test_info_i():
     "Make sure the I option works"
-    output = info(fname=data_fname, I=True)
+    output = info(fname=data_fname, I=0.1)
     assert output == '-R11.5/61.8/-3/7.9\n'
 
 
 def test_info_c_i():
     "Make sure the C and I options work together"
-    output = info(fname=data_fname, C=True, I=True)
-    assert output == '11.530961.8648-37.92890.14120.9338\n'
+    output = info(fname=data_fname, C=True, I=0.1)
+    assert output == '11.5 61.8 -3 7.9 0.1412 0.9338\n'
 
 
 def test_info_t():
