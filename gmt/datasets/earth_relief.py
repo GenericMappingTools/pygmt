df0e4934c3fefd88e46e1461cbec5542e3c49869
Dongdong Tian <seisman.info@gmail.com>
2018-06-22 17:54:25 +0800

    Extend `load_earth_relief` to support 30s and 15s grids

--- gmt/datasets/earth_relief.py
+++ gmt/datasets/earth_relief.py
@@ -1,5 +1,5 @@
 """
-Functions to download the Earth relief datasets from the GMT data sever.
+Functions to download the Earth relief datasets from the GMT data server.
 The grids are available in various resolutions.
 """
 import xarray as xr
@@ -17,31 +17,64 @@ def load_earth_relief(resolution="60m"):
     the cache. So you'll need an internet connection the first time around.
 
     These grids can also be accessed by passing in the file name
-    ``'@earth_relief_XXm'`` to any grid plotting/processing function. Higher
-    resolution SRTM grids can be accessed by using the ``'@earth_relief_XXs'``
-    special file names but they cannot be loaded using this function.
+    ``'@earth_relief_XXm'`` or ``'@earth_relief_XXs'`` to any grid
+    plotting/processing function.
 
     Parameters
     ----------
     resolution : str
-        The grid resolution. The prefix ``m`` stands for arc-minute. It can be
-        ``'60m'``, ``'30m'``, ``'10m'``, ``'05m'``, ``'02m'``, or ``'01m'``.
+        The grid resolution. The suffix ``m`` and ``s`` stand for arc-minute
+        and arc-second. It can be ``'60m'``, ``'30m'``, ``'10m'``, ``'05m'``,
+        ``'02m'``, ``'01m'``, ``'30s'`` or ``'15s'``.
 
     Returns
     -------
-    grid :  xarray.DataArray
+    grid : xarray.DataArray
         The Earth relief grid. Coordinates are latitude and longitude in
         degrees. Relief is in meters.
 
     """
+    _is_valid_resolution(resolution)
+    fname = which("@earth_relief_{}".format(resolution), download="u")
+    grid = xr.open_dataarray(fname)
+    return grid
+
+
+def _is_valid_resolution(resolution):
+    """
+    Check if a resolution is valid for the global Earth relief grid.
+
+    Parameters
+    ----------
+    resolution : str
+        Same as the input for load_earth_relief
+
+    Raises
+    ------
+    GMTInvalidInput
+        If given resolution is not valid.
+
+    Examples
+    --------
+
+    >>> _is_valid_resolution("60m")
+    >>> _is_valid_resolution("5m")
+    Traceback (most recent call last):
+        ...
+    gmt.exceptions.GMTInvalidInput: Invalid Earth relief resolution '5m'.
+    >>> _is_valid_resolution("15s")
+    >>> _is_valid_resolution("01s")
+    Traceback (most recent call last):
+        ...
+    gmt.exceptions.GMTInvalidInput: Invalid Earth relief resolution '01s'.
+
+    """
     valid_resolutions = ["{:02d}m".format(res) for res in [60, 30, 10, 5, 2, 1]]
+    valid_resolutions.extend(["{:02d}s".format(res) for res in [30, 15]])
     if resolution not in valid_resolutions:
         raise GMTInvalidInput(
             "Invalid Earth relief resolution '{}'.".format(resolution)
         )
-    fname = which("@earth_relief_{}".format(resolution), download="u")
-    grid = xr.open_dataarray(fname)
-    return grid
 
 
 def _shape_from_resolution(resolution):
@@ -67,9 +100,18 @@ def _shape_from_resolution(resolution):
     (361, 721)
     >>> _shape_from_resolution('10m')
     (1081, 2161)
+    >>> _shape_from_resolution('30s')
+    (21601, 43201)
+    >>> _shape_from_resolution('15s')
+    (43201, 86401)
 
     """
-    minutes = int(resolution[:2])
-    nlat = 180 * 60 // minutes + 1
-    nlon = 360 * 60 // minutes + 1
+    _is_valid_resolution(resolution)
+    unit = resolution[2]
+    if unit == "m":
+        seconds = int(resolution[:2]) * 60
+    elif unit == "s":
+        seconds = int(resolution[:2])
+    nlat = 180 * 60 * 60 // seconds + 1
+    nlon = 360 * 60 * 60 // seconds + 1
     return (nlat, nlon)
