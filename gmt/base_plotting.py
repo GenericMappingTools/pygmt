e5845ba38df2b4c04a55132df0edede05ca29321
Malte Ziebarth <malte.ziebarth@fmvkb.de>
2018-08-18 01:06:41 +0200

    Add basic contour functionality (#212)
    The `Figure.contour` method plots contours from tabular data.
    Add tests and support for data in vectors, matrix, and files.

--- gmt/base_plotting.py
+++ gmt/base_plotting.py
@@ -271,6 +271,83 @@ class BasePlotting:
                 lib.call_module("plot", arg_str)
 
     @fmt_docstring
+    @use_alias(
+        R="region",
+        J="projection",
+        B="frame",
+        S="skip",
+        G="label_placement",
+        W="pen",
+        L="triangular_mesh_pen",
+        i="columns",
+        C="levels",
+    )
+    @kwargs_to_strings(R="sequence", i="sequence_comma")
+    def contour(self, x=None, y=None, z=None, data=None, **kwargs):
+        """
+        Contour table data by direct triangulation.
+
+        Takes a matrix, (x,y,z) pairs, or a file name as input and plots lines,
+        polygons, or symbols at those locations on a map.
+
+        Must provide either *data* or *x*, *y*, and *z*.
+
+        [TODO: Insert more documentation]
+
+        {gmt_module_docs}
+
+        {aliases}
+
+        Parameters
+        ----------
+        x, y, z : 1d arrays
+            Arrays of x and y coordinates and values z of the data points.
+        data : str or 2d array
+            Either a data file name or a 2d numpy array with the tabular data.
+        {J}
+        {R}
+        A : bool or str
+            ``'[m|p|x|y]'``
+            By default, geographic line segments are drawn as great circle
+            arcs. To draw them as straight lines, use *A*.
+        {B}
+        C : Contour file or level(s)
+        D : Dump contour coordinates
+        E : Network information
+        G : Placement of labels
+        I : Color the triangles using CPT
+        L : Pen to draw the underlying triangulation (default none)
+        N : Do not clip contours
+        Q : Minimum contour length
+            ``'[p|t]'``
+        S : Skip input points outside region
+            ``'[p|t]'``
+        {W}
+        X : Origin shift x
+        Y : Origin shift y
+
+
+        """
+        kwargs = self._preprocess(**kwargs)
+
+        kind = data_kind(data, x, y, z)
+        if kind == "vectors" and z is None:
+            raise GMTInvalidInput("Must provided both x, y, and z.")
+
+        with Session() as lib:
+            # Choose how data will be passed in to the module
+            if kind == "file":
+                file_context = dummy_context(data)
+            elif kind == "matrix":
+                file_context = lib.virtualfile_from_matrix(data)
+            elif kind == "vectors":
+                file_context = lib.virtualfile_from_vectors(x, y, z)
+
+            with file_context as fname:
+                arg_str = " ".join([fname, build_arg_string(kwargs)])
+                lib.call_module("contour", arg_str)
+
+    @fmt_docstring
     @use_alias(R="region", J="projection", B="frame")
     @kwargs_to_strings(R="sequence")
     def basemap(self, **kwargs):
