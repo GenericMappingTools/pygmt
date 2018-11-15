d2779e21ac3ab540cbfa54983057add4501f0460
Dongdong Tian <seisman.info@gmail.com>
2018-11-15 16:57:27 -0500

    add tests for image

---
+++ gmt/tests/test_image.py
@@ -0,0 +1,21 @@
+"""
+Tests image.
+"""
+import os
+
+import pytest
+import numpy as np
+
+from .. import Figure
+from ..exceptions import GMTInvalidInput
+
+
+TEST_IMG = os.path.join(os.path.dirname(__file__), "baseline", "test_logo.png")
+
+
+@pytest.mark.mpl_image_compare
+def test_image():
+    "Place images on map"
+    fig = Figure()
+    fig.image(TEST_IMG, D="x0/0+w1i", F="+pthin,blue")
+    return fig
