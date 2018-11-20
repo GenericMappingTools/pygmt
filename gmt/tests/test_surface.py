10539a951bc21820034b70b273b4ef4b35c07c6b
Wei Ji <weiji1994@Hotmail.com>
2018-11-20 15:29:46 +1300

    Add more tests for surface to check invalid inputs
    Getting code coverage up to 100% for gridding.py.

--- gmt/tests/test_surface.py
+++ gmt/tests/test_surface.py
@@ -1,9 +1,13 @@
 """
 Tests for surface
 """
+import pytest
+
 from .. import surface
 from .. import which
 from ..datasets import load_tut_ship
+from ..exceptions import GMTInvalidInput
+from ..helpers import data_kind
 
 
 def test_surface_input_file():
@@ -34,3 +38,23 @@ def test_surface_input_xyz():
         x=ship_data.x, y=ship_data.y, z=ship_data.z, I="5m", R="245/255/20/30"
     )
     return outputfile
+
+
+def test_surface_input_xy_no_z():
+    """
+    Run surface by passing in x and y, but no z
+    """
+    ship_data = load_tut_ship()
+    with pytest.raises(GMTInvalidInput):
+        surface(x=ship_data.x, y=ship_data.y, I="5m", R="245/255/20/30")
+
+
+def test_surface_wrong_kind_of_input():
+    """
+    Run surface using grid input that is not file/matrix/vectors
+    """
+    ship_data = load_tut_ship()
+    data = ship_data.z.to_xarray()  # convert pandas.Series to xarray.DataArray
+    assert data_kind(data) == "grid"
+    with pytest.raises(GMTInvalidInput):
+        surface(data=data, I="5m", R="245/255/20/30")
