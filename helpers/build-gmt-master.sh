98c21853c32b7476e11ce423a84d0c8ab8759b59
Leonardo Uieda <leouieda@gmail.com>
2019-01-21 13:52:09 +0000

    Set the environment variables globaly

--- helpers/build-gmt-master.sh
+++ helpers/build-gmt-master.sh
@@ -3,12 +3,6 @@
 
 set -e
 
-# Paths for GMT installation
-INSTALLDIR="${GMTINSTALLDIR:-$HOME/gmt-install-dir}"
-COASTLINEDIR="$INSTALLDIR/coast"
-PATH="$INSTALLDIR/bin:$PATH"
-LD_LIBRARY_PATH="$INSTALLDIR/lib:$LD_LIBRARY_PATH"
-
 mkdir -p "$INSTALLDIR"
 mkdir -p "$COASTLINEDIR"
 git clone --depth=1 --branch=master https://github.com/GenericMappingTools/gmt.git tmp
