436ea2d72c3b09799e76a2a6fdc3c87831cf16fc
Leonardo Uieda <leouieda@gmail.com>
2019-01-21 13:46:09 +0000

    Get around cloning into the cached directory

--- helpers/build-gmt-master.sh
+++ helpers/build-gmt-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Download and build the GMT master branch.
+# Download and build the GMT master branch. Used by TravisCI.
 
 set -e
 
@@ -11,7 +11,10 @@ LD_LIBRARY_PATH="$INSTALLDIR/lib:$LD_LIBRARY_PATH"
 
 mkdir -p "$INSTALLDIR"
 mkdir -p "$COASTLINEDIR"
-git clone --depth=1 --branch=master https://github.com/GenericMappingTools/gmt.git gmt-master
+git clone --depth=1 --branch=master https://github.com/GenericMappingTools/gmt.git tmp
+mkdir -p gmt-master
+cp -r tmp/* gmt-master
+rm -rf tmp
 cd gmt-master
 bash ci/download-coastlines.sh
 bash TEST=false ci/build-gmt.sh
