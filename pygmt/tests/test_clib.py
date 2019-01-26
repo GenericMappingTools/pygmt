183b710e52fc0feac3dc60a3489f166c50b2245d
Leonardo Uieda <leouieda@gmail.com>
2019-01-26 08:05:15 -0500

    Update pygmt/tests/test_clib.py
    Co-Authored-By: seisman <seisman.info@gmail.com>

--- pygmt/tests/test_clib.py
+++ pygmt/tests/test_clib.py
@@ -112,7 +112,7 @@ def test_clib_name():
     for linux in ["linux", "linux2", "linux3"]:
         assert clib_name(linux) == "libgmt.so"
     assert clib_name("darwin") == "libgmt.dylib"
-    assert clib_name("win32", True) == "gmt_w64.dll"
+    assert clib_name("win32", is_64bit=True) == "gmt_w64.dll"
     assert clib_name("win32", False) == "gmt_w32.dll"
     with pytest.raises(GMTOSError):
         clib_name("meh")
