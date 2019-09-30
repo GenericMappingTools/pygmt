d377320acd90d15d4f80ed4cdea86f960f4b72a6
Wei Ji <weiji.leong@vuw.ac.nz>
2019-10-01 09:51:52 +1300

    Revert "Bad fix for test_begin_end by ending global session"
    This reverts commit 57efa55aec121983c55e76c83bda60063e29771e.

--- pygmt/tests/test_session_management.py
+++ pygmt/tests/test_session_management.py
@@ -2,11 +2,9 @@
 Test the session management modules.
 """
 import os
-import pytest
 
 from ..session_management import begin, end
 from ..clib import Session
-from ..exceptions import GMTCLibError
 
 
 def test_begin_end():
@@ -14,14 +12,11 @@ def test_begin_end():
     Run a command inside a begin-end modern mode block.
     First, end the global session. When finished, restart it.
     """
-    with pytest.raises(GMTCLibError, match="Module 'end' failed with status code 78:"):
-        end()  # Kill the global session first
-
+    end()  # Kill the global session
     begin()
     with Session() as lib:
         lib.call_module("basemap", "-R10/70/-3/8 -JX4i/3i -Ba")
     end()
-
     begin()  # Restart the global session
     assert os.path.exists("pygmt-session.pdf")
     os.remove("pygmt-session.pdf")
