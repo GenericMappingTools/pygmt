58dba3137030702edb7815dd95c5d19091d2e55a
Liam Toney <toney.liam@gmail.com>
2019-10-28 23:42:36 -0800

    Add legend specfile test

--- pygmt/tests/test_legend.py
+++ pygmt/tests/test_legend.py
@@ -4,6 +4,7 @@ Tests for legend
 import pytest
 
 from .. import Figure
+from ..helpers import GMTTempFile
 
 
 @pytest.mark.mpl_image_compare
@@ -47,5 +48,51 @@ def test_legend_entries():
     fig.plot(data="@Table_5_11.txt", style="t0.15i", color="orange", l="Oranges")
 
     fig.legend(position="JTR+jTR")
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_legend_specfile():
+    """
+    Test specfile functionality.
+    """
+
+    specfile_contents = """
+G -0.1i
+H 24 Times-Roman My Map Legend
+D 0.2i 1p
+N 2
+V 0 1p
+S 0.1i c 0.15i p300/12 0.25p 0.3i This circle is hachured
+S 0.1i e 0.15i yellow 0.25p 0.3i This ellipse is yellow
+S 0.1i w 0.15i green 0.25p 0.3i This wedge is green
+S 0.1i f0.1i+l+t 0.25i blue 0.25p 0.3i This is a fault
+S 0.1i - 0.15i - 0.25p,- 0.3i A dashed contour
+S 0.1i v0.1i+a40+e 0.25i magenta 0.25p 0.3i This is a vector
+S 0.1i i 0.15i cyan 0.25p 0.3i This triangle is boring
+V 0 1p
+D 0.2i 1p
+N 1
+G 0.05i
+G 0.05i
+G 0.05i
+L 9 4 R Smith et al., @%5%J. Geophys. Res., 99@%%, 2000
+G 0.1i
+P
+T Let us just try some simple text that can go on a few lines.
+T There is no easy way to predetermine how many lines will be required,
+T so we may have to adjust the box height to get the right size box.
+"""
+
+    specfile = GMTTempFile().name
+
+    with open(specfile, "w") as file:
+        file.write(specfile_contents)
+
+    fig = Figure()
+
+    fig.basemap(projection="x6i", region=[0, 1, 0, 1], frame=True)
+    fig.legend(specfile, position="JTM+jCM+w5i")
 
     return fig
