c033a5988651459bc63cabceed0098f4f7c0b411
Wei Ji <weiji.leong@vuw.ac.nz>
2020-05-12 15:33:23 +1200

    Move special_params dictionary into config class

--- pygmt/modules.py
+++ pygmt/modules.py
@@ -12,26 +12,6 @@ from .helpers import (
 )
 from .exceptions import GMTInvalidInput
 
-SPECIAL_PARAMS = {
-    "FONT": [
-        "FONT_ANNOT_PRIMARY",
-        "FONT_ANNOT_SECONDARY",
-        "FONT_HEADING",
-        "FONT_LABEL",
-        "FONT_TAG",
-        "FONT_TITLE",
-    ],
-    "FONT_ANNOT": ["FONT_ANNOT_PRIMARY", "FONT_ANNOT_SECONDARY"],
-    "FORMAT_TIME_MAP": ["FORMAT_TIME_PRIMARY_MAP", "FORMAT_TIME_SECONDARY_MAP"],
-    "MAP_ANNOT_OFFSET": ["MAP_ANNOT_OFFSET_PRIMARY", "MAP_ANNOT_OFFSET_SECONDARY"],
-    "MAP_GRID_CROSS_SIZE": [
-        "MAP_GRID_CROSS_SIZE_PRIMARY",
-        "MAP_GRID_CROSS_SIZE_SECONDARY",
-    ],
-    "MAP_TICK_LENGTH": ["MAP_TICK_LENGTH_PRIMARY", "MAP_TICK_LENGTH_SECONDARY"],
-    "MAP_TICK_PEN": ["MAP_TICK_PEN_PRIMARY", "MAP_TICK_PEN_SECONDARY"],
-}
-
 
 @fmt_docstring
 def grdinfo(grid, **kwargs):
@@ -183,10 +163,32 @@ class config:  # pylint: disable=invalid-name
     def __init__(self, **kwargs):
         # Save values so that we can revert to their initial values
         self.old_defaults = {}
+        self.special_params = {
+            "FONT": [
+                "FONT_ANNOT_PRIMARY",
+                "FONT_ANNOT_SECONDARY",
+                "FONT_HEADING",
+                "FONT_LABEL",
+                "FONT_TAG",
+                "FONT_TITLE",
+            ],
+            "FONT_ANNOT": ["FONT_ANNOT_PRIMARY", "FONT_ANNOT_SECONDARY"],
+            "FORMAT_TIME_MAP": ["FORMAT_TIME_PRIMARY_MAP", "FORMAT_TIME_SECONDARY_MAP"],
+            "MAP_ANNOT_OFFSET": [
+                "MAP_ANNOT_OFFSET_PRIMARY",
+                "MAP_ANNOT_OFFSET_SECONDARY",
+            ],
+            "MAP_GRID_CROSS_SIZE": [
+                "MAP_GRID_CROSS_SIZE_PRIMARY",
+                "MAP_GRID_CROSS_SIZE_SECONDARY",
+            ],
+            "MAP_TICK_LENGTH": ["MAP_TICK_LENGTH_PRIMARY", "MAP_TICK_LENGTH_SECONDARY"],
+            "MAP_TICK_PEN": ["MAP_TICK_PEN_PRIMARY", "MAP_TICK_PEN_SECONDARY"],
+        }
         with Session() as lib:
             for key in kwargs:
-                if key in SPECIAL_PARAMS:
-                    for k in SPECIAL_PARAMS[key]:
+                if key in self.special_params:
+                    for k in self.special_params[key]:
                         self.old_defaults[k] = lib.get_default(k)
                 else:
                     self.old_defaults[key] = lib.get_default(key)
