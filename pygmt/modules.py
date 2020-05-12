2c2566d744ef2dec7d6e853cf5214aae84c29d17
Wei Ji <23487320+weiji14@users.noreply.github.com>
2020-05-12 16:34:49 +1200

    Handle setting special parameters without default settings for config (#411)
    There are 8 parameters in gmt.conf that don't have default settings, meaning they can't be reverted back easily when using `pygmt.config`. This can be addressed for 7 of them in a whitelist (FONT_ANNOT, FORMAT_TIME_MAP, MAP_ANNOT_OFFSET, MAP_GRID_CROSS_SIZE, MAP_GRID_PEN, MAP_TICK_LENGTH, MAP_TICK_PEN) by manually setting the relevant `*_PRIMARY` and `*_SECONDARY` parameters. Tests added to check that those special parameters are indeed set and reverted back properly when using `pygmt.config` in a `with` statement.
    
    For the `FONT` config setting, we test to check that `FONT_ANNOT_PRIMARY`, `FONT_ANNOT_SECONDARY`, `FONT_LABEL`, and `FONT_TITLE` are modified when the `FONT` config is set. TODO check `FONT_HEADING` and `FONT_TAG` once `subplot` wrapper is done.

--- pygmt/modules.py
+++ pygmt/modules.py
@@ -163,9 +163,36 @@ class config:  # pylint: disable=invalid-name
     def __init__(self, **kwargs):
         # Save values so that we can revert to their initial values
         self.old_defaults = {}
+        self.special_params = {
+            "FONT": [
+                "FONT_ANNOT_PRIMARY",
+                "FONT_ANNOT_SECONDARY",
+                "FONT_HEADING",
+                "FONT_LABEL",
+                "FONT_TAG",
+                "FONT_TITLE",
+            ],
+            "FONT_ANNOT": ["FONT_ANNOT_PRIMARY", "FONT_ANNOT_SECONDARY"],
+            "FORMAT_TIME_MAP": ["FORMAT_TIME_PRIMARY_MAP", "FORMAT_TIME_SECONDARY_MAP"],
+            "MAP_ANNOT_OFFSET": [
+                "MAP_ANNOT_OFFSET_PRIMARY",
+                "MAP_ANNOT_OFFSET_SECONDARY",
+            ],
+            "MAP_GRID_CROSS_SIZE": [
+                "MAP_GRID_CROSS_SIZE_PRIMARY",
+                "MAP_GRID_CROSS_SIZE_SECONDARY",
+            ],
+            "MAP_GRID_PEN": ["MAP_GRID_PEN_PRIMARY", "MAP_GRID_PEN_SECONDARY"],
+            "MAP_TICK_LENGTH": ["MAP_TICK_LENGTH_PRIMARY", "MAP_TICK_LENGTH_SECONDARY"],
+            "MAP_TICK_PEN": ["MAP_TICK_PEN_PRIMARY", "MAP_TICK_PEN_SECONDARY"],
+        }
         with Session() as lib:
             for key in kwargs:
-                self.old_defaults[key] = lib.get_default(key)
+                if key in self.special_params:
+                    for k in self.special_params[key]:
+                        self.old_defaults[k] = lib.get_default(k)
+                else:
+                    self.old_defaults[key] = lib.get_default(key)
 
         # call gmt set to change GMT defaults
         arg_str = " ".join(
