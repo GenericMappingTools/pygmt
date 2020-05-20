9251d7c1196a35f6ed9a73718e710fc91952d075
Dongdong Tian <seisman.info@gmail.com>
2020-05-20 19:54:51 -0400

    Add a test for failing load_libgmt

--- pygmt/tests/test_clib_loading.py
+++ pygmt/tests/test_clib_loading.py
@@ -1,6 +1,7 @@
 """
 Test the functions that load libgmt
 """
+import os
 import pytest
 
 from ..clib.loading import clib_name, load_libgmt, check_libgmt
@@ -16,6 +17,22 @@ def test_check_libgmt():
 def test_load_libgmt():
     "Test that loading libgmt works and doesn't crash."
     check_libgmt(load_libgmt())
+
+
+def test_load_libgmt_fail():
+    "Test that loading fails when given a bad library path."
+    # save the old value (if any) before setting a fake "GMT_LIBRARY_PATH"
+    old_gmt_library_path = os.environ.get("GMT_LIBRARY_PATH")
+
+    os.environ["GMT_LIBRARY_PATH"] = "/not/a/real/path"
+    with pytest.raises(GMTCLibNotFoundError):
+        load_libgmt()
+
+    # revert back to the original status (if any)
+    if old_gmt_library_path:
+        os.environ["GMT_LIBRARY_PATH"] = old_gmt_library_path
+    else:
+        del os.environ["GMT_LIBRARY_PATH"]
 
 
 def test_clib_name():
