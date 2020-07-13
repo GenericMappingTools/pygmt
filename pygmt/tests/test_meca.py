45b01de8c68dce8103faaa0dc3b98e2b71347ec0
Liam Toney <ltoney@usgs.gov>
2020-07-12 18:45:28 -0600

    Add text, which currently fails

--- pygmt/tests/test_meca.py
+++ pygmt/tests/test_meca.py
@@ -74,3 +74,83 @@ def test_meca_mt_components():
         fig.shift_origin(xshift="3.5i")
 
     return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_meca_spec_dictionary():
+    """
+    Test supplying a dictionary to the `spec` argument.
+    """
+
+    fig = Figure()
+
+    # Right lateral strike slip
+    fig.meca(
+        dict(strike=0, dip=90, rake=0, magnitude=5),
+        lon=0,
+        lat=5,
+        depth=0,
+        scale="2.5c",
+        region=[-1, 4, 0, 6],
+        projection="M14c",
+        frame=2,
+        text="Right Strike Slip",
+    )
+
+    # Left lateral strike slip
+    fig.meca(
+        dict(strike=0, dip=90, rake=180, magnitude=5),
+        lon=2,
+        lat=5,
+        depth=0,
+        scale="2.5c",
+        text="Left Strike Slip",
+    )
+
+    # Thrust
+    fig.meca(
+        dict(strike=0, dip=45, rake=90, magnitude=5),
+        lon=0,
+        lat=3,
+        depth=0,
+        scale="2.5c",
+        text="Thrust",
+    )
+    fig.meca(
+        dict(strike=45, dip=45, rake=90, magnitude=5),
+        lon=2,
+        lat=3,
+        depth=0,
+        scale="2.5c",
+        text="Thrust",
+    )
+
+    # Normal
+    fig.meca(
+        dict(strike=0, dip=45, rake=-90, magnitude=5),
+        lon=0,
+        lat=1,
+        depth=0,
+        scale="2.5c",
+        text="Normal",
+    )
+    fig.meca(
+        dict(strike=45, dip=45, rake=-90, magnitude=5),
+        lon=2,
+        lat=1,
+        depth=0,
+        scale="2.5c",
+        text="Normal",
+    )
+
+    # Mixed
+    fig.meca(
+        dict(strike=10, dip=35, rake=129, magnitude=5),
+        lon=3.4,
+        lat=0.6,
+        depth=0,
+        scale="2.5c",
+        text="Mixed",
+    )
+
+    return fig
