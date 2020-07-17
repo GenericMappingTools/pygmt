8e19723f1e31cdba4fb1b71607b4b08d0d6be25d
Dongdong Tian <seisman.info@gmail.com>
2020-07-17 19:45:23 -0400

    Add mark.runfirst to test_image

--- pygmt/tests/test_image.py
+++ pygmt/tests/test_image.py
@@ -11,6 +11,7 @@ from .. import Figure
 TEST_IMG = os.path.join(os.path.dirname(__file__), "baseline", "test_logo.png")
 
 
+@pytest.mark.runfirst
 @pytest.mark.mpl_image_compare
 def test_image():
     "Place images on map"
