a53023187fe357f6ad8f6e8d4e6d56c341f5b5aa
Dongdong Tian <seisman.info@gmail.com>
2020-07-18 00:52:47 -0400

    Revert "Add mark.runfirst to test_image"
    This reverts commit 8e19723f1e31cdba4fb1b71607b4b08d0d6be25d.

--- pygmt/tests/test_image.py
+++ pygmt/tests/test_image.py
@@ -11,7 +11,6 @@ from .. import Figure
 TEST_IMG = os.path.join(os.path.dirname(__file__), "baseline", "test_logo.png")
 
 
-@pytest.mark.runfirst
 @pytest.mark.mpl_image_compare
 def test_image():
     "Place images on map"
