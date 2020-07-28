4cfb7a2fdc165e8cdeeab88027b3fb09e0eaa134
Wei Ji <23487320+weiji14@users.noreply.github.com>
2020-07-29 07:00:02 +1200

    Cleanup track files in test_x2sys_cross_input_two_filenames (#552)
    * Cleanup track files in test_x2sys_cross_input_two_filenames
    
    * Fix pylint expression-not-assigned error

--- pygmt/tests/test_x2sys_cross.py
+++ pygmt/tests/test_x2sys_cross.py
@@ -121,6 +121,7 @@ def test_x2sys_cross_input_two_filenames(mock_x2sys_home):
         columns = list(output.columns)
         assert columns[:6] == ["x", "y", "i_1", "i_2", "dist_1", "dist_2"]
         assert columns[6:] == ["head_1", "head_2", "vel_1", "vel_2", "z_X", "z_M"]
+        _ = [os.remove(f"track_{i}.xyz") for i in range(2)]  # cleanup track files
 
     return output
 
