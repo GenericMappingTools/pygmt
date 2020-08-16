71b4095f1c8d5b53f979987a8a5b89cba93c1636
Wei Ji <weiji.leong@vuw.ac.nz>
2020-08-17 10:28:48 +1200

    Note how datetime_to_string conversion relies on checking for a space

--- pygmt/helpers/decorators.py
+++ pygmt/helpers/decorators.py
@@ -359,9 +359,13 @@ def kwargs_to_strings(convert_bools=True, **conversions):
                     if issequence and is_nonstr_iter(value):
                         for index, item in enumerate(value):
                             try:
+                                # check if there is a space " " when converting
+                                # a pandas.Timestamp/xr.DataArray to a string.
+                                # If so, use np.datetime_as_string instead.
                                 assert " " not in str(item)
                             except AssertionError:
-                                # convert datetime-like items to string format
+                                # convert datetime-like item to ISO 8601
+                                # string format like YYYY-MM-DDThh:mm:ss.ffffff
                                 value[index] = np.datetime_as_string(
                                     np.asarray(item, dtype=np.datetime64)
                                 )
