cb7ff983ddb1b8838bea49fbf2ced9c6740d8532
Wei Ji <weiji.leong@vuw.ac.nz>
2020-09-07 18:27:48 +1200

    Use check_figures_equal on previously xfailing grdview tests

--- pygmt/tests/test_grdview.py
+++ pygmt/tests/test_grdview.py
@@ -74,184 +74,234 @@ def test_grdview_with_perspective(grid, region):
     return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_with_perspective_and_zscale(grid):
+@check_figures_equal()
+def test_grdview_with_perspective_and_zscale(grid, region):
     """
     Run grdview by passing in a grid and setting a perspective viewpoint with
     an azimuth from the SouthWest and an elevation angle 30 degrees from the
     z-plane, plus a z-axis scaling factor of 0.005.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, perspective=[225, 30], zscale=0.005)
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g", region=region, perspective=[225, 30], zscale=0.005
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, perspective=[225, 30], zscale=0.005)
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_with_perspective_and_zsize(grid):
+@check_figures_equal()
+def test_grdview_with_perspective_and_zsize(grid, region):
     """
     Run grdview by passing in a grid and setting a perspective viewpoint with
     an azimuth from the SouthWest and an elevation angle 30 degrees from the
     z-plane, plus a z-axis size of 10cm.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, perspective=[225, 30], zsize="10c")
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g", region=region, perspective=[225, 30], zsize="10c"
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, perspective=[225, 30], zsize="10c")
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_with_cmap_for_image_plot(grid):
+@check_figures_equal()
+def test_grdview_with_cmap_for_image_plot(grid, region):
     """
     Run grdview by passing in a grid and setting a colormap for producing an
     image plot.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, cmap="oleron", surftype="i")
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g", region=region, cmap="oleron", surftype="i"
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, cmap="oleron", surftype="i")
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_with_cmap_for_surface_monochrome_plot(grid):
+@check_figures_equal()
+def test_grdview_with_cmap_for_surface_monochrome_plot(grid, region):
     """
     Run grdview by passing in a grid and setting a colormap for producing a
     surface monochrome plot.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, cmap="oleron", surftype="s+m")
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g", region=region, cmap="oleron", surftype="s+m"
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, cmap="oleron", surftype="s+m")
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_with_cmap_for_perspective_surface_plot(grid):
+@check_figures_equal()
+def test_grdview_with_cmap_for_perspective_surface_plot(grid, region):
     """
     Run grdview by passing in a grid and setting a colormap for producing a
     surface plot with a 3D perspective viewpoint.
     """
-    fig = Figure()
-    fig.grdview(
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        cmap="oleron",
+        surftype="s",
+        perspective=[225, 30],
+        zscale=0.005,
+    )
+    fig_test = Figure()
+    fig_test.grdview(
         grid=grid, cmap="oleron", surftype="s", perspective=[225, 30], zscale=0.005
     )
-    return fig
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_on_a_plane(grid):
+@check_figures_equal()
+def test_grdview_on_a_plane(grid, region):
     """
     Run grdview by passing in a grid and plotting it on a z-plane, while
     setting a 3D perspective viewpoint.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, plane=-4000, perspective=[225, 30], zscale=0.005)
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        plane=-4000,
+        perspective=[225, 30],
+        zscale=0.005,
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, plane=-4000, perspective=[225, 30], zscale=0.005)
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_on_a_plane_with_colored_frontal_facade(grid):
+@check_figures_equal()
+def test_grdview_on_a_plane_with_colored_frontal_facade(grid, region):
     """
     Run grdview by passing in a grid and plotting it on a z-plane whose frontal
     facade is colored gray, while setting a 3D perspective viewpoint.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, plane="-4000+ggray", perspective=[225, 30], zscale=0.005)
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        plane="-4000+ggray",
+        perspective=[225, 30],
+        zscale=0.005,
+    )
+    fig_test = Figure()
+    fig_test.grdview(
+        grid=grid, plane="-4000+ggray", perspective=[225, 30], zscale=0.005
+    )
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_with_perspective_and_zaxis_frame(grid):
+@check_figures_equal()
+def test_grdview_with_perspective_and_zaxis_frame(grid, region):
     """
     Run grdview by passing in a grid and plotting an annotated vertical
     z-axis frame.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, perspective=[225, 30], zscale=0.005, frame="zaf")
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        perspective=[225, 30],
+        zscale=0.005,
+        frame="zaf",
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, perspective=[225, 30], zscale=0.005, frame="zaf")
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_surface_plot_styled_with_contourpen(grid):
+@check_figures_equal()
+def test_grdview_surface_plot_styled_with_contourpen(grid, region):
     """
     Run grdview by passing in a grid with styled contour lines plotted on top
     of a surface plot.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, cmap="relief", surftype="s", contourpen="0.5p,black,dash")
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        cmap="relief",
+        surftype="s",
+        contourpen="0.5p,black,dash",
+    )
+    fig_test = Figure()
+    fig_test.grdview(
+        grid=grid, cmap="relief", surftype="s", contourpen="0.5p,black,dash"
+    )
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_surface_mesh_plot_styled_with_meshpen(grid):
+@check_figures_equal()
+def test_grdview_surface_mesh_plot_styled_with_meshpen(grid, region):
     """
     Run grdview by passing in a grid with styled mesh lines plotted on top of a
     surface mesh plot.
     """
-    fig = Figure()
-    fig.grdview(grid=grid, cmap="relief", surftype="sm", meshpen="0.5p,black,dash")
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        cmap="relief",
+        surftype="sm",
+        meshpen="0.5p,black,dash",
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, cmap="relief", surftype="sm", meshpen="0.5p,black,dash")
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_on_a_plane_styled_with_facadepen(grid):
+@check_figures_equal()
+def test_grdview_on_a_plane_styled_with_facadepen(grid, region):
     """
     Run grdview by passing in a grid and plotting it on a z-plane with styled
     lines for the frontal facade.
     """
-    fig = Figure()
-    fig.grdview(
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        plane=-4000,
+        perspective=[225, 30],
+        zscale=0.005,
+        facadepen="0.5p,blue,dash",
+    )
+    fig_test = Figure()
+    fig_test.grdview(
         grid=grid,
         plane=-4000,
         perspective=[225, 30],
         zscale=0.005,
         facadepen="0.5p,blue,dash",
     )
-    return fig
+    return fig_ref, fig_test
 
 
-@pytest.mark.xfail(
-    reason="Baseline image not updated to use earth relief grid in GMT 6.1.0",
-)
-@pytest.mark.mpl_image_compare
-def test_grdview_drapegrid_dataarray(grid):
+@check_figures_equal()
+def test_grdview_drapegrid_dataarray(grid, region):
     """
     Run grdview by passing in both a grid and drapegrid as an xarray.DataArray,
     setting a colormap for producing an image plot.
     """
     drapegrid = 1.1 * grid
 
-    fig = Figure()
-    fig.grdview(grid=grid, drapegrid=drapegrid, cmap="oleron", surftype="c")
-    return fig
+    fig_ref = Figure()
+    fig_ref.grdview(
+        grid="@earth_relief_01d_g",
+        region=region,
+        drapegrid=drapegrid,
+        cmap="oleron",
+        surftype="c",
+    )
+    fig_test = Figure()
+    fig_test.grdview(grid=grid, drapegrid=drapegrid, cmap="oleron", surftype="c")
+    return fig_ref, fig_test
 
 
 def test_grdview_wrong_kind_of_drapegrid(grid):
