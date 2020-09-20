019b44f09f6f2fbcc767ec1891df5fdb19dcfeed
Dongdong Tian <seisman.info@gmail.com>
2020-09-20 17:28:46 -0400

    Manually update VERSIOn in documentation_options.js

--- 0.0.1a0/_static/documentation_options.js
+++ 0.0.1a0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '',
+    VERSION: '0.0.1a0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
