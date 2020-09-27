b9e2bf26e126d32e697094e6c7fa90d9b34d39c7
Dongdong Tian <seisman.info@gmail.com>
2020-09-27 16:13:54 -0400

    Add more docstrings

--- examples/gallery/plot/scatter.py
+++ examples/gallery/plot/scatter.py
@@ -2,9 +2,11 @@
 Scatter plots with a legend
 ---------------------------
 
-TODO: Add more docstrings.
+To create a scatter plot with a legend one may use a loop and create one scatter
+plot per item to appear in the legend and set the label accordingly.
 
-Modified from the matplotlib example: https://matplotlib.org/gallery/lines_bars_and_markers/scatter_with_legend.html
+Modified from the matplotlib example:
+https://matplotlib.org/gallery/lines_bars_and_markers/scatter_with_legend.html
 """
 
 import numpy as np
@@ -21,17 +23,19 @@ fig.basemap(
 )
 for color in ["blue", "orange", "green"]:
     x, y = np.random.rand(2, n)  # random X and Y data in [0,1]
-    sizes = np.random.rand(n) * 0.5  # random size, in cm
-    # plot data points as circles ('c'), with different sizes
+    sizes = np.random.rand(n) * 0.5  # random size [0,0.5], in cm
+    # plot data points as circles (style="c"), with different sizes
     fig.plot(
         x=x,
         y=y,
         style="c",
         sizes=sizes,
         color=color,
+        # Set the legend label,
+        # and set the circle size to be 0.25 cm (+S0.25c) in legend
         label=f"{color}+S0.25c",
-        transparency=70,
+        transparency=70,  # set transparency level for all symbols
     )
 
-fig.legend(transparency=30)
+fig.legend(transparency=30)  # set transparency level for legends
 fig.show()
