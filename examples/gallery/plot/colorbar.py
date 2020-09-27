83b14770afa6e3c6f24029021280a267d640a0e5
Wei Ji <weiji.leong@vuw.ac.nz>
2020-09-27 16:38:15 +1300

    Wrap colorbar docstring to 79 characters

--- examples/gallery/plot/colorbar.py
+++ examples/gallery/plot/colorbar.py
@@ -2,21 +2,26 @@
 Colorbar
 --------
 
-The :meth:`pygmt.Figure.colorbar` method creates a color scalebar. We must specify the
-colormap via the ``cmap`` argument, and set the placement via the ``position`` argument.
-The full list of color paletted tables can be found at :gmt-docs:`cookbook/cpts.html`.
-You can set the `position` of the colorbar using the following options:
+The :meth:`pygmt.Figure.colorbar` method creates a color scalebar. We must
+specify the colormap via the ``cmap`` argument, and set the placement via the
+``position`` argument. The full list of color palette tables can be found at
+:gmt-docs:`cookbook/cpts.html`. You can set the `position` of the colorbar
+using the following options:
 
-- j/J: justified inside/outside the mapframe using any 2 character combination of
-  vertical (**T** op, **M** iddle, **B** ottom) and horizontal (**L** eft, **C** enter,
-  **R** ight) alignment codes, e.g. `position="jTR"` for top right.
-- g: using map coordinates, e.g. `position="g170/-45"` for longitude 170E, latitude 45S.
-- x: using paper coordinates, e.g. `position="x5c/7c"` for 5cm,7cm from anchor point.
+- j/J: justified inside/outside the mapframe using any 2 character combination
+  of vertical (**T** op, **M** iddle, **B** ottom) and horizontal (**L** eft,
+  **C** enter, **R** ight) alignment codes, e.g. `position="jTR"` for top
+  right.
+- g: using map coordinates, e.g. `position="g170/-45"` for longitude 170E,
+  latitude 45S.
+- x: using paper coordinates, e.g. `position="x5c/7c"` for 5cm,7cm from anchor
+  point.
 - n: using normalized (0-1) coordinates, e.g. `position="n0.4/0.8"`.
 
-Note that the anchor point defaults to the bottom left (BL). Append +h to ``position``
-to get a horizontal colorbar instead of a vertical one. For more advanced styling
-options, see the full option list at :gmt-docs:`colorbar.html`.
+Note that the anchor point defaults to the bottom left (BL). Append +h to
+``position`` to get a horizontal colorbar instead of a vertical one. For more
+advanced styling options, see the full option list at
+:gmt-docs:`colorbar.html`.
 """
 import pygmt
 
