702404e1d2d011b71b99f3f002e990344e26b851
carocamargo <47150926+carocamargo@users.noreply.github.com>
2020-10-02 17:01:43 +0200

    Apply suggestions from code review
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

--- pygmt/gridops.py
+++ pygmt/gridops.py
@@ -129,14 +129,21 @@ def grdcut(grid, **kwargs):
 @kwargs_to_strings(R="sequence")
 def grdfilter(grid, **kwargs):
     """
-    filter a grid file in the time domain using one of the selected convolution
+    Filter a grid in the space (or time) domain.
+
+    Filter a grid file in the time domain using one of the selected convolution
     or non-convolution isotropic or rectangular filters and compute distances
-    using Cartesian or Spherical geometries. The output grid file can optionally
-    be generated as a sub-region of the input (via *region*) and/or with new increment
-    (via *spacing*) or registration (via *toggle*).
-    In this way, one may have “extra space” in the input data so that the edges
-    will not be used and the output can be within one half-width of the input edges.
-    If the filter is low-pass, then the output may be less frequently sampled than the input.
+    using Cartesian or Spherical geometries. The output grid file can
+    optionally be generated as a sub-region of the input (via *region*) and/or
+    with new increment (via *spacing*) or registration (via *toggle*). In this
+    way, one may have "extra space" in the input data so that the edges will
+    not be used and the output can be within one half-width of the input edges.
+    If the filter is low-pass, then the output may be less frequently sampled
+    than the input.
+
+    Full option list at :gmt-docs:`grdfilter.html`
+
+    {aliases}
 
     Parameters
     ----------
@@ -146,28 +153,37 @@ def grdfilter(grid, **kwargs):
         The name of the output netCDF file with extension .nc to store the grid
         in.
     filter : str
+        ``xwidth[/width2][modifiers]``.
         Name of filter type you which to apply, followed by the width
         b: Box Car; c: Cosine Arch; g: Gaussian; o: Operator; m: Median;
         p: Maximum Likelihood probability; h: histogram
         Example: F='m600' for a median filter with width of 600
     distance : str
-        Distance flag, that tells how grid (x,y) rrlated to the filter width as follows:
-        flag = p: grid (px,py) with width an odd number of pixels; Cartesian distances.
+        Distance *flag* tells how grid (x,y) relates to filter width as
+        follows:
 
-        flag = 0: grid (x,y) same units as width, Cartesian distances.
+        p: grid (px,py) with *width* an odd number of pixels; Cartesian
+        distances.
 
-        flag = 1: grid (x,y) in degrees, width in kilometers, Cartesian distances.
+        0: grid (x,y) same units as *width*, Cartesian distances.
 
-        flag = 2: grid (x,y) in degrees, width in km, dx scaled by cos(middle y), Cartesian distances.
+        1: grid (x,y) in degrees, *width* in kilometers, Cartesian distances.
 
-        The above options are fastest because they allow weight matrix to be computed only once.
-        The next three options are slower because they recompute weights for each latitude.
+        2: grid (x,y) in degrees, *width* in km, dx scaled by cos(middle y),
+        Cartesian distances.
 
-        flag = 3: grid (x,y) in degrees, width in km, dx scaled by cosine(y), Cartesian distance calculation.
+        The above options are fastest because they allow weight matrix to be
+        computed only once. The next three options are slower because they
+        recompute weights for each latitude.
 
-        flag = 4: grid (x,y) in degrees, width in km, Spherical distance calculation.
+        3: grid (x,y) in degrees, *width* in km, dx scaled by cosine(y),
+        Cartesian distance calculation.
 
-        flag = 5: grid (x,y) in Mercator -Jm1 img units, width in km, Spherical distance calculation.
+        4: grid (x,y) in degrees, *width* in km, Spherical distance
+        calculation.
+
+        5: grid (x,y) in Mercator ``projection='m1'`` img units, *width* in km,
+        Spherical distance calculation.
 
     increment : str
         x_inc [and optionally y_inc] is the grid spacing.
@@ -188,14 +204,30 @@ def grdfilter(grid, **kwargs):
         - xarray.DataArray if *outgrid* is not set
         - None if *outgrid* is set (grid output will be stored in *outgrid*)
 
-    Usage
-    -------
-    pygmt.grdfilter('input.nc',F='m1600',D='4', G='filtered_output.nc')
-    Applies a filter of 1600km (full width) in the input.nc and returns a a filtered field (saved as netcdf)
+    Examples
+    --------
+    >>> import os
+    >>> import pygmt
 
-    smooth_field=pygmt.grdfiler(dataarray,F='g600',D='4')
-    Applies a gaussian smoothing filter of 600 km in the input data array,
-    and returns a filtered data array withthe smoothed field.
+    >>> # Apply a filter of 600km (full width) to the @earth_relief_30m file
+    >>> # and return a filtered field (saved as netcdf)
+    >>> pygmt.grdfilter(
+    ...     grid="@earth_relief_30m",
+    ...     filter="m600",
+    ...     distance="4",
+    ...     region=[150, 250, 10, 40],
+    ...     spacing=0.5,
+    ...     outgrid="filtered_pacific.nc"
+    ... )
+    >>> os.remove("filtered_pacific.nc")  # cleanup file
+
+    >>> # Apply a gaussian smoothing filter of 600 km in the input data array,
+    >>> # and returns a filtered data array with the smoothed field.
+    >>> grid = pygmt.datasets.load_earth_relief()
+    >>> smooth_field = pygmt.grdfilter(
+    ...     grid=grid, filter="g600", distance="4"
+    ... )
+
     """
     kind = data_kind(grid)
 
