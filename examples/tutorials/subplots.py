0214c7cf3e84b4a1d8d4b3d4b046065616457680
Wei Ji <weiji.leong@vuw.ac.nz>
2020-10-15 20:28:35 +1300

    Expand subplot tutorial with subsection on "Making your first subplot"

--- examples/tutorials/subplots.py
+++ examples/tutorials/subplots.py
@@ -27,10 +27,10 @@ import pygmt
 # Define subplot layout
 # ---------------------
 #
-# The ``pygmt.subplots`` command is used to setup the layout, size, and other
-# attributes of the figure. It divides the whole canvas into regular grid areas
-# with n rows and m columns. Each grid area can contain an individual subplot.
-# For example:
+# The :meth:`pygmt.subplots` command is used to setup the layout, size, and
+# other attributes of the figure. It divides the whole canvas into regular grid
+# areas with n rows and m columns. Each grid area can contain an individual
+# subplot. For example:
 
 fig, axs = pygmt.subplots(nrows=2, ncols=3, figsize=("15c", "6c"), frame="lrtb")
 
@@ -83,3 +83,43 @@ fig.show()
 # .. code-block:: default
 #
 #     fig.end_subplot()
+
+###############################################################################
+# Making your first subplot
+# -------------------------
+# Next, let's use what we learned above to make a 2 row by 2 column subplot
+# figure. We'll also pick up on some new parameters to configure our subplot.
+import pygmt
+
+fig, axs = pygmt.subplots(
+    nrows=2,
+    ncols=2,
+    figsize=("15c", "6c"),
+    autolabel=True,
+    margins=["0.3c", "0.1c"],
+    title="My Subplot Heading",
+)
+fig.basemap(region=[0, 10, 0, 10], projection="X?", frame=["af", "WSne"], ax=axs[0, 0])
+fig.basemap(region=[0, 20, 0, 10], projection="X?", frame=["af", "WSne"], ax=axs[0, 1])
+fig.basemap(region=[0, 10, 0, 20], projection="X?", frame=["af", "WSne"], ax=axs[1, 0])
+fig.basemap(region=[0, 20, 0, 20], projection="X?", frame=["af", "WSne"], ax=axs[1, 1])
+fig.end_subplot()
+fig.show()
+
+###############################################################################
+# In this example, we define a 2-row, 2-column (2x2) subplot layout using
+# :meth:`pygmt.subplots`. The overall figure dimensions is set to be 15cm wide
+# and 6cm high (``figsize=["15c", "6c"]``). In addition, we used some optional
+# parameters to fine tune some details of the figure creation:
+#
+# - ``autolabel=True``: Each subplot is automatically labelled abcd
+# - ``margins=["0.2c", "0.1c"]``: adjusts the space between adjacent subplots.
+#   In this case, it is set as 0.2 cm in the X direction and 0.1 cm in the Y
+#   direction.
+# - ``title="My Subplot Heading"``: adds a title on top of the whole figure.
+#
+# Notice that each subplot was set to use a linear projection ``"X?"``.
+# Usually, we need to specify the width and height of the map frame, but it is
+# also possible to use a question mark ``"?"`` to let GMT decide automatically
+# on what is the most appropriate width/height for the each subplot's map
+# frame.
