6ea53ee341aa2f832af1869546373c7a7853814e
Wei Ji <weiji.leong@vuw.ac.nz>
2020-10-23 15:49:24 +1300

    Alias straight_lines(A), error_bars(E), close(L), position(D) for plot

--- pygmt/base_plotting.py
+++ pygmt/base_plotting.py
@@ -606,47 +606,49 @@ class BasePlotting:
 
     @fmt_docstring
     @use_alias(
-        R="region",
-        J="projection",
+        A="straight_lines",
         B="frame",
-        S="style",
-        G="color",
-        N="no_clip",
-        W="pen",
-        i="columns",
-        l="label",
         C="cmap",
+        D="position",
+        E="error_bars",
+        G="color",
+        J="projection",
+        L="close",
+        N="no_clip",
+        R="region",
+        S="style",
         U="timestamp",
         V="verbose",
+        W="pen",
         X="xshift",
         Y="yshift",
+        i="columns",
+        l="label",
         p="perspective",
         t="transparency",
     )
     @kwargs_to_strings(R="sequence", i="sequence_comma", p="sequence")
     def plot(self, x=None, y=None, data=None, sizes=None, direction=None, **kwargs):
         """
-        Plot lines, polygons, and symbols on maps.
-
-        Used to be psxy.
+        Plot lines, polygons, and symbols in 2-D.
 
         Takes a matrix, (x,y) pairs, or a file name as input and plots lines,
         polygons, or symbols at those locations on a map.
 
         Must provide either *data* or *x* and *y*.
 
-        If providing data through *x* and *y*, *color* (G) can be a 1d array
-        that will be mapped to a colormap.
+        If providing data through *x* and *y*, *color* can be a 1d array that
+        will be mapped to a colormap.
 
-        If a symbol is selected and no symbol size given, then psxy will
+        If a symbol is selected and no symbol size given, then plot will
         interpret the third column of the input data as symbol size. Symbols
         whose size is <= 0 are skipped. If no symbols are specified then the
-        symbol code (see *S* below) must be present as last column in the
-        input. If *S* is not used, a line connecting the data points will be
-        drawn instead. To explicitly close polygons, use *L*. Select a fill
-        with *G*. If *G* is set, *W* will control whether the polygon outline
-        is drawn or not. If a symbol is selected, *G* and *W* determines the
-        fill and outline/no outline, respectively.
+        symbol code (see *symbol* below) must be present as last column in the
+        input. If *symbol* is not used, a line connecting the data points will
+        be drawn instead. To explicitly close polygons, use *close*. Select a
+        fill with *color*. If *color* is set, *pen* will control whether the
+        polygon outline is drawn or not. If a symbol is selected, *color* and
+        *pen* determines the fill and outline/no outline, respectively.
 
         Full option list at :gmt-docs:`plot.html`
 
@@ -671,19 +673,33 @@ class BasePlotting:
             depending on the style options chosen.
         {J}
         {R}
-        A : bool or str
-            ``'[m|p|x|y]'``
+        straight_lines : bool or str
+            ``[m|p|x|y]``.
             By default, geographic line segments are drawn as great circle
-            arcs. To draw them as straight lines, use *A*.
+            arcs. To draw them as straight lines, use *straight_lines*.
+            Alternatively, add **m** to draw the line by first following a
+            meridian, then a parallel. Or append **p** to start following a
+            parallel, then a meridian. (This can be practical to draw a line
+            along parallels, for example). For Cartesian data, points are
+            simply connected, unless you append **x** or **y** to draw
+            stair-case curves that whose first move is along *x* or *y*,
+            respectively.
         {B}
         {CPT}
-        D : str
-            ``'dx/dy'``: Offset the plot symbol or line locations by the given
-            amounts dx/dy.
-        E : bool or str
-            ``'[x|y|X|Y][+a][+cl|f][+n][+wcap][+ppen]'``.
-            Draw symmetrical error bars.
+        position : str
+            ``dx/dy``.
+            Offset the plot symbol or line locations by the given amounts
+            *dx/dy* [Default is no offset]. If *dy* is not given it is set
+            equal to *dx*.
+        error_bars : bool or str
+            ``[x|y|X|Y][+a][+cl|f][+n][+wcap][+ppen]``.
+            Draw symmetrical error bars. Full documentation is at
+            :gmt-docs:`plot.html#e`.
         {G}
+        close : str
+          ``[+b|d|D][+xl|r|x0][+yl|r|y0][+ppen]``.
+          Force closed polygons. Full documentation is at
+          :gmt-docs:`plot.html#l`.
         no_clip : bool or str
             ``'[c|r]'``.
             Do NOT clip symbols that fall outside map border [Default plots
