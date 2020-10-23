a6b8e650ff9f72917fbffc29bc11705802c23872
Dongdong Tian <seisman.info@gmail.com>
2020-10-23 17:56:52 -0400

    Only skip the tests on macOS

--- pygmt/tests/test_grdimage.py
+++ pygmt/tests/test_grdimage.py
@@ -1,6 +1,7 @@
 """
 Test Figure.grdimage
 """
+import sys
 import numpy as np
 import pytest
 import xarray as xr
@@ -75,7 +76,7 @@ def test_grdimage_file():
 
 @pytest.mark.skip(
     reason="Upstream bug in GMT 6.1.1",
-    condition=gmt_version <= Version("6.1.1"),
+    condition=gmt_version <= Version("6.1.1") and sys.platform == "darwin",
 )
 @check_figures_equal()
 @pytest.mark.parametrize(
