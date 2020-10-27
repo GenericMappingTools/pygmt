fde003837dcac0d30ca739e9df42a5515567b9ed
Wei Ji <weiji.leong@vuw.ac.nz>
2020-10-27 17:29:36 +1300

    Set a unique artifact name for each OS/Python Version
    Also trigger a test failure to test if things work.

--- pygmt/tests/test_grdimage.py
+++ pygmt/tests/test_grdimage.py
@@ -154,7 +154,7 @@ def test_grdimage_central_meridians(grid, proj_type, lon0):
 # are still slightly different with an RMS error of 25, see issue at
 # https://github.com/GenericMappingTools/pygmt/issues/390
 # TO-DO remove tol=1.5 and pytest.mark.xfail once bug is solved in upstream GMT
-@check_figures_equal(tol=1.5)
+# @check_figures_equal(tol=1.5)
 @pytest.mark.parametrize("lat0", [0, 30])
 @pytest.mark.parametrize("lon0", [0, 123, 180])
 @pytest.mark.parametrize("proj_type", [pytest.param("Q", marks=pytest.mark.xfail), "S"])
