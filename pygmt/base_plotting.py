7dfe5b0959eb090f8c82196e6a640377724df3af
Dongdong Tian <seisman.info@gmail.com>
2020-11-05 20:38:49 -0500

    doc: Fix the description of grdcontour -G option

--- pygmt/base_plotting.py
+++ pygmt/base_plotting.py
@@ -295,6 +295,11 @@ class BasePlotting:
         {J}
         {R}
         {B}
+        label_placement : str
+            ``[d|f|n|l|L|x|X]params``.
+            The required argument controls the placement of labels along the
+            quoted lines. It supports five controlling algorithms. See
+            :gmt-docs:`grdcontour.html#g` for details.
         {G}
         {U}
         {V}
