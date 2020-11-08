dea4b1c41ce72a6b901b1bd0653f34945fd9016c
Loïc Houpert <10154151+lhoupert@users.noreply.github.com>
2020-11-08 18:25:07 +0000

    Apply suggestions from code review
    cleaning of the velo docstring
    
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

--- pygmt/base_plotting.py
+++ pygmt/base_plotting.py
@@ -1973,75 +1973,102 @@ class BasePlotting:
             controlled by :gmt-term:`PROJ_LENGTH_UNIT` unless **c**, **i** , or
             **p** is appended).
 
-            "**e**\\ *velscale/confidence*\\ [\\ **+f**\\ font]"
+            "**e**\\ *velscale/confidence*\\ [**+f**\\ *font*]"
 
-                Velocity ellipses in (N,E) convention. velscale sets the
-                scaling of the velocity arrows. The confidence sets the
+                Velocity ellipses in (N,E) convention. *velscale* sets the
+                scaling of the velocity arrows. The *confidence* sets the
                 2-dimensional confidence limit for the ellipse, e.g., 0.95 for
-                95% confidence ellipse. font sets the font and size of the text
-                [9p,Helvetica,black]. The ellipse will be filled with the color
-                or shade specified by the -G option [default transparent].
-                The arrow and the circumference of the ellipse will be drawn
-                with the pen attributes specified by the -W option. Parameters
-                are expected to be in the following columns:
+                95% confidence ellipse. *font* sets the font and size of the
+                text [9p,Helvetica,black]. The ellipse will be filled with the
+                color or shade specified by the *facecolor* option [default
+                transparent]. The arrow and the circumference of the ellipse
+                will be drawn with the pen attributes specified by the *pen*
+                option. Parameters are expected to be in the following columns:
 
-                    **1,2**: longitude, latitude of station (-: option
-                    interchanges order) **3,4**: eastward, northward velocity
-                    (-: option interchanges order) **5,6**: uncertainty of
-                    eastward, northward velocities (1-sigma, -: option
-                    interchanges order)  **7**: correlation between eastward
-                    and northward components **8**: name of station (optional).
+                    **1**,\\ **2**:
+                    longitude, latitude of station (**-:** option interchanges
+                    order)
+                    **3**,\\ **4**:
+                    eastward, northward velocity (**-:** option interchanges
+                    order)
+                    **5**,\\ **6**:
+                    uncertainty of eastward, northward velocities (1-sigma)
+                    (**-:** option interchanges order)
+                    **7**:
+                    correlation between eastward and northward components
+                    **8**:
+                    name of station (optional).
 
             "**n**\\ *barscale*"
 
-                Anisotropy bars. barscale sets the scaling of the bars.
+                Anisotropy bars. *barscale* sets the scaling of the bars.
                 Parameters are expected to be in the following columns:
-                    **1,2** : longitude, latitude of station (-: option
-                    interchanges order) **3,4** : eastward, northward
-                    components of anisotropy vector (-: option interchanges
+
+                    **1**,\\ **2**:
+                    longitude, latitude of station (**-:** option interchanges
                     order)
+                    **3**,\\ **4**:
+                    eastward, northward components of anisotropy vector (**-:**
+                    option interchanges order)
 
-            "**r**\\ *velscale/confidence*\\ [\\ **+f**\\ font]"
+            "**r**\\ *velscale/confidence*\\ [**+f**\\ *font*]"
 
-                Velocity ellipses in rotated convention. velscale sets the
-                scaling of the velocity arrows. The confidence sets the
+                Velocity ellipses in rotated convention. *velscale* sets the
+                scaling of the velocity arrows. The *confidence* sets the
                 2-dimensional confidence limit for the ellipse, e.g., 0.95 for
-                95% confidence ellipse. font sets the font and size of the text
-                [9p,Helvetica,black]. The ellipse will be filled with the color
-                or shade specified by the -G option [default transparent].
-                The arrow and the circumference of the ellipse will be drawn
-                with the pen attributes specified by the -W option. Parameters
-                are expected to be in the following columns:
-                    **1,2**: longitude, latitude, of station (-: option
-                    interchanges order) **3,4**: eastward, northward velocity
-                    (-: option interchanges order) **5,6**: semi-major,
-                    semi-minor axes **7**: counter-clockwise angle, in degrees,
-                    from horizontal axis to major axis of ellipse. 8: name of
-                    station (optional)
+                95% confidence ellipse. *font* sets the font and size of the
+                text [9p,Helvetica,black]. The ellipse will be filled with the
+                color or shade specified by the *facecolor* option [default
+                transparent]. The arrow and the circumference of the ellipse
+                will be drawn with the pen attributes specified by the *pen*
+                option. Parameters are expected to be in the following columns:
 
-            "**w**\\ *wedgescale/wedgemag*"
+                    **1**,\\ **2**:
+                    longitude, latitude, of station (**-:** option interchanges
+                    order)
+                    **3**,\\ **4**:
+                    eastward, northward velocity (**-:** option interchanges
+                    order)
+                    **5**,\\ **6**:
+                    semi-major, semi-minor axes
+                    **7**:
+                    counter-clockwise angle, in degrees, from horizontal axis
+                    to major axis of ellipse.
+                    **8**:
+                    name of station (optional)
 
-                Rotational wedges. wedgescale sets the size of the wedges.
-                Values are multiplied by wedgemag before plotting. For example,
-                setting wedgemag to 1.e7 works well for rotations of the order
-                of 100 nanoradians/yr. Use **-G** to set the fill color or
-                shade for the wedge, and **-E** to set the color or shade for
-                the uncertainty. Parameters are expected to be in the
-                following columns:
-                    **1,2**: longitude, latitude, of station (-: option
-                    interchanges order) **3**: rotation in radians
-                    **4**: rotation uncertainty in radians
+                Rotational wedges. *wedgescale* sets the size of the wedges.
+                Values are multiplied by *wedgemag* before plotting. For
+                example, setting *wedgemag* to 1.e7 works well for rotations of
+                the order of 100 nanoradians/yr. Use **facecolor** to set the
+                fill color or shade for the wedge, and **uncertainty_color**
+                to set the color or shade for the uncertainty. Parameters are
+                expected to be in the following columns:
+
+                    **1**,\\ **2**:
+                    longitude, latitude, of station (**-:** option interchanges
+                    order)
+                    **3**:
+                    rotation in radians
+                    **4**:
+                    rotation uncertainty in radians
 
             "**x**\\ *cross_scale*"
 
-                gives Strain crosses. cross_scale sets the size of the cross.
+                gives Strain crosses. *cross_scale* sets the size of the cross.
                 Parameters are expected to be in the following columns:
-                    **1,2**: longitude, latitude, of station
-                    (-: option interchanges order) 3: eps1, the most
-                    extensional eigenvalue of strain tensor, with extension
-                    taken positive. **4**: eps2, the most compressional
-                    eigenvalue of strain tensor, with extension taken positive.
-                    **5**: azimuth of eps2 in degrees CW from North.
+
+                    **1**,\\ **2**:
+                    longitude, latitude, of station (**-:** option interchanges
+                    order)
+                    **3**:
+                    eps1, the most extensional eigenvalue of strain tensor,
+                    with extension taken positive.
+                    **4**:
+                    eps2, the most compressional eigenvalue of strain tensor,
+                    with extension taken positive.
+                    **5**:
+                    azimuth of eps2 in degrees CW from North.
 
 
         Other Parameters
@@ -2057,14 +2084,14 @@ class BasePlotting:
 
         rescale : str
             can be used to rescale the uncertainties of velocities
-            (-Se and -Sr) and rotations (-Sw).
-            Can be combined with the confidence variable.
+            (``scaling='e'`` and ``scaling='r'``) and rotations
+            (``scaling='w'``). Can be combined with the confidence variable.
 
         uncertainty_color : str
             Sets the color or shade used for filling uncertainty wedges
-            (-Sw) or velocity error ellipses (-Se or -Sr). [If -E is not
-            specified, the uncertainty regions will be transparent].
-            More details on
+            (``scaling='w'``) or velocity error ellipses (``scaling='e'`` or
+            ``scaling='r'``). [If *uncertainty_color* is not specified, the
+            uncertainty regions will be transparent]. More details on
             :gmt-docs:`cookbook/features.html#gfill-attrib`.
 
         facecolor : str
@@ -2074,11 +2101,11 @@ class BasePlotting:
 
         line: str
             Draw lines. Ellipses and fault planes will have their outlines
-            drawn using current pen (see -W).
+            drawn using current pen (see *pen*).
 
         no_clip: str
             Do NOT skip symbols that fall outside the frame boundary specified
-            by -R. [Default plots symbols inside frame only].
+            by *region*. [Default plots symbols inside frame only].
 
         pen : str
             Set pen attributes for velocity arrows, ellipse circumference and
