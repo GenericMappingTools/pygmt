92cfc0e18a44411b7e855fd25581c85032c562c8
Will Schlitzer <schlitzer90@gmail.com>
2020-11-29 08:17:30 +0000

    Update comments to keep lines at or below 88 characters

--- examples/tutorials/contour-map.py
+++ examples/tutorials/contour-map.py
@@ -15,10 +15,12 @@ grid = pygmt.datasets.load_earth_relief(resolution="15s", region=[-92, -90, -1.5
 # -------------------
 #
 # The :meth:`pygmt.Figure.grdcontour` method takes the grid input.
-# It plots annotated contour lines, which are darker and have the elevation/depth written on them,
-# and unannotated contour lines. In the example below, the default contour line intervals are 500 meters,
+# It plots annotated contour lines, which are darker and have the
+# elevation/depth written on them, and unannotated contour lines.
+# In the example below, the default contour line intervals are 500 meters,
 # with an annotated contour line every 1000 meters.
-# By default, it plots the map with the equidistant cylindrical projection and with no frame.
+# By default, it plots the map with the
+# equidistant cylindrical projection and with no frame.
 
 fig = pygmt.Figure()
 fig.grdcontour(grid=grid)
@@ -29,7 +31,8 @@ fig.show()
 # ---------------------
 #
 # Use the ``annotation`` and ``interval`` arguments to adjust contour line intervals.
-# In the example below, there are contour intervals every 250 meters and annotated contour lines every 1,000 meters.
+# In the example below, there are contour intervals every 250 meters and
+# annotated contour lines every 1,000 meters.
 
 fig = pygmt.Figure()
 fig.grdcontour(
@@ -43,8 +46,9 @@ fig.show()
 # Contour limits
 # --------------
 #
-# The ``limit`` argument sets the minimum and maximum values for the contour lines, with the value.
-# The argument takes the low and high values, and is either a list (as below) or a string ``limit="-4000/0"``.
+# The ``limit`` argument sets the minimum and maximum values for the contour lines.
+# The argument takes the low and high values,
+# and is either a list (as below) or a string ``limit="-4000/0"``.
 
 fig = pygmt.Figure()
 fig.grdcontour(
@@ -59,7 +63,8 @@ fig.show()
 # Map settings
 # ------------
 #
-# The :meth:`pygmt.Figure.grdcontour` method accepts additional arguments, including setting the projection and frame.
+# The :meth:`pygmt.Figure.grdcontour` method accepts additional arguments,
+# including setting the projection and frame.
 
 fig = pygmt.Figure()
 fig.grdcontour(
