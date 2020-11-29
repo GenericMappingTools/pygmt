bc13ac871d63109e5bea407d29c0f651b01182ba
Will Schlitzer <schlitzer90@gmail.com>
2020-11-29 08:08:54 +0000

    Update examples/tutorials/contour-map.py
    typo correction
    
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/tutorials/contour-map.py
+++ examples/tutorials/contour-map.py
@@ -18,7 +18,7 @@ grid = pygmt.datasets.load_earth_relief(resolution="15s", region=[-92, -90, -1.5
 # It plots annotated contour lines, which are darker and have the elevation/depth written on them,
 # and unannotated contour lines. In the example below, the default contour line intervals are 500 meters,
 # with an annotated contour line every 1000 meters.
-# By default, it plots the map with a Equidistant cylindrical projection and with no frame.
+# By default, it plots the map with the equidistant cylindrical projection and with no frame.
 
 fig = pygmt.Figure()
 fig.grdcontour(grid=grid)
