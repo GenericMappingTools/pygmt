fa16924f32b70788b0fe2e4a3d37c85e4ab0ebe6
Will Schlitzer <schlitzer90@gmail.com>
2020-12-01 22:13:45 +0000

    Add custom color pallete example

--- examples/tutorials/color-figure.py
+++ examples/tutorials/color-figure.py
@@ -51,3 +51,37 @@ fig = pygmt.Figure()
 fig.grdimage(grid=grid, frame=True, projection="M6i", cmap="geo")
 fig.colorbar(frame=["x+lElevation", "y+lm"])
 fig.show()
+
+########################################################################################
+# Create a custom color pallete
+# -----------------------------
+#
+# The :meth:`pygmt.makecpt` method provides the option to create a custom color pallete
+# for a figure. The ``cmap`` argument sets the master color pallete to base the
+# new color pallete on. The ``series`` argument sets the minimum and maximum values, and
+# optionally the intervals between them; without it, the new color pallete defaults to
+# the min/max values of the master color pallete.  The ``series`` argument can be given
+# as a string (as below) or a list ``limit="-4000/-2000"``. Any values that fall outside
+# the range of the series will be plotted as black (lower than the minimum)
+# or white (higher than the maximum).
+#
+# The :meth:`pygmt.makecpt` method includes an ``output`` parameter, which allows the
+# custom color pallete to be saved in a .cpt file. It's default value is False, which
+# uses the custom color pallete as the default color pallete for
+# the figure and color bar.
+
+fig = pygmt.Figure()
+pygmt.makecpt(cmap="jet", series="-8500/2500/1000")
+fig.grdimage(grid=grid, projection="M6i", frame=True)
+fig.colorbar()
+fig.show()
+
+########################################################################################
+# The ``continuous`` argument forces the custom color pallete to be a continuous
+# color pallete.
+
+fig = pygmt.Figure()
+pygmt.makecpt(cmap="jet", series="-8500/2500/1000", continuous=True)
+fig.grdimage(grid=grid, projection="M6i", frame=True)
+fig.colorbar()
+fig.show()
