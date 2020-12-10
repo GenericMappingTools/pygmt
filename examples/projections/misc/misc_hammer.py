5c7edb806b11e0ebd69590ef89a8e2d13452ccd4
Will Schlitzer <schlitzer90@gmail.com>
2020-12-10 08:54:51 +0000

    Update misc_hammer.py region and central meridian default

--- examples/projections/misc/misc_hammer.py
+++ examples/projections/misc/misc_hammer.py
@@ -2,17 +2,12 @@
 Hammer projection
 =================
 
-``H[central meridian]/width``: Give the optional central meridian (default is 180)
-and the map width.
+``H[central meridian]/width``: Give the optional central meridian (default is the center
+ of the region) and the map width.
 """
 import pygmt
 
 fig = pygmt.Figure()
-# Use region "g" to specify global region
-fig.coast(region="g", projection="H12c", land="black", water="cornsilk", frame="afg")
-fig.show()
-
-fig = pygmt.Figure()
-# Add the optional central meridian argument (in this example, 0 degrees latitude)
-fig.coast(region="g", projection="H0/12c", land="black", water="cornsilk", frame="afg")
+# Use region "d" to specify global region (-180/180)
+fig.coast(region="d", projection="H12c", land="black", water="cornsilk", frame="afg")
 fig.show()
