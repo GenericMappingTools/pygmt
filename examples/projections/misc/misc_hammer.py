9bde04a8e6571768a8b94cee14a99ae95ab6a441
Will Schlitzer <schlitzer90@gmail.com>
2020-12-10 08:44:25 +0000

    Add Hammer projection with prime meridian as central meridian

--- examples/projections/misc/misc_hammer.py
+++ examples/projections/misc/misc_hammer.py
@@ -11,3 +11,8 @@ fig = pygmt.Figure()
 # Use region "g" to specify global region
 fig.coast(region="g", projection="H12c", land="black", water="cornsilk", frame="afg")
 fig.show()
+
+fig = pygmt.Figure()
+# Add the optional central meridian argument (in this example, 0 degrees latitude)
+fig.coast(region="g", projection="H0/12c", land="black", water="cornsilk", frame="afg")
+fig.show()
