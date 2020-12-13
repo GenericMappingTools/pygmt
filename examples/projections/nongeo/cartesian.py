2590217125bd4e93c139c4a85781dad49cd88f04
Will Schlitzer <schlitzer90@gmail.com>
2020-12-13 12:09:42 +0000

    Combine all arguments into plot()

--- examples/projections/nongeo/cartesian.py
+++ examples/projections/nongeo/cartesian.py
@@ -9,7 +9,13 @@ import pygmt
 fig = pygmt.Figure()
 # ``region`` sets the x and y ranges or the Cartesian figure.
 # The argument ``WSne`` is passed to ``frame`` to put axis labels only on the left and bottom axes.
-fig.basemap(projection="X15c/10c", region=[0, 10, 0, 50], frame=["af", "WSne"])
-# ``fig.plot()`` is used to plot lines on the figure.
-fig.plot(x=[3, 9, 2], y=[4, 9, 37], pen="3p,red")
+# The ``x`` and ``y`` parameters are used to plot lines on the figure.
+fig.plot(
+    x=[3, 9, 2],
+    y=[4, 9, 37],
+    pen="3p,red",
+    region=[0, 10, 0, 50],
+    projection="X15c/10c",
+    frame=["af", "WSne"],
+)
 fig.show()
