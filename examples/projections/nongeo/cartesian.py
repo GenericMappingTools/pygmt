2996fd0ef86c7495354da6700ce958e85be52922
Will Schlitzer <schlitzer90@gmail.com>
2020-12-14 09:50:07 +0000

    Move comments

--- examples/projections/nongeo/cartesian.py
+++ examples/projections/nongeo/cartesian.py
@@ -7,14 +7,14 @@ Cartesian
 import pygmt
 
 fig = pygmt.Figure()
-# ``region`` sets the x and y ranges or the Cartesian figure.
-# The argument ``WSne`` is passed to ``frame`` to put axis labels only on the left and bottom axes.
-# The ``x`` and ``y`` parameters are used to plot lines on the figure.
 fig.plot(
+    # The ``x`` and ``y`` parameters are used to plot lines on the figure.
     x=[3, 9, 2],
     y=[4, 9, 37],
     pen="3p,red",
+    # ``region`` sets the x and y ranges or the Cartesian figure.
     region=[0, 10, 0, 50],
+    # The argument ``WSne`` is passed to ``frame`` to put axis labels only on the left and bottom axes.
     projection="X15c/10c",
     frame=["af", "WSne"],
 )
