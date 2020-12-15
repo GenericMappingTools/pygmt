5a705bc04651a634eefb1babf516f9e374721f1a
Will Schlitzer <schlitzer90@gmail.com>
2020-12-15 07:05:23 +0000

    Add test_basemap_compass() and test_basemap_rose()

--- pygmt/tests/test_basemap.py
+++ pygmt/tests/test_basemap.py
@@ -80,3 +80,19 @@ def test_basemap_aliases():
     fig = Figure()
     fig.basemap(region=[0, 360, -90, 90], projection="W7i", frame=True)
     return fig
+
+@pytest.mark.mpl_image_compare
+def test_basemap_rose():
+    "Create a map with coast and use basemap to add a rose"
+    fig = pygmt.Figure()
+    fig.coast(region=[127.5, 128.5, 26, 27], shorelines="1/0.5p")
+    fig.basemap(rose="jBR+w5c")
+    return fig
+
+@pytest.mark.mpl_image_compare
+def test_basemap_compass():
+    "Create a map with coast and use basemap to add a compass"
+    fig = pygmt.Figure()
+    fig.coast(region=[127.5, 128.5, 26, 27], shorelines="1/0.5p")
+    fig.basemap(compass="jBR+w5c+d11.5")
+    return fig
