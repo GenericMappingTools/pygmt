fb5abf4233fe669bf23773d2f83bbd286d3b3314
Will Schlitzer <schlitzer90@gmail.com>
2020-12-15 07:07:29 +0000

    Change GMTInvalidInput error text for basemap in base_plotting.py

--- pygmt/base_plotting.py
+++ pygmt/base_plotting.py
@@ -1169,7 +1169,7 @@ class BasePlotting:
         kwargs = self._preprocess(**kwargs)
         if not ("B" in kwargs or "L" in kwargs or "Td" in kwargs or "Tm" in kwargs):
             raise GMTInvalidInput(
-                "Frame, map_scale, compass, or rose must be specified."
+                "At least one of frame, map_scale, compass, or rose must be specified."
             )
         with Session() as lib:
             lib.call_module("basemap", build_arg_string(kwargs))
