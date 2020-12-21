aa25635a3cefcc8977c340fc7c577236865a4540
Will Schlitzer <schlitzer90@gmail.com>
2020-12-21 08:15:07 +0000

    Add projection description to conic_equidistant.py

--- examples/projections/conic/conic_equidistant.py
+++ examples/projections/conic/conic_equidistant.py
@@ -1,9 +1,16 @@
 """
 Equidistant conic
 =================
+The equidistant conic projection was described by the Greek philosopher Claudius
+Ptolemy about A.D. 150. It is neither conformal or equal-area, but serves as a
+compromise between them. The scale is true along all meridians and the
+standard parallels.
 
 ``Dlon0/lat0/lat1/lat2/width``: Give projection center ``lon0/lat0``, two standard
 parallels ``lat1/lat2``, and the map width.
+
+For further information, see
+:gmt-docs:`cookbook/map-projections.html#equidistant-conic-projection-jd-jd`
 """
 import pygmt
 
