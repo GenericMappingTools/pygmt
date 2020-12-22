55109d479e7c4c7b5f14f8ac8c87009895540ef3
Will Schlitzer <schlitzer90@gmail.com>
2020-12-22 10:41:56 +0000

    Add description on misc_mollweide.py

--- examples/projections/misc/misc_mollweide.py
+++ examples/projections/misc/misc_mollweide.py
@@ -2,6 +2,13 @@
 Mollweide
 =========
 
+This pseudo-cylindrical, equal-area projection was developed by the German
+mathematician and astronomer Karl Brandan Mollweide in 1805. Parallels are unequally
+spaced straight lines with the meridians being equally spaced elliptical arcs. The
+scale is only true along latitudes 40°44’ north and south. The projection is used
+mainly for global maps showing data distributions. It is occasionally referenced under
+the name homalographic projection.
+
 ``W[central meridian]/width``: Give the optional central meridian (default is the center
  of the region) and the map width.
 """
