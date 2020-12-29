31468772d95604b4a8cae0bf371ef7c5922aca5d
Will Schlitzer <schlitzer90@gmail.com>
2020-12-29 08:57:37 +0000

    Adding line break between link text and URL for styling

--- pygmt/base_plotting.py
+++ pygmt/base_plotting.py
@@ -139,9 +139,11 @@ class BasePlotting:
         paint_country : str or list
             ``code1,code2,…[+l|L][+gfill][+ppen][+z]``
             Select painting or dumping country polygons from the
-            'Digital Chart of the World <https://en.wikipedia.org/wiki/Digital_Chart_of_the_World>`__.
+            'Digital Chart of the World
+            <https://en.wikipedia.org/wiki/Digital_Chart_of_the_World>`__.
             Append one or more comma-separated countries using the 2-character
-            `ISO 3166-1 alpha-2 convention <https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2>`__.
+            `ISO 3166-1 alpha-2 convention
+            <https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2>`__.
             To select a state of a country (if available), append
             .state, (e.g, US.TX for Texas).  To specify a whole continent, prepend = to
             any of the continent codes (e.g. =EU for Europe).
