adf4526e49c7908e1ff8ca1af8d7856d2b0c639d
Will Schlitzer <schlitzer90@gmail.com>
2020-12-30 10:37:21 +0000

    Update azim_equidistant.py to GMT argument doc standards

--- examples/projections/azim/azim_equidistant.py
+++ examples/projections/azim/azim_equidistant.py
@@ -8,10 +8,11 @@ projection center are correct. It is very useful for a global view on locations
 that lie within a certain distance or for comparing distances of different
 locations relative to the projection center.
 
-``elon0/lat0[/horizon]/scale`` or ``Elon0/lat0[/horizon]/width``
+**e***lon0***/***lat0*[**/***horizon*]**/***scale*`` or
+``**E***lon0***/***lat0*[**/***horizon*]**/***width*``
 
-``lon0/lat0`` specifies the projection center. The optional parameter
-``horizon`` specifies the max distance to the projection center (i.e. the
+*lon0***/***lat0* specifies the projection center. The optional parameter
+*horizon* specifies the max distance to the projection center (i.e. the
 visibile portion of the rest of the world map) in degrees <= 180° (default 180°).
 """
 import pygmt
