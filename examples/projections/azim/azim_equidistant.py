52434e796311e840316381d08050b4d273d10831
Will Schlitzer <schlitzer90@gmail.com>
2021-01-01 11:03:39 +0000

    Clean up docstring in azim_equidistant.py

--- examples/projections/azim/azim_equidistant.py
+++ examples/projections/azim/azim_equidistant.py
@@ -8,10 +8,10 @@ projection center are correct. It is very useful for a global view on locations
 that lie within a certain distance or for comparing distances of different
 locations relative to the projection center.
 
-**e**\ *lon0*\ **/**\ *lat0*\ [**/**\ *horizon*]\ **/**\ *scale* or
-**E**\ *lon0*\ **/**\ *lat0*\ [**/**\ *horizon*]\ **/**\ *width*
+**e**\ *lon0/at0*\ [*/horizon*]\ */scale* or
+**E**\ *lon0/lat0*\ [*/horizon*]\ */width*
 
-*lon0*\ **/**\ *lat0* specifies the projection center. The optional parameter
+*lon0/lat0* specifies the projection center. The optional parameter
 *horizon* specifies the max distance to the projection center (i.e. the
 visibile portion of the rest of the world map) in degrees <= 180° (default 180°). The
 size of the figure is set by *scale* or *width*.
