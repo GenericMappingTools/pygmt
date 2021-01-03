f1548c3349e27ded6d7de4ebb80b4175173c9c16
Will Schlitzer <schlitzer90@gmail.com>
2021-01-03 16:49:03 +0000

    Add test_inset_end_requirement() to test_inset.py

--- pygmt/tests/test_inset.py
+++ pygmt/tests/test_inset.py
@@ -54,3 +54,28 @@ def test_inset_end():
     # Add a title after calling inset_end()
     fig_test.basemap(frame="+tTest")
     return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_inset_end_requirement():
+    """Test that inset_end() does not need to be called if inset is added at
+    the end of the figure."""
+    fig_ref, fig_test = Figure(), Figure()
+    fig_ref.coast(
+        region="MG+r2", land="brown", water="lightblue", shorelines="thin", frame="a"
+    )
+    fig_ref.inset_begin(location="jTL+w3.5c+o0.2c", margin=0, border="+pgreen")
+    fig_ref.coast(
+        region="g", projection="G47/-20/4c", land="gray", water="white", E="MG+gred"
+    )
+    # fig_ref includes calling inset_end()
+    fig_ref.inset_end()
+    fig_test.coast(
+        region="MG+r2", land="brown", water="lightblue", shorelines="thin", frame="a"
+    )
+    fig_test.inset_begin(location="jTL+w3.5c+o0.2c", margin=0, border="+pgreen")
+    fig_test.coast(
+        region="g", projection="G47/-20/4c", land="gray", water="white", E="MG+gred"
+    )
+    # fig_test does not include calling inset_end()
+    return fig_ref, fig_test
