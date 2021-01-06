9763947e518222b8302484e62030cdb954b03af9
Will Schlitzer <schlitzer90@gmail.com>
2021-01-06 07:47:53 +0000

    Updating doc string for cyl_equal_area.py

--- examples/projections/cyl/cyl_equal_area.py
+++ examples/projections/cyl/cyl_equal_area.py
@@ -6,8 +6,10 @@ This cylindrical projection is actually several projections, depending on what
 latitude is selected as the standard parallel. However, they are all equal area and
 hence non-conformal. All meridians and parallels are straight lines.
 
-``Ylon0/lat0/width``: Give central meridian ``lon0``, the standard parallel ``lat0``,
-and the figure ``width``.
+**y**\ *lon0/lat0*\ */scale* or **Y**\ *lon0/lat0*\ */width*
+
+The projection is set with **y** or **Y**. The projection center is set by *lon0/lat0*,
+and the figure size is set with *scale* or *width*.
 """
 import pygmt
 
