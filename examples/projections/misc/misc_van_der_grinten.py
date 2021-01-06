6f13dc61bcdbb57eefc667df24a63d84595046a9
Will Schlitzer <schlitzer90@gmail.com>
2021-01-06 20:38:09 +0000

    Update doc string for misc_van_der_grinten.py

--- examples/projections/misc/misc_van_der_grinten.py
+++ examples/projections/misc/misc_van_der_grinten.py
@@ -7,8 +7,10 @@ neither equal-area nor conformal. Central meridian and Equator are straight line
 other meridians are arcs of circles. The scale is true along the Equator only. Its
 main use is to show the entire world enclosed in a circle.
 
-``V[central meridian]/width``: Give the optional central meridian (default is the center
-of the region) and the map width.
+**v**\ [*lon0/*]\ *scale* or **V**\ [*lon0/*]\ *width*
+
+The projection is set with **v** or **V**. The central meridian is set with the
+optional *lon0*, and the figure size is set with *scale* or *width*
 """
 import pygmt
 
