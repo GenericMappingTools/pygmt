0713d2d0e1ed0f3a312453c0cf7489a08ee2019a
Will Schlitzer <schlitzer90@gmail.com>
2021-01-13 09:53:37 +0000

    Update pygmt/base_plotting.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/base_plotting.py
+++ pygmt/base_plotting.py
@@ -147,7 +147,7 @@ class BasePlotting:
             To select a state of a country (if available), append
             .*state*, (e.g, US.TX for Texas).  To specify a whole continent, prepend = to
             any of the continent codes (e.g. =EU for Europe).
-            Append **+p**pen to draw polygon outlines (default is no outline)
+            Append **+p**\ *pen* to draw polygon outlines (default is no outline)
             and **+g**fill to fill them (default is no fill). Append **+l**|**+L**
             to *=continent* to only list countries in that continent; repeat if more
             than one continent is requested. Append **+z** to place the country code in
