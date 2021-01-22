ce22b30e65c603828bb70615c8c2119c5cd6073c
Will Schlitzer <schlitzer90@gmail.com>
2021-01-22 12:26:04 +0000

    Add gallery example with inset.py

--- examples/gallery/plot/inset.py
+++ examples/gallery/plot/inset.py
@@ -14,12 +14,12 @@ fig = pygmt.Figure()
 # "brown", the water to "lightblue", the shorelines width to "thin", and adding a frame
 fig.coast(region="MG+r2", land="brown", water="lightblue", shorelines="thin", frame="a")
 # Create an inset, setting the location to top left, the width to 3.5 centimeters, and
-# the x-offset to 0.2 centimeters. The margin is set to 0, and the border is green
+# the x-offset to 0.2 centimeters. The margin is set to 0, and the border is "green".
 with fig.inset(location="jTL+w3.5c+o0.2c", margin=0, border="+pgreen"):
     # Create a figure in the inset using coast. This example uses the azimuthal
     # orthogonal projection centered at 47E, 20S. The 4 centimeter width of this figure
     # extends beyond the inset width, and isn't shown. The land is set to "gray" and
-    # Madagascar is highlighted in "red"
+    # Madagascar is highlighted in "red".
     fig.coast(
         region="g", projection="G47/-20/4c", land="gray", water="white", dcw="MG+gred"
     )
