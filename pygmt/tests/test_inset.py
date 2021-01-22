5326f247ffdfe634042a776f9cd39116373954dd
Will Schlitzer <schlitzer90@gmail.com>
2021-01-22 17:37:49 +0000

    Add test_inset_context_manager() in test_inset.py

--- pygmt/tests/test_inset.py
+++ pygmt/tests/test_inset.py
@@ -34,3 +34,54 @@ def test_inset_aliases():
             dcw="MG+gred",
         )
     return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_inset_context_manager():
+    """
+    Test that the inset context manager works and, once closed, plotting
+    elements are added to the larger figure.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    fig_ref.coast(
+        region=[-74, -69.5, 41, 43],
+        borders="2/thin",
+        shorelines="thin",
+        projection="M15c",
+        land="lightyellow",
+        water="lightblue",
+    )
+    # Test passing the frame argument with basemap before the inset
+    fig_ref.basemap(frame="a")
+    with fig_ref.inset(location="jBL+w3c+o0.2c", margin=0, border="+pblack"):
+        fig_ref.coast(
+            region=[-80, -65, 35, 50],
+            projection="M3c",
+            land="gray",
+            borders=[1, 2],
+            shorelines="1/thin",
+            water="white",
+            dcw="US.MA+gred",
+        )
+    fig_test.coast(
+        region=[-74, -69.5, 41, 43],
+        borders="2/thin",
+        shorelines="thin",
+        projection="M15c",
+        land="lightyellow",
+        water="lightblue",
+    )
+
+    with fig_test.inset(location="jBL+w3c+o0.2c", margin=0, border="+pblack"):
+        fig_test.coast(
+            region=[-80, -65, 35, 50],
+            projection="M3c",
+            land="gray",
+            borders=[1, 2],
+            shorelines="1/thin",
+            water="white",
+            dcw="US.MA+gred",
+        )
+    # Test passing the frame argument with basemap after the inset
+    fig_test.basemap(frame="a")
+    return fig_ref, fig_test
