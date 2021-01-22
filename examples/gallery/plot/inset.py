ab48e96214d1446b268f678a6f41b998bd51bc4c
Will Schlitzer <schlitzer90@gmail.com>
2021-01-22 12:03:23 +0000

    Add gallery example with inset.py

---
+++ examples/gallery/plot/inset.py
@@ -0,0 +1,26 @@
+"""
+Inset
+-----
+
+The :meth:`pygmt.Figure.inset` method creates adds an inset figure to a larger
+figure. The function is called using a ``with`` statement, and it's location,
+border, offset, and margin parameters are set. Within the ``with`` statement,
+pygmt plotting functions can be called that add to the inset figure.
+"""
+import pygmt
+
+fig = pygmt.Figure()
+# Create the primary figure, setting the region to Madagascar, the land color to
+# "brown", the water to "lightblue", the shorelines width to "thin", and adding a frame
+fig.coast(region="MG+r2", land="brown", water="lightblue", shorelines="thin", frame="a")
+# Create an inset, setting the location to top left, the width to 3.5 centimeters, and
+# the x-offset to 0.2 centimeters. The margin is set to 0, and the border is green
+with fig.inset(location="jTL+w3.5c+o0.2c", margin=0, border="+pgreen"):
+    # Create a figure in the inset using coast. This example uses the azimuthal
+    # orthogonal projection centered at 47E, 20S. The 4 centimeter width of this figure
+    # extends beyond the inset width, and isn't shown. The land is set to "gray" and
+    # Madagascar is highlighted in "red"
+    fig.coast(
+        region="g", projection="G47/-20/4c", land="gray", water="white", dcw="MG+gred"
+    )
+fig.show()
--- pygmt/tests/test_inset.py
+++ pygmt/tests/test_inset.py
@@ -16,13 +16,21 @@ def test_inset_aliases():
     )
     with fig_ref.inset(D="jTL+w3.5c+o0.2c", M=0, F="+pgreen"):
         fig_ref.coast(
-            region="g", projection="G47/-20/4c", land="gray", water="white", E="MG+gred"
+            region="g",
+            projection="G47/-20/4c",
+            land="gray",
+            water="white",
+            dcw="MG+gred",
         )
     fig_test.coast(
         region="MG+r2", land="brown", water="lightblue", shorelines="thin", frame="a"
     )
     with fig_test.inset(location="jTL+w3.5c+o0.2c", margin=0, border="+pgreen"):
         fig_test.coast(
-            region="g", projection="G47/-20/4c", land="gray", water="white", E="MG+gred"
+            region="g",
+            projection="G47/-20/4c",
+            land="gray",
+            water="white",
+            dcw="MG+gred",
         )
     return fig_ref, fig_test
