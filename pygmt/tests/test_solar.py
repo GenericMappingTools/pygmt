de414f7143a99dfc117036d5c557afc84ffa2525
Will Schlitzer <schlitzer90@gmail.com>
2021-01-24 16:13:45 +0000

    Add test_solar_fill_transparency to test_solar.py

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -102,6 +102,21 @@ def test_solar_fill():
 
 
 @check_figures_equal()
+def test_solar_fill_transparency():
+    """
+    Test plotting solar terminator and night-area fill with a transparency
+    modifier on top of a coast figure.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.coast(R="d", J="W0/15c", G="darkgreen", S="lightblue")
+    fig_ref.solar(Td="+d1990-02-17T04:27:00", G="navyblue@75")
+    fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
+    fig_test.solar(day_night="+d1990-02-17T04:27:00", fill="navyblue@75")
+    return fig_ref, fig_test
+
+
+@check_figures_equal()
 def test_solar_pen():
     """
     Test plotting solar terminator and setting a pen on top of a coast figure.
