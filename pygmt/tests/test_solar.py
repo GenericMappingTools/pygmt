58add00af655aa23e2e52aaded592af6b18ba3a3
Will Schlitzer <schlitzer90@gmail.com>
2021-01-24 15:25:13 +0000

    Add test_solar_coast to test_solar.py

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -17,3 +17,17 @@ def test_solar():
     fig_ref.solar(R="d", J="W0/15c", Td="+d1990-02-17T04:25:00")
     fig_test.solar(region="d", projection="W0/15c", day_night="+d1990-02-17T04:25:00")
     return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_solar_coast():
+    """
+    Test plotting solar terminator on top of a coast figure.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.coast(R="d", J="W0/15c", G="darkgreen", S="lightblue")
+    fig_ref.solar(Td="+d1990-02-17T04:27:00")
+    fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
+    fig_test.solar(day_night="+d1990-02-17T04:27:00")
+    return fig_ref, fig_test
