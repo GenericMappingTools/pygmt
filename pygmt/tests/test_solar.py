8887811b9bb330de1f0345ec0f2b058111e3a1c5
Will Schlitzer <schlitzer90@gmail.com>
2021-01-24 16:28:40 +0000

    Add 5 tests to test_solar.py

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -128,3 +128,30 @@ def test_solar_pen():
     fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
     fig_test.solar(day_night="+d1990-02-17T04:27:00", pen="1p,red,-.-")
     return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_solar_xy_shift():
+    """
+    Test plotting solar terminator with an x-y shift on top of a coast figure.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.coast(R="d", J="W0/15c", G="darkgreen", S="lightblue")
+    fig_ref.solar(Td="+d1990-02-17T04:27:00", X=3, Y=5)
+    fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
+    fig_test.solar(day_night="+d1990-02-17T04:27:00", xshift=3, yshift=5)
+    return fig_ref, fig_test
+
+@check_figures_equal()
+def test_solar_perspective():
+    """
+    Test plotting solar terminator with a perspective shift on top of a coast figure.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.coast(R="d", J="W0/15c", G="darkgreen", S="lightblue")
+    fig_ref.solar(Td="+d1990-02-17T04:27:00", p="135/45")
+    fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
+    fig_test.solar(day_night="+d1990-02-17T04:27:00", perspective=[135, 45])
+    return fig_ref, fig_test
