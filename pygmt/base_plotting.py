0f7692477e887c6936a28110c3fdbaf80bd9caff
Will Schlitzer <schlitzer90@gmail.com>
2021-01-24 19:10:49 +0000

    Fix format

--- pygmt/base_plotting.py
+++ pygmt/base_plotting.py
@@ -109,7 +109,8 @@ class BasePlotting:
         {R}
         area_thresh : int, float, or str
             *min_area*\ [/*min_level*\ /*max_level*\ ][**+ag**\ \|\ **i**\
-            \|\ **s**\ \|\ **S**\ ][\ **+r**\ \|\ **l**\ ][\ **+p**\ *percent*].
+            \|\ **s**\ \|\ **S**\ ][\ **+r**\ \|\ **l**\ ][\ **+p**\
+            *percent*].
             Features with an area smaller than min_area in km^2 or of
             hierarchical level that is lower than min_level or higher than
             max_level will not be plotted.
