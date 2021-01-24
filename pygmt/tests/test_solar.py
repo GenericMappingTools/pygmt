9c1221e4770ce86d94620ce4d2c34ca142935e38
Will Schlitzer <schlitzer90@gmail.com>
2021-01-24 16:12:02 +0000

    Add test_solar_astronomical_twighlight to test_solar.py

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -39,6 +39,23 @@ def test_solar_civil_twighlight():
 
 
 @check_figures_equal()
+def test_solar_astronomical_twighlight():
+    """
+    Test passing the solar argument with the astronomical_twighlight argument.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.solar(R="d", J="W0/15c", B="a", Ta="+d1990-02-17T04:25:00")
+    fig_test.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
+        astronomical_twighlight="+d1990-02-17T04:25:00",
+    )
+    return fig_ref, fig_test
+
+
+@check_figures_equal()
 def test_solar_nautical_twighlight():
     """
     Test passing the solar argument with the nautical_twighlight argument.
