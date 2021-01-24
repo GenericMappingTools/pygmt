a5e808b6285e4cd31e8631bdc994b02695635e4d
Will Schlitzer <schlitzer90@gmail.com>
2021-01-24 16:06:35 +0000

    Add test_solar_pen to test_solar.py

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -14,8 +14,10 @@ def test_solar():
     """
     fig_ref, fig_test = Figure(), Figure()
     # Use single-character arguments for the reference image
-    fig_ref.solar(R="d", J="W0/15c", Td="+d1990-02-17T04:25:00")
-    fig_test.solar(region="d", projection="W0/15c", day_night="+d1990-02-17T04:25:00")
+    fig_ref.solar(R="d", J="W0/15c", B="a", Td="+d1990-02-17T04:25:00")
+    fig_test.solar(
+        region="d", projection="W0/15c", frame="a", day_night="+d1990-02-17T04:25:00"
+    )
     return fig_ref, fig_test
 
 
@@ -30,4 +32,33 @@ def test_solar_coast():
     fig_ref.solar(Td="+d1990-02-17T04:27:00")
     fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
     fig_test.solar(day_night="+d1990-02-17T04:27:00")
+    return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_solar_fill():
+    """
+    Test plotting solar terminator and night-area fill on top of a coast
+    figure.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.coast(R="d", J="W0/15c", G="darkgreen", S="lightblue")
+    fig_ref.solar(Td="+d1990-02-17T04:27:00", G="navyblue")
+    fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
+    fig_test.solar(day_night="+d1990-02-17T04:27:00", fill="navyblue")
+    return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_solar_pen():
+    """
+    Test plotting solar terminator and setting a pen on top of a coast figure.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.coast(R="d", J="W0/15c", G="darkgreen", S="lightblue")
+    fig_ref.solar(Td="+d1990-02-17T04:27:00", W="1p,red,-.-")
+    fig_test.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
+    fig_test.solar(day_night="+d1990-02-17T04:27:00", pen="1p,red,-.-")
     return fig_ref, fig_test
