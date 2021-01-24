9cd30dba3e7630f18f99ca5e65d66c6b28abf782
Will Schlitzer <schlitzer90@gmail.com>
2021-01-24 16:10:42 +0000

    Add test_solar_civil_twighlight and test_solar_nautical_twighlight to test_solar.py

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -8,15 +8,49 @@ from pygmt.helpers.testing import check_figures_equal
 
 
 @check_figures_equal()
-def test_solar():
+def test_solar_day_night():
     """
-    Test passing only the solar argument.
+    Test passing the solar argument with the day_night argument.
     """
     fig_ref, fig_test = Figure(), Figure()
     # Use single-character arguments for the reference image
     fig_ref.solar(R="d", J="W0/15c", B="a", Td="+d1990-02-17T04:25:00")
     fig_test.solar(
         region="d", projection="W0/15c", frame="a", day_night="+d1990-02-17T04:25:00"
+    )
+    return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_solar_civil_twighlight():
+    """
+    Test passing the solar argument with the civil_twighlight argument.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.solar(R="d", J="W0/15c", B="a", Tc="+d1990-02-17T04:25:00")
+    fig_test.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
+        civil_twighlight="+d1990-02-17T04:25:00",
+    )
+    return fig_ref, fig_test
+
+
+@check_figures_equal()
+def test_solar_nautical_twighlight():
+    """
+    Test passing the solar argument with the nautical_twighlight argument.
+    """
+    fig_ref, fig_test = Figure(), Figure()
+    # Use single-character arguments for the reference image
+    fig_ref.solar(R="d", J="W0/15c", B="a", Tn="+d1990-02-17T04:25:00")
+    fig_test.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
+        nautical_twighlight="+d1990-02-17T04:25:00",
     )
     return fig_ref, fig_test
 
