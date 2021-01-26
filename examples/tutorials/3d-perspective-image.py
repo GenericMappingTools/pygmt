5054ae4e28166f37f914384856860435a14c9e3f
Will Schlitzer <schlitzer90@gmail.com>
2021-01-26 07:31:19 +0000

    Add notebook explanation to 3d-perspective-image.py

--- examples/tutorials/3d-perspective-image.py
+++ examples/tutorials/3d-perspective-image.py
@@ -4,6 +4,13 @@ Creating a 3D perspective image
 
 Create 3-D perspective image or surface mesh from a grid
 using :meth:`pygmt.Figure.grdview`.
+
+This tutorial assumes the use of a Python notebook, such as IPython or Jupyter Notebook.
+To see the figures while using a Python script instead, use
+``fig.show(method="external)`` to display the figure in the default PDF viewer.
+
+To save the figure, use ``fig.savefig("figname.pdf")`` where ``"figname.pdf"``
+is the desired name and file extension for the saved figure.
 """
 
 import pygmt
