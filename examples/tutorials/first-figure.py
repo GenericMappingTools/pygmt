0d6450ad845157a61c4ab1e2bfbd1da3dedd5e54
Will Schlitzer <schlitzer90@gmail.com>
2021-01-26 07:44:15 +0000

    Add notebook explanation to first-figure.py

--- examples/tutorials/first-figure.py
+++ examples/tutorials/first-figure.py
@@ -4,6 +4,13 @@ Making your first figure
 
 Welcome to PyGMT! Here we'll cover some of basic concepts, like creating simple figures
 and naming conventions.
+
+This tutorial assumes the use of a Python notebook, such as IPython or Jupyter Notebook.
+To see the figures while using a Python script instead, use
+``fig.show(method="external)`` to display the figure in the default PDF viewer.
+
+To save the figure, use ``fig.savefig("figname.pdf")`` where ``"figname.pdf"``
+is the desired name and file extension for the saved figure.
 """
 
 ########################################################################################
