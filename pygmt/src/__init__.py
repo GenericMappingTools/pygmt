0085853106bac30c7543728ab1e608529d2f1afb
Will Schlitzer <schlitzer90@gmail.com>
2021-01-27 14:06:30 +0000

    change order of modules in __init__.py

--- pygmt/src/__init__.py
+++ pygmt/src/__init__.py
@@ -1,9 +1,11 @@
 """
 Source code for PyGMT modules.
 """
+# pylint: disable=import-outside-toplevel
+
+# GMT Primary modules
+from pygmt.src.solar import solar
+
 # GMT Supplementary modules
 from pygmt.src.meca import meca
 
-# pylint: disable=import-outside-toplevel
-# GMT Primary modules
-from pygmt.src.solar import solar
