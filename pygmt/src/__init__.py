8a43456cfe8378df081a178eb46429de7e5ddccb
Will Schlitzer <schlitzer90@gmail.com>
2021-01-28 09:59:08 +0000

    Move grdcontour to grdcontour.py

--- pygmt/src/__init__.py
+++ pygmt/src/__init__.py
@@ -5,6 +5,7 @@ Source code for PyGMT modules.
 # GMT Primary modules
 from pygmt.src.coast import coast
 from pygmt.src.colorbar import colorbar
+from pygmt.src.grdcontour import grdcontour
 from pygmt.src.logo import logo
 
 # GMT Supplementary modules
