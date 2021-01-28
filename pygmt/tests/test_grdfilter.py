d9b6112eefe740dd0969d38b535252bae803a3df
Will Schlitzer <schlitzer90@gmail.com>
2021-01-28 09:35:42 +0000

    Add test_grdfilter_dataarray_in_file_out

--- pygmt/tests/test_grdfilter.py
+++ pygmt/tests/test_grdfilter.py
@@ -19,20 +19,35 @@ def fixture_grid():
     """
     return load_earth_relief(registration="pixel")
 
+
 def test_grfilter_dataarray_in_dataarray_out(grid):
     """
     grdfilter an input DataArray, and output as DataArray.
     """
-    grid = load_earth_relief(registration="pixel")
-    smooth_field = grdfilter(grid=grid, filter="g600", distance="4")
+    # grid = load_earth_relief(registration="pixel")
+    result = grdfilter(grid=grid, filter="g600", distance="4")
     # check information of the output grid
-    assert isinstance(smooth_field, xr.DataArray)
-    assert smooth_field.coords["lat"].data.min() == -89.5
-    assert smooth_field.coords["lat"].data.max() == 89.5
-    assert smooth_field.coords["lon"].data.min() == -179.5
-    assert smooth_field.coords["lon"].data.max() == 179.5
-    assert smooth_field.sizes["lat"] == 180
-    assert smooth_field.sizes["lon"] == 360
+    assert isinstance(result, xr.DataArray)
+    assert result.coords["lat"].data.min() == -89.5
+    assert result.coords["lat"].data.max() == 89.5
+    assert result.coords["lon"].data.min() == -179.5
+    assert result.coords["lon"].data.max() == 179.5
+    assert result.sizes["lat"] == 180
+    assert result.sizes["lon"] == 360
+
+
+def test_grdfilter_dataarray_in_file_out(grid):
+    """
+    grdfilter an input DataArray, and output to a grid file.
+    """
+    with GMTTempFile(suffix=".nc") as tmpfile:
+        result = grdfilter(grid, outgrid=tmpfile.name, filter="g600", distance="4")
+        assert result is None  # grdcut returns None if output to a file
+        result = grdinfo(tmpfile.name, C=True)
+        assert (
+            result == "-180 180 -90 90 -6147.47265625 5164.11572266 1 1 360 180 1 1\n"
+        )
+
 
 def test_grdfilter_fails():
     """
