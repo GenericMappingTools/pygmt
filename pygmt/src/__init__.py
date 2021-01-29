04d8ab05ea57e0b021a3c30a1d5b0f7a249e48da
Michael Grund <michael.grund@kit.edu>
2021-01-29 17:43:25 +0100

    move rose function from base_plotting.py to rose.py

--- pygmt/src/__init__.py
+++ pygmt/src/__init__.py
@@ -2,4 +2,9 @@
 Source code for PyGMT modules.
 """
 # pylint: disable=import-outside-toplevel
+
+# GMT Supplementary modules
 from pygmt.src.meca import meca
+
+# GMT Primary modules
+from pygmt.src.rose import rose
