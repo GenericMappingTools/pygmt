5fb620087a269f8198b3e90ac1d75bafb99d5037
Michael Grund <michael_grund@gmx.de>
2021-01-30 17:03:08 +0100

    Add a gallery example for "logo"

---
+++ examples/gallery/plot/logo.py
@@ -0,0 +1,19 @@
+"""
+Logo
+----
+
+The :meth:`pygmt.Figure.logo` method allows to place the GMT logo on a map.  
+
+For more advanced options, see the full option list at :gmt-docs:`logo.html`.
+"""
+
+import pygmt
+
+fig = pygmt.Figure()
+fig.basemap(region=[0, 10, 0, 2], projection="X6c", frame=True)
+
+# add the GMT logo in the upper right corner of the current map, 
+# scaled up to be 3 cm wide and offset by 0.3 cm from the border
+fig.logo(position = "jTR+o0.3c/0.3c+w3c") 
+
+fig.show()
