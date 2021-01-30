cf2ef7b5396e8a6fa9de299e5573a6a0115d4ab2
Michael Grund <michael_grund@gmx.de>
2021-01-30 22:07:43 +0100

    Update examples/gallery/plot/logo.py
    Co-authored-by: Will Schlitzer <schlitzer90@gmail.com>

--- examples/gallery/plot/logo.py
+++ examples/gallery/plot/logo.py
@@ -14,6 +14,8 @@ fig.basemap(region=[0, 10, 0, 2], projection="X6c", frame=True)
 
 # add the GMT logo in the upper right corner of the current map,
 # scaled up to be 3 cm wide and offset by 0.3 cm from the border
-fig.logo(position="jTR+o0.3c/0.3c+w3c")
+#  scaled up to be 3 cm wide and offset by 0.3 cm from the vertical border
+# and 0.6 cm from the horizontal border
+fig.logo(position="jTR+o0.3c/0.6c+w3c")
 
 fig.show()
