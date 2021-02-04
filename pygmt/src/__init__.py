0648d04087bdc567b53fc30a5f97859f24290f26
Dongdong Tian <seisman.info@gmail.com>
2021-02-04 17:55:04 -0500

    Move grdfilter to src

--- pygmt/src/__init__.py
+++ pygmt/src/__init__.py
@@ -3,6 +3,7 @@ Source code for PyGMT modules.
 """
 # pylint: disable=import-outside-toplevel
 from pygmt.src.blockmedian import blockmedian
+from pygmt.src.grdfilter import grdfilter
 from pygmt.src.grdtrack import grdtrack
 from pygmt.src.makecpt import makecpt
 from pygmt.src.meca import meca
