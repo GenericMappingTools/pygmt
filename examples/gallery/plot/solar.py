b95b4042970d29fdbeeba94653afadf6d1b7b1c4
Will Schlitzer <schlitzer90@gmail.com>
2021-02-04 05:53:47 +0000

    Update examples/gallery/plot/solar.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/gallery/plot/solar.py
+++ examples/gallery/plot/solar.py
@@ -13,8 +13,8 @@ fig.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
 # Set a time for the day-night terminator, 1700 UTC on January 1, 2000
 # Set the pen line to be 1p thick
 # Set the fill for the night area to be navy blue at 80% transparency
-fig.solar(Td="+d2000-01-01T17:00:00+", fill="navyblue@95", pen="1p")
-fig.solar(Tc="+d2000-01-01T17:00:00+", fill="navyblue@85", pen="1p")
-fig.solar(Tn="+d2000-01-01T17:00:00+", fill="navyblue@80", pen="1p")
-fig.solar(Ta="+d2000-01-01T17:00:00+", fill="navyblue@80", pen="1p")
+fig.solar(day_night="+d2000-01-01T17:00:00", fill="navyblue@95", pen="1p")
+fig.solar(civil_twilight="+d2000-01-01T17:00:00", fill="navyblue@85", pen="1p")
+fig.solar(nautical_twilight="+d2000-01-01T17:00:00", fill="navyblue@80", pen="1p")
+fig.solar(astronomical_twilight="+d2000-01-01T17:00:00", fill="navyblue@80", pen="1p")
 fig.show()
