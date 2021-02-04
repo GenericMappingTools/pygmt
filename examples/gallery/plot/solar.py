e172457ff289b7252e2e38db1fac40036ece484b
Will Schlitzer <schlitzer90@gmail.com>
2021-02-04 06:18:10 +0000

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/gallery/plot/solar.py
+++ examples/gallery/plot/solar.py
@@ -2,7 +2,8 @@
 Solar
 -----
 
-Use :meth:`pygmt.Figure.solar` to plot the day-night terminator line.
+Use :meth:`pygmt.Figure.solar` to plot the day-night terminator line, 
+and civil, nautical, astronomical twilights.
 """
 import pygmt
 
@@ -10,7 +11,7 @@ fig = pygmt.Figure()
 # Create a figure showing the global region on a Mollweide projection
 # Land color is set to dark green and water color is set to light blue
 fig.coast(region="d", projection="W0/15c", land="darkgreen", water="lightblue")
-# Set a time for the day-night terminator, 1700 UTC on January 1, 2000
+# Set a time for the day-night terminator and twilights, 1700 UTC on January 1, 2000
 # Set the pen line to be 1p thick
 # Set the fill for the night area to be navy blue at 80% transparency
 fig.solar(day_night="+d2000-01-01T17:00:00", fill="navyblue@95", pen="1p")
