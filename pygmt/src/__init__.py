276840e39f8b88aa04b218585f8f9c30fb595e7e
Will Schlitzer <schlitzer90@gmail.com>
2021-02-05 06:54:25 +0000

    Move pylint comment to top of src/__init__.py

--- pygmt/src/__init__.py
+++ pygmt/src/__init__.py
@@ -1,7 +1,6 @@
 """
 Source code for PyGMT modules.
 """
-from pygmt.src.inset import inset
-
 # pylint: disable=import-outside-toplevel
+from pygmt.src.inset import inset
 from pygmt.src.meca import meca
