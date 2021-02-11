5073c8804d796e36cad4d3536a981ceaaa1971b9
Dongdong Tian <seisman.info@gmail.com>
2021-02-11 00:56:49 -0500

    Add verbose for debugging

--- pygmt/tests/test_image.py
+++ pygmt/tests/test_image.py
@@ -15,5 +15,5 @@ def test_image():
     Place images on map.
     """
     fig = Figure()
-    fig.image(TEST_IMG, D="x0/0+w1i", F="+pthin,blue")
+    fig.image(TEST_IMG, position="x0/0+w1i", box="+pthin,blue", verbose="d")
     return fig
