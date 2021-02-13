2d35585b4034ecfeef0d102e834c7cf158df16e9
Meghan Jones <meghanj@hawaii.edu>
2021-02-13 11:08:00 -1000

    Update colorbar docstring

--- pygmt/src/colorbar.py
+++ pygmt/src/colorbar.py
@@ -35,9 +35,9 @@ def colorbar(self, **kwargs):
     gradational colors (i.e., the lower and upper boundary of an interval
     have different colors) we will interpolate to give a continuous scale.
     Variations in intensity due to shading/illumination may be displayed by
-    setting the option -I. Colors may be spaced according to a linear
-    scale, all be equal size, or by providing a file with individual tile
-    widths.
+    setting the ``shading`` parameter. Colors may be spaced according to a
+    linear scale, all be equal size, or by providing a file with individual
+    tile widths.
 
     Full option list at :gmt-docs:`colorbar.html`
 
@@ -59,14 +59,14 @@ def colorbar(self, **kwargs):
         use **j** or **J** for setting *refpoint* via a 2-char justification
         code that refers to the (invisible) map domain rectangle, (3) use *n*
         for normalized (0-1) coordinates, or (4) use *x* for plot
-        coordinates (inches, cm, etc.). All but *x* requires both *region*
-        and *projection* to be specified. Append +w followed by the length
-        and width of the color bar. If width is not specified then it is
+        coordinates (inches, cm, etc.). All but *x* requires both ``region``
+        and ``projection`` to be specified. Append **+w** followed by the
+        length and width of the color bar. If width is not specified then it is
         set to 4% of the given length. Give a negative length to reverse
-        the scale bar. Append +h to get a horizontal scale
-        [Default is vertical (+v)]. By default, the anchor point on the
-        scale is assumed to be the bottom left corner (BL), but this can be
-        changed by appending +j followed by a 2-char justification code
+        the scale bar. Append **+h** to get a horizontal scale
+        [Default is vertical (**+v**)]. By default, the anchor point on the
+        scale is assumed to be the bottom left corner (**BL**), but this can be
+        changed by appending **+j** followed by a 2-char justification code
         *justify*.
     box : bool or str
         [**+c**\ *clearances*][**+g**\ *fill*][**+i**\ [[*gap*/]\ *pen*]]
@@ -97,8 +97,8 @@ def colorbar(self, **kwargs):
         Add illumination effects. Passing a single numerical value sets the
         range of intensities from -value to +value. If not specified, 1 is
         used. Alternatively, set ``shading=[low, high]`` to specify an
-        asymmetric intensity range from *low* to *high*. The default is no
-        illumination.
+        asymmetric intensity range from *low* to *high*. [Default is no
+        illumination].
     {V}
     {XY}
     {c}
