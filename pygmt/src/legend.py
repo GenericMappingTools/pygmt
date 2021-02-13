2b135ca773f5565467f285a240d2db1496c7752a
Meghan Jones <meghanj@hawaii.edu>
2021-02-13 11:46:10 -1000

    Update legend docstring

--- pygmt/src/legend.py
+++ pygmt/src/legend.py
@@ -44,8 +44,8 @@ def legend(self, spec=None, position="JTR+jTR+o0.2c", box="+gwhite+p1p", **kwarg
     Parameters
     ----------
     spec : None or str
-        Either None (default) for using the automatically generated legend
-        specification file, or a filename pointing to the legend
+        Either ``None`` [default] for using the automatically generated legend
+        specification file, or a *filename* pointing to the legend
         specification file.
     {J}
     {R}
@@ -54,14 +54,15 @@ def legend(self, spec=None, position="JTR+jTR+o0.2c", box="+gwhite+p1p", **kwarg
         **+w**\ *width*\ [/*height*]\ [**+j**\ *justify*]\ [**+l**\ *spacing*]
         [**+o**\ *dx*\ [/*dy*]].
         Defines the reference point on the map for the
-        legend. By default, uses 'JTR+jTR+o0.2c' which places the legend at
-        the top-right corner inside the map frame, with a 0.2 cm offset.
+        legend. By default, uses **JTR**\ +\ **jTR**\ +\ **o**\ *0.2c* which
+        places the legend at the top-right corner inside the map frame, with a
+        0.2 cm offset.
     box : bool or str
         [**+c**\ *clearances*][**+g**\ *fill*][**+i**\ [[*gap*/]\ *pen*]]
         [**+p**\ [*pen*]][**+r**\ [*radius*]][**+s**\ [[*dx*/*dy*/][*shade*]]].
         Without further options, draws a rectangular border around the legend
         using **MAP_FRAME_PEN**. By default, uses **+g**\ white\ **+p**\ 1p
-        which draws a box around the legend using a 1 point black pen and adds
+        which draws a box around the legend using a 1p black pen and adds
         a white background.
     {V}
     {XY}
