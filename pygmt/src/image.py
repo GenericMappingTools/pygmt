aade1f486ff0644e1359261fd33f03811d6ca0c2
Meghan Jones <meghanj@alum.mit.edu>
2021-02-14 08:58:52 -0500

    Update pygmt/src/image.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/src/image.py
+++ pygmt/src/image.py
@@ -47,7 +47,7 @@ def image(self, imagefile, **kwargs):
         [**+n**\ *nx*\ [/*ny*] ]\ [**+o**\ *dx*\ [/*dy*]].
         Sets reference point on the map for the image.
     box : bool or str
-        [**+c**\ *clearances*][**+g**\ *fill*][**+i**\ [[*gap*/]\ *pen*]]
+        [**+c**\ *clearances*][**+g**\ *fill*][**+i**\ [[*gap*/]\ *pen*]]\
         [**+p**\ [*pen*]][**+r**\ [*radius*]][**+s**\ [[*dx*/*dy*/][*shade*]]].
         Without further options, draws a rectangular border around the image
         using :gmt-term:`MAP_FRAME_PEN`.
