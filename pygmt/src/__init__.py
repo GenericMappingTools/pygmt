ace68803c5d5b5815d3b434890a04679b7e614d8
Will Schlitzer <schlitzer90@gmail.com>
2021-02-19 18:26:08 +0000

    Fix import statements

--- pygmt/src/__init__.py
+++ pygmt/src/__init__.py
@@ -24,6 +24,7 @@ from pygmt.src.makecpt import makecpt
 from pygmt.src.meca import meca
 from pygmt.src.plot import plot
 from pygmt.src.plot3d import plot3d
+from pygmt.src.solar import solar
 from pygmt.src.subplot import set_panel, subplot
 from pygmt.src.surface import surface
 from pygmt.src.text import text_ as text  # "text" is an argument within "text_"
