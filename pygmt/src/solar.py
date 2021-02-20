d8090fe9eda1bbc185c9963f2517b11294640087
Will Schlitzer <schlitzer90@gmail.com>
2021-02-20 15:07:13 +0000

    Remove old parameters, delete invalid tests

--- pygmt/src/solar.py
+++ pygmt/src/solar.py
@@ -22,10 +22,6 @@ from pygmt.helpers import (
     J="projection",
     G="fill",
     B="frame",
-    Td="day_night",
-    Tc="civil_twilight",
-    Tn="nautical_twilight",
-    Ta="astronomical_twilight",
     W="pen",
     U="timestamp",
     V="verbose",
@@ -51,37 +47,6 @@ def solar(self, terminator="d", terminator_datetime="", **kwargs):
     {B}
     fill : str
         Color or pattern for filling of terminators.
-    day_night : bool or str
-        [**+d**\ *date*].
-        Plots the day/night terminator. If the argument is ``True``, the
-        current day-night terminator is plotted. Optionally, **+d** can be
-        added, along with the date and time, to see the day/night
-        terminator at a specific time. The date is added in ISO format,
-        e.g. 12:15 UTC on April 25, 2000 is ``2000-04-25T12:15:00``.
-    civil_twilight : bool or str
-        [**+d**\ *date*].
-        Plots the civil twilight terminator. If the argument is
-        ``True``, the current civil twilight terminator is plotted.
-        Optionally, **+d** can be added, along with the date and time, to
-        see the day/night terminator at a specific time. The date is added
-        in ISO format,
-        e.g. 12:15 UTC on April 25, 2000 is ``2000-04-25T12:15:00``.
-    nautical_twilight : bool or str
-        [**+d**\ *date*].
-        Plots the nautical twilight terminator. If the argument is
-        ``True``, the current nautical twilight terminator is plotted.
-        Optionally, **+d** can be added, along with the date and time, to
-        see the day/night terminator at a specific time. The date is added
-        in ISO format,
-        e.g. 12:15 UTC on April 25, 2000 is ``2000-04-25T12:15:00``.
-    astronomical_twilight : bool or str
-        [**+d**\ *date*].
-        Plots the astronomical twilight terminator. If the argument is
-        ``True``, the current astronomical twilight terminator is
-        plotted. Optionally, **+d** can be added, along with the date and
-        time, to see the day/night terminator at a specific time. The date
-        is added in ISO format,
-        e.g. 12:15 UTC on April 25, 2000 is ``2000-04-25T12:15:00``.
     pen : str
         Set pen attributes for lines. The default pen
         is ``default,black,solid``.
@@ -92,12 +57,13 @@ def solar(self, terminator="d", terminator_datetime="", **kwargs):
     """
 
     kwargs = self._preprocess(**kwargs)
-    if not args_in_kwargs(args=["Td", "Tn", "Tc", "Ta"], kwargs=kwargs):
+    if "T" not in kwargs:
         term_string = get_terminator_type(terminator=terminator)
         if not terminator_datetime:
             terminator_datetime = datetime.datetime.now()
         datetime_string = get_datetime_string(terminator_datetime=terminator_datetime)
         kwargs["T"] = term_string + "+d" + datetime_string
+    print(kwargs["T"])
     with Session() as lib:
         lib.call_module("solar", build_arg_string(kwargs))
 
@@ -117,12 +83,12 @@ def get_terminator_type(terminator):
 
 def get_datetime_string(terminator_datetime):
     if type(terminator_datetime) == datetime.datetime:
-        return terminator_datetime.strftime("%Y-%m-%dT%H-%M-%S")
+        return terminator_datetime.strftime("%Y-%m-%dT%H:%M:%S")
     elif type(terminator_datetime) == str:
         try:
             terminator_timestamp = pd.to_datetime(terminator_datetime)
-            return terminator_timestamp.strftime("%Y-%m-%dT%H-%M-%S")
-        except ParserError:
+            return terminator_timestamp.strftime("%Y-%m-%dT%H:%M:%S")
+        except ValueError:
             raise GMTInvalidInput("""Unrecognized datetime string format.""")
     else:
         raise GMTInvalidInput(
--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -1,89 +1,21 @@
 """
 Tests for solar.
 """
+import datetime
+
 from pygmt import Figure
 from pygmt.helpers.testing import check_figures_equal
 
 
 @check_figures_equal()
-def test_solar_day_night():
+def test_solar_default_terminator_string_datetime():
     """
     Test passing the solar argument with the day_night argument and confirm the
     working aliases for the solar function.
     """
     fig_ref, fig_test = Figure(), Figure()
     # Use single-character arguments for the reference image
-    fig_ref.solar(
-        R="d",
-        J="W0/15c",
-        B="a",
-        Td="+d1990-02-17T04:25:00",
-        G="navyblue@75",
-        W="1p,red,-.-",
-        X=3,
-        Y=5,
-        p="135/45",
-    )
-    fig_test.solar(
-        region="d",
-        projection="W0/15c",
-        frame="a",
-        day_night="+d1990-02-17T04:25:00",
-        fill="navyblue@75",
-        pen="1p,red,-.-",
-        xshift=3,
-        yshift=5,
-        perspective=[135, 45],
-    )
+    fig_ref.solar(R="d", J="W0/15c", B="a", T="d+d1990-02-17T04:25:00")
+    fig_test.solar(R="d", J="W0/15c", B="a", terminator_datetime="1990-02-17 04:25:00")
     return fig_ref, fig_test
 
-
-@check_figures_equal()
-def test_solar_civil_twilight():
-    """
-    Test passing the solar argument with the civil_twilight argument.
-    """
-    fig_ref, fig_test = Figure(), Figure()
-    # Use single-character arguments for the reference image
-    fig_ref.solar(R="d", J="W0/15c", B="a", Tc="+d1990-02-17T04:25:00")
-    fig_test.solar(
-        region="d",
-        projection="W0/15c",
-        frame="a",
-        civil_twilight="+d1990-02-17T04:25:00",
-    )
-    return fig_ref, fig_test
-
-
-@check_figures_equal()
-def test_solar_astronomical_twilight():
-    """
-    Test passing the solar argument with the astronomical_twilight argument.
-    """
-    fig_ref, fig_test = Figure(), Figure()
-    # Use single-character arguments for the reference image
-    fig_ref.solar(R="d", J="W0/15c", B="a", Ta="+d1990-02-17T04:25:00")
-    fig_test.solar(
-        region="d",
-        projection="W0/15c",
-        frame="a",
-        astronomical_twilight="+d1990-02-17T04:25:00",
-    )
-    return fig_ref, fig_test
-
-
-@check_figures_equal()
-def test_solar_nautical_twilight():
-    """
-    Test passing the solar argument with the nautical_twilight argument.
-    """
-    fig_ref, fig_test = Figure(), Figure()
-    # Use single-character arguments for the reference image
-    fig_ref.solar(R="d", J="W0/15c", B="a", Tn="+d1990-02-17T04:25:00")
-    fig_test.solar(
-        region="d",
-        projection="W0/15c",
-        frame="a",
-        nautical_twilight="+d1990-02-17T04:25:00",
-    )
-    return fig_ref, fig_test
