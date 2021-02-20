338cb4f170a15cc05d84369b710718dee83fca59
Will Schlitzer <schlitzer90@gmail.com>
2021-02-20 08:39:04 +0000

    Allow solar to accept terminator types and datetime objects

--- pygmt/src/solar.py
+++ pygmt/src/solar.py
@@ -2,8 +2,18 @@
 solar - Plot day-night terminators.
 """
 
+import datetime
+
+import pandas as pd
 from pygmt.clib import Session
-from pygmt.helpers import build_arg_string, fmt_docstring, kwargs_to_strings, use_alias
+from pygmt.exceptions import GMTInvalidInput
+from pygmt.helpers import (
+    args_in_kwargs,
+    build_arg_string,
+    fmt_docstring,
+    kwargs_to_strings,
+    use_alias,
+)
 
 
 @fmt_docstring
@@ -24,7 +34,7 @@ from pygmt.helpers import build_arg_string, fmt_docstring, kwargs_to_strings, us
     p="perspective",
 )
 @kwargs_to_strings(R="sequence", p="sequence")
-def solar(self, **kwargs):
+def solar(self, terminator="d", terminator_datetime="", **kwargs):
     r"""
     Plot day-light terminators.
 
@@ -80,13 +90,42 @@ def solar(self, **kwargs):
     {U}
     {V}
     """
-    # if statement to determine if type is datetime object or string
-    # if string:
-    # try to parse date to datetime object
-    # if not recognizable
-    # raise error
-    # function to pass datetime object to return datetime string to pass to
-    # GMT API
+
     kwargs = self._preprocess(**kwargs)
+    if not args_in_kwargs(args=["Td", "Tn", "Tc", "Ta"], kwargs=kwargs):
+        term_string = get_terminator_type(terminator=terminator)
+        if not terminator_datetime:
+            terminator_datetime = datetime.datetime.now()
+        datetime_string = get_datetime_string(terminator_datetime=terminator_datetime)
+        kwargs["T"] = term_string + datetime_string
     with Session() as lib:
         lib.call_module("solar", build_arg_string(kwargs))
+
+
+def get_terminator_type(terminator):
+    if terminator in ["day_night", "d"]:
+        return "d"
+    elif terminator in ["nautical", "n"]:
+        return "n"
+    elif terminator in ["civil", "c"]:
+        return "c"
+    elif terminator in ["astro", "a", "astronomical"]:
+        return "a"
+    else:
+        raise GMTInvalidInput("""Unrecognized solar terminator type.""")
+
+
+def get_datetime_string(terminator_datetime):
+    if type(terminator_datetime) == datetime.datetime:
+        return terminator_datetime.strftime("%Y-%m-%dT%H-%M-%S")
+    elif type(terminator_datetime) == str:
+        try:
+            terminator_timestamp = pd.to_datetime(terminator_datetime)
+            return terminator_timestamp.strftime("%Y-%m-%dT%H-%M-%S")
+        except:
+            raise GMTInvalidInput("""Unrecognized datetime format.""")
+    else:
+        raise GMTInvalidInput(
+            """Accepted types for terminator_datetime are 
+        string and datetime object."""
+        )
