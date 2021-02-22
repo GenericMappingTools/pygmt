328b99858240b7fb422f65909ac5e7d72c850c51
Meghan Jones <meghanj@hawaii.edu>
2021-02-22 12:40:00 -0500

    Fix typo in tutorials (#953)

--- examples/tutorials/plot.py
+++ examples/tutorials/plot.py
@@ -12,7 +12,7 @@ automatically downloaded and saved to a cache directory the first time you use t
 
     This tutorial assumes the use of a Python notebook, such as IPython or Jupyter Notebook.
     To see the figures while using a Python script instead, use
-    ``fig.show(method="external)`` to display the figure in the default PDF viewer.
+    ``fig.show(method="external")`` to display the figure in the default PDF viewer.
 
     To save the figure, use ``fig.savefig("figname.pdf")`` where ``"figname.pdf"``
     is the desired name and file extension for the saved figure.
