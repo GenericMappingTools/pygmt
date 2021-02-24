c39ef2ab5e3e90ce00108999aa90d03a2a95bdc0
Michael Grund <michael.grund@kit.edu>
2021-02-24 08:52:00 +0100

    adjusted annotations

--- examples/projections/nongeo/polar-misc.py
+++ examples/projections/nongeo/polar-misc.py
@@ -12,23 +12,19 @@ pygmt.config(FONT_TITLE="14p,Helvetica,black")
 # ============
 fig.basemap(region=[0, 360, 0, 1], projection="P5c", frame=["xa45f", "+gbisque"])
 
-fig.text(
-    x=[270, 270],
-    y=[-1.7, -1.5],
-    text=["projection = P5c", "region = [0,360,0,1]"],
-    no_clip=True,
-)
+fig.text(position="TC", text="projection='P5c'", offset="0/2.0c", no_clip=True)
+
+fig.text(position="TC", text="region=[0, 360, 0, 1]", offset="0/1.5c", no_clip=True)
+
 fig.shift_origin(xshift="8c")
 
 # ============
 fig.basemap(region=[0, 360, 0, 1], projection="P5c+a", frame=["xa45f", "+gbisque"])
 
-fig.text(
-    x=[0, 0],
-    y=[1.7, 1.5],
-    text=["projection = P5c+a", "region = [0,360,0,1]"],
-    no_clip=True,
-)
+fig.text(position="TC", text="projection='P5c+a'", offset="0/2.0c", no_clip=True)
+
+fig.text(position="TC", text="region=[0, 360, 0, 1]", offset="0/1.5c", no_clip=True)
+
 fig.shift_origin(xshift="8c")
 
 # ============
@@ -36,12 +32,10 @@ fig.basemap(
     region=[0, 90, 0, 1], projection="P5c+a", frame=["xa45f", "ya0.2", "WNe+gbisque"]
 )
 
-fig.text(
-    x=[25, 20],
-    y=[1.5, 1.33],
-    text=["projection = P5c+a", "region = [0,90,0,1]"],
-    no_clip=True,
-)
+fig.text(position="TC", text="projection='P5c+a'", offset="0/2.0c", no_clip=True)
+
+fig.text(position="TC", text="region=[0, 90, 0, 1]", offset="0/1.5c", no_clip=True)
+
 fig.shift_origin(xshift="-16c", yshift="-7c")
 
 # ============
@@ -51,12 +45,10 @@ fig.basemap(
     frame=["xa45f", "ya0.2", "WNe+gbisque"],
 )
 
-fig.text(
-    x=[45, 45],
-    y=[1.5, 1.35],
-    text=["projection = P5c+a+t45", "region = [0,90,0,1]"],
-    no_clip=True,
-)
+fig.text(position="TC", text="projection='P5c+a+t45'", offset="0/2.0c", no_clip=True)
+
+fig.text(position="TC", text="region=[0, 90, 0, 1]", offset="0/1.5c", no_clip=True)
+
 fig.shift_origin(xshift="8c", yshift="1.3c")
 
 # ============
@@ -66,12 +58,12 @@ fig.basemap(
     frame=["xa45f", "ya", "WNse+gbisque"],
 )
 
+fig.text(position="TC", text="projection='P5c+a+t45'", offset="0/2.0c", no_clip=True)
+
 fig.text(
-    x=[45, 45],
-    y=[10000, 9000],
-    text=["projection = P5c+a+t45", "region = [0,90,3480,6371]"],
-    no_clip=True,
+    position="TC", text="region=[0, 90, 3480, 6371]", offset="0/1.5c", no_clip=True
 )
+
 fig.shift_origin(xshift="8c")
 
 # ============
@@ -81,11 +73,10 @@ fig.basemap(
     frame=["xa45f", "ya", "WNse+gbisque"],
 )
 
+fig.text(position="TC", text="projection='P5c+a+t45+z'", offset="0/2.0c", no_clip=True)
+
 fig.text(
-    x=[45, 45],
-    y=[10000, 9000],
-    text=["projection = P5c+a+t45+z", "region = [0,90,3480,6371]"],
-    no_clip=True,
+    position="TC", text="region=[0, 90, 3480, 6371]", offset="0/1.5c", no_clip=True
 )
 
 fig.show()
