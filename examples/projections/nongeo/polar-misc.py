86ad5f7a6416bfc95c1ebfcca21f3375cf4525ca
Michael Grund <michael.grund@kit.edu>
2021-03-02 08:56:26 +0100

    updated docstrings

--- examples/projections/nongeo/polar-misc.py
+++ examples/projections/nongeo/polar-misc.py
@@ -2,11 +2,14 @@ r"""
 Miscellaneous ways to use polar projections
 ===========================================
 
-Using this projection allows to plot polar coordinate data (e.g. angle
+Polar projections allow to plot polar coordinate data (e.g. angle
 :math:`\theta` and radius *r*). Limits are set via the ``region`` parameter
-([*theta_min*, *theta_max*, *radius_min*, *radius_max*]).
+([*theta_min*, *theta_max*, *radius_min*, *radius_max*]). When using
+**P**\ *width* you have to give the *width* of the figure. The lower-case
+version **p** is similar to **P** but expects a *scale* instead of
+a width (**p**\ *scale*).
 
-The following options are available:
+The following customizing options are available:
 
 - **+a**: by default, :math:`\theta` refers to the angle that is equivalent to
   a counterclockwise rotation with respect to the east direction (standard
@@ -22,6 +25,7 @@ The following options are available:
   equivalent to rotating the entire coordinate axis counterclockwise.
 
 - **+f**: reverses the radial direction.
+
   - Append **e** to indicate that the r-axis is an elevation angle, and the
     range of the r-axis should be between 0 and 90.
   - Appending **p** sets the current earth radius (determined by
@@ -29,13 +33,11 @@ The following options are available:
     to the maximum value of the r axis when the r axis is reversed.
   - Append *radius* to set the maximum value of the r axis.
 
-- **+z**: indicates that the r axis is marked as depth instead of radius (i.e.
+- **+z**: indicates that the r axis is marked as depth instead of radius (e.g.
   *r = radius - z*).
+
    - Append **p** to set radius to the current earth radius.
    - Append *radius* to set the value of the radius.
-
-The lower-case version **p** is similar to **P** but expects a scale instead of
-a width.
 
 """
 
