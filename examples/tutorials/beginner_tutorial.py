e9f6d68de17e557978b746fc184e7f22a4f025a7
Will Schlitzer <schlitzer90@gmail.com>
2021-03-02 21:51:57 +0000

    Add beginner_tutorial.py

---
+++ examples/tutorials/beginner_tutorial.py
@@ -0,0 +1,18 @@
+"""
+Tutorial
+========
+
+This is a tutorial to teach a new user how to use PyGMT.
+"""
+
+########################################################################################
+# Tutorial setup
+# --------------
+#
+# 1. This tutorial assumes that PyGMT has been properly installed. To confirm that it
+# is working, ensure that the conda environment has been activated and
+# run ``import pygmt`` in a Python console.
+# 2. This tutorial assumes the use of a Python notebook, such as IPython or Jupyter
+# Notebook. To see the figures while using a Python script instead,
+# use ``fig.show(method="external")`` to display the figure in the default PDF viewer.
+
