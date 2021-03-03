3c749e9b16d1c512df3f8feba22158502fba0a84
Michael Grund <michael_grund@gmx.de>
2021-03-03 08:48:32 +0100

    Update examples/projections/nongeo/polar-misc.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/projections/nongeo/polar-misc.py
+++ examples/projections/nongeo/polar-misc.py
@@ -89,7 +89,6 @@ fig.basemap(
 )
 
 fig.text(position="TC", text="projection='P5c+a'", offset="0/2.0c", no_clip=True)
-
 fig.text(position="TC", text="region=[0, 90, 0, 1]", offset="0/1.5c", no_clip=True)
 
 fig.shift_origin(xshift="-16c", yshift="-7c")
