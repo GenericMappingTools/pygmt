e5c71ebec69364fce2a6d2e36c1fbb042e00e0f7
Michael Grund <michael.grund@kit.edu>
2021-03-07 12:32:29 +0100

    corrected typo

--- examples/gallery/plot/points-categorical.py
+++ examples/gallery/plot/points-categorical.py
@@ -30,7 +30,7 @@ fig.basemap(
     projection="X10c/10c",
     frame=['xafg+l"Bill length in mm"',
            'yafg+l"Bill depth in mm"',
-           'WSen+t"Penguin size at Palmer Station"],
+           'WSen+t"Penguin size at Palmer Station"'],
 )
 
 # Define colormap to use for three categories
