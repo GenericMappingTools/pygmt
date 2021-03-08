65f418b2ac0ed5024af37a06a81782cbcb154d2e
core-man <coreman.seism@gmail.com>
2021-03-08 14:46:27 +0800

    Re-format

--- examples/gallery/plot/double-y-axes.py
+++ examples/gallery/plot/double-y-axes.py
@@ -24,42 +24,21 @@ fig = pygmt.Figure()
 # Plot y1
 # The left (W) and bottom (S) axes are plotted with annotations and tick marks
 fig.basemap(
-    region=[0, 10, 0, 10],
-    projection="X5c/5c",
-    frame=["WS", "xaf+lx", "yaf+ly1"],
+    region=[0, 10, 0, 10], projection="X5c/5c", frame=["WS", "xaf+lx", "yaf+ly1"]
 )
 # Plot the line for y1
 fig.plot(x=x, y=y1, pen="1p,blue")
 # Plot points for y1
-fig.plot(
-    x=x,
-    y=y1,
-    style="c0.2c",
-    color="blue",
-    label="y1",
-)
+fig.plot(x=x, y=y1, style="c0.2c", color="blue", label="y1")
 
 # Plot y2
 # The right axis (E) is plotted with annotations and tick marks
 # The top axis (t) is plotting without annotations and tick marks
-fig.basemap(
-    region=[0, 10, 100, 200],
-    frame=["Et", "yaf+ly2"],
-)
+fig.basemap(region=[0, 10, 100, 200], frame=["Et", "yaf+ly2"])
 # Plot the line for y2
-fig.plot(
-    x=x,
-    y=y2,
-    pen="1p,red",
-)
+fig.plot(x=x, y=y2, pen="1p,red")
 # Plot points for y2
-fig.plot(
-    x=x,
-    y=y2,
-    style="s0.28c",
-    color="red",
-    label="y2",
-)
+fig.plot(x=x, y=y2, style="s0.28c", color="red", label="y2")
 
 fig.legend(position="JTL+jTL+o0.1c", box=True)
 
