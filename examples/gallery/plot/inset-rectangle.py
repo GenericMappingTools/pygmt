ecadf0422b6d85f1b4b35980660d1a083f71e389
core-man <coreman.seism@gmail.com>
2021-03-09 15:59:59 +0800

    Explain r+s

--- examples/gallery/plot/inset-rectangle.py
+++ examples/gallery/plot/inset-rectangle.py
@@ -28,16 +28,19 @@ fig.coast(land="lightbrown", water="azure1", shorelines="2p", area_thresh=1000)
 # 0.2 centimeters. Draws a rectangular box around the inset with a fill color
 # of "white" and a pen of "1p".
 with fig.inset(position="jBL+w3c/3.6c+o0.1c", box="+gwhite+p1p"):
-    # Plot the Japan main land in the inset using coast
-    # "M?" means Mercator projection with map width automatically determined
-    # based on the width in the position parameter of inset.
+    # Plot the Japan main land in the inset using coast. "M?" means Mercator
+    # projection with map width automatically determined based on the width in
+    # the position parameter of inset
     fig.coast(
         region=[129, 146, 30, 46],
         projection="M?",
         dcw="JP+glightbrown+p0.2p",
         area_thresh=10000,
     )
-    # Plot a rectangle in the inset map to show the area of the primary figure
+    # Plot a rectangle ("r") in the inset map to show the area of the primary figure.
+    # "+s" means that the first two columns are the longitude and latitude of
+    # the bottom left corner of the rectangle, and the last two columns the
+    # longitude and latitude of the uppper right corner.
     rectangle = [[region[0], region[2], region[1], region[3]]]
     fig.plot(data=rectangle, style="r+s", pen="1p,blue")
 
