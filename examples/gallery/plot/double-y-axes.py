39080f7f90b547c1654a1a4b2c313c372d096997
core-man <coreman.seism@gmail.com>
2021-03-09 15:35:10 +0800

    Use customed frames, annotations, marks and labels

--- examples/gallery/plot/double-y-axes.py
+++ examples/gallery/plot/double-y-axes.py
@@ -22,19 +22,32 @@ y2 = x ** 2 + 110
 fig = pygmt.Figure()
 
 # Plot y1
-# The left (W) and bottom (S) axes are plotted with annotations and tick marks
-fig.basemap(
-    region=[0, 10, 0, 10], projection="X15c/15c", frame=["WS", "xaf+lx", "yaf+ly1"]
-)
+# The bottom axis (S) is plotted with annotations and tick marks
+# The top axis (t) is plotted without annotations and tick marks
+fig.basemap(region=[0, 10, 0, 10], projection="X15c/15c", frame=["St", "xaf+lx"])
+# The left axis (W) is plotted with customed annotations, tick marks and labels
+with pygmt.config(
+    MAP_FRAME_PEN="1.5p,blue",
+    MAP_TICK_PEN="0.5p,blue",
+    FONT_ANNOT_PRIMARY="12p,Helvetica,blue",
+    FONT_LABEL="16p,Helvetica,blue",
+):
+    fig.basemap(frame=["W", "yaf+ly1"])
+
 # Plot the line for y1
 fig.plot(x=x, y=y1, pen="1p,blue")
 # Plot points for y1
 fig.plot(x=x, y=y1, style="c0.2c", color="blue", label="y1")
 
 # Plot y2
-# The right axis (E) is plotted with annotations and tick marks
-# The top axis (t) is plotted without annotations and tick marks
-fig.basemap(region=[0, 10, 100, 200], frame=["Et", "yaf+ly2"])
+# The right axis (E) is plotted with customed annotations, tick marks, and labels
+with pygmt.config(
+    MAP_FRAME_PEN="1.5p,red",
+    MAP_TICK_PEN="0.5p,red",
+    FONT_ANNOT_PRIMARY="12p,Helvetica,red",
+    FONT_LABEL="16p,Helvetica,red",
+):
+    fig.basemap(region=[0, 10, 100, 200], frame=["E", "yaf+ly2"])
 # Plot the line for y2
 fig.plot(x=x, y=y2, pen="1p,red")
 # Plot points for y2
