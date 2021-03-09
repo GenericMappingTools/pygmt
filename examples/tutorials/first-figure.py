f8afad9440e857c9289983079d6e2cef462a0252
Yao Jiayuan <coreman.seism@gmail.com>
2021-03-10 01:20:08 +0800

    Apply suggestions from code review
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- examples/tutorials/first-figure.py
+++ examples/tutorials/first-figure.py
@@ -36,9 +36,10 @@ fig = pygmt.Figure()
 
 ########################################################################################
 # Add elements to the figure using its methods. For example, let's use
-# :meth:`pygmt.Figure.basemap` to start a map for a region indicated by a given
-# longitude and latitude bound, set the projection to Mercator (**M**), the
-# map width to 8 cm, and frame type to be generated automatically:
+# :meth:`pygmt.Figure.basemap` to start a map. We'll use the ``region`` parameter
+# provide the longitude and latitude bounds, the ``projection`` parameter to set
+# the projection to Mercator (**M**) and the map width to 8cm, and the ``frame``
+# parameter to generate a frame with automatic tick and annotation spacing.
 
 fig.basemap(region=[-90, -70, 0, 20], projection="M8c", frame=True)
 
@@ -75,26 +76,29 @@ fig.show()
 # Note for experienced GMT users
 # ------------------------------
 #
-# You’ll probably have noticed several things that are different from classic
+# You have probably noticed several things that are different from classic
 # command-line GMT. Many of these changes reflect the new GMT modern execution
 # mode that is part of GMT 6.
 #
-# 1. The name of method is ``coast`` instead of ``pscoast``. As a general rule, all
-#    ``ps*`` modules had their ``ps`` prefix removed. The exceptions are:
-#    ``psxy`` which is now ``plot``, ``psxyz`` which is now ``plot3d``, and ``psscale``
-#    which is now ``colorbar``.
+# 1. As a general rule, the ``ps`` prefix has been removed from all ``ps*``
+#    modules (PyGMT methods). For example, the name of the GMT 5 module ``pscoast``
+#    is ``coast`` in GMT 6 and PyGMT. The exceptions are:  ``psxy`` which is now
+#    ``plot``, ``psxyz`` which is now ``plot3d``, and ``psscale`` which is now
+#    ``colorbar``.
 #
-# 2. More details can be found at :gmt-docs:`/cookbook/introduction.html#modern-and-classic-mode`
+# 2. More details can be found in the :gmt-docs:`GMT cookbook introduction to
+#    modern mode </cookbook/introduction.html#modern-and-classic-mode>`.
 #
 # A few are PyGMT exclusive (like the ``savefig`` method).
 #
-# 1. The parameters don't use the GMT 1-letter syntax (**R**, **J**, **B**, etc). We use longer
+# 1. The PyGMT parameters (called options or arguments in GMT) don't use the GMT
+#    1-letter syntax (**R**, **J**, **B**, etc). We use longer
 #    aliases for these parameters and have some Python exclusive names. The mapping
-#    between the GMT parameters and their Python counterparts should be straight
-#    forward.
-# 2. Parameters like ``region`` can take lists as well as strings like ``1/2/3/4``.
+#    between the GMT parameters and their PyGMT aliases should be straightforward.
+#    For some modules, these aliases are still being developed.
+# 2. Parameters like ``region`` can take :class:`lists <list>` as well as strings like ``1/2/3/4``.
 # 3. If a GMT parameter has no options (like ``-B`` instead of ``-Baf``), use a ``True``
 #    in Python. An empty string would also be acceptable. For repeated parameters, such
-#    as ``-B+Loleron -Bxaf -By+lm``, provide a list: ``frame=["+Loleron", "xaf", "y+lm"]``.
+#    as ``-B+Loleron -Bxaf -By+lm``, provide a :class:`list`: ``frame=["+Loleron", "xaf", "y+lm"]``.
 # 4. There is no output redirecting to a PostScript file. The figure is generated in the
 #    background and will only be shown or saved when you ask for it.
