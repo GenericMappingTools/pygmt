8dec64395b7ea611c80f2e9eef4819ba27b746c1
Michael Grund <michael.grund@kit.edu>
2021-03-10 19:02:23 +0100

    some formatting

--- examples/gallery/line/linefronts.py
+++ examples/gallery/line/linefronts.py
@@ -16,16 +16,16 @@ hence the *gap* is adjusted to fit]. If *size* is missing it is set to 30%
 of the *gap*, except when *gap* is negative and *size* is thus required.
 Append **+l** or **+r** to plot symbols on the left or right side of the
 front [Default is centered]. Append **+**\ *type* to specify which symbol
-to plot: **b**\ ox, **c**\ ircle, **f**\ ault (default), **s**\ lip, or **t**\
-riangle. Slip means left-lateral or right-lateral strike-slip arrows
+to plot: **b**\ ox, **c**\ ircle, **f**\ ault (default), **s**\ lip, or
+**t**\ riangle. Slip means left-lateral or right-lateral strike-slip arrows
 (centered is not an option). The **+s** modifier optionally accepts the
 angle used to draw the vector (default is 20). Alternatively, use **+S**
 which draws arcuate arrow heads. Append **+o**\ *offset* to offset the first
 symbol from the beginning of the front by that amount (default is 0). The
 chosen symbol is drawn with the same pen as set for the line (i.e., via
-**pen**). To use an alternate pen, append **+p**\ *pen*. To skip the
-* outline, just use **+p** with no argument. To make the main front line
-* invisible, add **+i**.
+the ``pen`` parameter). To use an alternate pen, append **+p**\ *pen*.
+To skip the outline, just use **+p** with no argument. To make the main
+front line invisible, add **+i**.
 
 """
 
