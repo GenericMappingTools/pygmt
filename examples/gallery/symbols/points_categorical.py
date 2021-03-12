b06b51c820e8a70d0c7bea2ef9320e9e5dcb51b1
Michael Grund <michael_grund@gmx.de>
2021-03-12 08:54:44 +0100

    Update examples/gallery/symbols/points_categorical.py
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

--- examples/gallery/symbols/points_categorical.py
+++ examples/gallery/symbols/points_categorical.py
@@ -53,7 +53,7 @@ fig.plot(
     x=df.bill_length_mm,
     y=df.bill_depth_mm,
     # Vary each symbol size according to another feature (body mass, scaled by 7.5*10e-5)
-    sizes=df.body_mass_g * 7.5 * 10 ** -5,
+    sizes=df.body_mass_g * 7.5e-5,
     # Points colored by categorical number code
     color=df.species.cat.codes.astype(int),
     # Use colormap created by makecpt
