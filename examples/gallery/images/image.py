a2c8919ff5728589a1930bd751d6614661d18675
Yao Jiayuan <coreman.seism@gmail.com>
2021-03-14 03:56:02 +0800

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/gallery/images/image.py
+++ examples/gallery/images/image.py
@@ -3,7 +3,7 @@ Images on figures
 -----------------
 The :meth:`pygmt.Figure.image` method can be used to read and place an image
 file in many formats (e.g., png, jpg, eps, pdf) on a figure. We must specify
-the file as *str* via the ``imagefile`` parameter or simply use the filename as
+the filename via the ``imagefile`` parameter or simply use the filename as
 the first argument. You can also use a full URL pointing to your desired image.
 The ``position`` parameter allows us to set a reference point on the map for
 the image.
@@ -13,7 +13,7 @@ import os
 import pygmt
 
 fig = pygmt.Figure()
-fig.basemap(region=[0, 2, 0, 2], projection="X6c", frame=True)
+fig.basemap(region=[0, 2, 0, 2], projection="X10c", frame=True)
 
 # place and center the GMT logo from the GMT website to the position 1/1
 # on a basemap, scaled up to be 3 cm wide and draw a rectangular border
