e367ea82d36cc5c4194160d0aa0fd7d4a85283b4
Will Schlitzer <schlitzer90@gmail.com>
2021-03-18 20:17:59 +0000

    Update testing in test_solar.py

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -28,17 +28,15 @@ def test_solar_set_terminator_datetime_string():
     return fig_ref, fig_test
 
 
-@check_figures_equal()
+@pytest.mark.mpl_image_compare
 def test_solar_default_terminator():
     """
     Test passing the solar argument with a time string and no terminator type
     to confirm the default terminator type.
     """
-    fig_ref, fig_test = Figure(), Figure()
-    # Use single-character arguments for the reference image
-    fig_ref.solar(R="d", J="W0/15c", B="a", T="d+d1990-02-17T04:25:00")
-    fig_test.solar(R="d", J="W0/15c", B="a", terminator_datetime="1990-02-17 04:25:00")
-    return fig_ref, fig_test
+    fig = Figure()
+    fig.solar(R="d", J="W0/15c", B="a", terminator_datetime="1990-02-17 04:25:00")
+    return fig
 
 
 @check_figures_equal()
