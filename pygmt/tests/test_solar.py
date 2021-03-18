b545fb99dc19a700f69638cd1612731f36654a7c
Will Schlitzer <schlitzer90@gmail.com>
2021-03-18 20:48:58 +0000

    Update test_solar.py tests

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -9,23 +9,21 @@ from pygmt.exceptions import GMTInvalidInput
 from pygmt.helpers.testing import check_figures_equal
 
 
-@check_figures_equal()
+@pytest.mark.mpl_image_compare
 def test_solar_set_terminator_datetime_string():
     """
     Test passing the solar argument with the civil terminator and a datetime
     string.
     """
-    fig_ref, fig_test = Figure(), Figure()
-    # Use single-character arguments for the reference image
-    fig_ref.solar(R="d", J="W0/15c", B="a", T="c+d1990-02-17T04:25:00")
-    fig_test.solar(
-        R="d",
-        J="W0/15c",
-        B="a",
+    fig = Figure()
+    fig.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
         terminator="civil",
         terminator_datetime="1990-02-17 04:25:00",
     )
-    return fig_ref, fig_test
+    return fig
 
 
 @pytest.mark.mpl_image_compare
@@ -44,26 +42,24 @@ def test_solar_default_terminator():
     return fig
 
 
-@check_figures_equal()
+@pytest.mark.mpl_image_compare
 def test_solar_set_terminator_datetime_object():
     """
     Test passing the solar argument with the day_night terminator and a
     datetime string.
     """
-    fig_ref, fig_test = Figure(), Figure()
-    # Use single-character arguments for the reference image
-    fig_ref.solar(R="d", J="W0/15c", B="a", T="d+d1990-02-17T04:25:00")
+    fig = Figure()
     terminator_datetime = datetime.datetime(
         year=1990, month=2, day=17, hour=4, minute=25, second=0
     )
-    fig_test.solar(
+    fig.solar(
         R="d",
         J="W0/15c",
         B="a",
         termiantor="day_night",
         terminator_datetime=terminator_datetime,
     )
-    return fig_ref, fig_test
+    return fig
 
 
 def test_invalid_terminator_type():
