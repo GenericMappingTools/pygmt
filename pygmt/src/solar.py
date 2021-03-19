631b24d2a3fd7bfd94e7372730fb78833aaeb111
Will Schlitzer <schlitzer90@gmail.com>
2021-03-19 21:50:50 +0000

    update default time in docstring

--- pygmt/src/solar.py
+++ pygmt/src/solar.py
@@ -46,7 +46,7 @@ def solar(self, terminator="d", terminator_datetime=None, **kwargs):
     terminator_datetime : str or datetime object
         Set the UTC date and time of the displayed terminator. It can be
         passed as a string or a datetime object.
-        [Default is the current date and time]
+        [Default is the current UTC date and time]
     {R}
     {J}
     {B}
