f34e77842ab870e3124cf8b15df8a1080ab946f5
Meghan Jones <meghanj@alum.mit.edu>
2021-03-22 17:40:30 -0400

    Move internal function to top of module

--- pygmt/src/blockm.py
+++ pygmt/src/blockm.py
@@ -15,6 +15,61 @@ from pygmt.helpers import (
 )
 
 
+def _blockm(block_method, table, outfile, **kwargs):
+    r"""
+    Block average (x,y,z) data tables by mean or median estimation.
+
+    Reads arbitrarily located (x,y,z) triples [or optionally weighted
+    quadruples (x,y,z,w)] from a table and writes to the output a mean or
+    median (depending on ``block_method``) position and value for every
+    non-empty block in a grid region defined by the ``region`` and ``spacing``
+    parameters.
+
+    Parameters
+    ----------
+    block_method : str
+        Name of the GMT module to call. Must be "blockmean" or "blockmedian".
+
+    Returns
+    -------
+    output : pandas.DataFrame or None
+        Return type depends on whether the ``outfile`` parameter is set:
+
+        - :class:`pandas.DataFrame` table with (x, y, z) columns if ``outfile``
+          is not set
+        - None if ``outfile`` is set (filtered output will be stored in file
+          set by ``outfile``)
+    """
+
+    kind = data_kind(table)
+    with GMTTempFile(suffix=".csv") as tmpfile:
+        with Session() as lib:
+            if kind == "matrix":
+                if not hasattr(table, "values"):
+                    raise GMTInvalidInput(f"Unrecognized data type: {type(table)}")
+                file_context = lib.virtualfile_from_matrix(table.values)
+            elif kind == "file":
+                if outfile is None:
+                    raise GMTInvalidInput("Please pass in a str to 'outfile'")
+                file_context = dummy_context(table)
+            else:
+                raise GMTInvalidInput(f"Unrecognized data type: {type(table)}")
+
+            with file_context as infile:
+                if outfile is None:
+                    outfile = tmpfile.name
+                arg_str = " ".join([infile, build_arg_string(kwargs), "->" + outfile])
+                lib.call_module(module=block_method, args=arg_str)
+
+        # Read temporary csv output to a pandas table
+        if outfile == tmpfile.name:  # if user did not set outfile, return pd.DataFrame
+            result = pd.read_csv(tmpfile.name, sep="\t", names=table.columns)
+        elif outfile != tmpfile.name:  # return None if outfile set, output in outfile
+            result = None
+
+    return result
+
+
 @fmt_docstring
 @use_alias(
     I="spacing",
@@ -135,58 +190,3 @@ def blockmedian(table, outfile=None, **kwargs):
           set by ``outfile``)
     """
     return _blockm(block_method="blockmedian", table=table, outfile=outfile, **kwargs)
-
-
-def _blockm(block_method, table, outfile, **kwargs):
-    r"""
-    Block average (x,y,z) data tables by mean or median estimation.
-
-    Reads arbitrarily located (x,y,z) triples [or optionally weighted
-    quadruples (x,y,z,w)] from a table and writes to the output a mean or
-    median (depending on ``block_method``) position and value for every
-    non-empty block in a grid region defined by the ``region`` and ``spacing``
-    parameters.
-
-    Parameters
-    ----------
-    block_method : str
-        Name of the GMT module to call. Must be "blockmean" or "blockmedian".
-
-    Returns
-    -------
-    output : pandas.DataFrame or None
-        Return type depends on whether the ``outfile`` parameter is set:
-
-        - :class:`pandas.DataFrame` table with (x, y, z) columns if ``outfile``
-          is not set
-        - None if ``outfile`` is set (filtered output will be stored in file
-          set by ``outfile``)
-    """
-
-    kind = data_kind(table)
-    with GMTTempFile(suffix=".csv") as tmpfile:
-        with Session() as lib:
-            if kind == "matrix":
-                if not hasattr(table, "values"):
-                    raise GMTInvalidInput(f"Unrecognized data type: {type(table)}")
-                file_context = lib.virtualfile_from_matrix(table.values)
-            elif kind == "file":
-                if outfile is None:
-                    raise GMTInvalidInput("Please pass in a str to 'outfile'")
-                file_context = dummy_context(table)
-            else:
-                raise GMTInvalidInput(f"Unrecognized data type: {type(table)}")
-
-            with file_context as infile:
-                if outfile is None:
-                    outfile = tmpfile.name
-                arg_str = " ".join([infile, build_arg_string(kwargs), "->" + outfile])
-                lib.call_module(module=block_method, args=arg_str)
-
-        # Read temporary csv output to a pandas table
-        if outfile == tmpfile.name:  # if user did not set outfile, return pd.DataFrame
-            result = pd.read_csv(tmpfile.name, sep="\t", names=table.columns)
-        elif outfile != tmpfile.name:  # return None if outfile set, output in outfile
-            result = None
-
-    return result
