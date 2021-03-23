886c15faa9cb9c8d7e068e6fb1e7aef3a719c4c0
Will Schlitzer <schlitzer90@gmail.com>
2021-03-23 19:20:19 +0000

    update tests

--- pygmt/tests/test_solar.py
+++ pygmt/tests/test_solar.py
@@ -9,23 +9,6 @@ from pygmt.exceptions import GMTInvalidInput
 
 
 @pytest.mark.mpl_image_compare
-def test_solar_set_terminator_datetime_string():
-    """
-    Test passing the solar argument with the civil terminator and a datetime
-    string.
-    """
-    fig = Figure()
-    fig.solar(
-        region="d",
-        projection="W0/15c",
-        frame="a",
-        terminator="civil",
-        terminator_datetime="1990-02-17 04:25:00",
-    )
-    return fig
-
-
-@pytest.mark.mpl_image_compare
 def test_solar_default_terminator():
     """
     Test passing the solar argument with a time string and no terminator type
@@ -40,17 +23,61 @@ def test_solar_default_terminator():
     )
     return fig
 
-
 @pytest.mark.mpl_image_compare
-def test_solar_set_terminator_datetime_object():
+def test_solar_terminators():
+    """
+    Test passing the solar argument with a time string and no terminator type
+    to confirm the default terminator type.
+    """
+    fig = Figure()
+    fig.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
+        terminator="d",
+        pen="1/blue",
+        terminator_datetime="1990-02-17 04:25:00",
+    )
+    fig.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
+        terminator="a",
+        pen="1/red",
+        terminator_datetime="1990-02-17 04:25:00",
+    )
+    fig.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
+        terminator="c",
+        pen="1/green",
+        terminator_datetime="1990-02-17 04:25:00",
+    )
+    fig.solar(
+        region="d",
+        projection="W0/15c",
+        frame="a",
+        terminator="n",
+        pen="1/yellow",
+        terminator_datetime="1990-02-17 04:25:00",
+    )
+    return fig
+
+@pytest.mark.mpl_image_compare(filename="test_solar_set_terminator_datetime.png")
+@pytest.mark.parametrize(
+    "terminator_datetime",
+    [
+        "1990-02-17 04:25:00",
+        datetime.datetime(year=1990, month=2, day=17, hour=4, minute=25, second=0),
+    ],
+)
+def test_solar_set_terminator_datetime(terminator_datetime):
     """
     Test passing the solar argument with the day_night terminator and a
     datetime string.
     """
     fig = Figure()
-    terminator_datetime = datetime.datetime(
-        year=1990, month=2, day=17, hour=4, minute=25, second=0
-    )
     fig.solar(
         region="d",
         projection="W0/15c",
