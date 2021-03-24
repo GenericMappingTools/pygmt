eaef86ecb4384d29d866d067173be9837df7afbc
Michael Grund <michael.grund@kit.edu>
2021-03-24 08:07:44 +0100

    solve merge conflicts

--- pygmt/datasets/__init__.py
+++ pygmt/datasets/__init__.py
@@ -3,7 +3,7 @@
 # Load sample data included with GMT (downloaded from the GMT cache server).
 
 from pygmt.datasets.earth_relief import load_earth_relief
-from pygmt.datasets.tutorial import (
+from pygmt.datasets.samples import (
     load_fractures_compilation,
     load_japan_quakes,
     load_ocean_ridge_points,
