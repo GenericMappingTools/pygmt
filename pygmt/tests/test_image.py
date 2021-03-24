2487d82abacb46802d1ec9328cf883c2e7ab5fb7
Dongdong Tian <seisman.info@gmail.com>
2021-03-24 01:45:45 -0400

    Fix

--- pygmt/tests/test_image.py
+++ pygmt/tests/test_image.py
@@ -15,5 +15,5 @@ def test_image():
     Place images on map.
     """
     fig = Figure()
-    fig.image(TEST_IMG, position="x0/0+w1i", box="+pthin,blue", verbose="d")
+    fig.image(TEST_IMG, position="x0/0+w2c", box="+pthin,blue", verbose="d")
     return fig
