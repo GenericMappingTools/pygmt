f6ffaf8424eddff445fc369105c8260822d77856
shahid-0 <shahid.khannn66@gmail.com>
2021-03-27 15:18:33 +0500

    non-ascii to ascii

---
+++ ascii.py
@@ -0,0 +1,9 @@
+examples/gallery/lines/linestyles.py:Giving "dotted" or "." yields a dotted line, whereas a dashed pen is requested
+examples/gallery/lines/linestyles.py:with “dashed” or "-". Also combinations of dots and dashes, like ".-" for a
+examples/projections/misc/misc_robinson.py:the world map "look" right 22. The scale is true along latitudes 38. The projection was
+examples/projections/misc/misc_sinusoidal.py:"Equal-area Mercator" projection. The central meridian is a straight line; all other
+examples/projections/misc/misc_winkel_tripel.py:German word "tripel" refers to this junction of where each of these elements are least
+examples/projections/misc/misc_winkel_tripel.py:and Son started to use it in its world-renowned "The Times Atlas of the World" in the
+pygmt/src/coast.py:        Select filling or clipping of "dry" areas.
+pygmt/src/coast.py:        Select filling or clipping of "wet" areas.
+pygmt/src/grdtrack.py:    ``interpolation``; Default uses "natural" conditions (second partial)
