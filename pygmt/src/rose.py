5120edb10254003c160b8a2e0008893720f36455
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2021-03-31 18:48:11 +0200

    Update pygmt/src/rose.py
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- pygmt/src/rose.py
+++ pygmt/src/rose.py
@@ -183,7 +183,7 @@ def rose(self, length=None, azimuth=None, data=None, **kwargs):
         https://doi.org/10.18637/jss.v031.i10.
 
     inquire : bool
-        Inquire. Computes statistics needed to specify a useful
+        Computes statistics needed to specify a useful
         ``frame``. No plot is generated. The following statistics
         are written to stdout: *n*, *mean az*, *mean r*, *mean
         resultant length*, *max bin sum*, *scaled mean*, and
