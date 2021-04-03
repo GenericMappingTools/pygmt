774237cbb6691e8b33a9f03715ec7e2068ceacb1
Will Schlitzer <schlitzer90@gmail.com>
2021-04-03 08:57:30 +0100

    remove unused import

--- pygmt/tests/test_makecpt.py
+++ pygmt/tests/test_makecpt.py
@@ -9,7 +9,6 @@ from pygmt import Figure, makecpt
 from pygmt.datasets import load_earth_relief
 from pygmt.exceptions import GMTInvalidInput
 from pygmt.helpers import GMTTempFile
-from pygmt.helpers.testing import check_figures_equal
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 POINTS_DATA = os.path.join(TEST_DATA_DIR, "points.txt")
