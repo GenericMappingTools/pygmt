8a04578da966d09c05e74f461208fbb654230ebc
Will Schlitzer <schlitzer90@gmail.com>
2021-04-03 07:37:45 +0100

    change test_inset_context_manager() to use mpl_image_compare

--- pygmt/tests/test_inset.py
+++ pygmt/tests/test_inset.py
@@ -18,22 +18,15 @@ def test_inset_aliases():
     return fig
 
 
-@check_figures_equal()
+@pytest.mark.mpl_image_compare
 def test_inset_context_manager():
     """
     Test that the inset context manager works and, once closed, plotting
     elements are added to the larger figure.
     """
-    fig_ref, fig_test = Figure(), Figure()
-
-    fig_ref.basemap(region=[-74, -69.5, 41, 43], projection="M9c", frame=True)
-    fig_ref.basemap(rose="jTR+w3c")  # Pass rose argument with basemap before the inset
-    with fig_ref.inset(position="jBL+w3c+o0.2c", margin=0, box="+pblack"):
-        fig_ref.basemap(region=[-80, -65, 35, 50], projection="M3c", frame="afg")
-
-    fig_test.basemap(region=[-74, -69.5, 41, 43], projection="M9c", frame=True)
-    with fig_test.inset(position="jBL+w3c+o0.2c", margin=0, box="+pblack"):
-        fig_test.basemap(region=[-80, -65, 35, 50], projection="M3c", frame="afg")
-    fig_test.basemap(rose="jTR+w3c")  # Pass rose argument with basemap after the inset
-
-    return fig_ref, fig_test
+    fig = Figure()
+    fig.basemap(region=[-74, -69.5, 41, 43], projection="M9c", frame=True)
+    with fig.inset(position="jBL+w3c+o0.2c", margin=0, box="+pblack"):
+        fig.basemap(region=[-80, -65, 35, 50], projection="M3c", frame="afg")
+    fig.basemap(rose="jTR+w3c")  # Pass rose argument with basemap after the inset
+    return fig
