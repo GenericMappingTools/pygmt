d105c727c5d75f628c05cbed01a9062dd73d90c5
Dongdong Tian <seisman.info@gmail.com>
2021-04-05 00:47:11 -0400

    Set __commit__ to an empty string for non-dev releases (#1174)
    * Set __commit__ to an empty string for non-dev releases
    * Link to tags for non-dev releases
    
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

--- doc/conf.py
+++ doc/conf.py
@@ -150,7 +150,14 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {}
 repository = "GenericMappingTools/pygmt"
 repository_url = "https://github.com/GenericMappingTools/pygmt"
-commit_link = f'<a href="{repository_url}/commit/{ __commit__ }">{ __commit__[:8] }</a>'
+if __commit__:
+    commit_link = (
+        f'<a href="{repository_url}/commit/{ __commit__ }">{ __commit__[:8] }</a>'
+    )
+else:
+    commit_link = (
+        f'<a href="{repository_url}/releases/tag/{ __version__ }">{ __version__ }</a>'
+    )
 html_context = {
     "menu_links": [
         (
