fb2b4ce0c81b82a856b4f727d2d627bf272d60ef
Will Schlitzer <schlitzer90@gmail.com>
2021-04-06 20:12:41 +0100

    add histogram aliases to docstring

--- pygmt/src/histogram.py
+++ pygmt/src/histogram.py
@@ -38,11 +38,20 @@ from pygmt.helpers import (
 @kwargs_to_strings(R="sequence", T="sequence")
 def histogram(self, table, **kwargs):
     r"""
-    Plots a histogram, and can read data from a file or list, array, or dataframe.
+    Plots a histogram, and can read data from a file or
+    list, array, or dataframe.
 
     Full option list at :gmt-docs:`histogram.html`
 
     {aliases}
+
+    Parameters
+    ----------
+    table : str, list, or 1d array
+        A data file name, list, or 1d numpy array. This is a required argument.
+    {J}
+    {R}
+    {B}
     """
     with GMTTempFile() as outfile:
         with Session() as lib:
