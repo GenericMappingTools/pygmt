d5009bf157fed7dd661439e09b17c1c8e4f9190d
noorbuchi <buchin@allegheny.edu>
2021-04-07 14:15:50 -0400

    Add placeholder for tutorial

---
+++ examples/tutorials/datetime.py
@@ -0,0 +1,16 @@
+"""
+Plotting Datetime Points
+================
+
+THIS IS WORK IN PROGRESS
+
+.. note::
+
+    This tutorial assumes the use of a Python notebook, such as IPython or Jupyter Notebook.
+    To see the figures while using a Python script instead, use
+    ``fig.show(method="external")`` to display the figure in the default PDF viewer.
+
+    To save the figure, use ``fig.savefig("figname.pdf")`` where ``"figname.pdf"``
+    is the desired name and file extension for the saved figure.
+"""
+# sphinx_gallery_thumbnail_number = 6
