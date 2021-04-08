34af05f2d5eecbf4a8fb1b0c83073d3cc2b595ca
Will Schlitzer <schlitzer90@gmail.com>
2021-04-08 07:30:18 +0100

    run make format

--- pygmt/tests/test_histogram.py
+++ pygmt/tests/test_histogram.py
@@ -7,13 +7,15 @@ import pytest
 from pygmt import Figure
 
 
-
 @pytest.fixture(scope="module")
 def table():
-    return [1,1,1,1,1,1,2,2,2,3,4,5,6,7,8,8,8,8,8,8]
+    return [1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8]
+
 
 @pytest.mark.mpl_image_compare
 def test_histogram(table):
     fig = Figure()
-    fig.histogram(table=table, projection="X10c/25c", interval=1, frame="a", fill="green")
+    fig.histogram(
+        table=table, projection="X10c/25c", interval=1, frame="a", fill="green"
+    )
     return fig
