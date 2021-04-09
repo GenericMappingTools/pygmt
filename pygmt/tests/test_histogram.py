72c022c62c80f31e9b514ebe19b8a4b66fd6e375
Will Schlitzer <schlitzer90@gmail.com>
2021-04-09 07:01:49 +0100

    add docstrings to test_histogram.py

--- pygmt/tests/test_histogram.py
+++ pygmt/tests/test_histogram.py
@@ -2,18 +2,23 @@
 """
 Tests histogram.
 """
-import numpy as np
 import pytest
 from pygmt import Figure
 
 
 @pytest.fixture(scope="module")
 def table():
+    """
+    Returns a list of integers to be used in the histogram.
+    """
     return [1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8]
 
 
 @pytest.mark.mpl_image_compare
 def test_histogram(table):
+    """
+    Tests plotting a histogram using a list of integers.
+    """
     fig = Figure()
     fig.histogram(
         table=table, projection="X10c/25c", interval=1, frame="a", fill="green"
