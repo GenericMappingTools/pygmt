75b6922db81b27c16663d6b442a6f1bd30ef50f7
noorbuchi <buchin@allegheny.edu>
2021-04-11 00:51:12 -0400

    Add first example and short description

--- examples/tutorials/date_time_charts.py
+++ examples/tutorials/date_time_charts.py
@@ -2,7 +2,9 @@
 Plotting Datetime Charts
 ========================
 
-Plotting datetime charts is handled by :meth:`pygmt.Figure.basemap`.
+Creating datetime charts is handled by :meth:`pygmt.Figure.basemap`.
+
+Plotting datetime data points is handled by :meth:`pygmt.Figure.basemap`.
 
 .. note::
 
@@ -15,38 +17,86 @@ Plotting datetime charts is handled by :meth:`pygmt.Figure.basemap`.
 """
 # sphinx_gallery_thumbnail_number = 0
 
+import datetime
 import numpy as np
+import pandas as pd
 import pygmt
+import xarray as xr
 
 ########################################################################################
-# Date - Time Formats into `fig.basemap`
+# Datetime Input Types
 # ----------------------
 #
-# Explanation of supported formats.
+# PyGmt accepts a variety of datetime objects to plot data and create charts.
+# Aside from the built in Python datetime object, PyGmt supports input using
+# ``pandas``, ``numpy`` and ``xarray``. These data types can be used to plot
+# specific points and be passed into the ``region`` parameter to become and
+# axis for the data points.
 
 ########################################################################################
-# `numpy.datetime64`
+# ## Python's datetime
 
 
-# Code
+x = [datetime.date(2010, 6, 1), datetime.date(2011, 6, 1),
+    datetime.date(2012, 6, 1), datetime.date(2013, 6, 1)]
+y = [1, 2, 3, 5]
+
+fig = pygmt.Figure()
+fig.plot(projection="X10c/5c",
+    region=[datetime.date(2010, 1, 1), datetime.date(2014, 12, 1), 0, 6],
+    frame=["WSen", "af"],
+    x=x,
+    y=y,
+    style="c0.4c",
+    pen="1p",
+    color="red3")
+
+fig.show()
 
 ########################################################################################
 # `pandas.DatetimeIndex`
+
+# pandas.DatetimeIndex
+x = pd.date_range("2013", periods=3, freq="YS")
+y = [4, 5, 6]
+fig.plot(x, y, style="t0.4c", pen="1p", color="gold")
+
+fig.show()
 
 # Code
 
 ########################################################################################
 # `xarray.DataArray`
 
+# xarray.DataArray
+x = xr.DataArray(data=pd.date_range(start="2015-03", periods=3, freq="QS"))
+y = [7.5, 6, 4.5]
+fig.plot(x, y, style="s0.4c", pen="1p")
+
+fig.show()
+
 # Code
 
 ########################################################################################
 # Raw date-time in ISO format
 
+# raw datetime strings
+x = ["2016-02-01", "2016-06-04T14", "2016-10-04T00:00:15"]
+y = [7, 8, 9]
+fig.plot(x, y, style="a0.4c", pen="1p", color="dodgerblue")
+
+fig.show()
 # Code
 
 ########################################################################################
 # Python built-in: `datetime.datetime`
+
+# the Python built-in datetime and date
+x = [datetime.date(2018, 1, 1), datetime.datetime(2019, 6, 1, 20, 5, 45)]
+y = [6.5, 4.5]
+fig.plot(x, y, style="i0.4c", pen="1p", color="seagreen")
+
+fig.show()
 
 # Code
 
