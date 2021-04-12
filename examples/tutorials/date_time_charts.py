4953166f2c33d3510fa9d4599b4ec962e138be63
noorbuchi <buchin@allegheny.edu>
2021-04-12 18:50:00 -0400

    Add python built-in examples

--- examples/tutorials/date_time_charts.py
+++ examples/tutorials/date_time_charts.py
@@ -28,14 +28,28 @@ import xarray as xr
 # ----------------------
 #
 # PyGmt accepts a variety of datetime objects to plot data and create charts.
-# Aside from the built in Python datetime object, PyGmt supports input using
+# Aside from the built-in Python datetime object, PyGmt supports input using
 # ``pandas``, ``numpy`` and ``xarray``. These data types can be used to plot
-# specific points and be passed into the ``region`` parameter to become and
-# axis for the data points.
+# specific points as well as get passed into the ``region`` parameter to
+# create a range of the data on an axis.
+#
+# The following examples will demonstrate how to create plots
+# using the differnt datetime objects
 
-########################################################################################
-# ## Python's datetime
-
+###############################################################################
+# Using Python's ``Datetime``
+# ---------------------------------------
+#
+# In this example, Python's built-in ``datetime`` package is used
+# to create data points stored in list ``x``. The format of the
+# data is ``(Y, M, D)``. Additionally, dates are passed into the
+# ``region`` parameter in the format
+# ``(x_start, x_end, y_start, y_end)``,
+# where the date range is plotted on the x-axis.
+# An additonal notable parameter is `style`, where it's specified
+# that data points are to be plotted in an **X** shape with a size
+# of 0.3 centimeters.
+#
 
 x = [datetime.date(2010, 6, 1), datetime.date(2011, 6, 1),
     datetime.date(2012, 6, 1), datetime.date(2013, 6, 1)]
@@ -44,12 +58,43 @@ y = [1, 2, 3, 5]
 fig = pygmt.Figure()
 fig.plot(projection="X10c/5c",
     region=[datetime.date(2010, 1, 1), datetime.date(2014, 12, 1), 0, 6],
-    frame=["WSen", "af"],
+    frame=["WSen", "afg"],
+    x=x,
+    y=y,
+    style="x0.3c",
+    pen="1p",
+    )
+
+fig.show()
+
+###############################################################################
+# In addition to specifying the date, ``datetime`` supports
+# the exact time of the data points. Using :meth:`datetime.datetime`
+# the ``region`` paramter as well as data points can be created
+# using this format ``(Y, M, D, H, M, S)`` where ``H`` is hours
+# using 24 hour format.
+#
+# Some notable differences to the previous example include
+# - Modifying ``frame`` to only include West(left) and South
+# (bottom) border, and removing grid lines
+# - Using circles to plot data points shown through ``c``
+# in ``style`` parameter
+
+x = [datetime.datetime(2021,1,1,3,45,1), datetime.datetime(2021,1,1,6,15,1),
+    datetime.datetime(2021,1,1,13,30,1), datetime.datetime(2021,1,1,20,30,1)]
+y = [5, 3, 1, 2]
+
+fig = pygmt.Figure()
+fig.plot(projection="X10c/5c",
+    region=[datetime.datetime(2021, 1, 1, 0,0,0), datetime.datetime(2021, 1, 2, 0,0,0), 0, 6],
+    frame=["WS", "af"],
     x=x,
     y=y,
     style="c0.4c",
     pen="1p",
-    color="red3")
+    color="blue"
+ )
+
 
 fig.show()
 
