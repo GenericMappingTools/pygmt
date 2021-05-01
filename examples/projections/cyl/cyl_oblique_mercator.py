da42d8ff02d4ff3183cc6c5b25bd96b63c6a6291
Emily McMullan <eemcmullan92@gmail.com>
2021-05-01 12:09:28 -0400

    added three code examples

--- examples/projections/cyl/cyl_oblique_mercator.py
+++ examples/projections/cyl/cyl_oblique_mercator.py
@@ -7,14 +7,14 @@ projection. It is particularly useful when mapping regions of large lateral
 extent in an oblique direction. Both parallels and meridians are complex
 curves. The projection was developed in the early 1900s by several workers.
 
-**o**\ **a**\ *lon0/lat0* */azimuth*\ */scale*\ **[+v]** or
-**O**\ **A**\ *lon0/lat0* */azimuth*\ */width*\ **[+v]** or
+**oa**\ *lon0/lat0* */azimuth*\ */scale*\ **[+v]** or
+**OA**\ *lon0/lat0* */azimuth*\ */width*\ **[+v]** or
 
-**o**\ **b**\ *lon0/lat0* */lon1/lat1*\ */scale*\ **[+v]** or
-**O**\ **B**\ *lon0/lat0* */lon1/lat1*\ */width*\ **[+v]** or
+**ob**\ *lon0/lat0* */lon1/lat1*\ */scale*\ **[+v]** or
+**OB**\ *lon0/lat0* */lon1/lat1*\ */width*\ **[+v]** or
 
-**o**\ **c**\ *lon0/lat0* */lonp/latp*\ */scale*\ **[+v]** or
-**O**\ **C**\ *lon0/lat0* */lonp/latp*\ */width*\ **[+v]**
+**oc**\ *lon0/lat0* */lonp/latp*\ */scale*\ **[+v]** or
+**OC**\ *lon0/lat0* */lonp/latp*\ */width*\ **[+v]**
 
 The projection is set with **o** or **O**. The central meridian is set by
 *lon0/lat0*. The oblique equator is set by the azimuth in option one.
@@ -27,8 +27,8 @@ import pygmt
 fig = pygmt.Figure()
 
 # Option 1 using the origin and azimuth
-fig.coast(projection="Oa-120/25/-120/12c+v",
-    region= [-122, -107, 22, 35],
+fig.coast(projection="Oa-120/25/-30/12c+v",
+    region= "-122/35/-107/22+r",
     frame="afg",
     land="gray",
     water="lightblue",
@@ -36,18 +36,17 @@ fig.coast(projection="Oa-120/25/-120/12c+v",
 fig.show()
 
 # Option 2 using the origin and two points
-fig.coast(projection="Ob138/36/305/25/12c",
-    region= "JP",
+fig.coast(projection="Ob130/35/25/35/12c",
+    region= "130/35/145/40+r",
     frame="afg",
     land="gray",
     water="lightblue",
     )
 fig.show()
 
-
 # Option 3 using the origin projection pole
 fig.coast(projection="Oc280/25.5/22/69/12c",
-    region= [270, 305, 20, 35],
+    region= "270/20/305/25+r",
     frame="afg",
     land="gray",
     water="lightblue",
