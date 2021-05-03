3534f9ca4946864351ae282c7070fa780da48942
Emily McMullan <55964550+eemcmullan@users.noreply.github.com>
2021-05-03 10:31:38 -0400

    Apply suggestions from code review
    I reviewed these suggestions, and appreciate the feedback! Thank you for the improvements.
    
    Co-authored-by: Will Schlitzer <schlitzer90@gmail.com>

--- examples/projections/cyl/cyl_oblique_mercator_2.py
+++ examples/projections/cyl/cyl_oblique_mercator_2.py
@@ -7,11 +7,13 @@ projection. It is particularly useful when mapping regions of large lateral
 extent in an oblique direction. Both parallels and meridians are complex
 curves. The projection was developed in the early 1900s by several workers.
 
-**ob**\ *lon0/lat0* */lon1/lat1*\ */scale*\ **[+v]** or
-**OB**\ *lon0/lat0* */lon1/lat1*\ */width*\ **[+v]** or
+**ob**\ |**oB**\ *lon0/lat0* */lon1/lat1*\ */scale*\ **[+v]** or
+**Ob**\ |**OB**\*lon0/lat0* */lon1/lat1*\ */width*\ **[+v]** or
 
-The projection is set with **o** or **O**. The central meridian is set by
-*lon0/lat0*. The oblique equator is set by *lon1/lat1*. Align the y-axis
+The projection is set with **o** or **O**. The pole is set in the 
+northern hemisphere with **b** or the southern hemisphere 
+with **B**. The central meridian is set by *lon0/lat0*. The oblique 
+equator is set by *lon1/lat1*. Align the y-axis
 with the optional *+v*. The figure size is set with *scale* or *width*.
 """
 import pygmt
@@ -19,9 +21,11 @@ import pygmt
 fig = pygmt.Figure()
 # Using the origin and two points
 fig.coast(projection="Ob130/35/25/35/12c",
+    # Set bottom left and top right coordinates of the figure with "+r"
     region= "130/35/145/40+r",
     frame="afg",
     land="gray",
+    shorelines="1/thin",
     water="lightblue",
     )
 fig.show()
