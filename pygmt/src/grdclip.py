e3a42affee3f39b538fab007f79c5357ee55ba1e
Will Schlitzer <schlitzer90@gmail.com>
2021-05-06 15:53:19 +0100

    create grdclip.py

---
+++ pygmt/src/grdclip.py
@@ -0,0 +1,36 @@
+"""
+Grdclip - Wrap a grid
+"""
+from pygmt.clib import Session
+from pygmt.helpers import build_arg_string, fmt_docstring, kwargs_to_strings, use_alias
+
+@fmt_docstring
+@use_alias(
+    G="outgrid",
+    R="region",
+    Z="z_subregion",
+    f="coltypes",
+)
+@kwargs_to_strings(R="sequence")
+def grdclip(grid, **kwargs):
+    """
+    Grdclip
+    """
+    with GMTTempFile(suffix=".nc") as tmpfile:
+        with Session() as lib:
+            file_context = lib.virtualfile_from_data(check_kind="raster", data=grid)
+            with file_context as infile:
+                if "G" not in kwargs.keys():  # if outgrid is unset, output to tempfile
+                    kwargs.update({"G": tmpfile.name})
+                outgrid = kwargs["G"]
+                arg_str = " ".join([infile, build_arg_string(kwargs)])
+                lib.call_module("grdclip", arg_str)
+
+        if outgrid == tmpfile.name:  # if user did not set outgrid, return DataArray
+            with xr.open_dataarray(outgrid) as dataarray:
+                result = dataarray.load()
+                _ = result.gmt  # load GMTDataArray accessor information
+        else:
+            result = None  # if user sets an outgrid, return None
+
+        return result
