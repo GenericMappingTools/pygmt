85b6950009322d18c6816653dbfa9e33baa76753
Will Schlitzer <schlitzer90@gmail.com>
2021-05-08 19:38:52 +0100

    add test_grdclip.py

---
+++ pygmt/tests/test_grdclip.py
@@ -0,0 +1,27 @@
+"""
+Tests for grdcut.
+"""
+import os
+
+import numpy as np
+import pygmt
+import pytest
+import xarray as xr
+from pygmt import grdclip, grdinfo
+from pygmt.datasets import load_earth_relief
+from pygmt.exceptions import GMTInvalidInput
+from pygmt.helpers import GMTTempFile
+
+
+@pytest.fixture(scope="module", name="grid")
+def fixture_grid():
+    """
+    Load the grid data from the sample earth_relief file.
+    """
+    return load_earth_relief(resolution="10m", region=[-5, 5, -5, 5])
+
+
+def test_grdclip_below(grid):
+    test_grid = grdclip(grid=grid, below="-1500/-1800")
+    result_info = grdinfo(grid=test_grid, force_scan=0, per_column="n").strip().split()
+    assert int(result_info[4]) == -1800
