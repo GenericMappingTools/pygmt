a1c0bdf0aeee79f4722477b9ad7fd78f6750f89d
Will Schlitzer <schlitzer90@gmail.com>
2021-05-10 16:22:32 +0100

    Apply suggestions from code review
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

--- pygmt/tests/test_grdclip.py
+++ pygmt/tests/test_grdclip.py
@@ -3,6 +3,7 @@ Tests for grdclip.
 """
 import os
 
+import numpy.testing as npt
 import pytest
 from pygmt import grdclip, grdinfo
 from pygmt.datasets import load_earth_relief
@@ -23,15 +24,15 @@ def test_grdclip_outgrid(grid):
     """
     with GMTTempFile(suffix=".nc") as tmpfile:
         result = grdclip(
-            grid=grid, outgrid=tmpfile.name, below="-1500/-1800", above="30/40"
+            grid=grid, outgrid=tmpfile.name, below=[-1500, -1800], above=[30, 40]
         )
         assert result is None  # return value is None
         assert os.path.exists(path=tmpfile.name)  # check that outgrid exists
         result = (
             grdinfo(grid=tmpfile.name, force_scan=0, per_column="n").strip().split()
         )
-    assert int(result[4]) == -1800
-    assert int(result[5]) == 40
+    assert int(result[4]) == -1800  # minimum value
+    assert int(result[5]) == 40  # maximum value
 
 
 def test_grdclip_no_outgrid(grid):
@@ -39,6 +40,8 @@ def test_grdclip_no_outgrid(grid):
     Test the below and above parameters for grdclip with no set outgrid.
     """
     temp_grid = grdclip(grid=grid, below=[-1500, -1800], above=[30, 40])
-    result = grdinfo(grid=temp_grid, force_scan=0, per_column="n").strip().split()
-    assert int(result[4]) == -1800
-    assert int(result[5]) == 40
+    assert temp_grid.dims == ("lat", "lon")
+    assert temp_grid.gmt.gtype == 1  # Geographic grid
+    assert temp_grid.gmt.registration == 1  # Pixel registration
+    npt.assert_allclose(temp_grid.min(), -1800)
+    npt.assert_allclose(temp_grid.max(), 40)
