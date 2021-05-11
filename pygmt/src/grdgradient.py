e9d4ddf175f305bb56dc178839cc876c992174ea
Will Schlitzer <schlitzer90@gmail.com>
2021-05-11 11:57:41 +0100

    add azimuth docstring

--- pygmt/src/grdgradient.py
+++ pygmt/src/grdgradient.py
@@ -37,6 +37,24 @@ def grdgradient(grid, **kwargs):
     outgrid : str or None
         The name of the output netCDF file with extension .nc to store the grid
         in.
+    azimuth : str or list or xarray.DataArray
+        *azim*\ [/*azim2*].
+        Azimuthal direction for a directional derivative; *azim* is the
+        angle in the x,y plane measured in degrees positive clockwise from
+        north (the +y direction) toward east (the +x direction). The
+        negative of the directional derivative, -[dz/dx\*sin(*azim*) +
+        dz/dy\*cos(\ *azim*)], is found; negation yields positive values
+        when the slope of z(x,y) is downhill in the *azim* direction, the
+        correct sense for shading the illumination of an image by a light
+        source above the x,y plane shining from the *azim* direction.
+        Optionally, supply two azimuths, *azim*/*azim2*, in which case the
+        gradients in each of these directions are calculated and the one
+        larger in magnitude is retained; this is useful for illuminating data
+        with two directions of lineated structures, e.g., *0*/*270*
+        illuminates from the north (top) and west (left).  Finally, if *azim*
+        is a file it must be a grid of the same domain, spacing and
+        registration as *ingrid* that will update the azimuth at each output
+        node when computing the directional derivatives.
     {R}
     {V}
     """
