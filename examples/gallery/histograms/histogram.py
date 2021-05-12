57d4eb9d9d1dbf62ad4df65e15fb200668c3b4e0
Michael Grund <michael.grund@kit.edu>
2021-05-12 18:42:28 +0200

    formatting

--- examples/gallery/histograms/histogram.py
+++ examples/gallery/histograms/histogram.py
@@ -8,12 +8,18 @@ import pygmt
 
 fig = pygmt.Figure()
 
-fig.histogram(table = "@v3206_06.txt", 
-              region = [-6000,0,0,30],
-              series= 250, 
-              fill="red3", 
-              frame = ['WSne+t"Histograms"+glightgray','x+l"Topography (m)"', 'y+l"Frequency"+u" %"'], 
-              pen = "1p",
-              type = 1)
+fig.histogram(
+    table="@v3206_06.txt",
+    region=[-6000, 0, 0, 30],
+    series=250,
+    fill="red3",
+    frame=[
+        'WSne+t"Histograms"+glightgray',
+        'x+l"Topography (m)"',
+        'y+l"Frequency"+u" %"',
+    ],
+    pen="1p",
+    type=1,
+)
 
 fig.show()
