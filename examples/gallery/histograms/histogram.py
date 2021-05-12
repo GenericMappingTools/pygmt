25e77f5f111e0e35af273b1a8414325915ae19db
Michael Grund <michael.grund@kit.edu>
2021-05-12 18:59:34 +0200

    added comments

--- examples/gallery/histograms/histogram.py
+++ examples/gallery/histograms/histogram.py
@@ -10,15 +10,22 @@ fig = pygmt.Figure()
 
 fig.histogram(
     table="@v3206_06.txt",
+    # specify the "region" of interest [xmin, xmax, ymin, ymax]
     region=[-6000, 0, 0, 30],
+    # generate evenly spaced bins by increments of 250
     series=250,
+    # use red3 as color fill for the bars
     fill="red3",
+    # define the frame, add title and set background color to
+    # lightgray, add annotations for x and y axis
     frame=[
         'WSne+t"Histograms"+glightgray',
         'x+l"Topography (m)"',
         'y+l"Frequency"+u" %"',
     ],
+    # use a pen size of 1p to draw the outlines
     pen="1p",
+    # choose histogram type 1 = frequency_percent
     type=1,
 )
 
