630dc62fd49aacd1141788af55edecf4144b6864
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2021-05-12 18:39:00 +0200

    Add histogram gallery example

---
+++ examples/gallery/histograms/histogram.py
@@ -0,0 +1,19 @@
+"""
+Histogram
+---------
+The :meth:`pygmt.Figure.histogram` method can plot regular histograms.
+"""
+
+import pygmt
+
+fig = pygmt.Figure()
+
+fig.histogram(table = "@v3206_06.txt", 
+              region = [-6000,0,0,30],
+              series= 250, 
+              fill="red3", 
+              frame = ['WSne+t"Histograms"+glightgray','x+l"Topography (m)"', 'y+l"Frequency"+u" %"'], 
+              pen = "1p",
+              type = 1)
+
+fig.show()
