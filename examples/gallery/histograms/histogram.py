acb1cf93e16b650e0f353b19dcefb70ed0ccc9f0
Michael Grund <michael.grund@kit.edu>
2021-05-13 10:37:55 +0200

    updates based on code review

--- examples/gallery/histograms/histogram.py
+++ examples/gallery/histograms/histogram.py
@@ -4,29 +4,31 @@ Histogram
 The :meth:`pygmt.Figure.histogram` method can plot regular histograms.
 """
 
+import numpy as np
 import pygmt
+
+np.random.seed(100)
+
+# generate example topography data
+mu = 100  # mean of distribution
+sigma = 25  # standard deviation of distribution
+data = mu + sigma * np.random.randn(521)
 
 fig = pygmt.Figure()
 
 fig.histogram(
-    table="@v3206_06.txt",
-    # specify the "region" of interest [xmin, xmax, ymin, ymax]
-    region=[-6000, 0, 0, 30],
-    # generate evenly spaced bins by increments of 250
-    series=250,
+    table=data,
+    # generate evenly spaced bins by increments of 5
+    series=5,
     # use red3 as color fill for the bars
     fill="red3",
     # define the frame, add title and set background color to
     # lightgray, add annotations for x and y axis
-    frame=[
-        'WSne+t"Histogram"+glightgray',
-        'x+l"Topography (m)"',
-        'y+l"Frequency"+u" %"',
-    ],
+    frame=['WSne+t"Histogram"+glightgray', 'x+l"Topography (m)"', 'y+l"Frequency"'],
     # use a pen size of 1p to draw the outlines
     pen="1p",
-    # choose histogram type 1 = frequency_percent
-    type=1,
+    # choose histogram type 1 = counts [default]
+    type=0,
 )
 
 fig.show()
