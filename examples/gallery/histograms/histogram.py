8dbd810bf5dad3cf2db308b0db09de54759e21fa
Michael Grund <michael.grund@kit.edu>
2021-05-17 08:23:42 +0200

    updates based on code review

--- examples/gallery/histograms/histogram.py
+++ examples/gallery/histograms/histogram.py
@@ -21,13 +21,13 @@ fig = pygmt.Figure()
 
 fig.histogram(
     table=data,
+    # define the frame, add title and set background color to
+    # lightgray, add annotations for x and y axis
+    frame=['WSne+t"Histogram"+glightgray', 'x+l"Elevation (m)"', 'y+l"Counts"'],
     # generate evenly spaced bins by increments of 5
     series=5,
     # use red3 as color fill for the bars
     fill="red3",
-    # define the frame, add title and set background color to
-    # lightgray, add annotations for x and y axis
-    frame=['WSne+t"Histogram"+glightgray', 'x+l"Elevation (m)"', 'y+l"Counts"'],
     # use a pen size of 1p to draw the outlines
     pen="1p",
     # choose histogram type 0 = counts [default]
