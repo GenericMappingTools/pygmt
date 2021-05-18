3f3640ca95b151435ddf848b9911a6893b6e50cb
Will Schlitzer <schlitzer90@gmail.com>
2021-05-18 11:32:09 +0100

    adding increment docstring

--- pygmt/src/grdlandmask.py
+++ pygmt/src/grdlandmask.py
@@ -40,6 +40,26 @@ def grdlandmask(**kwargs):
     outgrid : str or None
         The name of the output netCDF file with extension .nc to store the grid
         in.
+    increment : str
+        *xinc*\ [**+e**\|\ **n**][/\ *yinc*\ [**+e**\|\ **n**]].
+        *x_inc* [and optionally *y_inc*] is the grid spacing. **Geographical
+        (degrees) coordinates**: Optionally, append a increment unit. Choose among
+        **m** to indicate arc minutes or **s** to indicate arc seconds. If one
+        of the units **e**, **f**, **k**, **M**, **n** or **u** is appended
+        instead, the increment is assumed to be given in meter, foot, km, Mile,
+        nautical mile or US survey foot, respectively, and will be converted to
+        the equivalent degrees longitude at the middle latitude of the region.
+        If *y_inc* is given but set to 0 it will be reset equal to *x_inc*;
+        otherwise it will be converted to degrees latitude. **All
+        coordinates**: If **+e** is appended then the corresponding max
+        *x* (*east*) or *y* (*north*) may be slightly adjusted to fit exactly
+        the given increment [by default the increment may be adjusted slightly
+        to fit the given domain]. Finally, instead of giving an increment you
+        may specify the *number of nodes* desired by appending **+n** to the
+        supplied integer argument; the increment is then recalculated from the
+        number of nodes, the *registration*, and the domain. The resulting
+        increment value depends on whether you have selected a
+        gridline-registered or pixel-registered grid.
     {R}
 
     Returns
