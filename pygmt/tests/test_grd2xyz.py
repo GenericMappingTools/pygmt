948ca7334407449e15ebd90a07b2e6e5f3a6b415
Will Schlitzer <schlitzer90@gmail.com>
2021-05-25 07:58:31 +0100

    add format tests in test_grd2xyz.py

--- pygmt/tests/test_grd2xyz.py
+++ pygmt/tests/test_grd2xyz.py
@@ -1,9 +1,12 @@
 """
 Tests for grd2xyz.
 """
+import numpy as np
+import pandas as pd
 import pytest
 from pygmt import grd2xyz
 from pygmt.datasets import load_earth_relief
+from pygmt.exceptions import GMTInvalidInput
 
 
 @pytest.fixture(scope="module", name="grid")
@@ -18,10 +21,30 @@ def test_grd2xyz(grid):
     """
     Make sure grd2xyz works as expected.
     """
-    xyz_data = grd2xyz(grid=grid)
+    xyz_data = grd2xyz(grid=grid, format="s")
     assert xyz_data.strip().split("\n") == [
         "-0.5 0.5 -4967",
         "0.5 0.5 -4852",
         "-0.5 -0.5 -4917",
         "0.5 -0.5 -4747.5",
     ]
+
+
+def test_grd2xyz_format(grid):
+    """
+    Test that correct formats are returned.
+    """
+    xyz_array = grd2xyz(grid=grid)
+    assert isinstance(xyz_array, np.ndarray)
+    xyz_df = grd2xyz(grid=grid, format="d")
+    assert isinstance(xyz_df, pd.DataFrame)
+    xyz_string = grd2xyz(grid=grid, format="s")
+    assert isinstance(xyz_string, str)
+
+
+def test_grd2xyz_invalid_format(grid):
+    """
+    Test that grd2xyz fails with incorrect format.
+    """
+    with pytest.raises(GMTInvalidInput):
+        grd2xyz(grid=grid, format=1)
