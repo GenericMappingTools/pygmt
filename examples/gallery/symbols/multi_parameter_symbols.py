c45d5ea33b3dba616bcddbb3990ec387aaad3484
core-man <coreman.seism@gmail.com>
2021-06-09 15:08:23 +0800

    Add new syntax for all the multi-parameter symbols

--- examples/gallery/symbols/multi_parameter_symbols.py
+++ examples/gallery/symbols/multi_parameter_symbols.py
@@ -4,10 +4,12 @@ Multi-parameter symbols
 
 The :meth:`pygmt.Figure.plot` method can plot individual multi-parameter
 symbols by passing the corresponding shortcuts listed below to the ``style``
-parameter. Additionally, we must define the required parameters in a 2d list or
+parameter. Additionally, we can define the required parameters in a 2d list or
 numpy array (``[[parameters]]`` for a single symbol or
 ``[[parameters_1],[parameters_2],[parameters_i]]`` for several ones) or use an
 appropriately formatted input file and pass it to ``data``.
+Alternatively, the required parameters can also be set after those shortcuts
+via the ``style`` parameter.
 
 The following symbols are available:
 
@@ -27,13 +29,21 @@ import pygmt
 fig = pygmt.Figure()
 fig.basemap(region=[0, 6.5, 0, 2], projection="x3c", frame=True)
 
-# ELLIPSE
-data = [[0.5, 1, 45, 3, 1]]
+# ELLIPSE 1
+data = [[0.5, 0.5, 45, 3, 1]]
 fig.plot(data=data, style="e", color="orange", pen="2p,black")
 
-# ROTATED RECTANGLE
-data = [[1.5, 1, 120, 5, 0.5]]
+# ELLIPSE 2
+# direction/major_axis/minor_axis are given via the style parameter
+fig.plot(x=0.5, y=1.5, style="e45/3/1", color="orange", pen="2p,black")
+
+# ROTATED RECTANGLE 1
+data = [[1.4, 1, 120, 5, 0.5]]
 fig.plot(data=data, style="j", color="red3", pen="2p,black")
+
+# ROTATED RECTANGLE 2
+# direction/width/height are given via the style parameter
+fig.plot(x=1.7, y=1, style="j120/5/0.5", color="red3", pen="2p,black")
 
 # RECTANGLE 1
 data = [[3, 0.5, 4, 1.5]]
@@ -51,8 +61,12 @@ fig.plot(data=data, style="R", color="seagreen", pen="2p,black")
 # width/height/radius are given via the style parameter
 fig.plot(x=5.0, y=1.0, style="R1.25/4/0.5", color="seagreen", pen="2p,black")
 
-# PIE WEDGE
-data = [[6.0, 1, 2.5, 45, 330]]
+# PIE WEDGE 1
+data = [[6.0, 0.5, 2.5, 45, 330]]
 fig.plot(data=data, style="w", color="lightgray", pen="2p,black")
+
+# PIE WEDGE 2
+# radius/startdir/stopdir are given via the style parameter
+fig.plot(x=6.0, y=1.5, style="w2.5/45/330", color="lightgray", pen="2p,black")
 
 fig.show()
