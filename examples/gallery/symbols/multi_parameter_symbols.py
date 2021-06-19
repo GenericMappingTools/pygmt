739de0dff124d378346cf5b3be32ec4cbf7c2d39
core-man <coreman.seism@gmail.com>
2021-06-19 23:41:45 +0800

    Update sytle

--- examples/gallery/symbols/multi_parameter_symbols.py
+++ examples/gallery/symbols/multi_parameter_symbols.py
@@ -4,33 +4,38 @@ Multi-parameter symbols
 
 The :meth:`pygmt.Figure.plot` method can plot individual multi-parameter
 symbols by passing the corresponding shortcuts (**e**, **j**, **r**, **R**,
-**w**) to the ``style`` parameter. Additionally, We need to define locations
-(lon, lat) via the ``x`` and ``y`` parameters (scalar for a single symbol or 1d
-list for servel ones) and two or three symbol parameters after those shortcuts
-via the ``style`` parameter. Alternately, those values can also be defined in a
-2d list or numpy array (``[[parameters]]`` for a single symbol or
-``[[parameters_1],[parameters_2],[parameters_i]]`` for several ones) or use an
-appropriately formatted input file and pass it to ``data``.
+**w**) to the ``style`` parameter:
 
-The following symbols are available:
+- **e**: ellipse
+- **j**: rotated rectangle
+- **r**: rectangle
+- **R**: rounded rectangle
+- **w**: pie wedge
 
-- **e**: ellipse, ``[[lon, lat, direction, major_axis, minor_axis]]``
-- **j**: rotated rectangle, ``[[lon, lat, direction, width, height]]``
-- **r**: rectangle, ``[[lon, lat, width, height]]``
-- **R**: rounded rectangle, ``[[lon, lat, width, height, radius]]``
-- **w**: pie wedge, ``[[lon, lat, radius, startdir, stopdir]]``, the last two
-  arguments are directions given in degrees counter-clockwise from horizontal
-
-Upper-case versions **E**, **J**, and **W** are similar to **e**, **j** and
-**w** but expect geographic azimuths and distances.
 """
 
 # sphinx_gallery_thumbnail_number = 2
 
 import pygmt
 
-# The 1st simple way: plot multi-parameter symbols using the same symbol style
-# x and y can be scalar (this example) or 1d lists
+########################################################################################
+# We can plot multi-parameter symbols using the same symbol style. We need to
+# define locations (lon, lat) via the ``x`` and ``y`` parameters (scalar for
+# a single symbol or 1d list for servel ones) and two or three symbol
+# parameters after those shortcuts via the ``style`` parameter.
+#
+# The symbol parameters in the ``style`` parameter are defined as:
+#
+# - **e**: ellipse, ``direction/major_axis/minor_axis``
+# - **j**: rotated rectangle, ``direction/width/height``
+# - **r**: rectangle, ``width/height``
+# - **R**: rounded rectangle, ``width/height/radius``
+# - **w**: pie wedge, ``radius/startdir/stopdir``, the last two arguments are
+#   directions given in degrees counter-clockwise from horizontal
+#
+# Upper-case versions **E**, **J**, and **W** are similar to **e**, **j** and
+# **w** but expect geographic azimuths and distances.
+
 fig = pygmt.Figure()
 fig.basemap(region=[0, 6, 0, 2], projection="x3c", frame=True)
 
@@ -47,8 +52,21 @@ fig.plot(x=5.5, y=1, style="w2.5/45/330", color="lightgray", pen="2p,black")
 
 fig.show()
 
-# The 2nd way: plot multi-parameter symbols using varying symbol styles
-# The data parameter can be 2d array
+########################################################################################
+# We can also plot symbols with varying parameters via defining those values in
+# a 2d list or numpy array (``[[parameters]]`` for a single symbol or
+# ``[[parameters_1],[parameters_2],[parameters_i]]`` for several ones) or using
+# an appropriately formatted input file and passing it to ``data``.
+#
+# The symbol parameters in the 2d list or numpy array are defined as:
+#
+# - **e**: ellipse, ``[[lon, lat, direction, major_axis, minor_axis]]``
+# - **j**: rotated rectangle, ``[[lon, lat, direction, width, height]]``
+# - **r**: rectangle, ``[[lon, lat, width, height]]``
+# - **R**: rounded rectangle, ``[[lon, lat, width, height, radius]]``
+# - **w**: pie wedge, ``[[lon, lat, radius, startdir, stopdir]]``, the last two
+#   arguments are directions given in degrees counter-clockwise from horizontal
+
 fig = pygmt.Figure()
 fig.basemap(region=[0, 6, 0, 4], projection="x3c", frame=["xa1f0.2", "ya0.5f0.1"])
 
