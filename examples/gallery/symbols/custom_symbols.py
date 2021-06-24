c0939210024c5cd6ce465e29d69e4a01641adff5
Michael Grund <michael.grund@kit.edu>
2021-06-24 17:43:46 +0200

    add code annotations

--- examples/gallery/symbols/custom_symbols.py
+++ examples/gallery/symbols/custom_symbols.py
@@ -6,8 +6,8 @@ The :meth:`pygmt.Figure.plot` method can plot individual custom symbols
 by passing the corresponding symbol name together with the **k** shortcut to
 the ``style`` parameter. In total 41 custom symbols are already included of
 which the following plot shows five examplary ones. The symbols are shown
-underneath their corresponding names. For the remaining symbols see
-:gmt-docs:`cookbook/custom-symbols.html`
+underneath their corresponding names. For the remaining symbols see the GMT
+cookbook :gmt-docs:`cookbook/custom-symbols.html`.
 """
 
 import pygmt
@@ -21,18 +21,28 @@ font = "15p,Helvetica-Bold"
 
 y = 1.25
 
+# use the volcano symbol with a size of 60p,
+# fill color is set to "seagreen"
 fig.plot(x=1, y=y, style="kvolcano/60p", pen=pen, color="seagreen")
 fig.text(x=1, y=y + 1.25, text="volcano", font=font)
 
+# use the astroid symbol with a size of 60p,
+# fill color is set to "red3"
 fig.plot(x=2.5, y=y, style="kastroid/60p", pen=pen, color="red3")
 fig.text(x=2.5, y=y + 1.25, text="astroid", font=font)
 
+# use the flash symbol with a size of 60p,
+# fill color is set to "darkorange"
 fig.plot(x=4, y=y, style="kflash/60p", pen=pen, color="darkorange")
 fig.text(x=4, y=y + 1.25, text="flash", font=font)
 
+# use the star4 symbol with a size of 60p,
+# fill color is set to "dodgerblue4"
 fig.plot(x=5.5, y=y, style="kstar4/60p", pen=pen, color="dodgerblue4")
 fig.text(x=5.5, y=y + 1.25, text="star4", font=font)
 
+# use the hurricane symbol with a size of 60p,
+# fill color is set to "magenta4"
 fig.plot(x=7, y=y, style="khurricane/60p", pen=pen, color="magenta4")
 fig.text(x=7, y=y + 1.25, text="hurricane", font=font)
 
