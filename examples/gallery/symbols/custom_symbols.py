985ebd19f6ab272cf3fad664239ff5d5c182ceea
Michael Grund <michael.grund@kit.edu>
2021-06-25 18:31:16 +0200

    updates based on code review

--- examples/gallery/symbols/custom_symbols.py
+++ examples/gallery/symbols/custom_symbols.py
@@ -19,31 +19,29 @@ fig.basemap(region=[0, 8, 0, 3], projection="X12c/4c", frame=True)
 pen = "1p,black"
 font = "15p,Helvetica-Bold"
 
-y = 1.25
-
 # use the volcano symbol with a size of 1.5c,
 # fill color is set to "seagreen"
-fig.plot(x=1, y=y, style="kvolcano/1.5c", pen=pen, color="seagreen")
-fig.text(x=1, y=y + 1.25, text="volcano", font=font)
+fig.plot(x=1, y=1.25, style="kvolcano/1.5c", pen=pen, color="seagreen")
+fig.text(x=1, y=2.5, text="volcano", font=font)
 
 # use the astroid symbol with a size of 1.5c,
 # fill color is set to "red3"
-fig.plot(x=2.5, y=y, style="kastroid/1.5c", pen=pen, color="red3")
-fig.text(x=2.5, y=y + 1.25, text="astroid", font=font)
+fig.plot(x=2.5, y=1.25, style="kastroid/1.5c", pen=pen, color="red3")
+fig.text(x=2.5, y=2.5, text="astroid", font=font)
 
 # use the flash symbol with a size of 1.5c,
 # fill color is set to "darkorange"
-fig.plot(x=4, y=y, style="kflash/1.5c", pen=pen, color="darkorange")
-fig.text(x=4, y=y + 1.25, text="flash", font=font)
+fig.plot(x=4, y=1.25, style="kflash/1.5c", pen=pen, color="darkorange")
+fig.text(x=4, y=2.5, text="flash", font=font)
 
 # use the star4 symbol with a size of 1.5c,
 # fill color is set to "dodgerblue4"
-fig.plot(x=5.5, y=y, style="kstar4/1.5c", pen=pen, color="dodgerblue4")
-fig.text(x=5.5, y=y + 1.25, text="star4", font=font)
+fig.plot(x=5.5, y=1.25, style="kstar4/1.5c", pen=pen, color="dodgerblue4")
+fig.text(x=5.5, y=2.5, text="star4", font=font)
 
 # use the hurricane symbol with a size of 1.5c,
 # fill color is set to "magenta4"
-fig.plot(x=7, y=y, style="khurricane/1.5c", pen=pen, color="magenta4")
-fig.text(x=7, y=y + 1.25, text="hurricane", font=font)
+fig.plot(x=7, y=1.25, style="khurricane/1.5c", pen=pen, color="magenta4")
+fig.text(x=7, y=2.5, text="hurricane", font=font)
 
 fig.show()
