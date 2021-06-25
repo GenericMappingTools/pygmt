d0f73ceb4e7fa1e7f8165766e3c8637427f68651
Michael Grund <michael.grund@kit.edu>
2021-06-25 08:21:38 +0200

    adjust symbol size and use cm instead of points

--- examples/gallery/symbols/custom_symbols.py
+++ examples/gallery/symbols/custom_symbols.py
@@ -21,29 +21,29 @@ font = "15p,Helvetica-Bold"
 
 y = 1.25
 
-# use the volcano symbol with a size of 60p,
+# use the volcano symbol with a size of 1.5c,
 # fill color is set to "seagreen"
-fig.plot(x=1, y=y, style="kvolcano/60p", pen=pen, color="seagreen")
+fig.plot(x=1, y=y, style="kvolcano/1.5c", pen=pen, color="seagreen")
 fig.text(x=1, y=y + 1.25, text="volcano", font=font)
 
-# use the astroid symbol with a size of 60p,
+# use the astroid symbol with a size of 1.5c,
 # fill color is set to "red3"
-fig.plot(x=2.5, y=y, style="kastroid/60p", pen=pen, color="red3")
+fig.plot(x=2.5, y=y, style="kastroid/1.5c", pen=pen, color="red3")
 fig.text(x=2.5, y=y + 1.25, text="astroid", font=font)
 
-# use the flash symbol with a size of 60p,
+# use the flash symbol with a size of 1.5c,
 # fill color is set to "darkorange"
-fig.plot(x=4, y=y, style="kflash/60p", pen=pen, color="darkorange")
+fig.plot(x=4, y=y, style="kflash/1.5c", pen=pen, color="darkorange")
 fig.text(x=4, y=y + 1.25, text="flash", font=font)
 
-# use the star4 symbol with a size of 60p,
+# use the star4 symbol with a size of 1.5c,
 # fill color is set to "dodgerblue4"
-fig.plot(x=5.5, y=y, style="kstar4/60p", pen=pen, color="dodgerblue4")
+fig.plot(x=5.5, y=y, style="kstar4/1.5c", pen=pen, color="dodgerblue4")
 fig.text(x=5.5, y=y + 1.25, text="star4", font=font)
 
-# use the hurricane symbol with a size of 60p,
+# use the hurricane symbol with a size of 1.5c,
 # fill color is set to "magenta4"
-fig.plot(x=7, y=y, style="khurricane/60p", pen=pen, color="magenta4")
+fig.plot(x=7, y=y, style="khurricane/1.5c", pen=pen, color="magenta4")
 fig.text(x=7, y=y + 1.25, text="hurricane", font=font)
 
 fig.show()
