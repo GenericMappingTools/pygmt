b8d7a43783ebcde0d657c8eb5a54b16b944412ae
Will Schlitzer <schlitzer90@gmail.com>
2021-07-09 15:22:31 +0100

    update test_grdproject.py to not test unchanged values in grid transformation

--- pygmt/tests/test_grdproject.py
+++ pygmt/tests/test_grdproject.py
@@ -3,7 +3,6 @@ Tests for grdproject.
 """
 import os
 
-import numpy.testing as npt
 import pytest
 from pygmt import grdinfo, grdproject
 from pygmt.datasets import load_earth_relief
@@ -28,20 +27,17 @@ def test_grdproject_file_out(grid):
         assert result is None  # return value is None
         assert os.path.exists(path=tmpfile.name)  # check that outgrid exists
         result = grdinfo(tmpfile.name, per_column=True).strip().split()
-        assert float(result[4]) == -5130.48193359  # minimum value
-        assert float(result[5]) == -152.585281372  # maximum value
 
 
 def test_grdproject_no_outgrid(grid):
     """
     Test grdproject with no set outgrid.
     """
+    assert grid.gmt.gtype == 1  # Geographic grid
     temp_grid = grdproject(grid=grid, projection="M10c")
     assert temp_grid.dims == ("y", "x")
     assert temp_grid.gmt.gtype == 0  # Rectangular grid
     assert temp_grid.gmt.registration == 1  # Pixel registration
-    npt.assert_allclose(temp_grid.min(), -5130.48193359)
-    npt.assert_allclose(temp_grid.max(), -152.585281372)
 
 
 def test_grdproject_fails(grid):
