0b0cbbf70a3fdbdc6c262754379191156a8278e9
yohaimagen <mayochay@gmail.com>
2021-07-18 08:52:30 +0300

    Explain the acceptable data formats, levels, and annotation parameters.

--- examples/gallery/lines/contours.py
+++ examples/gallery/lines/contours.py
@@ -1,7 +1,13 @@
 """
 Contours
 --------
-The :meth:`pygmt.Figure.contour` method can plot contour lines from a table of points.
+The :meth:`pygmt.Figure.contour` method can plot contour lines from a table of points by direct triangulation.
+The data to the triangulation can by provided in one of three options:
+    1: ``x``, ``y``, ``z`` 1d data columns
+    2: ``data`` 2d data matrix with 3 columns corresponding to ``x``, ``y``, ``z``
+    3: ''data'' path string to a file containing the ``x``, ``y``, ``z`` in a tabular format
+The parameters ``levels`` and ``annotation`` are deciding on the contours intervals and intervals of the
+annotation on the contours
 """
 
 
@@ -12,6 +18,7 @@ X, Y = np.meshgrid(np.linspace(-10, 10, 50), np.linspace(-10, 10, 50))
 Z = X**2 + Y**2
 x, y, z = X.flatten(), Y.flatten(), Z.flatten()
 
+
 fig = pygmt.Figure()
 fig.contour(
     region=[-10, 10, -10, 10],
@@ -20,6 +27,8 @@ fig.contour(
     x=x,
     y=y,
     z=z,
+    levels=5,
+    annotation=20,
     pen=1
 )
 fig.show()
