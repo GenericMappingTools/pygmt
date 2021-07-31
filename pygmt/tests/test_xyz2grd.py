3b241a6e13478c66332a9b63688008bc3b819bde
Will Schlitzer <schlitzer90@gmail.com>
2021-07-31 06:57:28 +0100

    add parameter name in test_xyz2grd.py

--- pygmt/tests/test_xyz2grd.py
+++ pygmt/tests/test_xyz2grd.py
@@ -19,7 +19,7 @@ def test_xyz2grd_input_file():
     """
     Run xyz2grd by passing in a filename.
     """
-    output = xyz2grd("@tut_ship.xyz", spacing=5, region=[245, 255, 20, 30])
+    output = xyz2grd(table="@tut_ship.xyz", spacing=5, region=[245, 255, 20, 30])
     assert isinstance(output, xr.DataArray)
     assert output.gmt.registration == 0  # Gridline registration
     assert output.gmt.gtype == 0  # Cartesian type
