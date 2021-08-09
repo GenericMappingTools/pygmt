e6f6514880eb2de0c1fd3d4698e1e8d799b2587e
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2021-08-09 20:02:14 +0200

    Update pygmt/helpers/decorators.py
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- pygmt/helpers/decorators.py
+++ pygmt/helpers/decorators.py
@@ -27,11 +27,11 @@ COMMON_OPTIONS = {
             Select map :doc:`projection </projections/index>`.""",
     "A": r"""
         area_thresh : int, float, or str
-        *min_area*\ [/*min_level*/*max_level*][**+a**\[**g**\|\ **i**]\
-        [**s**\|\ **S**][**+l**\|\ **r**][**+p**\ *percent*].
-        Features with an area smaller than *min_area* in km\ :sup:`2` or of
-        hierarchical level that is lower than *min_level* or higher than
-        *max_level* will not be plotted.""",
+            *min_area*\ [/*min_level*/*max_level*][**+a**\[**g**\|\ **i**]\
+            [**s**\|\ **S**][**+l**\|\ **r**][**+p**\ *percent*].
+            Features with an area smaller than *min_area* in km\ :sup:`2` or of
+            hierarchical level that is lower than *min_level* or higher than
+            *max_level* will not be plotted.""",
     "B": r"""
         frame : bool or str or list
             Set map boundary
