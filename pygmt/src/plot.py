e29edaf5ab58c1e2091d117c2a5a57ed61863475
yohaimagen <mayochay@gmail.com>
2021-08-24 11:24:03 +0300

    Update pygmt/src/plot.py
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

--- pygmt/src/plot.py
+++ pygmt/src/plot.py
@@ -218,7 +218,7 @@ def plot(self, x=None, y=None, data=None, size=None, direction=None, **kwargs):
         "S" not in kwargs and kind == "file"
     ):  # checking that the data is a file path to set default style
         try:
-            with open(which(data), "r") as file:
+            with open(which(data), mode="r", encoding="utf8") as file:
                 line = file.readline()
             if (
                 "@GMULTIPOINT" in line or "@GPOINT" in line
