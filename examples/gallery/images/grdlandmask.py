a56a2816063879654d2c85b03f22c639a9f77d7e
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2021-08-27 19:00:44 +0200

    Add gallery example for grdlandmask
    According to #1421 here's now the gallery example showcasing the use of ``grdlandmask``.

---
+++ examples/gallery/images/grdlandmask.py
@@ -0,0 +1,24 @@
+"""
+Create 'wet-dry' mask grid
+--------------------
+The :meth:`pygmt.grdlandmash` method allows to set 
+all nodes on land or water to a specified value using
+the ``maskvalues`` parameter. 
+"""
+
+import pygmt
+
+fig = pygmt.Figure()
+
+# Assign a value of 0 for all water masses and a value of 1 for all land masses
+grid = pygmt.grdlandmask(region=[-65, -40, -40, -20], spacing = "5m", maskvalues = "0/1", resolution = "l")
+
+# Plot clipped grid
+fig.basemap(
+    region=region, projection="M12c", frame=True
+)
+
+fig.grdimage(grid=grid, cmap="lajolla")
+fig.colorbar(position="JMR+o0.5c/0c+w8c")
+
+fig.show()
