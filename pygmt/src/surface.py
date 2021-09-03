8b3eeb2e4d958ab7521c242ef2d26275f9a86808
yohaimagen <mayochay@gmail.com>
2021-09-03 14:17:46 +0300

    add required_z=True to surface

--- pygmt/src/surface.py
+++ pygmt/src/surface.py
@@ -78,9 +78,7 @@ def surface(x=None, y=None, z=None, data=None, **kwargs):
         - None if ``outfile`` is set (grid output will be stored in file set by
           ``outfile``)
     """
-    kind = data_kind(data, x, y, z)
-    if kind == "vectors" and z is None:
-        raise GMTInvalidInput("Must provide z with x and y.")
+    kind = data_kind(data, x, y, z, required_z=True)
 
     with GMTTempFile(suffix=".nc") as tmpfile:
         with Session() as lib:
