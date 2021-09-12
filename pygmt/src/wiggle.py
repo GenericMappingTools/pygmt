108a6923a6d7ba4349511fefe7452c9c32a74641
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2021-09-12 18:17:34 +0200

    Figure.wiggle: Deprecate parameter "columns" to "incols" (remove in v0.6.0)

--- pygmt/src/wiggle.py
+++ pygmt/src/wiggle.py
@@ -2,10 +2,11 @@
 wiggle - Plot z=f(x,y) anomalies along tracks.
 """
 from pygmt.clib import Session
-from pygmt.helpers import build_arg_string, fmt_docstring, kwargs_to_strings, use_alias
+from pygmt.helpers import build_arg_string, fmt_docstring, kwargs_to_strings, use_alias, deprecate_parameter
 
 
 @fmt_docstring
+@deprecate_parameter("columns", "incols", "v0.4.1", remove_version="v0.6.0")
 @use_alias(
     B="frame",
     D="position",
@@ -25,7 +26,7 @@ from pygmt.helpers import build_arg_string, fmt_docstring, kwargs_to_strings, us
     e="find",
     g="gap",
     h="header",
-    i="columns",
+    i="incols",
     p="perspective",
 )
 @kwargs_to_strings(R="sequence", c="sequence_comma", i="sequence_comma", p="sequence")
@@ -49,7 +50,7 @@ def wiggle(self, x=None, y=None, z=None, data=None, **kwargs):
     data : str or {table-like}
         Pass in either a file name to an ASCII data table, a 2D
         {table-classes}.
-        Use parameter ``columns`` to choose which columns are x, y, z,
+        Use parameter ``incols`` to choose which columns are x, y, z,
         respectively.
     {J}
     {R}
@@ -86,11 +87,7 @@ def wiggle(self, x=None, y=None, z=None, data=None, **kwargs):
     {e}
     {g}
     {h}
-    columns : str or 1d array
-        Choose which columns are x, y, and z, respectively if input is provided
-        via *data*. E.g. ``columns = [0, 1, 2]`` or ``columns = "0,1,2"`` if
-        the *x* values are stored in the first column, *y* values in the second
-        one and *z* values in the third one. Note: zero-based indexing is used.
+    {i}
     {p}
     """
     kwargs = self._preprocess(**kwargs)  # pylint: disable=protected-access
