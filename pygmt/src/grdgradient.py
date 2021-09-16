0e3a9ac1639b166236eea94e59bf8e209167c472
Will Schlitzer <schlitzer90@gmail.com>
2021-09-16 19:54:58 -0400

    update docstrings

--- pygmt/src/grdgradient.py
+++ pygmt/src/grdgradient.py
@@ -110,20 +110,23 @@ def grdgradient(grid, **kwargs):
         *offset*) if it is not given. To use *offset* and/or *sigma* from a
         previous calculation, leave out the argument to the modifier(s) and
         see **-Q** for usage.  As a final option, you may add **+a**\ *ambient*
-        to add *ambient* to all nodes after gradient calculations are completed.
+        to add *ambient* to all nodes after gradient calculations are 
+        completed.
     norm_control : str
         **c**\|\ **r**\|\ **R**
-        Controls how normalization via **-N** is carried out.  When multiple grids
-        should be normalized the same way (i.e., with the same *offset* and/or *sigma*),
-        we must pass these values via **-N**.  However, this is inconvenient if we
-        compute these values from a grid.  Use **-Qc** to save the results of
-        *offset* and *sigma* to a statistics file; if grid output is not needed
-        for this run then do not specify **-G**. For subsequent runs, just use
-        **-Qr** to read these values.  Using **-QR** will read then delete the
-        statistics file. See TILES for more information.
+        Controls how normalization via **-N** is carried out.  When multiple 
+        grids should be normalized the same way (i.e., with the same *offset* 
+        and/or *sigma*),
+        we must pass these values via `normalize`.  However, this is 
+        inconvenient if we compute these values from a grid.  Use **c** to save 
+        the results  of *offset* and *sigma* to a statistics file; if grid 
+        output is not  needed for this run then do not specify `outgrid`. For 
+        subsequent runs,  just use **r** to read these values.  Using **R** 
+        will read then delete the statistics file. See TILES for more 
+        information.
     slopefile : str
         Name of output grid file with scalar magnitudes of gradient vectors.
-        Requires **-D** but makes **-G** optional.
+        Requires `direction` but makes `outgrid` optional.
 
     {R}
     {V}
