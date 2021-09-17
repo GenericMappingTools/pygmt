4f9ee59482eb9247e477ce988be38695849fed19
Will Schlitzer <schlitzer90@gmail.com>
2021-09-16 20:08:48 -0400

    update docstrings

--- pygmt/src/grdgradient.py
+++ pygmt/src/grdgradient.py
@@ -100,34 +100,32 @@ def grdgradient(grid, **kwargs):
         maximum output magnitude of *amp*. If *amp* is not given, default
         *amp* = 1. If *offset* is not given, it is set to the average of
         *g*. **True** yields *gn* = *amp* \* (*g* - *offset*)/max(abs(\ *g* -
-        *offset*)). **-Ne** normalizes using a cumulative Laplace
+        *offset*)). **e** normalizes using a cumulative Laplace
         distribution yielding *gn* = *amp* \* (1.0 -
         exp(sqrt(2) \* (*g* - *offset*)/ *sigma*)), where
         *sigma* is estimated using the L1 norm of (*g* - *offset*) if it is
-        not given. **-Nt** normalizes using a cumulative Cauchy distribution
+        not given. **t** normalizes using a cumulative Cauchy distribution
         yielding *gn* = (2 \* *amp* / PI) \* atan( (*g* - *offset*)/
         *sigma*) where *sigma* is estimated using the L2 norm of (*g* -
         *offset*) if it is not given. To use *offset* and/or *sigma* from a
         previous calculation, leave out the argument to the modifier(s) and
-        see **-Q** for usage.  As a final option, you may add **+a**\ *ambient*
-        to add *ambient* to all nodes after gradient calculations are 
-        completed.
+        see `norm_control` for usage.  As a final option, you may add **+a**\ 
+        *ambient* to add *ambient* to all nodes after gradient calculations 
+        are completed.
     norm_control : str
         **c**\|\ **r**\|\ **R**
-        Controls how normalization via **-N** is carried out.  When multiple 
-        grids should be normalized the same way (i.e., with the same *offset* 
-        and/or *sigma*),
+        Controls how normalization via `normalize` is carried out.  When 
+        multiple  grids should be normalized the same way (i.e., with the same 
+        *offset*  and/or *sigma*),
         we must pass these values via `normalize`.  However, this is 
-        inconvenient if we compute these values from a grid.  Use **c** to save 
-        the results  of *offset* and *sigma* to a statistics file; if grid 
-        output is not  needed for this run then do not specify `outgrid`. For 
-        subsequent runs,  just use **r** to read these values.  Using **R** 
-        will read then delete the statistics file. See TILES for more 
-        information.
+        inconvenient if we compute these values from a grid.  Use **c** to 
+        save  the results  of *offset* and *sigma* to a statistics file; if 
+        grid output is not  needed for this run then do not specify `outgrid`. 
+        For  subsequent runs,  just use **r** to read these values.  Using 
+        **R**  will read then delete the statistics file.
     slopefile : str
         Name of output grid file with scalar magnitudes of gradient vectors.
         Requires `direction` but makes `outgrid` optional.
-
     {R}
     {V}
     {n}
