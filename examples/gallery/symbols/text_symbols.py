887aaba2ab8410efa9b48b0b67e6bb4ea3c16c5d
Michael Grund <michael.grund@kit.edu>
2021-09-18 19:03:25 +0200

    formatting

--- examples/gallery/symbols/text_symbols.py
+++ examples/gallery/symbols/text_symbols.py
@@ -6,11 +6,11 @@ A text symbol can be drawn by passing **l**\ *size*\ **+t**\ *string* to
 the ``style`` parameter where *size* defines the size of the text symbol
 (note: the size is only approximate; no individual scaling is done for
 different characters) and *string* can be a letter or a text string
-(less than 256 characters). Optionally, you can append **+f**\ *font* select
-a particular font [Default is FONT_ANNOT_PRIMARY] as well as **+j**\ *justify*
-to change the justification [Default is CM]. Outline and fill color of the
-text symbols can be customized via the ``pen`` and ``color`` parameters,
-respectively.
+(less than 256 characters). Optionally, you can append **+f**\ *font* to
+select a particular font [Default is FONT_ANNOT_PRIMARY] as well as
+**+j**\ *justify* to change the justification [Default is CM]. Outline
+* and fill color of the text symbols can be customized via the ``pen``
+* and ``color`` parameters, respectively.
 """
 
 import pygmt
