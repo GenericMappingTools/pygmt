b0d287f2692b33232f93492851d37752d9ba9a21
Michael Grund <michael.grund@kit.edu>
2021-09-18 19:00:43 +0200

    add annotations

--- examples/gallery/symbols/text_symbols.py
+++ examples/gallery/symbols/text_symbols.py
@@ -20,12 +20,18 @@ fig = pygmt.Figure()
 fig.basemap(region=[0, 8, 0, 3], projection="X12c/4c", frame=True)
 
 pen = "1.5p"
+# plot an uppercase "A" of size 3.5c, color fill is set to "dodgerblue3"
 fig.plot(x=1, y=1.5, style="l3.5c+tA", color="dodgerblue3", pen=pen)
+# plot an "asterisk" of size 3.5c, color fill is set to "red3"
 fig.plot(x=2.5, y=1, style="l3.5c+t*", color="red3", pen=pen)
-fig.plot(
-    x=4, y=1.5, style="l3.5c+tZ+f12p,Courier-Bold,black", color="seagreen3", pen=pen
-)
-fig.plot(x=5.5, y=1.5, style="l3.5c+ts+f12p,Times-Italic,black", color="gold", pen=pen)
+# plot an uppercase "Z" of size 3.5c and use the "Courier-Bold" font,
+# color fill is set to "seagreen3"
+fig.plot(x=4, y=1.5, style="l3.5c+tZ+fCourier-Bold", color="seagreen3", pen=pen)
+# plot a lowercase "s" of size 3.5c and use the "Times-Italic" font,
+# color fill is set to "gold"
+fig.plot(x=5.5, y=1.5, style="l3.5c+ts+fTimes-Italic", color="gold", pen=pen)
+# plot the pi symbol (\160 is octal code for pi) of size 3.5c, for this use
+# the "Symbol" font, color fill is set to "magenta4"
 fig.plot(x=7, y=1.5, style="l3.5c+t\160+fSymbol", color="magenta4", pen=pen)
 
 fig.show()
