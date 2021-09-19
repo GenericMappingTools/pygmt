e248dcda8c4e4f313b83b4fb53b63ce587f8ed9f
Meghan Jones <meghanj@alum.mit.edu>
2021-09-18 20:38:45 -0400

    Apply suggestions from code review
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

--- pygmt/src/grdhisteq.py
+++ pygmt/src/grdhisteq.py
@@ -1,8 +1,7 @@
 """
-grdhisteq - Compute directional gradients from a grid.
+grdhisteq - Perform histogram equalization for a grid.
 """
 
-import xarray as xr
 from pygmt.clib import Session
 from pygmt.helpers import (
     GMTTempFile,
@@ -11,6 +10,7 @@ from pygmt.helpers import (
     kwargs_to_strings,
     use_alias,
 )
+from pygmt.io import load_dataarray
 
 
 @fmt_docstring
@@ -21,12 +21,45 @@ from pygmt.helpers import (
     N="gaussian",
     Q="quadratic",
     V="verbose",
-    n="interpolation",
+    h="header",
 )
 @kwargs_to_strings(R="sequence")
 def grdhisteq(grid, **kwargs):
     r"""
     Perform histogram equalization for a grid.
+
+    Allows the user to find the data values which divide a given grid file into
+    patches of equal area. One common use of **grdhisteq** is in a kind of
+    histogram equalization of an image. In this application, the user might
+    have a grid of flat topography with a mountain in the middle. Ordinary gray
+    shading of this file (using :meth:`pygmt.Figure.grdimage` or
+    :meth:`pygmt.Figure.grdview`) with a linear mapping from topography to
+    graytone will result in most of the image being very dark gray, with the
+    mountain being almost white. One could use **grdhisteq** to write to a
+    :class:`pandas.DataFrame` or ASCII file with a list of those data values
+    which divide the range of the data into *n_cells* segments, each of which
+    has an equal area in the image. Using **awk** or :meth:`pygmt.makecpt` one
+    can take this output and build a CPT; using the CPT with
+    :meth:`pygmt.Figure.grdimage` will result in an image with all levels of
+    gray occurring equally. Alternatively, see :meth:`pygmt.grd2cpt`.
+
+    The second common use of **grdhisteq** is in writing a grid with statistics
+    based on some kind of cumulative distribution function. In this
+    application, the output has relative highs and lows in the same (x,y)
+    locations as the input file, but the values are changed to reflect their
+    place in some cumulative distribution. One example would be to find the
+    lowest 10% of the data: Take a grid, run **grdhisteq** and make a grid
+    using *n_cells* = 10, and then contour the result to trace the 1 contour.
+    This will enclose the lowest 10% of the data, regardless of their original
+    values. Another example is in equalizing the output of
+    :meth:`pygmt.grdgradient`. For shading purposes it is desired that the data
+    have a smooth distribution, such as a Gaussian. If you run **grdhisteq** on
+    output from :meth:`pygmt.grdgradient` and make a grid file output with the
+    Gaussian option, you will have a grid whose values are distributed
+    according to a Gaussian distribution with zero mean and unit variance. The
+    locations of these values will correspond to the locations of the input;
+    that is, the most negative output value will be in the (x,y) location of
+    the most negative input value, and so on.
 
     Full option list at :gmt-docs:`grdhisteq.html`
 
@@ -39,11 +72,12 @@ def grdhisteq(grid, **kwargs):
     outgrid : str or None
         The name of the output netCDF file with extension .nc to store the grid
         in.
-    divisions : int or str
+    divisions : int or bool
         The number of divisions of data range to make [Default is 16].
 
     {R}
     {V}
+    {h}
 
     Returns
     -------
--- pygmt/tests/test_grdhisteq.py
+++ pygmt/tests/test_grdhisteq.py
@@ -3,9 +3,9 @@ Tests for grdhisteq.
 """
 import os
 
-import numpy.testing as npt
 import pytest
-from pygmt import grdhisteq, grdinfo
+import xarray.testing as xrt
+from pygmt import grdhisteq
 from pygmt.datasets import load_earth_relief
 from pygmt.helpers import GMTTempFile
 
@@ -20,23 +20,24 @@ def fixture_grid():
 
 def test_grdhisteq_outgrid(grid):
     """
-    Test the azimuth and direction parameters for grdhisteq with a set outgrid.
+    Test the gaussian parameter of grdhisteq with a set outgrid.
     """
     with GMTTempFile(suffix=".nc") as tmpfile:
-        result = grdhisteq(grid=grid, outgrid=tmpfile.name)
+        result = grdhisteq(grid=grid, gaussian=True, outgrid=tmpfile.name)
         assert result is None  # return value is None
         assert os.path.exists(path=tmpfile.name)  # check that outgrid exists
-        result = (
-            grdinfo(grid=tmpfile.name, force_scan="a", per_column="n").strip().split()
-        )
 
 
 def test_grdhisteq_no_outgrid(grid):
     """
-    Test the azimuth and direction parameters for grdhisteq with no set
-    outgrid.
+    Test the quadratic and region parameters for grdhisteq with no set outgrid.
     """
-    temp_grid = grdhisteq(grid=grid)
-    assert temp_grid.dims == ("lat", "lon")
+    temp_grid = grdhisteq(grid=grid, quadratic=True, region=[-3, 1, 2, 5])
     assert temp_grid.gmt.gtype == 1  # Geographic grid
     assert temp_grid.gmt.registration == 1  # Pixel registration
+    expected_grid = xr.DataArray(
+        data=[[4.0, 0.0, 8.0, 11.0], [13.0, 4.0, 8.0, 13.0], [15.0, 15.0, 15.0, 15.0]],
+        coords=dict(lon=[-2.5, -1.5, -0.5, 0.5], lat=[2.5, 3.5, 4.5]),
+        dims=["lat", "lon"],
+    )
+    xrt.assert_allclose(a=temp_grid, b=expected_grid)
