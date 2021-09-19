556dc47c6a617a7627725e60ce9c7336dc503857
Meghan Jones <meghanj@alum.mit.edu>
2021-09-18 21:03:36 -0400

    Add more code supporting table or grid output

--- pygmt/src/grdhisteq.py
+++ pygmt/src/grdhisteq.py
@@ -92,9 +92,14 @@ def grdhisteq(grid, **kwargs):
         with Session() as lib:
             file_context = lib.virtualfile_from_data(check_kind="raster", data=grid)
             with file_context as infile:
-                if "G" not in kwargs.keys():  # if outgrid is unset, output to tempfile
-                    kwargs.update({"G": tmpfile.name})
-                outgrid = kwargs["G"]
+                if "G" not in kwargs:  # table output if outgrid is unset
+                    kwargs.update({">": tmpfile.name})
+                else:  # NetCDF or xarray.DataArray output if outgrid is set
+                    if (
+                        kwargs["G"] is True
+                    ):  # xarray.DataArray output if outgrid is True
+                        kwargs.update({"G": tmpfile.name})
+                    outgrid = kwargs["G"]
                 arg_str = " ".join([infile, build_arg_string(kwargs)])
                 lib.call_module("grdhisteq", arg_str)
 
