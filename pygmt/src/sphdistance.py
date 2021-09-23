663805fceff1b8a8a223881671c976b064befd07
Will Schlitzer <schlitzer90@gmail.com>
2021-09-23 08:34:45 +0100

    run make format

--- pygmt/src/sphdistance.py
+++ pygmt/src/sphdistance.py
@@ -50,7 +50,7 @@ def sphdistance(data, **kwargs):
     -------
     ret: xarray.DataArray or None
         Return type depends on whether the ``outgrid`` parameter is set:
-        
+
         - :class:`xarray.DataArray` if ``outgrid`` is not set
         - None if ``outgrid`` is set (grid output will be stored in file set by
           ``outgrid``)
