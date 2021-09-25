d43419522640478754cf1ada8bad34042dfe2ad7
Will Schlitzer <schlitzer90@gmail.com>
2021-09-25 07:23:10 +0100

    add test for xy inputs

--- pygmt/tests/test_sphdistance.py
+++ pygmt/tests/test_sphdistance.py
@@ -20,6 +20,22 @@ def fixture_table():
     return np.array(coords_list)
 
 
+def test_sphdistance_xy_inputs():
+    """
+    Test inputs using separate xy arguments.
+    """
+    y = [22.3, 22.6, 22.4, 23.3]
+    x = [85.5, 82.3, 85.8, 86.5]
+    temp_grid = sphdistance(x=x, y=y, spacing=[1, 2], region=[82, 87, 22, 24])
+    assert temp_grid.dims == ("lat", "lon")
+    assert temp_grid.gmt.gtype == 1  # Geographic grid
+    assert temp_grid.gmt.registration == 0  # Gridline registration
+    npt.assert_allclose(temp_grid.max(), 232977.546875)
+    npt.assert_allclose(temp_grid.min(), 0)
+    npt.assert_allclose(temp_grid.median(), 0)
+    npt.assert_allclose(temp_grid.mean(), 62469.17)
+
+
 def test_sphdistance_outgrid(array):
     """
     Test sphdistance with a set outgrid.
