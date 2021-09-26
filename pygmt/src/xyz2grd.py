160157ff75d046fbfa0952e9e298210d72fdc664
Will Schlitzer <schlitzer90@gmail.com>
2021-09-26 11:01:37 +0100

    replace "onecolumn" to "flags"

--- pygmt/src/xyz2grd.py
+++ pygmt/src/xyz2grd.py
@@ -19,7 +19,7 @@ from pygmt.io import load_dataarray
     I="spacing",
     R="region",
     V="verbose",
-    Z="onecolumn",
+    Z="flags",
     r="registration",
 )
 @kwargs_to_strings(R="sequence")
@@ -63,7 +63,7 @@ def xyz2grd(table, **kwargs):
     {I}
     {R}
     {V}
-    onecolumn : str
+    flags : str
         [*flags*]
         Read a 1-column ASCII [or binary] table. This assumes that all the
         nodes are present and sorted according to specified ordering
