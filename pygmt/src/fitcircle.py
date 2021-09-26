7c1ae68443d772cffb77a09e93587fcf3a91420c
Will Schlitzer <schlitzer90@gmail.com>
2021-09-26 11:30:04 +0100

    add fitcircle.py

---
+++ pygmt/src/fitcircle.py
@@ -0,0 +1,102 @@
+"""
+filter1d - Time domain filtering of 1-D data tables
+"""
+import warnings
+
+import pandas as pd
+import xarray as xr
+from pygmt.clib import Session
+from pygmt.exceptions import GMTInvalidInput
+from pygmt.helpers import (
+    GMTTempFile,
+    build_arg_string,
+    fmt_docstring,
+    kwargs_to_strings,
+    use_alias,
+)
+
+
+@fmt_docstring
+@use_alias(
+    L="normalize",
+    S="small_circle",
+    V="verbose",
+)
+def fitcircle(data, output_type="pandas", outfile=None, **kwargs):
+    r"""
+    Fit coordinators to create vectors on a sphere.
+
+    **fitcircle** reads lon,lat [or lat,lon] values from the first two
+    columns on standard input [or *table*]. These are converted to
+    Cartesian three-vectors on the unit sphere. Then two locations are
+    found: the mean of the input positions, and the pole to the great circle
+    which best fits the input positions. The user may choose one or both of
+    two possible solutions to this problem. The first is called **-L1** and
+    the second is called **-L2**. When the data are closely grouped along a
+    great circle both solutions are similar. If the data have large
+    dispersion, the pole to the great circle will be less well determined
+    than the mean. Compare both solutions as a qualitative check.
+
+    The **-L1** solution is so called because it approximates the
+    minimization of the sum of absolute values of cosines of angular
+    distances. This solution finds the mean position as the Fisher average
+    of the data, and the pole position as the Fisher average of the
+    cross-products between the mean and the data. Averaging cross-products
+    gives weight to points in proportion to their distance from the mean,
+    analogous to the "leverage" of distant points in linear regression in the plane.
+
+    The **-L2** solution is so called because it approximates the
+    minimization of the sum of squares of cosines of angular distances. It
+    creates a 3 by 3 matrix of sums of squares of components of the data
+    vectors. The eigenvectors of this matrix give the mean and pole
+    locations. This method may be more subject to roundoff errors when there
+    are thousands of data. The pole is given by the eigenvector
+    corresponding to the smallest eigenvalue; it is the least-well
+    represented factor in the data and is not easily estimated by either method.
+
+    Full option list at :gmt-docs:`fitcircle.html`
+
+    {aliases}
+
+    Parameters
+    ----------.
+    output_type : str
+        Determine the format the xyz data will be returned in [Default is
+        ``pandas``]:
+
+            - ``numpy`` - :class:`numpy.ndarray`
+            - ``pandas``- :class:`pandas.DataFrame`
+            - ``file`` - ASCII file (requires ``outfile``)
+    outfile : str
+        The file name for the output ASCII file.
+
+    Returns
+    -------
+    ret : pandas.DataFrame or numpy.ndarray or None
+        Return type depends on ``outfile`` and ``output_type``:
+
+        - None if ``outfile`` is set (output will be stored in file set by
+          ``outfile``)
+        - :class:`pandas.DataFrame` or :class:`numpy.ndarray` if ``outfile`` is
+          not set (depends on ``output_type`` [Default is
+          :class:`pandas.DataFrame`])
+
+    """
+    with GMTTempFile() as tmpfile:
+        with Session() as lib:
+            file_context = lib.virtualfile_from_data(check_kind="vector", data=data)
+            with file_context as infile:
+                if outfile is None:
+                    outfile = tmpfile.name
+                arg_str = " ".join([infile, build_arg_string(kwargs), "->" + outfile])
+                lib.call_module("fitcircle", arg_str)
+
+        # Read temporary csv output to a pandas table
+        if outfile == tmpfile.name:  # if user did not set outfile, return pd.DataFrame
+            result = pd.read_csv(tmpfile.name, sep="\t", comment=">")
+        elif outfile != tmpfile.name:  # return None if outfile set, output in outfile
+            result = None
+
+        if output_type == "numpy":
+            result = result.to_numpy()
+    return result
