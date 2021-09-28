7e2dfeeff1c3bc8b756812ece351a97cd192a458
Will Schlitzer <schlitzer90@gmail.com>
2021-09-28 09:00:22 +0100

    add test_fitcircle

---
+++ pygmt/tests/test_fitcircle.py
@@ -0,0 +1,23 @@
+"""
+Tests for fitcircle.
+"""
+
+import os
+
+import numpy as np
+import pandas as pd
+import pytest
+from pygmt import fitcircle
+from pygmt.exceptions import GMTInvalidInput
+from pygmt.helpers import GMTTempFile
+from pygmt.src import which
+
+
+@pytest.fixture(scope="module", name="data")
+def fixture_table():
+    """
+    Load the sample data from the sat_03 remote file.
+    """
+    fname = which("@sat_03.txt", download="c")
+    data = pd.read_csv(fname, header=None, skiprows=1, sep="\t",)
+    return data
