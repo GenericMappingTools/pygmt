34814c4a9fee5fa1f8d3489e894c81a35fcd0f60
Meghan Jones <meghanj@alum.mit.edu>
2021-10-05 17:01:00 -0400

    Check output values in surface tests

--- pygmt/tests/test_surface.py
+++ pygmt/tests/test_surface.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import pytest
 import xarray as xr
+from numpy import testing as npt
 from pygmt import surface, which
 from pygmt.exceptions import GMTInvalidInput
 from pygmt.helpers import GMTTempFile, data_kind
@@ -22,17 +23,33 @@ def fixture_fname():
 @pytest.fixture(scope="module", name="data")
 def fixture_data(fname):
     """
-    Load Table 5.11 in Davis: Statistics and Data Analysis in Geology, J.
-    Wiley
+    Load Table 5.11 in Davis: Statistics and Data Analysis in Geology.
     """
     return pd.read_csv(fname, sep=r"\s+", header=None, names=["x", "y", "z"])
+
 
 @pytest.fixture(scope="module", name="region")
 def fixture_region():
     """
     Define the data region.
     """
-    return [0,  6.5, -0.2, 6.5]
+    return [-0.2, 6.6, -0.2, 6.2]
+
+
+def check_values(grid):
+    """
+    Check the attributes and values of the DataArray returned by surface.
+    """
+    assert isinstance(grid, xr.DataArray)
+    assert grid.gmt.registration == 0  # Gridline registration
+    assert grid.gmt.gtype == 0  # Cartesian type
+    assert grid.coords["y"].data.min() == -0.2
+    assert grid.coords["y"].data.max() == 6.2
+    assert grid.coords["x"].data.min() == -0.2
+    assert grid.coords["x"].data.max() == 6.6
+    npt.assert_allclose(grid[0, 0].values, 1011.1221)
+    assert grid.sizes["y"] == 33
+    assert grid.sizes["x"] == 35
 
 
 def test_surface_input_file(fname, region):
@@ -40,9 +57,7 @@ def test_surface_input_file(fname, region):
     Run surface by passing in a filename.
     """
     output = surface(data=fname, spacing="0.2", region=region)
-    assert isinstance(output, xr.DataArray)
-    assert output.gmt.registration == 0  # Gridline registration
-    assert output.gmt.gtype == 0  # Cartesian type
+    check_values(output)
 
 
 def test_surface_input_data_array(data, region):
@@ -51,7 +66,7 @@ def test_surface_input_data_array(data, region):
     """
     data = data.values  # convert pandas.DataFrame to numpy.ndarray
     output = surface(data=data, spacing="0.2", region=region)
-    assert isinstance(output, xr.DataArray)
+    check_values(output)
 
 
 def test_surface_input_xyz(data, region):
@@ -65,7 +80,7 @@ def test_surface_input_xyz(data, region):
         spacing="0.2",
         region=region,
     )
-    assert isinstance(output, xr.DataArray)
+    check_values(output)
 
 
 def test_surface_wrong_kind_of_input(data, region):
@@ -84,13 +99,11 @@ def test_surface_with_outgrid_param(data, region):
     """
     data = data.values  # convert pandas.DataFrame to numpy.ndarray
     with GMTTempFile(suffix=".nc") as tmpfile:
-        output = surface(
-            data=data, spacing="0.2", region=region, outgrid=tmpfile.name
-        )
+        output = surface(data=data, spacing="0.2", region=region, outgrid=tmpfile.name)
         assert output is None  # check that output is None since outgrid is set
         assert os.path.exists(path=tmpfile.name)  # check that outgrid exists at path
         with xr.open_dataarray(tmpfile.name) as grid:
-            assert isinstance(grid, xr.DataArray)  # ensure netcdf grid loads ok
+            check_values(grid)
 
 
 def test_surface_deprecate_outfile_to_outgrid(data, region):
@@ -106,9 +119,8 @@ def test_surface_deprecate_outfile_to_outgrid(data, region):
             )
             assert output is None  # check that output is None since outfile is set
             assert os.path.exists(path=tmpfile.name)  # check that file exists at path
-
             with xr.open_dataarray(tmpfile.name) as grid:
-                assert isinstance(grid, xr.DataArray)  # ensure netcdf grid loads ok
+                check_values(grid)
         assert len(record) == 1  # check that only one warning was raised
 
 
@@ -122,9 +134,7 @@ def test_surface_short_aliases(data, region):
         with GMTTempFile(suffix=".nc") as tmpfile:
             output = surface(data=data, I="0.2", R=region, G=tmpfile.name)
             assert output is None  # check that output is None since outgrid is set
-            assert os.path.exists(
-                path=tmpfile.name
-            )  # check that outgrid exists at path
+            assert os.path.exists(path=tmpfile.name)  # check that file exists at path
             with xr.open_dataarray(tmpfile.name) as grid:
-                assert isinstance(grid, xr.DataArray)  # ensure netcdf grid loads ok
+                check_values(grid)
         assert len(record) == 3
