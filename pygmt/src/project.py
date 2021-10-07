8345f2a63c38a5af9ebaf176030de55c58f2b23e
Meghan Jones <meghanj@alum.mit.edu>
2021-10-07 14:18:10 -0400

    Format math in docstring

--- pygmt/src/project.py
+++ pygmt/src/project.py
@@ -33,18 +33,20 @@ from pygmt.helpers import (
 )
 @kwargs_to_strings(E="sequence", L="sequence", T="sequence", W="sequence")
 def project(points, center, outfile=None, **kwargs):
-    """
+    r"""
     Project data onto lines or great circles, or generate tracks.
 
-    Project reads arbitrary (x, y [, z]) data and returns any combination of
-    (x, y, z, p, q, r, s), where (p, q) are the coordinates in the projection,
-    (r, s) is the position in the (x, y) coordinate system of the point on the
-    profile (q = 0 path) closest to (x, y), and z is all remaining columns in
-    the input (beyond the required x and y columns).
+    Project reads arbitrary :math:`(x, y [, z])` data and returns any
+    combination of :math:`(x, y, z, p, q, r, s)`, where :math:`(p, q)` are the
+    coordinates in the projection, :math:`(r, s)` is the position in the
+    :math:`(x, y)` coordinate system of the point on the profile (:math:`q = 0`
+    path) closest to :math:`(x, y)`, and :math:`z` is all remaining columns in
+    the input (beyond the required :math:`x` and :math:`y` columns).
 
-    Alternatively, Project may be used to generate (r, s, p) triples at equal
-    increments ``dist`` along a profile. In this case (``generate`` option),
-    the value of ``points`` is ignored (you can use, e.g., ``points=None``).
+    Alternatively, :doc:`pygmt.project` may be used to generate
+    :math:`(r, s, p)` triples at equal increments along a profile using the
+    ``generate`` parameter. In this case, the value of ``points`` is ignored
+    (you can use, e.g., ``points=None``).
 
     Projections are defined in any (but only) one of three ways:
 
@@ -54,25 +56,26 @@ def project(points, center, outfile=None, **kwargs):
 
     To spherically project data along a great circle path, an oblique
     coordinate system is created which has its equator along that path, and the
-    zero meridian through the Center. Then the oblique longitude (p)
+    zero meridian through the Center. Then the oblique longitude (:math:`p`)
     corresponds to the distance from the Center along the great circle, and the
-    oblique latitude (q) corresponds to the distance perpendicular to the great
-    circle path. When moving in the increasing (p) direction, (toward B or in
-    the azimuth direction), the positive (q) direction is to your left. If a
-    Pole has been specified, then the positive (q) direction is toward the
-    pole.
+    oblique latitude (:math:`q`) corresponds to the distance perpendicular to
+    the great circle path. When moving in the increasing (:math:`p`) direction,
+    (toward B or in the azimuth direction), the positive (:math:`q`) direction
+    is to your left. If a Pole has been specified, then the positive
+    (:math:`q`) direction is toward the pole.
 
     To specify an oblique projection, use the ``rotationpole`` option to set
     the pole. Then the equator of the projection is already determined and the
-    ``center`` option is used to locate the p = 0 meridian. The center cx/cy
-    will be taken as a point through which the p = 0 meridian passes. If you do
-    not care to choose a particular point, use the South pole (ox = 0, oy =
-    -90).
+    ``center`` option is used to locate the :math:`p = 0` meridian. The center
+    *cx/cy* will be taken as a point through which the :math:`p = 0` meridian
+    passes. If you do not care to choose a particular point, use the South pole
+    (*cx* = 0, *cy* = -90).
 
     Data can be selectively windowed by using the ``length`` and ``width``
     options. If ``width`` is used, the projection width is set to use only
-    points with w_min < q < w_max. If ``length`` is set, then the length is set
-    to use only those points with l_min < p < l_max. If the ``endpoint`` option
+    points with :math:`w_{{min}} < q < w_{{max}}`. If ``length`` is set, then
+    the length is set to use only those points with
+    :math:`l_{{min}} < p < l_{{max}}`. If the ``endpoint`` option
     has been used to define the projection, then ``length="w"`` may be used to
     window the length of the projection to exactly the span from O to B.
 
@@ -81,33 +84,34 @@ def project(points, center, outfile=None, **kwargs):
     y axis), NOT the usual cartesian theta, which is counterclockwise from the
     x axis. azimuth = 90 - theta.
 
-    No assumptions are made regarding the units for x, y, r, s, p, q, dist,
-    l_min, l_max, w_min, w_max. If -Q is selected, map units are assumed and x,
-    y, r, s must be in degrees and p, q, dist, l_min, l_max, w_min, w_max will
-    be in km.
+    No assumptions are made regarding the units for
+    :math:`x, y, r, s, p, q, dist, l_{{min}}, l_{{max}}, w_{{min}}, w_{{max}}`.
+    If -Q is selected, map units are assumed and :math:`x, y, r, s` must be in
+    degrees and :math:`p, q, dist, l_{{min}}, l_{{max}}, w_{{min}}, w_{{max}}`
+    will be in km.
 
     Calculations of specific great-circle and geodesic distances or for
     back-azimuths or azimuths are better done using :gmt-docs:`mapproject` as
     project is strictly spherical.
 
-    Project is CASE SENSITIVE: use lower case for the **xyzpqrs** letters
-    in ``flags``.
+    :doc:`pygmt.project` is case sensitive: use lower case for the
+    **xyzpqrs** letters in ``flags``.
 
     {aliases}
 
     Parameters
     ----------
     points : pandas.DataFrame or str
-        Either a table with (x, y) or (lon, lat) values in the first two
-        columns, or a filename (e.g. csv, txt format). More columns may be
+        Either a table with :math:`(x, y)` or (lon, lat) values in the first
+        two columns, or a filename (e.g. csv, txt format). More columns may be
         present.
 
     center : str or list
         *cx*/*cy*.
         *cx/cy* sets the origin of the projection, in Definition 1 or 2. If
         Definition 3 is used, then *cx/cy* are the coordinates of a
-        point through which the oblique zero meridian (*p* = 0) should pass.
-        The *cx/cy* is not required to be 90 degrees from the pole.
+        point through which the oblique zero meridian (:math:`p = 0`) should
+        pass. The *cx/cy* is not required to be 90 degrees from the pole.
 
     azimuth : float or str
         defines the azimuth of the projection (Definition 1).
@@ -128,38 +132,39 @@ def project(points, center, outfile=None, **kwargs):
         **rsp** and ``flags`` is not allowed.
 
     generate : str
-        *dist* [/*colat*][**+c**|**h**].
+        *dist* [/*colat*][**+c**\|\ **h**].
         Generate mode. No input is read and the value of ``points`` is ignored
-        (you can use, e.g., ``points=None``). Create (*r*, *s*, *p*) output
-        points every *dist* units of *p*. See `maptypeunits` option.
+        (you can use, e.g., ``points=None``). Create :math:`(r, s, p)` output
+        points every *dist* units of :math:`p`. See `maptypeunits` option.
         Alternatively, append */colat* for a small circle instead [Default is a
         colatitude of 90, i.e., a great circle]. If setting a pole with
         ``rotationpole`` and you want the small circle to go through *cx*/*cy*,
         append **+c** to compute the required colatitude. Use ``center`` and
         ``endpoint`` to generate a circle that goes through the center and end
         point. Note, in this case the center and end point cannot be farther
-        apart than 2*abs(*colat*). Finally, if you append **+h** then we will
-        report the position of the pole as part of the segment header [no
-        header].
+        apart than :math:`2|\mbox{{colat}}|`. Finally, if you append **+h** then
+        we will report the position of the pole as part of the segment header
+        [Default is no header].
 
     length : str or list
-        [**w**| *l_min*/*l_max*].
+        [**w**\|\ *l_min*/*l_max*].
         Length controls. Project only those points whose *p* coordinate is
-        within *l_min* < *p* < *l_max*. If ``endpoint`` has been set, then you
-        may alternatively use **w** to stay within the distance from ``center``
-        to ``endpoint``.
+        within :math:`l_{{min}} < p < l_{{max}}`. If ``endpoint`` has been set,
+        then you may alternatively use **w** to stay within the distance from
+        ``center`` to ``endpoint``.
 
     flatearth : bool
         If `True`, Make a Cartesian coordinate transformation in the plane.
         [Default uses spherical trigonometry.]
 
     maptypeunits : bool
-        If `True`, project assumes *x*, *y*, *r*, *s* are in degrees while *p*,
-        *q*, *dist*, *l_min*, *l_max*, *w_min*, *w_max* are in km. If not set
-        (or `False`), then all these are assumed to be in the same units.
+        If `True`, project assumes :math:`x, y, r, s` are in degrees while
+        :math:`p, q, dist, l_{{min}}, l_{{max}}, w_{{min}}, {{w_max}}` are in
+        km. If not set (or ``False``), then all these are assumed to be in the
+        same units.
 
     sort : bool
-        Sort the output into increasing *p* order. Useful when projecting
+        Sort the output into increasing :math:`p` order. Useful when projecting
         random data into a sequential profile.
 
     rotationpole : str or list
@@ -171,11 +176,11 @@ def project(points, center, outfile=None, **kwargs):
 
     width : str or list
         *w_min*/*w_max*
-        Width controls. Project only those points whose *q* coordinate is
-        within *w_min* < *q* < *w_max*.
+        Width controls. Project only those points whose :math:`q` coordinate is
+        within :math:`w_{{min}} < q < w_{{max}}`.
 
     ellipse : str
-        *major*/*minor*/*azimuth* [**+e** | **n**].
+        *major*/*minor*/*azimuth* [**+e**\|\ **n**].
         Used in conjunction with ``center`` (sets its center) and ``generate``
         (sets the distance increment) to create the coordinates of an ellipse
         with *major* and *minor* axes given in km (unless ``flatearth`` is
@@ -186,8 +191,8 @@ def project(points, center, outfile=None, **kwargs):
         a specific number of unique equidistant points via ``generate``. For
         degenerate ellipses you can just supply a single *diameter* instead.  A
         geographic diameter may be specified in any desired unit other than km
-        [Default] by appending the unit (e.g., 3d for degrees); if so we assume
-        the increment is also given in the same unit.  **Note**:
+        by appending the unit (e.g., 3d for degrees) [Default is km]; if so we
+        assume the increment is also given in the same unit.  **Note**:
         For the Cartesian ellipse (which requires ``flatearth``), we expect
         *direction* counter-clockwise from the horizontal instead of an
         *azimuth*.
