241a3331175a074f24feb9229c4808ab1e8d2edd
Will Schlitzer <schlitzer90@gmail.com>
2021-10-11 17:13:42 +0100

    run make format

--- pygmt/datasets/__init__.py
+++ pygmt/datasets/__init__.py
@@ -2,6 +2,7 @@
 #
 # Load sample data included with GMT (downloaded from the GMT cache server).
 
+from pygmt.datasets.earth_age import load_earth_age
 from pygmt.datasets.earth_relief import load_earth_relief
 from pygmt.datasets.samples import (
     load_fractures_compilation,
@@ -12,4 +13,3 @@ from pygmt.datasets.samples import (
     load_sample_bathymetry,
     load_usgs_quakes,
 )
-from pygmt.datasets.earth_age import load_earth_age
