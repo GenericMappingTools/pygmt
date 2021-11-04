b1a8c4b9291d68d094c5838cf0eb06263760a239
Michael Grund <michael.grund@kit.edu>
2021-11-04 17:40:05 +0100

    use 'summarize' as alias

--- pygmt/src/blockm.py
+++ pygmt/src/blockm.py
@@ -70,7 +70,7 @@ def _blockm(block_method, data, x, y, z, outfile, **kwargs):
 @use_alias(
     I="spacing",
     R="region",
-    S="restype",
+    S="summarize",
     V="verbose",
     a="aspatial",
     b="binary",
@@ -112,12 +112,12 @@ def blockmean(data=None, x=None, y=None, z=None, outfile=None, **kwargs):
 
     {I}
 
-    restype: str
+    summarize: str
         [**m**\|\ **n**\|\ **s**\|\ **w**].
-        Result type calculated by blockmean. Use **n** to report the number of
-        input points inside each block, **s** to report the sum of all z-values
-        inside a block, **w** to report the sum of weights [Default or **m**
-        reports mean value].
+        Type of summary values calculated by blockmean. Use **n** to report the
+        number of input points inside each block, **s** to report the sum of
+        all z-values inside a block, **w** to report the sum of weights
+        [Default or **m** reports mean value].
 
     {R}
 
