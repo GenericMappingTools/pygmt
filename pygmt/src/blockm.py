708e4b0733afe625c524d2e8e8e78953dae17a64
Michael Grund <michael.grund@kit.edu>
2021-11-07 15:03:39 +0100

    some formatting

--- pygmt/src/blockm.py
+++ pygmt/src/blockm.py
@@ -114,7 +114,7 @@ def blockmean(data=None, x=None, y=None, z=None, outfile=None, **kwargs):
 
     summary: str
         [**m**\|\ **n**\|\ **s**\|\ **w**].
-        Type of summary values calculated by blockmean. 
+        Type of summary values calculated by blockmean.
         - **n**: report the number of input points inside each block
         - **s**: report the sum of all z-values inside a block
         - **w**: report the sum of weights
