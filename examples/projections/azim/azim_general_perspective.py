df0b77f8ecaddfd12d638c9b6631515a0b1c915f
Michael Grund <michael.grund@kit.edu>
2021-12-14 13:14:48 +0100

    update docstring

--- examples/projections/azim/azim_general_perspective.py
+++ examples/projections/azim/azim_general_perspective.py
@@ -6,22 +6,22 @@ The general perspective projection imitates the view of the Earth from a finite
 point in space. In a full view of the earth one third of its surface area can
 be seen.
 
-**g**\ *lon0/lat0*\ */altitude*\ */scale*\ [**+a**\ *azimuth*]\
+**g**\ *lon0/lat0*\ */scale*\ [**+a**\ *azimuth*]\
 [**+t**\ *tilt*]\ [**+v**\ *vwidth/vheight*]\ [**+w**\ *twist*]\
-[**+z**\ *altitude*] or **G**\ *lon0/lat0*\ */altitude*\ */width*\
+[**+z**\ *altitude*] or **G**\ *lon0/lat0*\ */width*\
 [**+a**\ *azimuth*]\ [**+t**\ *tilt*]\ [**+v**\ *vwidth/vheight*]\
 [**+w**\ *twist*]\ [**+z**\ *altitude*]
 
-The projection type is set with **g** or **G**.
-*lon0/lat0* specifies the projection center and *altitude* sets the height
-in km of the viewpoint above local sea level (If altitude is less than 10,
-then it is the distance from the center of the earth to the viewpoint in earth
-radii). With *azimuth* the direction (in degrees) in which you are looking is
-specified, measured clockwise from north. *tilt* is given in degrees and is the
-viewing angle relative to zenith. A tilt of 0° is looking straight down, 60° is
-looking 30° above horizon. *twist* is the clockwise rotation of the image (in
-degrees). *Width* and *Height* describe the viewport angle in degrees, and
-*scale* or *width* determine the size of the figure.
+The projection type is set with **g** or **G**. *lon0/lat0* specifies the
+projection center and *scale* or *width* determine the size of the figure.
+With **+a**\ *azimuth* the direction (in degrees) in which you are looking is
+specified, measured clockwise from north. **+t**\ *tilt* is given in degrees
+and is the viewing angle relative to zenith. A tilt of 0° is looking straight
+down, 60° is looking 30° above horizon. The viewport angle in degrees is
+described via **+v**\ *vwidth/vheight* and **+w**\ *twist* is the clockwise
+rotation of the image (in degrees). **+z**\ *altitude* sets the height in km
+of the viewpoint above local sea level (If altitude is less than 10, then it is
+the distance from the center of the earth to the viewpoint in earth radii).
 
 The example shows the coast of Northern Europe viewed from 250 km above sea
 level looking 30° from north at a tilt of 45°. The height and width of the
