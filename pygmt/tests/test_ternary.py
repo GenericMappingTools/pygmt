539961c500263fc5edcc6b29d14f842d9307076f
Will Schlitzer <schlitzer90@gmail.com>
2021-12-17 19:04:51 +0000

    add test_ternary.py

---
+++ pygmt/tests/test_ternary.py
@@ -0,0 +1,22 @@
+"""
+Tests for pygmt.ternary.
+"""
+import pytest
+from pygmt import Figure, makecpt
+
+@pytest.mark.mpl_image_compare
+def test_coast_region():
+    """
+    Test plotting a regional map with coastlines.
+    """
+    fig = Figure()
+    makecpt(cmap="turbo", series=[0, 80, 10])
+    fig.ternary(table="@ternary.txt",
+                region="0/100/0/100/0/100",
+                cmap=True,
+                projection="X6i",
+                frame=["bafg+lAir",
+                       "cafg+lLimestone",
+                       "aafg+lWater"],
+                style="c0.1c")
+    return fig
