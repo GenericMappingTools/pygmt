6c8ae6ab4b8333822a5052a2d40328fccbd154b8
Will Schlitzer <schlitzer90@gmail.com>
2021-12-20 22:01:16 +0000

    add test in test_grdsample.py; switch to use xarray

--- pygmt/tests/test_grdsample.py
+++ pygmt/tests/test_grdsample.py
@@ -4,7 +4,8 @@ Tests for grdsample.
 import os
 
 import pytest
-from pygmt import grdinfo, grdsample
+import xarray as xr
+from pygmt import grdinfo, grdsample, load_dataarray
 from pygmt.datasets import load_earth_relief
 from pygmt.helpers import GMTTempFile
 
@@ -15,24 +16,67 @@ def fixture_grid():
     Load the grid data from the sample earth_relief file.
     """
     return load_earth_relief(
-        resolution="01d", region=[-5, 5, -5, 5], registration="pixel"
+        resolution="01d", registration="pixel", region=[124, 130, -25, -20]
     )
 
 
-def test_grdsample_file_out(grid):
+@pytest.fixture(scope="module", name="expected_grid")
+def fixture_grid_result():
     """
-    grdsample with an outgrid set and the spacing is changed.
+    Load the expected grdsample grid result.
+    """
+    return xr.DataArray(
+        data=[
+            [433.65625, 447.96875, 540.0625],
+            [397.28125, 386.15625, 484.125],
+            [334.96875, 417.25, 385.1875],
+            [303.40625, 373.625, 395.0],
+            [311.15625, 321.15625, 389.875],
+        ],
+        coords=dict(
+            lon=[
+                125.0,
+                127.0,
+                129.0,
+            ],
+            lat=[
+                -24.5,
+                -23.5,
+                -22.5,
+                -21.5,
+                -20.5,
+            ],
+        ),
+        dims=["lat", "lon"],
+    )
+
+
+def test_grdsample_file_out(grid, expected_grid):
+    """
+    Test grdsample with an outgrid set and the spacing is changed.
     """
     with GMTTempFile(suffix=".nc") as tmpfile:
-        result = grdsample(grid=grid, outgrid=tmpfile.name, spacing=[1, 0.5])
+        result = grdsample(grid=grid, outgrid=tmpfile.name, spacing=[2, 1])
         assert result is None  # return value is None
         assert os.path.exists(path=tmpfile.name)  # check that outgrid exists
-        result = grdinfo(tmpfile.name, per_column=True).strip().split()
-        assert float(result[6]) == 1  # x-increment
-        assert float(result[7]) == 0.5  # y-increment
+        temp_grid = load_dataarray(tmpfile.name)
+        xr.testing.assert_allclose(a=temp_grid, b=expected_grid)
 
 
-def test_grdsample_no_outgrid(grid):
+def test_grdsample_dataarray_out(grid, expected_grid):
+    """
+    Test grdsample with no outgrid set and the spacing is changed.
+    """
+    result = grdsample(grid=grid, spacing=[2, 1])
+    # check information of the output grid
+    assert isinstance(result, xr.DataArray)
+    assert result.gmt.gtype == 1  # Geographic grid
+    assert result.gmt.registration == 1  # Pixel registration
+    # check information of the output grid
+    xr.testing.assert_allclose(a=result, b=expected_grid)
+
+
+def test_grdsample_registration_changes(grid):
     """
     Test grdsample with no set outgrid and applying registration changes.
     """
