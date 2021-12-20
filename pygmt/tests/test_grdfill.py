649411bc1d37f8fabff9520c0ff13ff03ff045fb
Will Schlitzer <schlitzer90@gmail.com>
2021-12-20 20:22:03 +0000

    change region in test_grdfill.py to use land coordinates over smaller region

--- pygmt/tests/test_grdfill.py
+++ pygmt/tests/test_grdfill.py
@@ -18,143 +18,28 @@ def fixture_grid():
     Load the grid data from the sample earth_relief file and set value(s) to
     NaN.
     """
-    grid = load_earth_relief(registration="pixel", region=[-5, 5, -5, 5])
-    grid[3:5, 3:5] = np.nan
-    grid[5:7, 5:7] = np.inf
+    grid = load_earth_relief(registration="pixel", region=[125, 130, -25, -20])
+    grid[2:4, 1:3] = np.nan
+    grid[0:2, 2:4] = np.inf
     return grid
 
 
 @pytest.fixture(scope="module", name="expected_grid")
 def fixture_grid_result():
     """
-    Load the expected grdcut grid result.
+    Load the expected grdfill grid result.
     """
     return xr.DataArray(
         data=[
-            [
-                -4560.0,
-                -4421.0,
-                -4341.0,
-                -4664.0,
-                -4578.0,
-                -4642.0,
-                -4796.0,
-                -5001.5,
-                -4578.0,
-                -4678.0,
-            ],
-            [
-                -4684.5,
-                -4759.5,
-                -4615.0,
-                -4389.5,
-                -4034.0,
-                -4130.0,
-                -3763.5,
-                -4578.0,
-                -4262.5,
-                -4667.0,
-            ],
-            [
-                -4875.0,
-                -4867.0,
-                -4762.0,
-                -4788.0,
-                -4680.5,
-                -4545.0,
-                -4377.0,
-                -4095.5,
-                -3960.0,
-                -4166.0,
-            ],
-            [
-                -4988.0,
-                -5068.0,
-                -4964.5,
-                20.0,
-                20.0,
-                -4396.5,
-                -4469.5,
-                -4305.5,
-                -4111.5,
-                -3679.0,
-            ],
-            [
-                -5115.5,
-                -5130.5,
-                -5019.5,
-                20.0,
-                20.0,
-                -4747.5,
-                -4431.5,
-                -4176.0,
-                -4169.5,
-                -3992.0,
-            ],
-            [
-                -5095.5,
-                -5124.0,
-                -5102.5,
-                -5040.0,
-                -4967.0,
-                np.inf,
-                np.inf,
-                -4422.0,
-                -4213.0,
-                -3966.5,
-            ],
-            [
-                -5074.0,
-                -5099.5,
-                -5122.0,
-                -5059.5,
-                -4984.5,
-                np.inf,
-                np.inf,
-                -4452.0,
-                -4208.0,
-                -3865.5,
-            ],
-            [
-                -4760.5,
-                -4895.0,
-                -5069.5,
-                -5105.0,
-                -4937.0,
-                -4708.0,
-                -4537.5,
-                -4370.5,
-                -4062.5,
-                -3742.5,
-            ],
-            [
-                -4154.0,
-                -4148.5,
-                -4115.5,
-                -4996.0,
-                -4762.0,
-                -4599.0,
-                -4355.5,
-                -4120.5,
-                -3771.0,
-                -3064.5,
-            ],
-            [
-                -2899.5,
-                -2042.0,
-                -656.0,
-                -160.0,
-                -3484.5,
-                -3897.5,
-                -3833.0,
-                -3833.0,
-                -2966.5,
-                -1247.5,
-            ],
+            [442.5, 439.0, np.inf, np.inf, 508.0],
+            [393.0, 364.5, np.inf, np.inf, 506.5],
+            [362.0, 20.0, 20.0, 373.5, 402.5],
+            [321.5, 20.0, 20.0, 356.0, 422.5],
+            [282.5, 318.0, 326.5, 379.5, 383.5],
         ],
         coords=dict(
-            lon=[-4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5],
-            lat=[-4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5],
+            lon=[125.5, 126.5, 127.5, 128.5, 129.5],
+            lat=[-24.5, -23.5, -22.5, -21.5, -20.5],
         ),
         dims=["lat", "lon"],
     )
@@ -162,7 +47,7 @@ def fixture_grid_result():
 
 def test_grdfill_dataarray_out(grid, expected_grid):
     """
-    grdfill with a DataArray output.
+    Test grdfill with a DataArray output.
     """
     result = grdfill(grid=grid, mode="c20")
     # check information of the output grid
@@ -175,7 +60,7 @@ def test_grdfill_dataarray_out(grid, expected_grid):
 
 def test_grdfill_file_out(grid, expected_grid):
     """
-    grdfill with an outgrid set.
+    Test grdfill with an outgrid set.
     """
     with GMTTempFile(suffix=".nc") as tmpfile:
         result = grdfill(grid=grid, mode="c20", outgrid=tmpfile.name)
