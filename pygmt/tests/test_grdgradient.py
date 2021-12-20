744e1f10afae7fd64b366b89ede35027df874979
Will Schlitzer <schlitzer90@gmail.com>
2021-12-20 21:01:17 +0000

    update test_grdgradient.py to use xarray testing

--- pygmt/tests/test_grdgradient.py
+++ pygmt/tests/test_grdgradient.py
@@ -5,10 +5,11 @@ import os
 
 import numpy.testing as npt
 import pytest
-from pygmt import grdgradient, grdinfo
+from pygmt import grdgradient, grdinfo, load_dataarray
 from pygmt.datasets import load_earth_relief
 from pygmt.exceptions import GMTInvalidInput
 from pygmt.helpers import GMTTempFile
+import xarray as xr
 
 
 @pytest.fixture(scope="module", name="grid")
@@ -16,10 +17,63 @@ def fixture_grid():
     """
     Load the grid data from the sample earth_relief file.
     """
-    return load_earth_relief(resolution="01d", region=[-5, 5, -5, 5])
+    return load_earth_relief(
+        resolution="01d", registration="pixel", region=[125, 130, -25, -20]
+    )
 
 
-def test_grdgradient_outgrid(grid):
+@pytest.fixture(scope="module", name="expected_grid")
+def fixture_grid_result():
+    """
+    Load the expected grdgradient grid result.
+    """
+    return xr.DataArray(
+        data=[
+            [
+                -4.44407284e-04,
+                -5.23089548e-04,
+                -2.83417467e-04,
+                -6.09701383e-04,
+                -5.56207578e-06,
+            ],
+            [
+                -3.89257970e-04,
+                -1.49117477e-04,
+                -1.05610132e-04,
+                -4.98196052e-04,
+                -2.81143642e-04,
+            ],
+            [
+                -2.58729182e-04,
+                1.10914145e-04,
+                -2.62369547e-04,
+                -4.26519982e-04,
+                -4.27756924e-04,
+            ],
+            [
+                -2.55958090e-04,
+                -3.33979144e-04,
+                -4.43578669e-04,
+                7.77622772e-05,
+                7.69931285e-05,
+            ],
+            [
+                -2.86219700e-04,
+                -3.75151722e-04,
+                -4.55771631e-04,
+                4.72632033e-04,
+                -3.38737620e-04,
+            ],
+        ],
+        coords=dict(
+            lon=[125.5, 126.5, 127.5, 128.5, 129.5],
+            lat=[-24.5, -23.5, -22.5, -21.5, -20.5],
+        ),
+        dims=["lat", "lon"],
+    )
+
+
+def test_grdgradient_outgrid(grid, expected_grid):
     """
     Test the azimuth and direction parameters for grdgradient with a set
     outgrid.
@@ -28,30 +82,22 @@ def test_grdgradient_outgrid(grid):
         result = grdgradient(grid=grid, outgrid=tmpfile.name, azimuth=10, direction="c")
         assert result is None  # return value is None
         assert os.path.exists(path=tmpfile.name)  # check that outgrid exists
-        result = (
-            grdinfo(grid=tmpfile.name, force_scan="a", per_column="n").strip().split()
-        )
-    npt.assert_allclose(float(result[4]), -0.0045060496)  # min
-    npt.assert_allclose(float(result[5]), 0.0575332976)  # max
-    # Check spherically weighted statistics below
-    npt.assert_allclose(float(result[10]), 0.000384754501283)  # median
-    npt.assert_allclose(float(result[12]), 0.00285958005568)  # mean
+        temp_grid = load_dataarray(tmpfile.name)
+        xr.testing.assert_allclose(a=temp_grid, b=expected_grid)
 
 
-def test_grdgradient_no_outgrid(grid):
+def test_grdgradient_no_outgrid(grid, expected_grid):
     """
     Test the azimuth and direction parameters for grdgradient with no set
     outgrid.
     """
-    temp_grid = grdgradient(grid=grid, azimuth=10, direction="c")
-    assert temp_grid.dims == ("lat", "lon")
-    assert temp_grid.gmt.gtype == 1  # Geographic grid
-    assert temp_grid.gmt.registration == 1  # Pixel registration
-    npt.assert_allclose(temp_grid.min(), -0.0045060496)
-    npt.assert_allclose(temp_grid.max(), 0.0575332976)
-    npt.assert_allclose(temp_grid.median(), 0.0004889865522272885)
-    npt.assert_allclose(temp_grid.mean(), 0.0028633063193410635)
-
+    result = grdgradient(grid=grid, azimuth=10, direction="c")
+    # check information of the output grid
+    assert isinstance(result, xr.DataArray)
+    assert result.gmt.gtype == 1  # Geographic grid
+    assert result.gmt.registration == 1  # Pixel registration
+    # check information of the output grid
+    xr.testing.assert_allclose(a=result, b=expected_grid)
 
 def test_grdgradient_fails(grid):
     """
