52642f64a757c6e346ab821b08365c02928599d1
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2021-12-20 15:38:55 +0100

    Apply suggestions from code review
    Co-authored-by: TIAN Dongdong <seisman.info@gmail.com>

--- pygmt/__init__.py
+++ pygmt/__init__.py
@@ -134,10 +134,9 @@ def show_versions():
 
         for gs_cmd in cmds:
             try:
-                gs_version = subprocess.check_output(
+                return subprocess.check_output(
                     [gs_cmd, "--version"], universal_newlines=True
                 ).strip()
-                return gs_version
             except FileNotFoundError:
                 continue
         return None
@@ -147,10 +146,9 @@ def show_versions():
         Get GMT version.
         """
         try:
-            gmt_version = subprocess.check_output(
+            return subprocess.check_output(
                 ["gmt", "--version"], universal_newlines=True
             ).strip()
-            return gmt_version
         except FileNotFoundError:
             return None
 
