271ba2839a6ec71f48399a20d6df3f488790c9ce
Will Schlitzer <schlitzer90@gmail.com>
2022-02-05 14:27:52 +0000

    change "Examples" to "Example"

--- pygmt/src/grdproject.py
+++ pygmt/src/grdproject.py
@@ -96,8 +96,8 @@ def grdproject(grid, **kwargs):
         - None if ``outgrid`` is set (grid output will be stored in file set by
           ``outgrid``)
 
-    Examples
-    --------
+    Example
+    -------
     >>> import pygmt  # doctest: +SKIP
     >>> # Load a grid of @earth_relief_30m data, with an x-range of 10 to 30,
     >>> # and a y-range of 15 to 25
