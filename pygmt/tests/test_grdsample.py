ca2b939ff661391dead62a9248191b2a8e29a3d3
Will Schlitzer <schlitzer90@gmail.com>
2022-02-10 15:28:08 +0000

    update test_grdsample.py to use static Earth relief file

--- pygmt/tests/test_grdsample.py
+++ pygmt/tests/test_grdsample.py
@@ -6,18 +6,16 @@ import os
 import pytest
 import xarray as xr
 from pygmt import grdsample, load_dataarray
-from pygmt.datasets import load_earth_relief
 from pygmt.helpers import GMTTempFile
+from pygmt.helpers.testing import load_static_earth_relief
 
 
 @pytest.fixture(scope="module", name="grid")
 def fixture_grid():
     """
-    Load the grid data from the sample earth_relief file.
+    Load the grid data from the static_earth_relief file.
     """
-    return load_earth_relief(
-        resolution="01d", registration="pixel", region=[124, 130, -25, -20]
-    )
+    return load_static_earth_relief()
 
 
 @pytest.fixture(scope="module", name="expected_grid")
@@ -27,25 +25,15 @@ def fixture_grid_result():
     """
     return xr.DataArray(
         data=[
-            [433.65625, 447.96875, 540.0625],
-            [397.28125, 386.15625, 484.125],
-            [334.96875, 417.25, 385.1875],
-            [303.40625, 373.625, 395.0],
-            [311.15625, 321.15625, 389.875],
+            [460.84375, 482.78125, 891.09375],
+            [680.46875, 519.09375, 764.9375],
+            [867.75, 579.03125, 852.53125],
+            [551.75, 666.6875, 958.21875],
+            [411.3125, 518.4375, 931.28125],
         ],
         coords=dict(
-            lon=[
-                125.0,
-                127.0,
-                129.0,
-            ],
-            lat=[
-                -24.5,
-                -23.5,
-                -22.5,
-                -21.5,
-                -20.5,
-            ],
+            lon=[-52, -50, -48],
+            lat=[-19.5, -18.5, -17.5, -16.5, -15.5],
         ),
         dims=["lat", "lon"],
     )
@@ -56,7 +44,9 @@ def test_grdsample_file_out(grid, expected_grid):
     Test grdsample with an outgrid set and the spacing is changed.
     """
     with GMTTempFile(suffix=".nc") as tmpfile:
-        result = grdsample(grid=grid, outgrid=tmpfile.name, spacing=[2, 1])
+        result = grdsample(
+            grid=grid, outgrid=tmpfile.name, spacing=[2, 1], region=[-53, -47, -20, -15]
+        )
         assert result is None  # return value is None
         assert os.path.exists(path=tmpfile.name)  # check that outgrid exists
         temp_grid = load_dataarray(tmpfile.name)
@@ -67,7 +57,7 @@ def test_grdsample_dataarray_out(grid, expected_grid):
     """
     Test grdsample with no outgrid set and the spacing is changed.
     """
-    result = grdsample(grid=grid, spacing=[2, 1])
+    result = grdsample(grid=grid, spacing=[2, 1], region=[-53, -47, -20, -15])
     # check information of the output grid
     assert isinstance(result, xr.DataArray)
     assert result.gmt.gtype == 1  # Geographic grid
@@ -81,7 +71,7 @@ def test_grdsample_registration_changes(grid):
     Test grdsample with no set outgrid and applying registration changes.
     """
     assert grid.gmt.registration == 1  # Pixel registration
-    translated_grid = grdsample(grid=grid, translate=True)
+    translated_grid = grdsample(grid=grid, translate=True, region=[-53, -47, -20, -15])
     assert translated_grid.gmt.registration == 0  # Gridline registration
     registration_grid = grdsample(grid=translated_grid, registration="p")
     assert registration_grid.gmt.registration == 1  # Pixel registration
