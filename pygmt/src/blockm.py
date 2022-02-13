057ab003374c51e3a8eb27e51094f20a04afe31d
Dongdong Tian <seisman.info@gmail.com>
2022-02-13 14:25:07 +0800

    Fix formatting issue

--- pygmt/src/blockm.py
+++ pygmt/src/blockm.py
@@ -152,7 +152,7 @@ def blockmean(data=None, x=None, y=None, z=None, outfile=None, **kwargs):
     -------
     >>> import pygmt  # doctest: +SKIP
     >>> # Load a table of ship observations of bathymetry off Baja California
-    >>> data = pygmt.datasets.load_sample_data(name='bathymetry')
+    >>> data = pygmt.datasets.load_sample_data(name="bathymetry")
     >>> # Calculate block mean values within 5 by 5 minute bins
     >>> data_bmean = pygmt.blockmean(
     ...     data=data, region=[245, 255, 20, 30], spacing="5m"
