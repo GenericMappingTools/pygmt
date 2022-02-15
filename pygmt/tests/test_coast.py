8fd9b59fdb441b654225d082f23c43aa392a63e0
Michael Grund <michael.grund@kit.edu>
2022-02-15 23:26:27 +0100

    format

--- pygmt/tests/test_coast.py
+++ pygmt/tests/test_coast.py
@@ -3,8 +3,8 @@ Tests for fig.coast.
 """
 import pytest
 from pygmt import Figure
-from pygmt.exceptions import GMTInvalidInput
 from pygmt.datasets import load_earth_relief
+from pygmt.exceptions import GMTInvalidInput
 
 
 @pytest.mark.mpl_image_compare
@@ -75,19 +75,18 @@ def test_coast_dcw_list():
     return fig
 
 
-
 @pytest.mark.mpl_image_compare
 def test_coast_clip_land():
     """
-    Test to clip dry areas
+    Test to clip dry areas.
     """
     region = [-28, -10, 62, 68]
     grid = load_earth_relief(resolution="10m", region=region)
 
     fig = Figure()
     fig.basemap(region=region, projection="M12c", frame=True)
-    fig.coast(resolution="l", clip = "land")
-    fig.grdimage(grid=grid, shading = True, cmap="batlow")
+    fig.coast(resolution="l", clip="land")
+    fig.grdimage(grid=grid, shading=True, cmap="batlow")
     fig.coast(clip="end", frame=True)
     return fig
 
@@ -95,14 +94,14 @@ def test_coast_clip_land():
 @pytest.mark.mpl_image_compare
 def test_coast_clip_water():
     """
-    Test to clip wet areas
+    Test to clip wet areas.
     """
     region = [-28, -10, 62, 68]
     grid = load_earth_relief(resolution="10m", region=region)
 
     fig = Figure()
     fig.basemap(region=region, projection="M12c", frame=True)
-    fig.coast(resolution="l", clip = "water")
-    fig.grdimage(grid=grid, shading = True, cmap="batlow")
+    fig.coast(resolution="l", clip="water")
+    fig.grdimage(grid=grid, shading=True, cmap="batlow")
     fig.coast(clip="end", frame=True)
     return fig
