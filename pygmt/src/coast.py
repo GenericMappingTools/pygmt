05c384ac5f298c928b5e9e6a2c6b5fe2addb7c07
Michael Grund <michael.grund@kit.edu>
2022-02-18 09:14:40 +0100

    add inline example for clip

--- pygmt/src/coast.py
+++ pygmt/src/coast.py
@@ -186,6 +186,25 @@ def coast(self, clip=None, **kwargs):
     {p}
     {t}
     {V}
+
+    Example
+    -------
+    >>> # Initiate a clip path for Africa so that the subsequent colorimage of
+    >>> # gridded topography is only seen over land
+    >>> import pygmt  # doctest: +SKIP
+    >>> # Load a grid of @earth_relief_05m data, with an x-range of -30 to 30,
+    >>> # and a y-range of -40 to 40
+    >>> grid = pygmt.datasets.load_earth_relief(
+    ...     resolution="30m", region=[-30, 30, -40, 40]
+    ... )  # doctest: +SKIP
+    >>> # Initiate clip path for land areas
+    >>> fig.coast(
+    ...     projection="M12c", resolution="l", clip="land", frame=True
+    ... )  # doctest: +SKIP
+    >>> # Plot the clipped grid
+    >>> fig.grdimage(grid="@earth_relief_05m", cmap="relief")  # doctest: +SKIP
+    >>> # End clip path
+    >>> fig.coast(clip="end")  # doctest: +SKIP
     """
     kwargs = self._preprocess(**kwargs)  # pylint: disable=protected-access
 
