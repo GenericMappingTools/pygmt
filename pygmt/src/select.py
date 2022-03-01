d75abb0d96ba7374cfebb6348e2f818eef8952c8
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2022-03-01 18:08:44 +0100

    Apply suggestions from code review
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/src/select.py
+++ pygmt/src/select.py
@@ -152,10 +152,14 @@ def select(data=None, outfile=None, **kwargs):
     -------
     >>> import pygmt  # doctest: +SKIP
     >>> # Load a table of ship observations of bathymetry off Baja California
-    >>> data = pygmt.datasets.load_sample_data(name="bathymetry")   # doctest: +SKIP
+    >>> data = pygmt.datasets.load_sample_data(
+    ...     name="bathymetry"
+    ... )  # doctest: +SKIP
     >>> # Only return the data points that lie within the region between
     >>> # longitudes 246 and 247 and latitudes 20 and 21
-    >>> pygmt.select(data=ship_data, region=[246, 247, 20, 21])  # doctest: +SKIP
+    >>> pygmt.select(
+    ...     data=ship_data, region=[246, 247, 20, 21]
+    ... )  # doctest: +SKIP
     """
 
     with GMTTempFile(suffix=".csv") as tmpfile:
