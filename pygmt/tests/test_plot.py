13065b308489adb1d90f8f22fa2bd9882f3c0491
Dongdong Tian <seisman.info@gmail.com>
2022-03-01 09:31:04 +0800

    Fix UnicodeDecodeError with shapefiles for plot and plot3d (#1695)

--- pygmt/tests/test_plot.py
+++ pygmt/tests/test_plot.py
@@ -543,6 +543,17 @@ def test_plot_ogrgmt_file_multipoint_non_default_style():
 
 
 @pytest.mark.mpl_image_compare
+def test_plot_shapefile():
+    """
+    Make sure that plot works for shapefile.
+
+    See https://github.com/GenericMappingTools/pygmt/issues/1616.
+    """
+    fig = Figure()
+    fig.plot(data="@RidgeTest.shp", pen="1p")
+    return fig
+
+
 def test_plot_dataframe_incols():
     """
     Make sure that the incols parameter works for pandas.DataFrame.
