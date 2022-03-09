b5fa22b0c604faa0245ad9b9928f9900b6daa53a
Dongdong Tian <seisman.info@gmail.com>
2022-03-10 00:06:15 +0800

    Update existing inline examples to the new doctest style (#1793)

--- pygmt/src/select.py
+++ pygmt/src/select.py
@@ -11,6 +11,8 @@ from pygmt.helpers import (
     use_alias,
 )
 
+__doctest_skip__ = ["select"]
+
 
 @fmt_docstring
 @use_alias(
@@ -150,16 +152,12 @@ def select(data=None, outfile=None, **kwargs):
 
     Example
     -------
-    >>> import pygmt  # doctest: +SKIP
+    >>> import pygmt
     >>> # Load a table of ship observations of bathymetry off Baja California
-    >>> data = pygmt.datasets.load_sample_data(
-    ...     name="bathymetry"
-    ... )  # doctest: +SKIP
+    >>> ship_data = pygmt.datasets.load_sample_data(name="bathymetry")
     >>> # Only return the data points that lie within the region between
     >>> # longitudes 246 and 247 and latitudes 20 and 21
-    >>> pygmt.select(
-    ...     data=ship_data, region=[246, 247, 20, 21]
-    ... )  # doctest: +SKIP
+    >>> out = pygmt.select(data=ship_data, region=[246, 247, 20, 21])
     """
 
     with GMTTempFile(suffix=".csv") as tmpfile:
