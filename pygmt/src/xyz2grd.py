4454b65a16e1f8e879968328d94f0a27acebdf9a
Dongdong Tian <seisman.info@gmail.com>
2022-03-11 21:59:56 +0800

    Apply suggestions from code review

--- pygmt/src/xyz2grd.py
+++ pygmt/src/xyz2grd.py
@@ -138,11 +138,11 @@ def xyz2grd(data=None, x=None, y=None, z=None, **kwargs):
     -------
     >>> import numpy as np
     >>> import pygmt
-    >>> # prepare input arrays
+    >>> # generate a grid for z=x**2+y**2, with an x-range of 0 to 3, 
+    >>> # and a range of 10.5 to 12.5. The x- and y-spacing are 1.0 and 0.5.
     >>> x, y = np.meshgrid([0, 1, 2, 3], [10.5, 11.0, 11.5, 12.0, 12.5])
     >>> z = x**2 + y**2
     >>> xx, yy, zz = x.flatten(), y.flatten(), z.flatten()
-
     >>> grid = pygmt.xyz2grd(x=xx, y=yy, z=zz, spacing=(1.0, 0.5))
     """
     with GMTTempFile(suffix=".nc") as tmpfile:
