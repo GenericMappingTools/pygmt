13dca899b04b2aa95b3bc980e639686dfcab2dd7
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-03-12 10:26:51 -0500

    Figure.contour: Remove parameter "columns", use "incols" instead
    Also modified the original deprecation test into a regression test for https://github.com/GenericMappingTools/pygmt/issues/1313

--- pygmt/src/contour.py
+++ pygmt/src/contour.py
@@ -14,7 +14,6 @@ from pygmt.helpers import (
 
 
 @fmt_docstring
-@deprecate_parameter("columns", "incols", "v0.4.0", remove_version="v0.6.0")
 @check_data_input_order("v0.5.0", remove_version="v0.7.0")
 @use_alias(
     A="annotation",
