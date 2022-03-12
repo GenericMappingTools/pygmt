df1b179d641873836af89662aa70aa379a6a22f1
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-03-11 21:39:41 -0500

    Refactor test_triangulate to use Table_5_11_mean.xyz instead of tut_ship

--- pygmt/tests/test_triangulate.py
+++ pygmt/tests/test_triangulate.py
@@ -6,7 +6,7 @@ import os
 import pandas as pd
 import pytest
 import xarray as xr
-from pygmt import triangulate
+from pygmt import triangulate, which
 from pygmt.datasets import load_sample_bathymetry
 from pygmt.exceptions import GMTInvalidInput
 from pygmt.helpers import GMTTempFile, data_kind
@@ -17,16 +17,19 @@ def fixture_dataframe():
     """
     Load the table data from the sample bathymetry dataset.
     """
-    return load_sample_bathymetry()
+    fname = which("@Table_5_11_mean.xyz", download="c")
+    return pd.read_csv(
+        fname, sep=r"\s+", header=None, names=["x", "y", "z"], skiprows=1
+    )
 
 
 def test_triangulate_input_file():
     """
     Run triangulate by passing in a filename.
     """
-    output = triangulate(data="@tut_ship.xyz")
+    output = triangulate(data="@Table_5_11_mean.xyz")
     assert isinstance(output, pd.DataFrame)
-    assert output.shape == (161935, 3)
+    assert output.shape == (67, 3)
 
 
 def test_triangulate_input_data_array(dataframe):
@@ -36,7 +39,7 @@ def test_triangulate_input_data_array(dataframe):
     data = dataframe.to_numpy()
     output = triangulate(data=data)
     assert isinstance(output, pd.DataFrame)
-    assert output.shape == (161935, 3)
+    assert output.shape == (67, 3)
 
 
 def test_triangulate_input_xyz(dataframe):
@@ -44,28 +47,28 @@ def test_triangulate_input_xyz(dataframe):
     Run triangulate by passing in x, y, z numpy.ndarrays individually.
     """
     output = triangulate(
-        x=dataframe.longitude,
-        y=dataframe.latitude,
-        z=dataframe.bathymetry,
+        x=dataframe.x,
+        y=dataframe.y,
+        z=dataframe.z,
     )
     assert isinstance(output, pd.DataFrame)
-    assert output.shape == (161935, 3)
+    assert output.shape == (67, 3)
 
 
 def test_triangulate_input_xy_no_z(dataframe):
     """
     Run triangulate by passing in x and y, but no z.
     """
-    output = triangulate(x=dataframe.longitude, y=dataframe.latitude)
+    output = triangulate(x=dataframe.x, y=dataframe.y)
     assert isinstance(output, pd.DataFrame)
-    assert output.shape == (161935, 3)
+    assert output.shape == (67, 3)
 
 
 def test_triangulate_wrong_kind_of_input(dataframe):
     """
     Run triangulate using grid input that is not file/matrix/vectors.
     """
-    data = dataframe.bathymetry.to_xarray()  # convert pandas.Series to xarray.DataArray
+    data = dataframe.z.to_xarray()  # convert pandas.Series to xarray.DataArray
     assert data_kind(data) == "grid"
     with pytest.raises(GMTInvalidInput):
         triangulate(data=data)
