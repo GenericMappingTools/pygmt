7cff590829d014d7df846019e2dc1485147f9801
Dongdong Tian <seisman.info@gmail.com>
2022-03-27 23:44:32 +0800

    Add docstrings to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+"""
+A simple setup.py script to support editable installs.
+"""
+# References:
+# - https://github.com/pypa/setuptools/issues/2816
+# - https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+
 from setuptools import setup
 
 setup()
