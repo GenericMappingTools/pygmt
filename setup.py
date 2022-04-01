033d369c9c702aea5798587a45a932dbd2afafda
Dongdong Tian <seisman.info@gmail.com>
2022-04-01 21:22:48 +0800

    Update the link for development mode

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ A simple setup.py script to support editable installs.
 """
 # References:
 # - https://github.com/pypa/setuptools/issues/2816
-# - https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+# - https://setuptools.pypa.io/en/latest/userguide/quickstart.html#development-mode
 
 from setuptools import setup
 
