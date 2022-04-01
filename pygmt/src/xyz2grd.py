7195cbac71882e6df59b3411bef28fba49e23456
Meghan Jones <meghanj@alum.mit.edu>
2022-04-01 11:33:57 -0400

    Refactor xyz2grd

--- pygmt/src/xyz2grd.py
+++ pygmt/src/xyz2grd.py
@@ -157,9 +157,8 @@ def xyz2grd(data=None, x=None, y=None, z=None, **kwargs):
                 check_kind="vector", data=data, x=x, y=y, z=z, required_z=True
             )
             with file_context as infile:
-                if "G" not in kwargs:  # if outgrid is unset, output to tempfile
-                    kwargs.update({"G": tmpfile.name})
-                outgrid = kwargs["G"]
+                if (outgrid := kwargs.get("G")) is None: # if outgrid is unset, output to tmpfile
+                    kwargs["G"] = outgrid = tmpfile.name
                 arg_str = " ".join([infile, build_arg_string(kwargs)])
                 lib.call_module("xyz2grd", arg_str)
 
