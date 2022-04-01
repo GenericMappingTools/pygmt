41b6d58401e63dfb18acbc33ff0289805c2d76c5
Meghan Jones <meghanj@alum.mit.edu>
2022-04-01 11:32:33 -0400

    Refactor nearneighbor

--- pygmt/src/nearneighbor.py
+++ pygmt/src/nearneighbor.py
@@ -139,8 +139,8 @@ def nearneighbor(data=None, x=None, y=None, z=None, **kwargs):
                 check_kind="vector", data=data, x=x, y=y, z=z, required_z=True
             )
             with table_context as infile:
-                if "G" not in kwargs:  # if outgrid is unset, output to tmpfile
-                    kwargs.update({"G": tmpfile.name})
+                if (outgrid := kwargs.get("G")) is None: # if outgrid is unset, output to tmpfile
+                    kwargs["G"] = outgrid = tmpfile.name
                 outgrid = kwargs["G"]
                 arg_str = " ".join([infile, build_arg_string(kwargs)])
                 lib.call_module(module="nearneighbor", args=arg_str)
