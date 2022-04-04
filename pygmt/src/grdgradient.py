c7725e08c1474ed92425507cfe11ceee70b73bf4
Wei Ji <23487320+weiji14@users.noreply.github.com>
2022-04-04 13:24:10 -0400

    Update pygmt/src/grdgradient.py using walrus operator
    Co-authored-by: Meghan Jones <meghanj@alum.mit.edu>

--- pygmt/src/grdgradient.py
+++ pygmt/src/grdgradient.py
@@ -174,9 +174,8 @@ def grdgradient(grid, **kwargs):
         with Session() as lib:
             file_context = lib.virtualfile_from_data(check_kind="raster", data=grid)
             with file_context as infile:
-                if kwargs.get("G") is None:  # if outgrid is unset, output to tempfile
-                    kwargs.update({"G": tmpfile.name})
-                outgrid = kwargs["G"]
+                if (outgrid := kwargs.get("G")) is None:
+                    kwargs["G"] = outgrid = tmpfile.name  # output to tmpfile
                 arg_str = " ".join([infile, build_arg_string(kwargs)])
                 lib.call_module("grdgradient", arg_str)
 
