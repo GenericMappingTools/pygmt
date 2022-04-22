a01f67714e78c373d389836665edbb23c33b7f7c
Jack Beagley <jack.beagley@outlook.com>
2022-04-22 19:51:16 +1200

    Update pygmt/src/basemap.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/src/basemap.py
+++ pygmt/src/basemap.py
@@ -65,7 +65,7 @@ def basemap(self, **kwargs):
     box : bool or str
         [**+c**\ *clearances*][**+g**\ *fill*][**+i**\ [[*gap*/]\ *pen*]]\
         [**+p**\ [*pen*]][**+r**\ [*radius*]][**+s**\ [[*dx*/*dy*/][*shade*]]].
-        If set to ``True``, draws a rectangular border around the color scale.
+        If set to ``True``, draws a rectangular border around the map scale.
         Alternatively, specify a different pen with **+p**\ *pen*. Add
         **+g**\ *fill* to fill the scale panel [default is no fill]. Append
         **+c**\ *clearance* where *clearance* is either gap, xgap/ygap, or
