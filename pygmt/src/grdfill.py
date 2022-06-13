461fb154a892c98e23e633de5ac36a6a9b22decd
Will Schlitzer <schlitzer90@gmail.com>
2022-06-13 12:22:06 +0100

    Update pygmt/src/grdfill.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/src/grdfill.py
+++ pygmt/src/grdfill.py
@@ -72,7 +72,7 @@ def grdfill(grid, **kwargs):
     >>> earth_relief_holes = pygmt.datasets.load_sample_data(
     ...     name="earth_relief_holes"
     ... )
-    >>> # Perform grd filling operations on the sample grid
+    >>> # Perform grid filling operations on the sample grid
     >>> # Set all empty values to "20"
     >>> filled_grid = pygmt.grdfill(grid=earth_relief_holes, mode="c20")
     """
