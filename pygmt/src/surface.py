89100c9e4bf411190b7f92e3de9e2f64e3bce6aa
Dongdong Tian <seisman.info@gmail.com>
2022-06-24 08:19:38 +0800

    pygmt.surface: Remove parameter 'outfile', use 'outgrid' instead (#1976)

--- pygmt/src/surface.py
+++ pygmt/src/surface.py
@@ -7,7 +7,6 @@ from pygmt.helpers import (
     GMTTempFile,
     build_arg_string,
     check_data_input_order,
-    deprecate_parameter,
     fmt_docstring,
     kwargs_to_strings,
     use_alias,
@@ -18,7 +17,6 @@ __doctest_skip__ = ["surface"]
 
 
 @fmt_docstring
-@deprecate_parameter("outfile", "outgrid", "v0.5.0", remove_version="v0.7.0")
 @check_data_input_order("v0.5.0", remove_version="v0.7.0")
 @use_alias(
     I="spacing",
