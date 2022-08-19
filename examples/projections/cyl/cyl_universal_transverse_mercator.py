da72affed882691ab4e8f0139cce56aabd924663
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-08-19 15:41:26 +0200

    Add sentence for figure 'GMT_utm_zones'

--- examples/projections/cyl/cyl_universal_transverse_mercator.py
+++ examples/projections/cyl/cyl_universal_transverse_mercator.py
@@ -7,7 +7,8 @@ Mercator (UTM) which was adopted by the US Army for large-scale military maps.
 Here, the globe is divided into 60 zones between 84°S and 84°N, most of which
 are 6° (in longitude) wide. Each of these UTM zones have their unique central
 meridian. Furthermore, each zone is divided into latitude bands but these are
-not needed to specify the projection for most cases.
+not needed to specify the projection for most cases. See Figure Universal
+Transverse Mercator for all zone designations.
 
 In order to minimize the distortion in any given zone, a scale factor of 0.9996
 has been factored into the formulae (although a standard, you can change this
