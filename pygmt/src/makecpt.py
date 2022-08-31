cc55495de30b75b273731e0e6ef3fcc074848a4d
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-08-31 15:00:06 +0200

    Fix syntax in description of 'color_model' parameter

--- pygmt/src/makecpt.py
+++ pygmt/src/makecpt.py
@@ -89,7 +89,7 @@ def makecpt(**kwargs):
         appended then we create labels for each category to be used when the
         CPT is plotted. The *label* may be a comma-separated list of category
         names (you can skip a category by not giving a name), or give
-        *start* [-], where we automatically build monotonically increasing
+        *start*\[-], where we automatically build monotonically increasing
         labels from *start* (a single letter or an integer). Append - to build
         ranges *start*-*start+1* instead.
     series : list or str
