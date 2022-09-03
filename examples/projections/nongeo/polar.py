32a0e7e7ceca9d6a8466e16b587c30045b634b3c
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-09-03 16:08:06 +0200

    Adjust explanation of agrument for 'frame' parameter

--- examples/projections/nongeo/polar.py
+++ examples/projections/nongeo/polar.py
@@ -63,7 +63,7 @@ fig.basemap(
     region=[0, 360, 0, 1],
     # set map width to 5 cm
     projection="P5c",
-    # set the frame and color
+    # set the frame, color, and title
     # @^ allows for a line break within the title
     frame=["xa45f", "+gbisque+tprojection='P5c' @^ region=[0, 360, 0, 1]"],
 )
@@ -80,7 +80,7 @@ fig.basemap(
     # set map width to 5 cm and interpret input data as geographic azimuth
     # instead of standard angle
     projection="P5c+a",
-    # set the frame and color
+    # set the frame, color, and title
     frame=["xa45f", "+gbisque+tprojection='P5c+a' @^ region=[0, 360, 0, 1]"],
 )
 
@@ -96,7 +96,7 @@ fig.basemap(
     # set map width to 5 cm and interpret input data as geographic azimuth
     # instead of standard angle
     projection="P5c+a",
-    # set the frame and color
+    # set the frame, color, and title
     frame=["xa45f", "ya0.2", "WNe+gbisque+tprojection='P5c+a' @^ region=[0, 90, 0, 1]"],
 )
 
@@ -113,7 +113,7 @@ fig.basemap(
     # instead of standard angle, rotate coordinate system counterclockwise by
     # 45 degrees
     projection="P5c+a+t45",
-    # set the frame and color
+    # set the frame, color, and title
     frame=["xa30f", "ya0.2", "WNe+gbisque+tprojection='P5c+a+t45' @^ region=[0, 90, 0, 1]"],
 )
 
@@ -130,7 +130,7 @@ fig.basemap(
     # instead of standard angle, rotate coordinate system counterclockwise by
     # 45 degrees
     projection="P5c+a+t45",
-    # set the frame and color
+    # set the frame, color, and title
     frame=["xa30f", "ya", "WNse+gbisque+tprojection='P5c+a+t45' @^ region=[0, 90, 3480, 6371]"],
 )
 
@@ -147,7 +147,7 @@ fig.basemap(
     # instead of standard angle, rotate coordinate system counterclockwise by
     # 45 degrees, r-axis is marked as depth
     projection="P5c+a+t45+z",
-    # set the frame and color
+    # set the frame, color, and title
     frame=["xa30f", "ya", "WNse+gbisque+tprojection='P5c+a+t45+\z' @^ region=[0, 90, 3480, 6371]"],
 )
 
