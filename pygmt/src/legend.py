441b8b7112677bafa407ae890a7624dcfae1f13d
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-09-08 21:20:56 +0200

    Remove italic style in 'legend.py' (doc)

--- pygmt/src/legend.py
+++ pygmt/src/legend.py
@@ -55,7 +55,7 @@ def legend(self, spec=None, position="JTR+jTR+o0.2c", box="+gwhite+p1p", **kwarg
         **+w**\ *width*\ [/*height*]\ [**+j**\ *justify*]\ [**+l**\ *spacing*]\
         [**+o**\ *dx*\ [/*dy*]].
         Defines the reference point on the map for the
-        legend. By default, uses **JTR**\ +\ **jTR**\ +\ **o**\ *0.2c* which
+        legend. By default, uses **JTR**\ +\ **jTR**\ +\ **o**\ 0.2c which
         places the legend at the top-right corner inside the map frame, with a
         0.2 cm offset.
     box : bool or str
