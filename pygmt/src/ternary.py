9b364a84a66aa8a40d6841bc10d5e68db985b9c4
Will Schlitzer <schlitzer90@gmail.com>
2022-09-13 07:16:33 -0400

    Update pygmt/src/ternary.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/src/ternary.py
+++ pygmt/src/ternary.py
@@ -50,7 +50,8 @@ def ternary(self, data, **kwargs):
         counter-clockwise direction].
     region : str or list
         [*amin*, *amax*, *bmin*, *bmax*, *cmin*, *cmax*].
-        Give the min and max limits for each of the three axis **a**, **b**, and **c**.
+        Give the min and max limits for each of the three axes **a**, **b**, 
+        and **c**.
     {CPT}
     {G}
     style : str
