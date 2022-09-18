60e922ab0cb22ae73e2589e4c6a2cdc450b3d59c
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-09-18 18:43:53 +0200

    Remove bold style of 'Note'

--- pygmt/src/select.py
+++ pygmt/src/select.py
@@ -77,7 +77,7 @@ def select(data=None, outfile=None, **kwargs):
         or (**c**)rude). The resolution drops off by ~80% between data sets.
         [Default is **l**]. Append (**+f**) to automatically select a lower
         resolution should the one requested not be available [Default is abort
-        if not found]. **Note** that because the coastlines differ in details
+        if not found]. Note that because the coastlines differ in details
         it is not guaranteed that a point will remain inside [or outside] when
         a different resolution is selected.
     gridmask : str
