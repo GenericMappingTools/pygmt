58df83a8d53496462f6f7ce651e208fbc005fbc2
Will Schlitzer <schlitzer90@gmail.com>
2022-09-21 10:34:37 -0400

    add new tests

--- pygmt/tests/test_colorbar.py
+++ pygmt/tests/test_colorbar.py
@@ -65,3 +65,53 @@ def test_colorbar_shading_list():
     fig.basemap(region=[0, 10, 0, 10], projection="X15c", frame="a")
     fig.colorbar(cmap="geo", shading=[-0.7, 0.2], frame=True)
     return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_colorbar_frame_parameters():
+    """
+    Create colorbar with arguments for xlabel, ylabel, and annotation, and no
+    argument passed to frame.
+    """
+    fig = Figure()
+    fig.colorbar(
+        cmap="rainbow",
+        position="x0c/0c+w1c/0.5c",
+        xlabel="test-x",
+        ylabel="test-y",
+        annotation=0.25,
+    )
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_colorbar_frame_list_parameters():
+    """
+    Create colorbar with a list passed to frame and an argument passed to
+    xlabel.
+    """
+    fig = Figure()
+    fig.colorbar(
+        cmap="rainbow",
+        position="x0c/0c+w1c/0.5c",
+        frame=["a.25", "y+lylabel"],
+        xlabel="test-x",
+    )
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_colorbar_frame_string_parameters():
+    """
+    Create colorbar with a string passed to frame and arguments passed to
+    xlabel and ylabel.
+    """
+    fig = Figure()
+    fig.colorbar(
+        cmap="rainbow",
+        position="x0c/0c+w1c/0.5c",
+        frame="a.5",
+        xlabel="test-x",
+        ylabel="test-y",
+    )
+    return fig
