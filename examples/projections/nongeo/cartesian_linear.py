e7c775510a5d2751919a2424e67e6f62238e33df
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-10-09 15:53:50 +0200

    Fix typo

--- examples/projections/nongeo/cartesian_linear.py
+++ examples/projections/nongeo/cartesian_linear.py
@@ -13,7 +13,7 @@ fig.plot(
     x=[3, 9, 2],
     y=[4, 9, 37],
     pen="3p,red",
-    # The region parameter sets the x and y ranges or the
+    # The region parameter sets the x and y ranges of the
     # Cartesian projection
     region=[0, 10, 0, 50],
     projection="X15c/10c",
