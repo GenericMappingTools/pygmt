ee2e9cd8b1f32fad2899557f87d3f7d6da0f2555
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-11-17 22:36:03 +0100

    Use plural in tutorial 'text.py'

--- examples/tutorials/basics/text.py
+++ examples/tutorials/basics/text.py
@@ -21,7 +21,7 @@ import pygmt
 # Here we create a simple map and add an annotation using the ``text``, ``x``,
 # and ``y`` parameters to specify the annotation text and position in the
 # projection frame. ``text`` accepts *str* types, while ``x``, and ``y``
-# accepts either *int* or *float* numbers, or a list/array of numbers.
+# accept either *int* or *float* numbers, or a list/array of numbers.
 
 fig = pygmt.Figure()
 with pygmt.config(MAP_FRAME_TYPE="plain"):
