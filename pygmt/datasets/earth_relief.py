eec094994f515a7328a99484d39ed82ccd9f231c
Will Schlitzer <schlitzer90@gmail.com>
2022-11-25 09:27:38 -0500

    add synbath and gebcosi to docstring

--- pygmt/datasets/earth_relief.py
+++ pygmt/datasets/earth_relief.py
@@ -76,9 +76,16 @@ def load_earth_relief(
         - **igpp** : IGPP Global Earth Relief [Default option]. See
           :gmt-datasets:`earth-relief.html`.
 
+        - **synbath** : IGPP Global Earth Relief dataset that uses
+          stastical properties of young seafloor to provide more realistic
+          relief of young areas with small seamounts.
+
         - **gebco** : GEBCO Global Earth Relief with only observed relief and
           inferred relief via altimetric gravity. See
           :gmt-datasets:`earth-gebco.html`.
+
+        - **gebcosi** : GEBCO Global Earth Relief that gives sub-ice (si)
+          elevations.
 
     Returns
     -------
