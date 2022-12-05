54a8559c48e19448adda781577accfab1179fb1e
Will Schlitzer <schlitzer90@gmail.com>
2022-12-05 09:45:10 -0500

    Add an internal function to load GMT remote datasets (#2200)
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>
    Co-authored-by: Wei Ji <23487320+weiji14@users.noreply.github.com>

--- pygmt/tests/test_datasets_earth_relief.py
+++ pygmt/tests/test_datasets_earth_relief.py
@@ -30,6 +30,11 @@ def test_earth_relief_01d_igpp_synbath(data_source):
     data = load_earth_relief(
         resolution="01d", registration="gridline", data_source=data_source
     )
+    assert data.name == "elevation"
+    assert data.attrs["units"] == "meters"
+    assert data.attrs["long_name"] == "Earth elevation relative to the geoid"
+    assert data.attrs["vertical_datum"] == "EGM96"
+    assert data.attrs["horizontal_datum"] == "WGS84"
     assert data.shape == (181, 361)
     npt.assert_allclose(data.lat, np.arange(-90, 91, 1))
     npt.assert_allclose(data.lon, np.arange(-180, 181, 1))
@@ -219,4 +224,26 @@ def test_earth_relief_invalid_data_source_with_use_srtm():
             registration="gridline",
             use_srtm=True,
             data_source="gebco",
+        )
+
+
+@pytest.mark.parametrize("data_source", ["igpp", "gebco", "gebcosi", "synbath"])
+def test_earth_relief_incorrect_resolution_registration(data_source):
+    """
+    Test that an error is raised when trying to load a grid registration with
+    an unavailable resolution.
+    """
+    with pytest.raises(GMTInvalidInput):
+        load_earth_relief(
+            resolution="03s",
+            region=[0, 1, 3, 5],
+            registration="pixel",
+            data_source=data_source,
+        )
+    with pytest.raises(GMTInvalidInput):
+        load_earth_relief(
+            resolution="15s",
+            region=[0, 1, 3, 5],
+            registration="gridline",
+            data_source=data_source,
         )
