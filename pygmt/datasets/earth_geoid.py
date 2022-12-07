e25dd3491dd1487567a976add1529377c643d7f4
Will Schlitzer <schlitzer90@gmail.com>
2022-12-07 09:09:14 -0500

    fix "EMG2008" to "EGM2008"

--- pygmt/datasets/earth_geoid.py
+++ pygmt/datasets/earth_geoid.py
@@ -1,5 +1,5 @@
 """
-Function to download the EMG2008 Global Earth Geoid from the GMT data server,
+Function to download the EGM2008 Global Earth Geoid from the GMT data server,
 and load as :class:`xarray.DataArray`.
 
 The grids are available in various resolutions.
@@ -13,7 +13,7 @@ __doctest_skip__ = ["load_earth_age"]
 @kwargs_to_strings(region="sequence")
 def load_earth_geoid(resolution="01d", region=None, registration=None):
     r"""
-    Load the EMG2008 Global Earth Geoid in various resolutions.
+    Load the EGM2008 Global Earth Geoid in various resolutions.
 
     The grids are downloaded to a user data directory
     (usually ``~/.gmt/server/earth/earth_goid/``) the first time you invoke
