f125b1212ffc44a8c375fe6d96bd47d8cd925590
Will Schlitzer <schlitzer90@gmail.com>
2022-12-15 08:47:25 -0500

    Add load_earth_free_air_anomaly function for Earth free-air anomaly dataset (#2238)
    * Add earth_free_air_anomaly.py
    * Add load_earth_free_air_anomaly to API index
    * Add tests and cache files for load_earth_free_air_anomaly
    
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>
    Co-authored-by: Michael Grund <23025878+michaelgrund@users.noreply.github.com>

--- pygmt/datasets/load_remote_dataset.py
+++ pygmt/datasets/load_remote_dataset.py
@@ -147,6 +147,26 @@ datasets = {
             "02m": Resolution(["pixel"], True),
         },
     ),
+    "earth_free_air_anomaly": GMTRemoteDataset(
+        title="free air anomaly",
+        name="free_air_anomaly",
+        long_name="IGPP Global Earth Free-Air Anomaly",
+        units="mGal",
+        extra_attributes={"horizontal_datum": "WGS84"},
+        resolutions={
+            "01d": Resolution(["pixel", "gridline"], False),
+            "30m": Resolution(["pixel", "gridline"], False),
+            "20m": Resolution(["pixel", "gridline"], False),
+            "15m": Resolution(["pixel", "gridline"], False),
+            "10m": Resolution(["pixel", "gridline"], False),
+            "06m": Resolution(["pixel", "gridline"], False),
+            "05m": Resolution(["pixel", "gridline"], True),
+            "04m": Resolution(["pixel", "gridline"], True),
+            "03m": Resolution(["pixel", "gridline"], True),
+            "02m": Resolution(["pixel", "gridline"], True),
+            "01m": Resolution(["pixel"], True),
+        },
+    ),
 }
 
 
