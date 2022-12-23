9af6c898b4b49dbad21ed28a20c27885bcda210a
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-12-23 22:45:37 +0100

    Improve docstring for 'cyl_equidistant.py'

--- examples/projections/cyl/cyl_equidistant.py
+++ examples/projections/cyl/cyl_equidistant.py
@@ -10,7 +10,10 @@ are straight lines.
 **q**\ [*lon0*/\ [*lat0*/]]\ *scale* or **Q**\ [*lon0*/\ [*lat0*/]]\ *width*
 
 The projection is set with **q** or **Q**, and the figure size is set with
-*scale* or *width*.
+*scale* or *width*. Optionally, the central meridian can be set with *lon0*
+[Default is the middle of the map]. Optionally, the standard parallel can
+be set with *lat0* [Default is the equator]. When supplied, the central
+meridian must be supplied as well.
 """
 import pygmt
 
