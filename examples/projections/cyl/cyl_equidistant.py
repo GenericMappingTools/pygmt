313d591bb949c181e8565308b9e069b8e88658e0
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-12-23 16:09:09 +0100

    Update projection cyl

--- examples/projections/cyl/cyl_equidistant.py
+++ examples/projections/cyl/cyl_equidistant.py
@@ -7,7 +7,7 @@ latitudes. The most common form is the Plate Carrée projection, where the
 scaling of longitudes and latitudes is the same. All meridians and parallels
 are straight lines.
 
-**q**\ *scale* or **Q**\ *width*
+**q**\ [*lon0*\/[*lat0*\/]]\ *scale* or **Q**\ [*lon0*\/[*lat0*\/]]\ *width*
 
 The projection is set with **q** or **Q**, and the figure size is set with
 *scale* or *width*.
