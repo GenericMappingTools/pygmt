45a971827f2144e2257f591879f225af4c736090
Michael Grund <michael.grund@kit.edu>
2022-12-26 12:15:21 +0100

    add docs

--- examples/gallery/embellishments/ternary.py
+++ examples/gallery/embellishments/ternary.py
@@ -1,18 +1,25 @@
 """
 Ternary diagram
 ---------------
-
-To plot circles (diameter = 0.1 cm) on a 10-centimeter-wide ternary diagram at the
-positions listed in the sample dataset `rock_compositions`, with default annotations and
-gridline spacings, using the specified labeling.
+The pygmt.Figure.ternary method can draw ternary diagrams. The example shows
+how to plot circles with a diameter of 0.1 centimeters (``style=0.1c``) on a
+10-centimeter-wide (```width=10c``) ternary diagram at the positions listed
+in the first three columns of the sample dataset `rock_compositions`, with
+default annotations and gridline spacings, using the specified labeling
+defined via ``alabel``, ``blabel`` and ``clabel``. Points are colored based
+on the values given in the fourth columns of the sample dataset via
+``cmap=True``.
 """
 
 import pygmt
 
 fig = pygmt.Figure()
 
+# Load sample data
 data = pygmt.datasets.load_sample_data(name="rock_compositions")
 
+# Define a colormap to be used for the values given in the fourth column
+# of the input dataset
 pygmt.makecpt(cmap="batlow", series=[0, 80, 10])
 
 fig.ternary(
@@ -28,8 +35,14 @@ fig.ternary(
         "aafg+lLimestone component+u %",
         "bafg+lWater component+u %",
         "cagf+lAir component+u %",
-        "+givory",
     ],
 )
 
+# Shift origin -1 centimeters in y direction to avoid overlap
+# between ternary diagram and colorbar
+fig.shift_origin(yshift="-1c")
+
+# Add a colorbar indicating the values given in the fourth column of
+# the input dataset
+fig.colorbar(frame=["x+lPermittivity"])
 fig.show()
