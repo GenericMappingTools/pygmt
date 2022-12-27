040883e568054bc37316c30ef774c0827b6d23e9
Will Schlitzer <schlitzer90@gmail.com>
2022-12-27 07:27:26 -0500

    update registration docstring in earth_vertical_gravity_gradient.py

--- pygmt/datasets/earth_vertical_gravity_gradient.py
+++ pygmt/datasets/earth_vertical_gravity_gradient.py
@@ -45,9 +45,9 @@ def load_earth_vertical_gravity_gradient(
 
     registration : str
         Grid registration type. Either ``"pixel"`` for pixel registration or
-        ``"gridline"`` for gridline registration. Default is ``None``, where
-        a pixel-registered grid is returned unless only the
-        gridline-registered grid is available.
+        ``"gridline"`` for gridline registration. Default is ``"gridline"``
+        when available.
+
 
     Returns
     -------
