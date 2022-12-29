9053380919a4523010163e6331835ed46a2813f0
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-12-29 10:28:16 +0100

    Do not use plural within several nouns in 'rose.py'

--- pygmt/src/rose.py
+++ pygmt/src/rose.py
@@ -80,7 +80,7 @@ def rose(self, data=None, length=None, azimuth=None, **kwargs):
 
     orientation : bool
         Specifies that the input data are orientation data (i.e., have a
-        180 degrees ambiguity) instead of true 0-360 degree directions
+        180 degree ambiguity) instead of true 0-360 degree directions
         [Default is 0-360 degrees]. We compensate by counting each record
         twice: First as azimuth and second as azimuth +180. Ignored if
         ``region`` is given as (-90, 90) or (0, 180).
