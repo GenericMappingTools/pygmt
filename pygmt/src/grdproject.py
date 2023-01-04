c07f8565333e6fde9a6ffbad1370f9bc08d5d436
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2023-01-04 09:52:22 +0100

    doc: Use plural for units for the other quantities (#2278)
    Co-authored-by: Will Schlitzer <schlitzer90@gmail.com>

--- pygmt/src/grdproject.py
+++ pygmt/src/grdproject.py
@@ -79,13 +79,15 @@ def grdproject(grid, **kwargs):
     scaling : str
         [**c**\|\ **i**\|\ **p**\|\ **e**\|\ **f**\|\
         **k**\|\ **M**\|\ **n**\|\ **u**].
-        Force 1:1 scaling, i.e., output or output data are in actual projected
-        meters [**e**]. To specify other units, append **f** (foot),
-        **k** (km), **M** (statute mile), **n** (nautical mile), **u**
-        (US survey foot), **i** (inch), **c** (cm), or **p** (point).
+        Force 1:1 scaling, i.e., output or input data are in actual projected
+        meters [**e**]. To specify other units, append **f** (feet),
+        **k** (kilometers), **M** (statute miles), **n** (nautical miles),
+        **u** (US survey feet), **i** (inches), **c** (centimeters), or
+        **p** (points).
     unit : str
-        Append **c**, **i**, or **p** to indicate that cm, inch, or point
-        should be the projected measure unit. Cannot be used with ``scaling``.
+        Append **c**, **i**, or **p** to indicate that centimeters, inches, or
+        points should be the projected measure unit. Cannot be used with
+        ``scaling``.
     {verbose}
     {interpolation}
     {registration}
