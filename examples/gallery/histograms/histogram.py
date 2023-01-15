5ff4c523ba5d9b7b5f3ab841f583e4b672b67ef4
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-01-15 17:27:46 +0100

    Use upper-case letters at beginning of comments

--- examples/gallery/histograms/histogram.py
+++ examples/gallery/histograms/histogram.py
@@ -12,7 +12,7 @@ import pygmt
 
 np.random.seed(100)
 
-# generate random elevation data from a normal distribution
+# Generate random elevation data from a normal distribution
 mean = 100  # mean of distribution
 stddev = 25  # standard deviation of distribution
 data = mean + stddev * np.random.randn(521)
@@ -21,16 +21,16 @@ fig = pygmt.Figure()
 
 fig.histogram(
     data=data,
-    # define the frame, add a title, and set the background color to
-    # lightgray, add labels for x and y axes
+    # Define the frame, add a title, and set the background color to
+    # lightgray. Add labels for x and y axes
     frame=["WSne+tHistogram+glightgray", "x+lElevation (m)", "y+lCounts"],
-    # generate evenly spaced bins by increments of 5
+    # Generate evenly spaced bins by increments of 5
     series=5,
-    # use red3 as color fill for the bars
+    # Use red3 as color fill for the bars
     fill="red3",
-    # use a pen width of 1 point to draw the outlines
+    # Use a pen width of 1 point to draw the outlines
     pen="1p",
-    # choose histogram type 0 = counts [default]
+    # Choose histogram type 0 = counts [default]
     histtype=0,
 )
 
