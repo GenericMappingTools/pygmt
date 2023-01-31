79460fb15305170b4e4058c727fdcb0b1d338916
Dongdong Tian <seisman.info@gmail.com>
2023-01-31 20:26:55 +0800

    Add the first test

---
+++ pygmt/tests/test_timestamp.py
@@ -0,0 +1,21 @@
+"""
+Tests for Figure.timestamp.
+"""
+import pytest
+from pygmt import Figure
+
+
+@pytest.fixture(scope="module", name="faketime")
+def fixture_faketime():
+    """
+    Fake datetime passed to the "timefmt" parameter so that the timestamp is
+    fixed.
+    """
+    return "1970-01-01T00:00:00"
+
+
+@pytest.mark.mpl_image_compare
+def test_timestamp(faketime):
+    fig = Figure()
+    fig.timestamp(timefmt=faketime)
+    return fig
