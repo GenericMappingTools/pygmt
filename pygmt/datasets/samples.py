ea9132c9008a9e22c7006843f02697d7b33c8413
Dongdong Tian <seisman.info@gmail.com>
2023-01-31 12:38:43 +0800

    Add the fmt_dataset_list decorator to list available datasets in docstrings

--- pygmt/datasets/samples.py
+++ pygmt/datasets/samples.py
@@ -1,6 +1,7 @@
 """
 Functions to load sample data.
 """
+import textwrap
 from typing import Callable, NamedTuple
 
 import pandas as pd
@@ -284,6 +285,29 @@ datasets = {
 }
 
 
+def fmt_dataset_list(module_func):
+    """
+    Decorator to replace the ``{dataset_list}`` placeholder in the docstring.
+
+    Parameters
+    ----------
+    module_func : function
+        The function to decorate.
+
+    Returns
+    -------
+    module_func
+        The same *module_func* but with the docstring formatted.
+    """
+    texts = "\n        ".join(
+        f'- ``"{name}"``: {dataset.description}.' for name, dataset in datasets.items()
+    )
+
+    docstrings = module_func.__doc__.format(dataset_list=texts)
+    module_func.__doc__ = textwrap.dedent(docstrings)
+    return module_func
+
+
 def list_sample_data():
     """
     Report datasets available for tests and documentation examples.
@@ -300,6 +324,7 @@ def list_sample_data():
     return {name: dataset.description for name, dataset in datasets.items()}
 
 
+@fmt_dataset_list
 def load_sample_data(name):
     """
     Load an example dataset from the GMT server.
@@ -311,7 +336,9 @@ def load_sample_data(name):
     Parameters
     ----------
     name : str
-        Name of the dataset to load.
+        Name of the dataset to load. Available datasets are:
+
+        {dataset_list}
 
     Returns
     -------
