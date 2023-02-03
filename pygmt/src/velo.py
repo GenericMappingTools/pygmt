01acc2ad276d6591bd142d60fb09c2a54e21dfe3
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-02-03 20:29:49 +0100

    Update docstring for 'fill' in 'velo.py'

--- pygmt/src/velo.py
+++ pygmt/src/velo.py
@@ -176,7 +176,7 @@ def velo(self, data=None, **kwargs):
         and ``spec="r"``) and rotations (``spec="w"``). Can be combined with
         the ``confidence`` variable.
     uncertaintyfill : str
-        Sets the color or shade used for filling uncertainty wedges
+        Set the color or pattern for filling of uncertainty wedges
         (``spec="w"``) or velocity error ellipses (``spec="e"`` or
         ``spec="r"``). If ``uncertaintyfill`` is not specified, the
         uncertainty regions will be transparent. **Note**: Using ``cmap`` and
@@ -184,7 +184,7 @@ def velo(self, data=None, **kwargs):
         selected measure in ``zvalue`` [magnitude error]. More details at
         :gmt-docs:`cookbook/features.html#gfill-attrib`.
     fill : str
-        Select color or pattern for filling of symbols [Default is no fill].
+        Set the color or pattern for filling of symbols [Default is no fill].
         **Note**: Using ``cmap`` (and optionally ``zvalue``) will update the
         symbol fill color based on the selected measure in ``zvalue``
         [magnitude]. More details at
