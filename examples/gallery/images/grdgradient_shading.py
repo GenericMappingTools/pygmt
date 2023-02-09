b20ea8fc9178b9131397d5c98faa06db0a74bd10
JingHuiTong <jhtong33@gmail.com>
2023-02-09 16:28:05 +0800

    Add documents to explain azimuth and normalize

--- examples/gallery/images/grdgradient_shading.py
+++ examples/gallery/images/grdgradient_shading.py
@@ -2,40 +2,49 @@
 Calculating grid gradient, azimuth, and normalize
 --------------------------------------
 The :func:`pygmt.grdgradient` function calculates the gradient of a grid file.
+As input :func:`pygmt.grdgradient` gets a :class:`xarray.DataArray` object or 
+a path string to a grid file, calculates the respective gradient and returns 
+it as an :class:`xarray.DataArray` object. The ``azimuth`` parameter in order 
+to set the illumination light source direction. The ``normalize`` parameter 
+enhances the three-dimensional sense of the topography. It calculates the 
+azimuthal gradient of each point along a certain azimuth angle, then adjust 
+the brightness value of the color according to the positive/negative of the 
+azimuthal gradient and the amplitude of each point.
 """
 import pygmt
 
 # Define region of interest around Caucasus 
 region = [35, 50, 35, 45]
 # Define figure configuration
-pygmt.config(FONT_TITLE = "15p,5", MAP_TITLE_OFFSET = "10p")
+pygmt.config(FONT_TITLE = "15p,5", MAP_TITLE_OFFSET = "10p", MAP_FRAME_TYPE="plain")
 # Load sample grid (1 arc-minutes global relief) in target area
 grid = pygmt.datasets.load_earth_relief(resolution="01m", region=region)
 # Define a colormap to be used for topography. 
-cmap = pygmt.makecpt(cmap='terra', series=[-5000, 5000])
+pygmt.makecpt(cmap='terra', series=[-5000, 5000])
 
 fig = pygmt.Figure()
+# Setup subplots with 3x4 panels
 with fig.subplot(nrows=3, ncols=4, figsize=("24c", "21c")):
-    # Azimuth: define where is the light source. e.g. 0/90 illuminates from
-    # the north (top) and east (right), and so on. 
+    # e.g. 0/90 illuminates light source from the north (top) and east
+    # (right), and so on. 
     for i, azi in enumerate(["0/90", "0/180", "0/300"]):
-        # Normalize: to enhance the three-dimensional sense of the topography. 
-        # It calculates the azimuthal gradient of each point along a certain
-        # azimuth angle, then adjust the brightness value of the color 
-        # according to the positive/negative of the azimuthal gradient and the
-        # amplitude of each point.
+        # `amp` controls the brightness value of the color
+        # `e` and `t` are cumulative Laplace distribution and cumulative
+        # Cauchy distribution, respectively.  
         for j, nor in enumerate(["2t", "2e", "10t", "10e"]):
             index = i * 4 + j
             # making a file with intensities
             shade = pygmt.grdgradient(grid=grid, azimuth=azi, normalize=nor)
+            
             title = f"A={azi}, N={nor}"
+            
             with fig.set_panel(panel=index):
-                fig.grdimage(
-                grid = grid,
-                shading = shade,
-                projection = "M5c",
-                frame = [f'WSne+t"{title}"', "a4f2"],
-                cmap = cmap
+                 fig.grdimage(
+                 grid = grid,
+                 shading = shade,
+                 projection = "M5c",
+                 frame = [f'WSne+t"{title}"', "a4f2"],
+                 cmap = True
                 )
         
 fig.show()
