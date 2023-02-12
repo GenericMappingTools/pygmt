9eba2a9558915cb0409942490bd171682f2db888
Dongdong Tian <seisman.info@gmail.com>
2023-02-12 17:20:52 +0800

    Add more timestamp tests

--- pygmt/tests/test_timestamp.py
+++ pygmt/tests/test_timestamp.py
@@ -8,10 +8,10 @@ from pygmt import Figure
 @pytest.fixture(scope="module", name="faketime")
 def fixture_faketime():
     """
-    Fake datetime passed to the "timefmt" parameter so that the timestamp is
-    fixed.
+    Fake datetime that will be passed to the "timefmt" paramater, so that the
+    timestamp string always has a fixed value.
     """
-    return "1970-01-01T00:00:00"
+    return "9999-99-99T99:99:99"
 
 
 @pytest.mark.mpl_image_compare
@@ -21,4 +21,51 @@ def test_timestamp(faketime):
     """
     fig = Figure()
     fig.timestamp(timefmt=faketime)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_timestamp_label(faketime):
+    """
+    Check if the "label" parameter works.
+    """
+    fig = Figure()
+    fig.timestamp(label="Powered by PyGMT", timefmt=faketime)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_timestamp_justification():
+    """
+    Check if the "justification" parameter works.
+
+    Only a subset of justification codes are tested to avoid overlapping
+    timestamps.
+    """
+    fig = Figure()
+    fig.basemap(projection="X10c/5c", region=[0, 10, 0, 5], frame=0)
+    for just in ["BL", "BR", "TL", "TR"]:
+        fig.timestamp(justification=just, timefmt=just)
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_timestamp_offset():
+    """
+    Check if the "offset" parameter works.
+    """
+    fig = Figure()
+    fig.basemap(projection="X10c/5c", region=[0, 10, 0, 5], frame="g1")
+    for offset in ["1c", "1c/2c", ("1c", "3c")]:
+        fig.timestamp(offset=offset, timefmt=f"offset={offset}")
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_timestamp_font(faketime):
+    """
+    Test if the font parameter works as expected.
+    """
+    fig = Figure()
+    fig.timestamp(font="Times-Roman", label="Powered by GMT", timefmt=faketime)
     return fig
