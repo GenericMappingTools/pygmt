c7711835652bd2c8d387ef412c14b48fa3541eb0
Dongdong Tian <seisman.info@gmail.com>
2023-02-12 18:08:38 +0800

    Fix docstrings

--- pygmt/src/timestamp.py
+++ pygmt/src/timestamp.py
@@ -32,9 +32,9 @@ def timestamp(
         The text string shown after the GMT timestamp logo.
     justification : str
         Justification of the timestamp. *justification* is a two-character code
-        that is a combination of a horizontal (**L**\(eft), **C**\(enter), or
-        **R**\(ight)) and a vertical (**T**\(op), **M**\(iddle), or
-        **B**\(ottom)) code.
+        that is a combination of a horizontal (**L**\ (eft), **C**\ (enter), or
+        **R**\ (ight)) and a vertical (**T**\ (op), **M**\ (iddle), or
+        **B**\ (ottom)) code.
     offset : str or tuple
         *offset* or (*offset_x*, *offset_y*).
         Offset the anchor point of the timestamp by *offset_x* and *offset_y*.
