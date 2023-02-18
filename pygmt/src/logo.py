549b2664fe4428cd01d6168ea94250df13833b66
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-02-18 16:02:11 +0100

    Use general verb form in 'logo.py'

--- pygmt/src/logo.py
+++ pygmt/src/logo.py
@@ -39,7 +39,7 @@ def logo(self, **kwargs):
     position : str
         [**g**\|\ **j**\|\ **J**\|\ **n**\|\ **x**]\ *refpoint*\
         **+w**\ *width*\ [**+j**\ *justify*]\ [**+o**\ *dx*\ [/*dy*]].
-        Sets reference point on the map for the image.
+        Set reference point on the map for the image.
     box : bool or str
         Without further arguments, draws a rectangular border around the
         GMT logo.
