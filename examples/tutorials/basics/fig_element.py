9004d9aa5a97df17cf6161b9fdbaa00bcaaa321b
JingHuiTong <jhtong33@gmail.com>
2023-02-25 17:27:08 +0800

    Adding fig_element.py

---
+++ examples/tutorials/basics/fig_element.py
@@ -0,0 +1,79 @@
+"""
+Figure elements
+=================================
+
+Setting frame, ticks, title, etc., of the plot is handled by the ``frame``
+parameter that most plotting methods of the :class:`pygmt.Figure` class
+contain.
+"""
+
+import pygmt
+
+fig = pygmt.Figure()
+
+
+x = range(0, 11, 2)
+y_1 = [10, 11, 15, 8, 9, 13]
+
+fig.plot(
+    region=[0, 10, 0, 20],
+    projection="X10c/8c",
+    frame=["WSne+tTitle", "xa2f1g2+lxlabel", "ya5f1g5+lylabel"],
+    x=x,
+    y=y_1,
+    style="t0.3c",
+    # fill="darkorange",
+    label="fig.plot(style)"
+)
+y_2 = [4, 5, 6, 3, 5, 5]
+fig.plot(
+    x=x, 
+    y=y_2,
+    pen="1.5p,red",
+    label="fig.plot(pen)")
+#============Figure
+fig.text(x=12, y=22, text="Figure", font="12p,2,darkblue", justify="TC" , no_clip=True)
+fig.text(x=12, y=20.5, text="pygmt.Figure()", font="10p,8", justify="TC", no_clip=True)
+#============Title
+fig.text(x=6, y=22, text='frame="+tTitle"', font="10p,8", justify="TL", no_clip=True)
+#============xlabel
+fig.text(x=5, y=-3, text='frame="x+lxlabel"', font="10p,8", justify="CM", no_clip=True)
+#============ylabel
+fig.text(x=-1.7, y=10, text='frame="y+lylabel"', font="10p,8", justify="CM", angle=90, no_clip=True)
+#============x-majorticks
+fig.plot(x=10, y=-0.2, style="c1c", pen="2p,darkblue", no_clip=True)
+fig.text(x=10, y=-1.4, text="Annonate", font="12p,2,darkblue", justify="TC" , no_clip=True)
+fig.text(x=10, y=-2.6, text='frame="xa2"', font="10p,8", justify="CM", no_clip=True)
+#============y-majorticks
+fig.plot(x=-0.2, y=20, style="c1c", pen="2p,darkblue", no_clip=True)
+fig.text(x=0, y=23.2, text="Annonate", font="12p,2,darkblue", justify="TC" , no_clip=True)
+fig.text(x=0, y=21.7, text='frame="ya5"', font="10p,8", justify="CM", no_clip=True)
+#============x-minorticks
+fig.plot(x=1, y=-0.2, style="c0.7c", pen="2p,darkblue", no_clip=True)
+fig.text(x=1, y=-1.4, text="Frame", font="12p,2,darkblue", justify="TC" , no_clip=True)
+fig.text(x=1, y=-2.6, text='frame="xf1"', font="10p,8", justify="CM", no_clip=True)
+#============y-minorticks
+fig.plot(x=0, y=2, style="c0.7c", pen="2p,darkblue", no_clip=True)
+fig.text(x=-0.2, y=1, text='frame="yf1"', font="10p,8", justify="MR", no_clip=True)
+#============grid
+fig.plot(x=2, y=15, style="c0.5c", pen="2p,darkblue")
+fig.text(x=2, y=16, text="Grid", font="12p,2,darkblue", justify="BC" )
+fig.text(x=2, y=17.5, text='frame="xg2"', font="10p,8", justify="BC")
+#============map boundaries
+fig.plot(x=10, y=9, style="c0.5c", pen="2p,darkblue", no_clip=True)
+fig.text(x=10.2, y=8, text="Map\nBoundary", font="12p,2,darkblue", justify="BL", no_clip=True)
+fig.text(x=10.2, y=7, text='frame="WSne"', font="10p,8", justify="BL", no_clip=True)
+#============fig.plot
+fig.plot(x=6, y=8, style="c0.7c", pen="2p,darkblue")
+fig.plot(x=4, y=6, style="c0.7c", pen="2p,darkblue")
+fig.text(x=5, y=6, text='fig.plot()', font="10p,8", justify="BL")
+
+
+
+
+fig.legend()
+#============Legned
+fig.text(x=7.5, y= 17, text="Legend", font="12p,2,darkblue", justify="TL" , no_clip=True)
+fig.text(x=7.5, y= 15.8, text="fig.legend()", font="10p,8", justify="TL", no_clip=True)
+
+fig.show()
