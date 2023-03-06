edb05d04bba136488855beb56b7283abd824020f
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2023-03-06 12:08:21 +0100

    Update timestamp.py

--- examples/gallery/embellishments/timestamp.py
+++ examples/gallery/embellishments/timestamp.py
@@ -2,17 +2,17 @@
 Timestamp
 ---------
 The :meth:`pygmt.Figure.timestamp` method can draw the
-GMT timestamp logo on the map/plot. A custom label
-can be added via the ``label`` parameter.
+GMT time stamp logo on the map/plot. A custom label 
+can be added via the ``label`` parameter. The timestamp 
+will always be shown in the bottom-left corner of the 
+figure.
 """
 
 import pygmt
 
 fig = pygmt.Figure()
+fig.basemap(region=[20, 30, -10, 10], projection="X10c/5c")
 fig.timestamp()
-fig.show()
-
 # Plot the GMT timestamp logo with a custom label.
-fig = pygmt.Figure()
-fig.timestamp(label="Powered by PyGMT")
+fig.timestamp(label="Powered by PyGMT", justification="TL")
 fig.show()
