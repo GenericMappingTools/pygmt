6a6756c8ba9cb1578a1aca237934d52f255e7f4e
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2023-03-06 09:15:51 +0100

    Update timestamp.py

--- examples/gallery/embellishments/timestamp.py
+++ examples/gallery/embellishments/timestamp.py
@@ -1,6 +1,14 @@
+"""
+Timestamp
+---------
+The :meth:`pygmt.Figure.timestamp` method can draw the
+GMT time stamp logo on the map/plot. A custom label 
+can be added via the ``label`` parameter.
+"""
+
 import pygmt
 
-# define static format string
+# Define static format string
 pygmt.config(FORMAT_TIME_STAMP="2023-03-01T20:45:15")
 
 fig = pygmt.Figure()
