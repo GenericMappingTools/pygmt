5bff1cfd3cfe889f8b61585a44b954fffca4dc71
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-03-11 09:58:34 +0100

    Add missing mathematical expression

--- pygmt/src/binstats.py
+++ pygmt/src/binstats.py
@@ -100,9 +100,9 @@ def binstats(data, **kwargs):
         in ``outgrid``). Here, the ``spacing`` setting is expected to
         set the y-increment only and we compute the x-increment given
         the geometry. Because the horizontal spacing between hexagon
-        centers in x and y have a ratio of, we will automatically
-        adjust *xmax* in ``region`` to fit a whole number of hexagons.
-        **Note**: Hexagonal tiling requires Cartesian data.
+        centers in x and y have a ratio of :math:`sqrt(3)`, we will
+        automatically adjust *xmax* in ``region`` to fit a whole number
+        of hexagons. **Note**: Hexagonal tiling requires Cartesian data.
     weight : str
         Input data have an extra column containing observation point weight.
         If weights are given then weighted statistical quantities will be
