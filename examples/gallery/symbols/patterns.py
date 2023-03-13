e2b520ea67fd9627db8af3fb42e1a4eefe2378ee
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-03-13 08:33:15 +0100

    Split syntax for pattern in two lines to fit line length

--- examples/gallery/symbols/patterns.py
+++ examples/gallery/symbols/patterns.py
@@ -21,7 +21,8 @@ or similar parameters:
 
 The required argument has the following form:
 
-**P**\ \|**p**\ *pattern*\ [**+b**\ *color*\ ][**+f**\ *color*\ ][**+r**\ *dpi*]
+**p**\ *pattern*\ [**+b**\ *color*\ ][**+f**\ *color*\ ][**+r**\ *dpi*] or
+**P**\ *pattern*\ [**+b**\ *color*\ ][**+f**\ *color*\ ][**+r**\ *dpi*]
 
 *pattern* can either be a number in the range 1-90 or the name of a
 1-, 8-, or 24-bit image raster file. The former will result in one of the 90
