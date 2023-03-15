713ad0ab613a7a80d1ad89aa5713ac72572b21f3
Dongdong Tian <seisman.info@gmail.com>
2023-03-15 23:37:44 +0800

    Add data types GMT_DATASEGMENT, GMT_DATATABLE and GMT_DATASET

---
+++ pygmt/datatypes.py
@@ -0,0 +1,82 @@
+import ctypes as ctp
+
+
+class GMT_DATASEGMENT(ctp.Structure):
+    """
+    For holding segment lines in memory.
+    """
+
+    _fields_ = [
+        ("n_rows", ctp.c_uint64),  # Number of points in this segment
+        ("n_columns", ctp.c_uint64),  # Number of fields in each record (>= 2)
+        ("min", ctp.POINTER(ctp.c_double)),  # Minimum coordinate for each column
+        ("max", ctp.POINTER(ctp.c_double)),  # Maximum coordinate for each column
+        # Data x, y, and possibly other columns
+        ("data", ctp.POINTER(ctp.POINTER(ctp.c_double))),
+        ("label", ctp.c_char_p),  # Label string (if applicable)
+        ("header", ctp.c_char_p),  # Segment header (if applicable)
+        ("text", ctp.POINTER(ctp.c_char_p)),  # text beyond the data
+        ("hidden", ctp.c_void_p),  # Book-keeping variables "hidden" from the API
+    ]
+
+
+class GMT_DATATABLE(ctp.Structure):
+    """
+    To hold an array of line segment structures and header information in one
+    container.
+    """
+
+    _fields_ = [
+        ("n_headers", ctp.c_uint),  # Number of file header records (0 if no header)
+        ("n_columns", ctp.c_uint64),  # Number of columns (fields) in each record
+        ("n_segments", ctp.c_uint64),  # Number of segments in the array
+        ("n_records", ctp.c_uint64),  # Total number of data records across all segments
+        ("min", ctp.POINTER(ctp.c_double)),  # Minimum coordinate for each column
+        ("max", ctp.POINTER(ctp.c_double)),  # Maximum coordinate for each column
+        (
+            "header",
+            ctp.POINTER(ctp.c_char_p),
+        ),  # Array with all file header records, if any
+        (
+            "segment",
+            ctp.POINTER(ctp.POINTER(GMT_DATASEGMENT)),
+        ),  # Pointer to array of segments
+        ("hidden", ctp.c_void_p),  # Book-keeping variables "hidden" from the API
+    ]
+
+
+class GMT_DATASET(ctp.Structure):
+    """
+    Single container for an array of GMT tables (files)
+    """
+
+    _fields = [
+        ("n_tables", ctp.c_uint64),  # The total number of tables (files) contained
+        ("n_columns", ctp.c_uint64),  # The number of data columns
+        ("n_segments", ctp.c_uint64),  #  The total number of segments across all tables
+        (
+            "n_records",
+            ctp.c_uint64,
+        ),  # The total number of data records across all tables
+        ("min", ctp.POINTER(ctp.c_double)),  # Minimum coordinate for each column
+        ("max", ctp.POINTER(ctp.c_double)),  # Maximum coordinate for each column
+        (
+            "table",
+            ctp.POINTER(ctp.POINTER(GMT_DATATABLE)),
+        ),  # Pointer to array of tables
+        (
+            "type",
+            ctp.c_uint,
+        ),  # The datatype (numerical, text, or mixed) of this dataset
+        ("geometry", ctp.c_unit),  # The geometry of this dataset
+        (
+            "ProjRefPROJ4",
+            ctp.c_char_p,
+        ),  # To store a referencing system string in PROJ.4 format
+        (
+            "ProjRefWKT",
+            ctp.c_char_p,
+        ),  # To store a referencing system string in WKT format
+        ("ProjRefEPSG", ctp.c_int),  # To store a referencing system EPSG code
+        ("hidden", ctp.c_void_p),  # Book-keeping variables "hidden" from the API
+    ]
