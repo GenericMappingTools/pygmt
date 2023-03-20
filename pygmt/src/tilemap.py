fde94ad448bf61bb4e0d7f0f1a03f71fd8967fd1
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-03-20 22:57:29 +1300

    Reproject and plot map in lonlat coordinates by default
    GIven a region in lonlat coordinates, ensure that the output figure is also plotted in lonlat instead of Web Mercator.

--- pygmt/src/tilemap.py
+++ pygmt/src/tilemap.py
@@ -43,6 +43,13 @@ def tilemap(
     :func:`pygmt.datasets.load_tile_map` into a georeferenced form, and plots
     the tiles as a basemap or overlay using :meth:`pygmt.Figure.grdimage`.
 
+    **Note**: By default, standard web map tiles served in a Web Mercator
+    (EPSG:3857) Cartesian format will be reprojected to a geographic coordinate
+    reference system (OGC:WGS84) and plotted with longitude/latitude bounds
+    when ``lonlat=True``. If reprojection is not desired, please set
+    ``lonlat=False`` and provide Web Mercator (EPSG:3857) coordinates to the
+    ``region`` parameter.
+
     {aliases}
 
     Parameters
@@ -128,6 +135,13 @@ def tilemap(
         wait=wait,
         max_retries=max_retries,
     )
+
+    # Reproject raster from Web Mercator (EPSG:3857) to lonlat (OGC:CRS84) if
+    # bounding box region was provided in lonlat
+    if lonlat and raster.rio.crs == "EPSG:3857":
+        raster = raster.rio.reproject(dst_crs="OGC:CRS84")
+        raster.gmt.gtype = 1  # set to geographic type
+
     with GMTTempFile(suffix=".tif") as tmpfile:
         raster.rio.to_raster(raster_path=tmpfile.name)
         with Session() as lib:
--- pygmt/tests/test_tilemap.py
+++ pygmt/tests/test_tilemap.py
@@ -9,10 +9,32 @@ rioxarray = pytest.importorskip("rioxarray")
 
 
 @pytest.mark.mpl_image_compare
-def test_tilemap():
+def test_tilemap_web_mercator():
     """
-    Create a simple tilemap plot.
+    Create a tilemap plot in Web Mercator projection (EPSG:3857).
     """
     fig = Figure()
-    fig.tilemap(region=[-180.0, 180.0, -90, 90], zoom=0, frame="afg")
+    fig.tilemap(
+        region=[-20000000.0, 20000000.0, -20000000.0, 20000000.0],
+        zoom=0,
+        lonlat=False,
+        frame="afg",
+    )
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_tilemap_ogc_wgs84():
+    """
+    Create a tilemap plot using longitude/latitude coordinates (OGC:WGS84),
+    centred on the international date line.
+    """
+    fig = Figure()
+    fig.tilemap(
+        region=[-180.0, 180.0, -90, 90],
+        zoom=0,
+        frame="afg",
+        projection="R180/5c",
+        verbose=True,
+    )
     return fig
