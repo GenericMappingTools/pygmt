52317e0b7f029e34055d3469082ecaa53ea8a6a7
JingHuiTong <jhtong33@gmail.com>
2023-04-04 15:22:57 +0800

    typo

--- examples/get_started/03_figure_element.py
+++ examples/get_started/03_figure_element.py
@@ -7,9 +7,9 @@ The figure shows the naming of figure elements in PyGMT.
   figure must start with the creation of a :meth:`pygmt.Figure()` instance
 - ``frame``: setting plot or map boundaries (**WSNE**, **wsne**, or
   **lbtr**), adding annotations, ticks, gridlines (**afg**), axis labels
-   (**+l**), and title (**+t**), e.g., in :meth:`pygmt.Figure.basemap`.
-  Detailed examples can be found at
-  `frame and axes attributes<https://www.pygmt.org/latest/tutorials/basics/frames.html>`_.
+  (**+l**), and title (**+t**), e.g., in :meth:`pygmt.Figure.basemap`.
+  Detailed examples can be found at `frame and axes attributes<https://www.
+  pygmt.org/latest/tutorials/basics/frames.html>`_.
 - :meth:`pygmt.Figure.plot`: plotting the lines or symbols based on ``pen``
   or ``style`` parameters, respectively
 - :meth:`pygmt.Figure.text`: plotting text strings whereby the ``font``
