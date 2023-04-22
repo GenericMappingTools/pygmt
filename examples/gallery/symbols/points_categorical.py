340d7fe36b0911116929db97c466c99dbcad9ad7
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-04-22 13:53:33 +0200

    Shorten comment

--- examples/gallery/symbols/points_categorical.py
+++ examples/gallery/symbols/points_categorical.py
@@ -74,8 +74,7 @@ fig.plot(
     # Use bill length and bill depth as x and y data input, respectively
     x=df.bill_length_mm,
     y=df.bill_depth_mm,
-    # Vary each symbol size according to another feature (body mass,
-    # scaled by 7.5e-5)
+    # Vary symbol size according the body mass, scaled by 7.5e-5
     size=df.body_mass_g * 7.5e-5,
     # Points colored by categorical number code (refers to the species)
     fill=df.species.cat.codes.astype(int),
