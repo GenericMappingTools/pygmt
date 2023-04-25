e3fa14a615914cfe76c3fd3e2e8d322458f2c390
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-04-25 15:43:08 +0200

    Improve docstring for 'color_model' parameter

--- pygmt/src/makecpt.py
+++ pygmt/src/makecpt.py
@@ -85,8 +85,9 @@ def makecpt(**kwargs):
         :gmt-term:`COLOR_FOREGROUND`, and :gmt-term:`COLOR_NAN`]. Use
         ``background="i"`` to match the colors for the lowest and highest
         values in the input (instead of the output) CPT.
-    color_model :
-        [**R**\|\ **r**\|\ **h**\|\ **c**][**+c**\ [*label*]].
+    color_model : str
+        [**R**\|\ **r**\|\ **h**\|\ **c**]\
+        [**+c**\ [*label*\|\ *start*\ [**-**]]].
         Force output CPT to be written with r/g/b codes, gray-scale values or
         color name (**R**, default) or r/g/b codes only (**r**), or h-s-v codes
         (**h**), or c/m/y/k codes (**c**).  Optionally or alternatively, append
@@ -94,9 +95,9 @@ def makecpt(**kwargs):
         appended then we create labels for each category to be used when the
         CPT is plotted. The *label* may be a comma-separated list of category
         names (you can skip a category by not giving a name), or give
-        *start*\[**-**], where we automatically build monotonically increasing
-        labels from *start* (a single letter or an integer). Append **-** to
-        build ranges *start*-*start+1* instead.
+        *start*, where we automatically build monotonically increasing
+        labels from *start* (a single letter or an integer). Additionally
+        append **-** to build ranges *start*-*start+1* as labels instead.
     series : list or str
         [*min/max/inc*\[**+b**\|\ **l**\|\ **n**]\|\ *file*\|\ *list*].
         Define the range of the new CPT by giving the lowest and highest
