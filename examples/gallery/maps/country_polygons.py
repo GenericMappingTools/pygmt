fc7233295e45e4c4dd0ee9253524b435b6d9a44d
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2023-05-01 19:06:44 +0200

    Apply suggestions from code review
    Co-authored-by: Will Schlitzer <schlitzer90@gmail.com>

--- examples/gallery/maps/country_polygons.py
+++ examples/gallery/maps/country_polygons.py
@@ -1,9 +1,10 @@
 """
 Highlight country polygons
 --------------------------
-The :meth:`pygmt.Figure.coast` method allows to
+The :meth:`pygmt.Figure.coast` method can
 highlight country polygons via the ``dcw``
-parameter.
+parameter. It accepts the country code, and 
+can draw its borders and add a color to its landmass.
 """
 
 # sphinx_gallery_thumbnail_number = 2
@@ -19,17 +20,23 @@ fig.coast(
     land="gray",
     water="white",
     dcw=[
+        # Great Britain with seagrean land
         "GB+gseagreen",
+        # Italy with a red border
         "IT+p0.75p,red3",
+        # Spain with a magenta dashed border
         "ES+p0.75p,magenta4,-",
+        # Romania with a black dotted border
         "RO+p1p,black,.",
+        # Germany with orange land and a blue border
         "DE+gorange+p1p,dodgerblue4",
     ],
 )
 fig.show()
 
 ###############################################################################
-# To highlight continents instead of single countries ...
+# Entire continents can also be highlighted by adding "=" in 
+front of the continent code to differentiate it from a country code.
 
 fig = pygmt.Figure()
 
@@ -41,12 +48,19 @@ fig.coast(
     water="white",
     frame="afg",
     dcw=[
+        # Europe
         "=EU+gseagreen",
+        # Africa
         "=AF+gred3",
+        # North America
         "=NA+gmagenta4",
+        # South America
         "=SA+gorange",
+        # Asia
         "=AS+gdodgerblue4",
+        # Oceania
         "=OC+gtomato",
+        # Antarctica
         "=AN+ggray30",
     ],
 )
