12b6bfa93f26412157220a3e7fd34d3e8eb8461f
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-05-28 17:10:33 +0200

    Add text of footnote

--- examples/projections/misc/misc_robinson.py
+++ examples/projections/misc/misc_robinson.py
@@ -6,8 +6,12 @@ The Robinson projection, presented by the American geographer and cartographer
 Arthur H. Robinson in 1963, is a modified cylindrical projection that is
 neither conformal nor equal-area. Central meridian and all parallels are
 straight lines; other meridians are curved. It uses lookup tables rather than
-analytic expressions to make the world map "look" right. The scale is true
-along latitudes 38. The projection was originally developed for use by Rand
+analytic expressions to make the world map "look" right. Robinson provided a
+table of y-coordinates for latitudes every 5 degrees. To project values for
+intermediate latitudes one must interpolate the table. Different interpolants
+may result in slightly different maps. GMT uses the interpolant selected by
+the parameter :gmt-term:`GMT_INTERPOLANT` in the file.
+The scale is true along latitudes 38. The projection was originally developed for use by Rand
 McNally and is currently used by the National Geographic Society.
 
 **n**\ [*lon0/*]\ *scale* or **N**\ [*lon0/*]\ *width*
