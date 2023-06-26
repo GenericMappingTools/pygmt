d5bbb9cf89fc1f060d892a974d9957a40e9fa229
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2023-06-26 10:30:01 +0200

    Create tilemaps.py

---
+++ examples/gallery/maps/tilemaps.py
@@ -0,0 +1,32 @@
+"""
+Tilemaps
+--------------------
+The :meth:`pygmt.Figure.tilemap` method allows to plot
+tiles from a tile server or local file as a basemap or overlay. 
+"""
+import pygmt 
+
+fig = pygmt.Figure()
+fig.tilemap(region=[-157.84, -157.8, 21.255, 21.285],
+            projection="M12c",
+            zoom=14,
+            # Use tiles from OpenStreetMap tile server
+            source="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            frame="afg")
+
+fig.show()
+
+###############################################################################
+# It's also possible to use tiles provided via the 
+# [contextily](https://github.com/geopandas/contextily) library.
+
+import contextily
+
+fig = pygmt.Figure()
+fig.tilemap(region=[-157.84, -157.8, 21.255, 21.285],
+            projection="M12c",
+            # Use the Stamen.Watercolor option from contextily
+            source=contextily.providers.Stamen.Watercolor,
+            frame="afg")
+
+fig.show()
