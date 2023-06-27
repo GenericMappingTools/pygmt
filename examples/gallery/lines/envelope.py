bbbe9ea69c9ee5f78b9b8cfb9f233a915c1c4fb1
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-06-27 21:52:51 +0200

    Fix typo

--- examples/gallery/lines/envelope.py
+++ examples/gallery/lines/envelope.py
@@ -62,7 +62,7 @@ fig.basemap(
 fig.plot(
     data=df,
     # Add an outline around the envelope
-    # Here, a dashed pen (+p) with 0.5 points thickness and
+    # Here, a dashed pen (+p) with 0.5-points thickness and
     # "gray30" color is used
     close="+D+p0.5p,gray30,dashed",
     fill="gray@50",
