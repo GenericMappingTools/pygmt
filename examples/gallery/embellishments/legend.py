bb2b42366717d54417a5730c43fcb18664fcd9b7
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-07-16 22:57:44 +0200

    Fix higlighting

--- examples/gallery/embellishments/legend.py
+++ examples/gallery/embellishments/legend.py
@@ -7,8 +7,8 @@ symbols plotted using :meth:`pygmt.Figure.plot`. Legend entries are only
 created when the ``label`` parameter is used. For more complicated legends,
 including legends with multiple columns, users have to write an ASCII file
 with instructions for the layout of the items in the legend and pass it
-to the `spec` parameter of :meth:`pygmt.Figure.legend`. For details on how
-to set up such a file, please see the GMT documentation at
+to the ``spec`` parameter of :meth:`pygmt.Figure.legend`. For details on
+how to set up such a file, please see the GMT documentation at
 https://docs.generic-mapping-tools.org/latest/legend.html#legend-codes.
 """
 import pygmt
