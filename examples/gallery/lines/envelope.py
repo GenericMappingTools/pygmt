3d89b8eb9c84aa1aff82adbbea4e0d635ebf9e6d
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2023-07-24 10:01:36 +0200

    Use hyphen (code review)
    Co-authored-by: Michael Grund <23025878+michaelgrund@users.noreply.github.com>

--- examples/gallery/lines/envelope.py
+++ examples/gallery/lines/envelope.py
@@ -3,7 +3,7 @@ Envelope
 --------
 The ``close`` parameter of the :meth:`pygmt.Figure.plot` method can be
 used to build a symmetrical or an asymmetrical envelope. The user can
-give either the deviations or the bounds in y direction. For the first
+give either the deviations or the bounds in y-direction. For the first
 case append ``"+d"`` or ``"+D"`` and for the latter case ``"+b"``.
 """
 
