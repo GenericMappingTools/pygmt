0bb30b51dee12abccb02a79908da704086aeb284
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-08-25 22:24:38 +0200

    Add general syntax for 'scale' option in 'cartesian_power.py'

--- examples/projections/nongeo/cartesian_power.py
+++ examples/projections/nongeo/cartesian_power.py
@@ -2,10 +2,12 @@ r"""
 Cartesian power
 ===============
 
-**X**\ *width*\ [**p**\ *pvalue*][/*height*\ [**p**\ *pvalue*]]: Give the
-*width* of the figure and the optional argument *height*. Each axis with
-a power transformation requires **p** and the exponent for that axis
-after its size argument.
+**X**\ *width*\ [**p**\ *pvalue*][/*height*\ [**p**\ *pvalue*]] or
+**x**\ *x-scale*\ [**p**\ *pvalue*][/*y-scale*\ [**p**\ *pvalue*]]
+
+Give the *width* of the figure and the optional argument *height*.
+Each axis with a power transformation requires **p** and the exponent
+for that axis after its size argument.
 """
 import numpy as np
 import pygmt
