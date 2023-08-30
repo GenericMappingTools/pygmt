00b427b4d83240922edeab369fb1404d03309445
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-08-31 09:55:12 +1200

    Apply suggestions from code review
    Co-authored-by: Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>

--- examples/gallery/images/rgb_image.py
+++ examples/gallery/images/rgb_image.py
@@ -3,7 +3,7 @@ RGB Image
 ---------
 The :meth:`pygmt.Figure.grdimage` method can be used to plot Red, Green, Blue
 (RGB) images, or any 3-band false color combination. Here, we'll use
-:meth:`rioxarray.open_rasterio` to read a GeoTIFF file into an
+:py:func:`rioxarray.open_rasterio` to read a GeoTIFF file into an
 :class:`xarray.DataArray` format, and plot it on a map.
 
 The example below shows a Worldview 2 satellite image over
@@ -17,23 +17,24 @@ import pygmt
 import rioxarray
 
 ###############################################################################
-# Read 3-band data from GeoTIFF into an xarray.DataArray object
+# Read 3-band data from GeoTIFF into an xarray.DataArray object:
 with rioxarray.open_rasterio(
     filename="https://oin-hotosm.s3.us-east-1.amazonaws.com/64d6a49a19cb3a000147a65b/0/64d6a49a19cb3a000147a65c.tif",
     overview_level=5,
 ) as img:
     # Subset to area of Lāhainā in EPSG:32604 coordinates
     image = img.rio.clip_box(minx=738000, maxx=755000, miny=2300000, maxy=2318000)
-    image = image.load()  # force loading dataarray into memory
+    image = image.load()  # Force loading the DataArray into memory
 image
 
 ###############################################################################
-# Plot the RGB imagery
+# Plot the RGB imagery:
 fig = pygmt.Figure()
 with pygmt.config(FONT_TITLE="Times-Roman"):  # Set title font to Times-Roman
     fig.grdimage(
         grid=image,
-        projection="x1:100000",  # map scale where 1 cm on map equals 1 km on the ground
+        # Use a map scale where 1 cm on the map equals 1 km on the ground
+        projection="x1:100000", 
         frame=[r"WSne+tL@!a¯hain@!a¯, Hawai`i on 9 Aug 2023", "af"],
     )
 fig.show()
