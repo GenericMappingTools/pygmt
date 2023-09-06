9aee5952f76bab2f766700aac90d0ec3091515da
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2023-09-06 14:41:16 +0200

    Fix typos in tutorials (#2668)

--- examples/tutorials/advanced/3d_perspective_image.py
+++ examples/tutorials/advanced/3d_perspective_image.py
@@ -79,7 +79,7 @@ fig.show()
 # lines on the surface. :meth:`pygmt.Figure.colorbar` can be used to add a
 # color bar to the figure. The ``cmap`` parameter does not need to be passed
 # again. To keep the color bar's alignment similar to the figure, use ``True``
-# as the ``perspective`` parameter.
+# as argument for the ``perspective`` parameter.
 
 fig = pygmt.Figure()
 fig.grdview(
--- examples/tutorials/advanced/date_time_charts.py
+++ examples/tutorials/advanced/date_time_charts.py
@@ -193,9 +193,9 @@ fig.show()
 # initialized as a list of :class:`xarray.DataArray` objects. This object
 # provides a wrapper around regular PyData formats. It also allows the data to
 # have labeled dimensions while supporting operations that use various pieces
-# of metadata.The following code uses :func:`pandas.date_range` object to fill
-# the DataArray with data, but this is not essential for the creation of a
-# valid DataArray.
+# of metadata.The following code uses :func:`pandas.date_range` object to
+# fill the DataArray with data, but this is not essential for the creation of
+# a valid DataArray.
 
 x = xr.DataArray(data=pd.date_range(start="2020-01-01", periods=4, freq="Q"))
 y = [4, 7, 5, 6]
