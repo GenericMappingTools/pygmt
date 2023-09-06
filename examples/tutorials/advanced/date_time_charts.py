eed44149ab256318fe1049bcc1a87dd71f200dc6
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-09-06 19:11:28 +0200

    Fix line length

--- examples/tutorials/advanced/date_time_charts.py
+++ examples/tutorials/advanced/date_time_charts.py
@@ -194,8 +194,8 @@ fig.show()
 # object provides a wrapper around regular PyData formats. It also allows the
 # data to have labeled dimensions while supporting operations that use various
 # pieces of metadata. The following code uses :func:`pandas.date_range` to fill
-# the DataArray with data, but this is not essential for the creation of a valid
-# DataArray.
+# the DataArray with data, but this is not essential for the creation of a
+# valid DataArray.
 
 x = xr.DataArray(data=pd.date_range(start="2020-01-01", periods=4, freq="Q"))
 y = [4, 7, 5, 6]
