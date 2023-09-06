d6a9011bf5f7685125bdfaf1749c9e0b1247efe3
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-09-06 18:31:20 +0200

    Clearify formulation

--- examples/tutorials/advanced/date_time_charts.py
+++ examples/tutorials/advanced/date_time_charts.py
@@ -189,13 +189,13 @@ fig.show()
 # Using :class:`xarray.DataArray`
 # -------------------------------
 #
-# In this example, instead of using :func:`pandas.date_range`, ``x`` is
-# initialized as a list of individual dates within an xarray.DataArray object.
-# This object provides a wrapper around regular PyData formats. It also allows
-# the data to have labeled dimensions while supporting operations that use
-# various pieces of metadata. The following code uses a
-# :func:`pandas.date_range` object to fill the DataArray with data, but this
-# is not essential for the creation of a valid DataArray.
+# In this example, instead of using a list of :class:`pandas.DatetimeIndex`
+# objects, ``x`` is initialized as an :class:`xarray.DataArray` object. This
+# object provides a wrapper around regular PyData formats. It also allows the
+# data to have labeled dimensions while supporting operations that use various
+# pieces of metadata. The following code uses a :func:`pandas.date_range`
+# object to fill the DataArray withcdata, but this is not essential for the
+# creation of a valid DataArray.
 
 x = xr.DataArray(data=pd.date_range(start="2020-01-01", periods=4, freq="Q"))
 y = [4, 7, 5, 6]
