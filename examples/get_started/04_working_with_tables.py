0410ddc5cd9a492ef8996e9fd02766a6ae50835e
Dongdong Tian <seisman.info@gmail.com>
2023-10-08 20:19:53 +0800

    Apply suggestions from code review
    Co-authored-by: Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>

--- examples/get_started/04_working_with_tables.py
+++ examples/get_started/04_working_with_tables.py
@@ -4,10 +4,10 @@
 
 Generally, PyGMT accepts two different types of data inputs: tables and grids.
 
-- A table is a 2D array of data, with *M* rows and *N* columns. Each column 
+- A table is a 2-D array of data, with *M* rows and *N* columns. Each column 
   represents a different variable (e.g., *x*, *y* and *z*) and each row 
   represents a different record. 
-- A grid is a 2D array of data that is regularly spaced in the x and y directions. 
+- A grid is a 2-D array of data that is regularly spaced in the x and y directions. 
 
 In this tutorial, we'll focus on working with table inputs, and cover grids in 
 the next tutorial.
@@ -23,9 +23,9 @@ your data and analysis needs, and work more efficiently with PyGMT.
 # ASCII table file
 # ----------------
 #
-# Most PyGMT functions/methods that accept table input data has a ``data`` parameter.
+# Most PyGMT functions/methods that accept table input data have a ``data`` parameter.
 # The easiest way to provide table input data to PyGMT is by specifying the
-# file name of an ASCII table (e.g., `data="table-input.txt"`). This is useful
+# file name of an ASCII table (e.g., ``data="input_data.dat"``). This is useful
 # when your data is stored in a separate text file.
 
 import numpy as np
@@ -35,7 +35,7 @@ import pygmt
 data = np.array([[1.0, 2.0], [5.0, 4.0], [8.0, 3.0]])
 np.savetxt("input_data.dat", data, fmt="%f")
 
-# Pass the file name to the `data` parameter
+# Pass the file name to the data parameter
 fig = pygmt.Figure()
 fig.basemap(region=[0, 10, 0, 5], projection="X10c/5c", frame=True)
 fig.plot(data="input_data.dat", style="p0.2c", fill="blue")
@@ -48,17 +48,17 @@ Path("input_data.dat").unlink()
 # %%
 # Besides a plain string to a table file, following variants are also accepted:
 #
-# - a `pathlib.Path` object
-# - a full URL. PyGMT will download the file to the current directory first.
-# - a file name prefixed with `@` (e.g., `data="@input_data.dat"`), which is
+# - A `pathlib.Path` object.
+# - A full URL. PyGMT will download the file to the current directory first.
+# - A file name prefixed with ``@`` (e.g., ``data="@input_data.dat"``), which is
 #   a special syntax in GMT to indicate that the file is a remote file
-#   hosted on the GMT data server
+#   hosted on the GMT data server.
 
 # %%
-# 2-D Numpy array or pandas.DataFrame
+# 2-D numpy array or pandas.DataFrame
 # -----------------------------------
 #
-# The `data` parameter also accepts a 2D numpy array or a pandas.DataFrame object.
+# The ``data`` parameter also accepts a 2-D numpy array or a pandas.DataFrame object.
 # This is useful when you want to plot data that is already in memory.
 
 import pandas as pd
@@ -66,26 +66,24 @@ import pandas as pd
 fig = pygmt.Figure()
 fig.basemap(region=[0, 10, 0, 5], projection="X10c/5c", frame=True)
 
-# Pass a 2D numpy array to the `data` parameter
+# Pass a 2-D numpy array to the data parameter
 fig.plot(data=np.array([[1.0, 2.0], [5.0, 4.0]]), style="t0.2c", fill="red")
 
-# Pass a pandas.DataFrame to the `data` parameter
+# Pass a pandas.DataFrame to the data parameter
 df = pd.DataFrame(np.array([[1.0, 3.0], [5.0, 2.0]]), columns=["x", "y"])
 fig.plot(data=df, style="a0.5c", fill="blue")
 
 fig.show()
 
 # %%
-#
 # Scalar values or 1-D arrays
 # ---------------------------
 #
-# In addition to the `data` parameter, some PyGMT functions/methods also provide
-# invididual parameters (e.g., `x` and `y` for data coordinates) which allow you
+# In addition to the ``data`` parameter, some PyGMT functions/methods also provide
+# invididual parameters (e.g., ``x`` and ``y`` for data coordinates) which allow you
 # to specify the data. These parameters accept individual scalar values or 1-D arrays
 # (lists or 1-D numpy arrays). This is useful if you want to plot a single data
 # point or already have arrays of data in memory.
-#
 
 fig = pygmt.Figure()
 
