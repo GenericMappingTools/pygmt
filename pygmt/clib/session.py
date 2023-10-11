a06a5b43bf454613c60c46b89838ec892f05259b
Dongdong Tian <seisman.info@gmail.com>
2023-10-11 11:03:40 +0800

    Merge read_virtualfile and read_virtualfile_to_data into a single function

--- pygmt/clib/session.py
+++ pygmt/clib/session.py
@@ -1682,18 +1682,23 @@ class Session:
             raise GMTCLibError("Failed to extract region from current figure.")
         return wesn
 
-    def read_virtualfile(self, vfname):
+    def read_virtualfile(self, vfname, kind=None):
         """
-        Read data from a virtual file.
+        Read data from a virtual file and cast it into a GMT data container if requested.
 
         Parameters
         ----------
         vfname : str
             Name of the virtual file to read.
 
+        kind : str
+            Cast the data into a GMT data container. Choose from "grid" or
+            "dataset". If None, will return a ctypes void pointer.
+
         Returns
         -------
-        Pointer to the data, which can be casted into GMT data types.
+        Pointer to the GMT data container. If ``kind`` is None, returns a
+        ctypes void pointer instead.
         """
         c_read_virtualfile = self.get_libgmt_func(
             "GMT_Read_VirtualFile",
@@ -1703,29 +1708,18 @@ class Session:
             ],
             restype=ctp.c_void_p,
         )
-        return c_read_virtualfile(self.session_pointer, vfname.encode())
+        pointer = c_read_virtualfile(self.session_pointer, vfname.encode())
+        if kind is None:  # Return the ctypes void pointer
+            return pointer
 
-    def read_virtualfile_to_data(self, vfname, kind):
-        """
-        Read a virtual file and convert to a GMT data container.
-
-        Parameters
-        ----------
-        vfname : str
-            Name of the virtual file to read.
-        kind : str
-            The kind of data container to create. Choose from "grid" or
-            "dataset".
-
-        Returns
-        -------
-        Pointer to the GMT_GRID or GMT_DATASET data container.
-        """
+        # The GMT C API function GMT_Read_VirtualFile returns a void pointer.
+        # It usually needs to be cast to a pointer to GMT data container (e.g.,
+        # GMT_GRID or GMT_DATASET).
         type = {
             # "grid": GMT_GRID,  # implemented in PR #2398
             "dataset": GMT_DATASET,
         }[kind]
-        return ctp.cast(self.read_virtualfile(vfname), ctp.POINTER(type))
+        return ctp.cast(pointer, ctp.POINTER(type))
 
     @contextmanager
     def virtualfile_to_data(self, kind):
@@ -1765,7 +1759,7 @@ class Session:
             List of vectors containing the data from the GMT_DATASET object.
         """
         # Read the virtual file and cast it to a pointer to a GMT_DATASET
-        ds = self.read_virtualfile_to_data(vfile, kind="dataset").contents
+        ds = self.read_virtualfile(vfile, kind="dataset").contents
 
         # Loop over the tables, segments, and columns to get the data as vectors
         vectors = []
