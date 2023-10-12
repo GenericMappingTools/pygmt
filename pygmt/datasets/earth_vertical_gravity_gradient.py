019d13ee4e6f58dc1c293bb1ce8866f2b74eee8e
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2023-10-12 02:24:04 +0200

    Remote datasets: Include images of the official documentation in API reference (#2728)
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/datasets/earth_vertical_gravity_gradient.py
+++ pygmt/datasets/earth_vertical_gravity_gradient.py
@@ -18,6 +18,12 @@ def load_earth_vertical_gravity_gradient(
     Load the IGPP Global Earth Vertical Gravity Gradient dataset in various
     resolutions.
 
+    .. figure:: https://www.generic-mapping-tools.org/remote-datasets/_images/GMT_vgg.jpg # noqa: W505
+       :width: 80 %
+       :align: center
+
+       IGPP Global Earth Vertical Gravity Gradient dataset.
+
     The grids are downloaded to a user data directory
     (usually ``~/.gmt/server/earth/earth_vgg/``) the first time you invoke
     this function. Afterwards, it will load the grid from the data directory.
