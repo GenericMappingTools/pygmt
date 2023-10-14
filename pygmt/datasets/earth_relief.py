c9d6147bcf383903ffc75fb94f9787b6b3a1d231
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2023-10-14 10:37:38 +0200

    Remote datasets: Update filenames of remote dataset images (#2745)

--- pygmt/datasets/earth_relief.py
+++ pygmt/datasets/earth_relief.py
@@ -23,7 +23,7 @@ def load_earth_relief(
     Load the Earth relief datasets (topography and bathymetry) in various
     resolutions.
 
-    .. figure:: https://www.generic-mapping-tools.org/remote-datasets/_images/GMT_gebcosi.jpg # noqa: W505
+    .. figure:: https://www.generic-mapping-tools.org/remote-datasets/_images/GMT_earth_gebcosi.jpg # noqa: W505
        :width: 80 %
        :align: center
 
