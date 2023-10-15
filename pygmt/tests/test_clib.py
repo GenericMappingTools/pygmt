3de76667a175493dda96be524cff3bc063515b89
Dongdong Tian <seisman.info@gmail.com>
2023-10-15 20:28:07 +0800

    Fix more tests

--- pygmt/tests/test_clib.py
+++ pygmt/tests/test_clib.py
@@ -440,7 +440,7 @@ def test_virtualfile_from_data_required_z_matrix(array_func, kind):
     data = array_func(dataframe)
     with clib.Session() as lib:
         with lib.virtualfile_from_data(
-            data=data, required_z=True, check_kind="vector"
+            data=data, names="xyz", check_kind="vector"
         ) as vfile:
             with GMTTempFile() as outfile:
                 lib.call_module("info", f"{vfile} ->{outfile.name}")
@@ -463,9 +463,7 @@ def test_virtualfile_from_data_required_z_matrix_missing():
     data = np.ones((5, 2))
     with clib.Session() as lib:
         with pytest.raises(GMTInvalidInput):
-            with lib.virtualfile_from_data(
-                data=data, required_z=True, check_kind="vector"
-            ):
+            with lib.virtualfile_from_data(data=data, names="xyz", check_kind="vector"):
                 pass
 
 
@@ -481,10 +479,7 @@ def test_virtualfile_from_data_fail_non_valid_data(data):
             continue
         with clib.Session() as lib:
             with pytest.raises(GMTInvalidInput):
-                lib.virtualfile_from_data(
-                    x=variable[0],
-                    y=variable[1],
-                )
+                lib.virtualfile_from_data(vectors=variable[:2])
 
     # Test all combinations where at least one data variable
     # is not given in the x, y, z case:
@@ -494,19 +489,12 @@ def test_virtualfile_from_data_fail_non_valid_data(data):
             continue
         with clib.Session() as lib:
             with pytest.raises(GMTInvalidInput):
-                lib.virtualfile_from_data(
-                    x=variable[0], y=variable[1], z=variable[2], required_z=True
-                )
+                lib.virtualfile_from_data(vectors=variable[:3], names="xyz")
 
     # Should also fail if given too much data
     with clib.Session() as lib:
         with pytest.raises(GMTInvalidInput):
-            lib.virtualfile_from_data(
-                x=data[:, 0],
-                y=data[:, 1],
-                z=data[:, 2],
-                data=data,
-            )
+            lib.virtualfile_from_data(vectors=data[:, :3], data=data, names="xyz")
 
 
 def test_virtualfile_from_vectors(dtypes):
