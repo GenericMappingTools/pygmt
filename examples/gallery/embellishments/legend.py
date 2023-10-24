1b860a0354104ee1c6fe36c4715f45d25df165f0
yvonnefroelich <yvonne.froehlich@kit.edu>
2023-10-24 06:32:26 +0200

    Combinde to one figure

--- examples/gallery/embellishments/legend.py
+++ examples/gallery/embellishments/legend.py
@@ -12,8 +12,6 @@ For more complicated legends, users may want to write an ASCII file with
 instructions for the layout of the legend items and pass it to the ``spec``
 parameter of :meth:`pygmt.Figure.legend`. For details on how to set up such a
 file, please see the GMT documentation at :gmt-docs:`legend.html#legend-codes`.
-In this example the same plot is created twice, first with a vertical (default)
-and then with a horizontal legend.
 """
 
 # %%
@@ -21,41 +19,47 @@ import numpy as np
 import pygmt
 
 # Set up some test data
-x = np.arange(-10, 10, 0.1)
-y1 = np.sin(x)
-y2 = np.cos(x)
+x = np.arange(-10, 10, 0.2)
+y1 = np.sin(x) + 1.1
+y2 = np.cos(x) + 1.1
+y3 = np.sin(x/2) - 1.1
+y4 = np.cos(x/2) - 1.1
 
 
 # Create new Figure() object
 fig = pygmt.Figure()
 
+fig.basemap(
+    projection="X10c/7c",
+    region=[-10, 10, -3.5, 3.5],
+    frame=["WSne", "xaf+lx", "ya1f0.5+ly"],
+)
+
 # -----------------------------------------------------------------------------
-# Left: Vertical legend (one column, default)
-fig.basemap(projection="X10c", region=[-10, 10, -1.5, 1.5], frame=True)
+# Top: Vertical legend (one column, default)
 
 # Use the label parameter to state to text label for the legend entry
-fig.plot(x=x, y=y1, pen="1p,green3", label="Sine")
+fig.plot(x=x, y=y1, pen="1p,green3", label="Sine(x)")
 
-fig.plot(x=x, y=y2, style="c0.1c", fill="dodgerblue", label="Cosine")
+fig.plot(x=x, y=y2, style="c0.07c", fill="dodgerblue", label="Cosine(x)")
 
 # Add a legend to the plot; place it within the plot bounding box at position
 # ("J") TopRight with a anchor point ("j") TopRight and an offset of 0.2
 # centimeters in x and y directions; surround the legend with a box
 fig.legend(position="JTR+jTR+o0.2c", box=True)
 
-fig.shift_origin(xshift="+w1c")
 
 # -----------------------------------------------------------------------------
-# Right: Horizontal legend (here two columns)
-fig.basemap(projection="X10c", region=[-10, 10, -1.5, 1.5], frame=["wStr", "af"])
+# Bottom: Horizontal legend (here two columns)
 
 # +N sets the number of columns corresponding to the given number, here two
-fig.plot(x=x, y=y1, pen="1p,green3", label="Sine+N2")
+fig.plot(x=x, y=y3, pen="1p,darkred,-", label="Sine(x/2)+N2")
 
-fig.plot(x=x, y=y2, style="c0.1c", fill="dodgerblue", label="Cosine")
+fig.plot(x=x, y=y4, style="s0.07c", fill="orange", label="Cosine(x/2)")
 
-# For multi-column legends users have to provide the width via +w, here it is
-# set to 4.5 centimeters
-fig.legend(position="JTR+jTR+o0.2c+w4.5c", box=True)
+# For multi-column legends, users have to provide the width via +w, here it is
+# set to 5 centimeters; position and anchor point are set to BottoRight
+fig.legend(position="JBR+jBR+o0.2c+w5c", box=True)
+
 
 fig.show()
