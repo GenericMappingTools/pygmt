c3ebb8e742366703db24bb318241934a15adbf87
Dongdong Tian <seisman.info@gmail.com>
2023-11-05 18:53:39 +0800

    Add Figure.choropleth to plot choropleth maps

--- examples/gallery/maps/choropleth_map.py
+++ examples/gallery/maps/choropleth_map.py
@@ -2,15 +2,14 @@
 Choropleth map
 ==============
 
-The :meth:`pygmt.Figure.plot` method allows us to plot geographical data such as
-polygons which are stored in a :class:`geopandas.GeoDataFrame` object. Use
-:func:`geopandas.read_file` to load data from any supported OGR format such as a
+The :meth:`pygmt.Figure.choropleth` method allows us to plot geographical data such as
+polygons which are stored in a :class:`geopandas.GeoDataFrame` object or a OGR_GMT file.
+Use :func:`geopandas.read_file` to load data from any supported OGR formats such as a
 shapefile (.shp), GeoJSON (.geojson), geopackage (.gpkg), etc. You can also use a full
 URL pointing to your desired data source. Then, pass the :class:`geopandas.GeoDataFrame`
-as an argument to the ``data`` parameter of :meth:`pygmt.Figure.plot`, and style the
-geometry using the ``pen`` parameter. To fill the polygons based on a corresponding
-column you need to set ``fill="+z"`` as well as select the appropriate column using the
-``aspatial`` parameter as shown in the example below.
+as an argument to the ``data`` parameter of :meth:`pygmt.Figure.choropleth`, and style
+the geometry using the ``pen`` parameter. To fill the polygons based on a corresponding
+column you need to specify the colum name to the ``column`` parameter.
 """
 
 # %%
@@ -46,4 +45,25 @@ fig.colorbar(
     move_text="label",
 )
 
+<<<<<<< HEAD
+=======
+# The dataset contains different attributes, here we select the "population" column to
+# plot.
+
+# First, we define the colormap to fill the polygons based on the "population" column.
+pygmt.makecpt(
+    cmap="acton",
+    series=[gdf["population"].min(), gdf["population"].max(), 10],
+    continuous=True,
+    reverse=True,
+)
+
+# Next, we plot the polygons and fill them using the defined colormap. The target column
+# is specified by the `column` parameter.
+fig.choropleth(data=gdf, column="population", pen="0.3p,gray10", cmap=True)
+
+# Add colorbar legend
+fig.colorbar(frame="x+lPopulation", position="jML+o-0.5c+w3.5c/0.2c")
+
+>>>>>>> e5f21af07 (Add Figure.choropleth to plot choropleth maps)
 fig.show()
