cdf162cdc1d9793cd983a83c33707fa885d95de1
Dongdong Tian <seisman.info@gmail.com>
2023-11-14 14:19:48 +0800

    rioxarray needs to be imported

--- pygmt/src/tilemap.py
+++ pygmt/src/tilemap.py
@@ -1,11 +1,16 @@
 """
 tilemap - Plot XYZ tile maps.
 """
-import importlib
-
 from pygmt.clib import Session
 from pygmt.datasets.tile_map import load_tile_map
 from pygmt.helpers import build_arg_string, fmt_docstring, kwargs_to_strings, use_alias
+
+try:
+    import rioxarray  # noqa: F401
+
+    _HAS_RIOXARRAY = True
+except ImportError:
+    _HAS_RIOXARRAY = False
 
 
 @fmt_docstring
@@ -111,7 +116,7 @@ def tilemap(
     """
     kwargs = self._preprocess(**kwargs)  # pylint: disable=protected-access
 
-    if importlib.util.find_spec("rioxarray") is None:
+    if not _HAS_RIOXARRAY:
         raise ImportError(
             "Package `rioxarray` is required to be installed to use this function. "
             "Please use `python -m pip install rioxarray` or "
