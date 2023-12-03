bb520305791e57c0913f463150fb21b64e46fafd
Dongdong Tian <seisman.info@gmail.com>
2023-12-03 18:03:23 +0800

    Update pygmt/helpers/tempfile.py
    Co-authored-by: Michael Grund <23025878+michaelgrund@users.noreply.github.com>

--- pygmt/helpers/tempfile.py
+++ pygmt/helpers/tempfile.py
@@ -131,7 +131,7 @@ def tempfile_from_geojson(geojson):
         os.remove(tmpfile.name)  # ensure file is deleted first
         ogrgmt_kwargs = {"filename": tmpfile.name, "driver": "OGR_GMT", "mode": "w"}
         try:
-            # OGR_GMT only support 32-bit integers.
+            # OGR_GMT only supports 32-bit integers.
             # We need to map int/int64 types:
             # 1. to int32 type if the column is a 32-bit integer
             # 2. to float64 type if the column is big 64-bit integer
