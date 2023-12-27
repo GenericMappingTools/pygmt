8eff9dff25ef7b1c07a8237ca51df00dad946402
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-12-27 16:10:55 +1300

    Refactor test_image to plot @circuit.png instead of test_logo.png
    So that we don't need to pull test_logo.png from dvc, or run test_logo first to generate the image.

--- pygmt/tests/test_image.py
+++ pygmt/tests/test_image.py
@@ -1,13 +1,10 @@
 """
 Test Figure.image.
 """
-import os
 import sys
 
 import pytest
 from pygmt import Figure
-
-TEST_IMG = os.path.join(os.path.dirname(__file__), "baseline", "test_logo.png")
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="crashes on Windows")
@@ -17,5 +14,5 @@ def test_image():
     Place images on map.
     """
     fig = Figure()
-    fig.image(TEST_IMG, position="x0/0+w2c", box="+pthin,blue")
+    fig.image(imagefile="@circuit.png", position="x0/0+w2c", box="+pthin,blue")
     return fig
