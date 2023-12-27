f85aae7fd1c50745da50e93699ccabf1cddef70d
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-12-28 11:04:33 +1300

    Benchmark test_accessor_set_geographic_cartesian_roundtrip

--- pygmt/tests/test_accessor.py
+++ pygmt/tests/test_accessor.py
@@ -47,6 +47,7 @@ def test_accessor_set_pixel_registration():
     assert grid.gmt.registration == 1  # ensure changed to pixel registration
 
 
+@pytest.mark.benchmark
 def test_accessor_set_geographic_cartesian_roundtrip():
     """
     Check that we can set a grid to switch between the default Cartesian
