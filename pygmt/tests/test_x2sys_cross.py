f97d92efbafde01d951285577a6251dd5ac34269
Dongdong Tian <seisman.info@gmail.com>
2024-01-03 18:52:51 +0800

    Fix test_x2sys_cross.py

--- pygmt/tests/test_x2sys_cross.py
+++ pygmt/tests/test_x2sys_cross.py
@@ -142,8 +142,9 @@ def test_x2sys_cross_input_dataframe_with_nan(tracks):
             tag=tag, fmtfile="xyz", suffix="xyzt", units=["de", "se"], force=True
         )
 
-        tracks[0].loc[tracks[0]["z"] < -15, "z"] = np.nan  # set some values to NaN
-        output = x2sys_cross(tracks=tracks, tag=tag, coe="i")
+        newtracks = tracks.copy()
+        newtracks[0].loc[tracks[0]["z"] < -15, "z"] = np.nan  # set some values to NaN
+        output = x2sys_cross(tracks=newtracks, tag=tag, coe="i")
 
         assert isinstance(output, pd.DataFrame)
         assert output.shape == (3, 12)
