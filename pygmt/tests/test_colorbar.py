2a57a6580d42d16efea368c1590ca5ae6f8ae661
Dongdong Tian <seisman.info@gmail.com>
2024-01-10 13:25:13 +0800

    Add the test_colorbar_shading_list test back

--- pygmt/tests/test_colorbar.py
+++ pygmt/tests/test_colorbar.py
@@ -14,3 +14,14 @@ def test_colorbar():
     fig = Figure()
     fig.colorbar(cmap="rainbow", position="x0c/0c+w4c", frame=True)
     return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_colorbar_shading_list():
+    """
+    Create colorbar and set shading by passing the high/low values as a list.
+    """
+    fig = Figure()
+    fig.basemap(region=[0, 10, 0, 10], projection="X8c", frame="a")
+    fig.colorbar(cmap="geo", shading=[-0.7, 0.2], frame=True)
+    return fig
