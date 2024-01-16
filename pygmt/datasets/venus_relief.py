4de76a4ea533c5ec04b9dc18f3364ef5c3bef2ef
Dongdong Tian <seisman.info@gmail.com>
2024-01-16 17:02:36 +0800

    Updates

--- pygmt/datasets/venus_relief.py
+++ pygmt/datasets/venus_relief.py
@@ -1,6 +1,6 @@
 """
-Function to download the Venus relief dataset from the GMT data server, and
-load as :class:`xarray.DataArray`.
+Function to download the Venus relief dataset from the GMT data server, and load as
+:class:`xarray.DataArray`.
 
 The grids are available in various resolutions.
 """
@@ -27,23 +27,21 @@ def load_venus_relief(
 
        Venus relief dataset.
 
-    The grids are downloaded to a user data directory
-    (usually ``~/.gmt/server/venus/venus_relief/``) the first time you invoke
-    this function. Afterwards, it will load the grid from the data directory.
-    So you'll need an internet connection the first time around.
+    The grids are downloaded to a user data directory (usually
+    ``~/.gmt/server/venus/venus_relief/``) the first time you invoke this function.
+    Afterwards, it will load the grid from the data directory. So you'll need an
+    internet connection the first time around.
 
     These grids can also be accessed by passing in the file name
-    **@venus_relief**\_\ *res*\[_\ *reg*] to any grid processing function or
-    plotting method. *res* is the grid resolution (see below), and *reg* is the
-    grid registration type (**p** for pixel registration or **g** for gridline
-    registration).
+    **@venus_relief**\_\ *res*\[_\ *reg*] to any grid processing function or plotting
+    method. *res* is the grid resolution (see below), and *reg* is the grid registration
+    type (**p** for pixel registration or **g** for gridline registration).
 
-    The default color palette table (CPT) for this dataset is
-    *@venus_relief.cpt*. It's implicitly used when passing in the file name of
-    the dataset to any grid plotting method if no CPT is explicitly specified.
-    When the dataset is loaded and plotted as an :class:`xarray.DataArray`
-    object, the default CPT is ignored, and GMT's default CPT (*turbo*) is
-    used. To use the dataset-specific CPT, you need to explicitly set
+    The default color palette table (CPT) for this dataset is *@venus_relief.cpt*. It's
+    implicitly used when passing in the file name of the dataset to any grid plotting
+    method if no CPT is explicitly specified. When the dataset is loaded and plotted as
+    an :class:`xarray.DataArray` object, the default CPT is ignored, and GMT's default
+    CPT (*turbo*) is used. To use the dataset-specific CPT, you need to explicitly set
     ``cmap="@venus_relief.cpt"``.
 
     Refer to :gmt-datasets:`venus-relief.html` for more details about available
@@ -52,15 +50,14 @@ def load_venus_relief(
     Parameters
     ----------
     resolution : str
-        The grid resolution. The suffix ``d``, ``m`` and ``s`` stand for
-        arc-degrees, arc-minutes and arc-seconds. It can be ``"01d"``,
-        ``"30m"``, ``"20m"``, ``"15m"``, ``"10m"``, ``"06m"``, ``"05m"``,
-        ``"04m"``, ``"03m"``, ``"02m"``, and ``"01m"``.
+        The grid resolution. The suffix ``d``, ``m`` and ``s`` stand for arc-degrees,
+        arc-minutes and arc-seconds. It can be ``"01d"``, ``"30m"``, ``"20m"``,
+        ``"15m"``, ``"10m"``, ``"06m"``, ``"05m"``, ``"04m"``, ``"03m"``, ``"02m"``, and
+        ``"01m"``.
     region : str or list
         The subregion of the grid to load, in the form of a list
         [*xmin*, *xmax*, *ymin*, *ymax*] or a string *xmin/xmax/ymin/ymax*.
-        Required for grids with resolutions higher than 5 arc-minutes
-        (i.e., ``"05m"``).
+        Required for grids with resolutions higher than 5 arc-minutes (i.e., ``"05m"``).
     registration
         Grid registration type. Either ``"pixel"`` for pixel registration or
         ``"gridline"`` for gridline registration.
@@ -68,23 +65,21 @@ def load_venus_relief(
     Returns
     -------
     grid : :class:`xarray.DataArray`
-        The Venus relief grid. Coordinates are latitude and longitude in
-        degrees. Relief is in meters.
+        The Venus relief grid. Coordinates are latitude and longitude in degrees. Relief
+        is in meters.
 
     Note
     ----
     The registration and coordinate system type of the returned
-    :class:`xarray.DataArray` grid can be accessed via the GMT accessors
-    (i.e., ``grid.gmt.registration`` and ``grid.gmt.gtype`` respectively).
-    However, these properties may be lost after specific grid operations (such
-    as slicing) and will need to be manually set before passing the grid to any
-    PyGMT data processing or plotting functions. Refer to
-    :class:`pygmt.GMTDataArrayAccessor` for detailed explanations and
-    workarounds.
+    :class:`xarray.DataArray` grid can be accessed via the GMT accessors (i.e.,
+    ``grid.gmt.registration`` and ``grid.gmt.gtype`` respectively). However, these
+    properties may be lost after specific grid operations (such as slicing) and will
+    need to be manually set before passing the grid to any PyGMT data processing or
+    plotting functions. Refer to :class:`pygmt.GMTDataArrayAccessor` for detailed
+    explanations and workarounds.
 
     Examples
     --------
-
     >>> from pygmt.datasets import load_venus_relief
     >>> # load the default grid (gridline-registered 1 arc-degree grid)
     >>> grid = load_venus_relief()
