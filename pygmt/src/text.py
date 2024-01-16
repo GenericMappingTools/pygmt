47f23983b4fe7ad1118e68169294258a5908bc89
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-01-16 19:07:54 +0100

    Remove bold style

--- pygmt/src/text.py
+++ pygmt/src/text.py
@@ -137,7 +137,7 @@ def text_(  # noqa: PLR0912
         [Default is 15% of the font size]. Only used if ``pen`` or ``fill``
         are specified. Append the unit you want (**c** for centimeters,
         **i** for inches, or **p** for points; if not given we consult
-        :gmt-term:`PROJ_LENGTH_UNIT`) or **%** for a percentage of the font
+        :gmt-term:`PROJ_LENGTH_UNIT`) or *%* for a percentage of the font
         size. Optionally, use modifier **+t** to set the shape of the text
         box when using ``fill`` and/or ``pen``. Append lower case **o**
         to get a straight rectangle [Default is **o**]. Append upper case
