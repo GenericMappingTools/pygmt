ddc33051cf8389cf1050c1f477f73c5926c8c0e2
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-01-26 09:27:23 +0100

    Add docs

--- examples/gallery/lines/line_segment_ends.py
+++ examples/gallery/lines/line_segment_ends.py
@@ -36,12 +36,15 @@ fig.basemap(region=[0, 260, 0, 100], projection="x1p", frame="rltb")
 for line_cap in ["butt", "round", "square"]:
     # Change GMT default locally
     with pygmt.config(PS_LINE_CAP=line_cap):
-        # Plot dashed line
         color = dict_col[line_cap]
-        fig.plot(x=x, y=y, pen=f"10p,{color},20_20:0")
+        # Draw a 10-point thick line with 20-point long segments and gaps
+        fig.plot(x=x, y=y, pen=f"10p,{color},20_20")
 
-    fig.plot(x=x, y=y, pen="1p")
-    fig.plot(x=x, y=y, style="c0.1c", fill="white", pen="0.5p")
+    # Draw a 1-point thick solid black line to highlight PS_LINE setting appearances
+    fig.plot(x=x, y=y, pen="1p,black,solid")
+    # Mark data points as cicles
+    fig.plot(x=x, y=y, style="c0.1c", fill="white", pen="0.5p,")
+    # Add label for PS_LINE setting
     fig.text(text=line_cap, x=x[-1] + 20, y=y[-1], justify="LM")
 
     y = y - 20
@@ -59,11 +62,11 @@ fig.basemap(region=[0, 260, 0, 100], projection="x1p", frame="rltb")
 
 for line_join in ["bevel", "round", "miter"]:
     with pygmt.config(PS_LINE_JOIN=line_join, PS_MITER_LIMIT=1):
-        # Plot solid line
         color = dict_col[line_join]
+        # Draw a 7-point thick solid line
         fig.plot(x=x, y=y, pen=f"7p,{color},solid")
 
-    fig.plot(x=x, y=y, pen="1p")
+    fig.plot(x=x, y=y, pen="1p,black,solid")
     fig.plot(x=x, y=y, style="c0.1c", fill="white", pen="0.5p")
     fig.text(text=line_join, x=x[1] - 10, y=y[1], justify="RB")
 
