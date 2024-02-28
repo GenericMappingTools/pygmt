44f4ffde19140611e9203894f3218b2e2b08dfe1
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2024-02-28 02:59:10 +0100

    Figure: Remove deprecated "xshift" ("X") and "yshift" ("Y") parameters, use "Figure.shift_origin" instead (deprecated since v0.8.0) (#3044)
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/helpers/decorators.py
+++ pygmt/helpers/decorators.py
@@ -575,27 +575,21 @@ def use_alias(**aliases):
                 )
                 raise GMTInvalidInput(msg)
 
-            # xshift (X) is deprecated since v0.8.0.
+            # xshift (X) is deprecated since v0.8.0 and removed in v0.12.0.
             if "X" in kwargs or "xshift" in kwargs:
-                if "xshift" in kwargs:
-                    kwargs["X"] = kwargs.pop("xshift")
                 msg = (
-                    "Parameters 'X' and 'xshift' are deprecated since v0.8.0 "
-                    "and will be removed in v0.12.0. "
+                    "Parameters 'X' and 'xshift' are no longer supported since v0.12.0. "
                     "Use Figure.shift_origin(xshift=...) instead."
                 )
-                warnings.warn(msg, category=SyntaxWarning, stacklevel=2)
+                raise GMTInvalidInput(msg)
 
-            # yshift (Y) is deprecated since v0.8.0.
+            # yshift (Y) is deprecated since v0.8.0 and removed in v0.12.0.
             if "Y" in kwargs or "yshift" in kwargs:
-                if "yshift" in kwargs:
-                    kwargs["Y"] = kwargs.pop("yshift")
                 msg = (
-                    "Parameters 'Y' and 'yshift' are deprecated since v0.8.0. "
-                    "and will be removed in v0.12.0. "
+                    "Parameters 'Y' and 'yshift' are no longer supported since v0.12.0. "
                     "Use Figure.shift_origin(yshift=...) instead."
                 )
-                warnings.warn(msg, category=SyntaxWarning, stacklevel=2)
+                raise GMTInvalidInput(msg)
 
             return module_func(*args, **kwargs)
 
