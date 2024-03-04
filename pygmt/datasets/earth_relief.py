15f42b72064deba77870b627d93a17c98a879996
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-03-04 21:14:36 +0100

    Separte Earth relief dataset in 'igpp' and 'gebco'

--- pygmt/datasets/earth_relief.py
+++ pygmt/datasets/earth_relief.py
@@ -160,9 +160,13 @@ def load_earth_relief(
             )
     else:
         dataset_prefix = earth_relief_sources[data_source]
-
+    # Choose earth relief dataset
+    if data_source in ["igpp", "synbath"]:
+        dataset_name = "earth_igpp"
+    else if data_sourcce in ["gebco", "gebcosi"]:
+        dataset_name = "earth_gebco"
     grid = _load_remote_dataset(
-        dataset_name="earth_relief",
+        dataset_name=dataset_name,
         dataset_prefix=dataset_prefix,
         resolution=resolution,
         region=region,
