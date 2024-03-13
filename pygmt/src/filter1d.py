da5b23247f41434363cc8f1b7112a424034b1580
Dongdong Tian <seisman.info@gmail.com>
2024-03-13 14:34:02 +0800

    fix

--- pygmt/src/filter1d.py
+++ pygmt/src/filter1d.py
@@ -104,6 +104,7 @@ def filter1d(
     -------
     ret
         Return type depends on ``outfile`` and ``output_type``:
+
         - None if ``outfile`` is set (output will be stored in file set by ``outfile``)
         - :class:`pandas.DataFrame` or :class:`numpy.ndarray` if ``outfile`` is not set
           (depends on ``output_type``)
