d3a754d8e7aa3950b118833a9b5804a44deb46d6
Dongdong Tian <seisman.info@gmail.com>
2024-03-15 21:26:13 +0800

    fix

--- pygmt/src/triangulate.py
+++ pygmt/src/triangulate.py
@@ -224,7 +224,7 @@ class triangulate:  # noqa: N801
         ret
             Return type depends on ``outfile`` and ``output_type``:
 
-            - None if ``outfile`` is set (output will be stored in file set by
+            - ``None`` if ``outfile`` is set (output will be stored in file set by
               ``outfile``)
             - :class:`pandas.DataFrame` or :class:`numpy.ndarray` if ``outfile`` is not
               set (depends on ``output_type``)
