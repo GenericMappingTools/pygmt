175ba3c7cc5c5126f015ef7e7a1ba95b6ad8dacc
Dongdong Tian <seisman.info@gmail.com>
2024-03-21 13:29:36 +0800

    GMT_DATASET: Return an empty DataFrame if the file has no data

--- pygmt/datatypes/dataset.py
+++ pygmt/datatypes/dataset.py
@@ -13,8 +13,8 @@ class _GMT_DATASET(ctp.Structure):  # noqa: N801
     """
     GMT dataset structure for holding multiple tables (files).
 
-    This class is only meant for internal use by PyGMT and is not exposed to users.
-    See the GMT source code gmt_resources.h for the original C struct definitions.
+    This class is only meant for internal use and is not exposed to users. See the GMT
+    source code ``gmt_resources.h`` for the original C struct definitions.
 
     Examples
     --------
@@ -151,6 +151,8 @@ class _GMT_DATASET(ctp.Structure):  # noqa: N801
         the same. The same column in all segments of all tables are concatenated. The
         trailing text column is also concatenated as a single string column.
 
+        If the object has no data, an empty DataFrame will be returned.
+
         Returns
         -------
         df
@@ -185,8 +187,8 @@ class _GMT_DATASET(ctp.Structure):  # noqa: N801
         >>> df.dtypes.to_list()
         [dtype('float64'), dtype('float64'), dtype('float64'), string[python]]
         """
-        # Deal with numeric columns
         vectors = []
+        # Deal with numeric columns
         for icol in range(self.n_columns):
             colvector = []
             for itbl in range(self.n_tables):
@@ -211,5 +213,5 @@ class _GMT_DATASET(ctp.Structure):  # noqa: N801
                 pd.Series(data=np.char.decode(textvector), dtype=pd.StringDtype())
             )
 
-        df = pd.concat(objs=vectors, axis=1)
+        df = pd.concat(objs=vectors, axis=1) if vectors else pd.DataFrame()
         return df
