02d01512cbb7e73e40bd6ad410b96024ebbe7731
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-04-14 22:02:36 +0200

    Fix typo

--- pygmt/src/plot.py
+++ pygmt/src/plot.py
@@ -151,7 +151,7 @@ def plot(  # noqa: PLR0912
     {fill}
         *fill* can be a 1-D array, but it is only valid if using ``x``/``y``
         and ``cmap=True`` is also required.
-    fill_curves : str
+    fillcurves : str
         [**c**\|\ **s**][**+l**\ *seclabel*][**+g**\ *fill*][**p**\ *pen*][**+r**\ *pen*][**+y**\ [*level*]].
         Fill the middle area between two curves :math:`y_0(x)` and :math:`y_1(x)`.
     intensity : float, bool, or 1-D array
