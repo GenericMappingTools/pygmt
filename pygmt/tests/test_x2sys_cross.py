b9b4098e94a970fc00387ef83609b5a7df62114e
Dongdong Tian <seisman.info@gmail.com>
2024-05-07 11:12:47 +0800

    Improve tests

--- pygmt/tests/test_x2sys_cross.py
+++ pygmt/tests/test_x2sys_cross.py
@@ -50,15 +50,17 @@ def test_x2sys_cross_input_file_output_file():
         x2sys_init(tag=tag, fmtfile="xyz", force=True)
         outfile = tmpdir_p / "tmp_coe.txt"
         output = x2sys_cross(
-            tracks=["@tut_ship.xyz"],
-            tag=tag,
-            coe="i",
-            outfile=outfile,
+            tracks=["@tut_ship.xyz"], tag=tag, coe="i", outfile=outfile
         )
-
         assert output is None  # check that output is None since outfile is set
         assert outfile.stat().st_size > 0  # check that outfile exists at path
-        _ = pd.read_csv(outfile, sep="\t", header=2)  # ensure ASCII text file loads ok
+        result = pd.read_csv(outfile, sep="\t", comment=">", header=2)
+        assert result.shape == (14338, 12)
+        columns = list(result.columns)
+        assert columns[:6] == ["# x", "y", "i_1", "i_2", "dist_1", "dist_2"]
+        assert columns[6:] == ["head_1", "head_2", "vel_1", "vel_2", "z_X", "z_M"]
+        npt.assert_allclose(result["i_1"].min(), 45.2099, rtol=1.0e-4)
+        npt.assert_allclose(result["i_1"].max(), 82945.93699, rtol=1.0e-4)
 
 
 @pytest.mark.usefixtures("mock_x2sys_home")
