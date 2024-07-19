277a30d4566df1859a766e8c855e0dcc8bd41891
Dongdong Tian <seisman.info@gmail.com>
2024-07-19 16:52:35 +0800

    Remove the to_dataarray method

--- pygmt/datatypes/cube.py
+++ pygmt/datatypes/cube.py
@@ -5,13 +5,10 @@ Wrapper for the GMT_CUBE data type.
 import ctypes as ctp
 from typing import ClassVar
 
-import numpy as np
-import xarray as xr
 from pygmt.datatypes.header import (
     _GMT_GRID_HEADER,
     GMT_GRID_UNIT_LEN80,
     GMT_GRID_VARNAME_LEN80,
-    _parse_nameunits,
     gmt_grdfloat,
 )
 
@@ -46,48 +43,3 @@ class _GMT_CUBE(ctp.Structure):  # noqa: N801
         # Units in 3rd direction (complements x_units, y_units, z_units)
         ("units", ctp.c_char * GMT_GRID_UNIT_LEN80),
     ]
-
-    def to_dataarray(self):
-        """
-        Convert the GMT_CUBE to an xarray.DataArray.
-
-        Returns
-        -------
-            xarray.DataArray: The data array representation of the GMT_CUBE.
-        """
-        # The grid header
-        header = self.header.contents
-
-        name = "cube"
-        # Dimensions and attributes
-        dims = header.dims
-        dim_attrs = header.dim_attrs
-
-        # Patch for the 3rd dimension
-        dims.append("z")
-        z_attrs = {"actual_range": np.array(self.z_range[:]), "axis": "Z"}
-        long_name, units = _parse_nameunits(self.units.decode())
-        if long_name:
-            z_attrs["long_name"] = long_name
-        if units:
-            z_attrs["units"] = units
-        dim_attrs.append(z_attrs)
-
-        # The coordinates, given as a tuple of the form (dims, data, attrs)
-        coords = [
-            (dims[0], self.y[: header.n_rows], dim_attrs[0]),
-            (dims[1], self.x[: header.n_columns], dim_attrs[1]),
-            # header->n_bands is used for the number of layers for 3-D cubes
-            (dims[2], self.z[: header.n_bands], dim_attrs[1]),
-        ]
-
-        # The data array without paddings
-        pad = header.pad[:]
-        data = np.reshape(
-            self.data[: header.mx * header.my * header.n_bands],
-            (header.my, header.mx, header.n_bands),
-        )[pad[2] : header.my - pad[3], pad[0] : header.mx - pad[1], :]
-
-        # Create the xarray.DataArray object
-        grid = xr.DataArray(data, coords=coords, name=name, attrs=header.dataA_attrs)
-        return grid
