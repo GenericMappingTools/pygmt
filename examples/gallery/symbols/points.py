96fab42c527f266b2cb9f721f61da3695cb4dd76
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-07-20 16:11:25 +0200

    Remove confusing word

--- examples/gallery/symbols/points.py
+++ examples/gallery/symbols/points.py
@@ -2,8 +2,8 @@
 Points
 ======
 
-The :meth:`pygmt.Figure.plot` method can plot data points. The plot symbol
-and size are set with the ``style`` parameter.
+The :meth:`pygmt.Figure.plot` method can plot data points. The symbol and
+size are set with the ``style`` parameter.
 """
 
 # %%
