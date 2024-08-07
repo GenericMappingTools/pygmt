047fbeb25fc9a91dfffe4e4b0a5c6b9743dde270
Dongdong Tian <seisman.info@gmail.com>
2024-08-07 10:57:23 +0800

    Fix the formatting of the list

--- examples/tutorials/advanced/earth_relief.py
+++ examples/tutorials/advanced/earth_relief.py
@@ -88,7 +88,7 @@ fig.show()
 # In addition to providing global data, the ``region`` parameter for
 # :func:`pygmt.datasets.load_earth_relief` can be used to provide data for a specific
 # area. The ``region`` parameter is required for resolutions at 5 arc-minutes or higher,
-# and accepts a list in the form of ``[*xmin*, *xmax*, *ymin*, *ymax*]``.
+# and accepts a list in the form of [*xmin*, *xmax*, *ymin*, *ymax*].
 #
 # The example below uses data with a 10 arc-minutes resolution, and plots it on a
 # 15-centimeters-wide figure with a Mercator projection and a CPT set to *geo*.
