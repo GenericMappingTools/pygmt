364b0f20440b530942bec7e607b261d3d82b04c3
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-09-12 10:14:49 +0200

    Rewrapp to 88 characters

--- examples/gallery/lines/vector_styles.py
+++ examples/gallery/lines/vector_styles.py
@@ -2,9 +2,9 @@
 Cartesian, circular, and geographic vectors
 ===========================================
 
-The :meth:`pygmt.Figure.plot` method can plot Cartesian, circular, and
-geographic vectors. The ``style`` parameter controls vector attributes.
-See also the :doc:`Vector attributes example </gallery/lines/vector_heads_tails>`.
+The :meth:`pygmt.Figure.plot` method can plot Cartesian, circular, and geographic
+vectors. The ``style`` parameter controls vector attributes. See also the
+:doc:`Vector attributes example </gallery/lines/vector_heads_tails>`.
 """
 
 # %%
@@ -28,8 +28,8 @@ x = np.linspace(-116, -116, 12)  # x vector coordinates
 y = np.linspace(33.5, 42.5, 12)  # y vector coordinates
 direction = np.zeros(x.shape)  # direction of vectors
 length = np.linspace(0.5, 2.4, 12)  # length of vectors
-# Cartesian vectors (v) with red fill and pen (+g, +p), vector head at the
-# end (+e), and 40 degree angle (+a) with no indentation for the vector head (+h)
+# Cartesian vectors (v) with red fill and pen (+g, +p), vector head at the end (+e), and
+# 40 degree angle (+a) with no indentation for the vector head (+h)
 style = "v0.2c+e+a40+gred+h0+p1p,red"
 fig.plot(x=x, y=y, style=style, pen="1p,red", direction=[direction, length])
 fig.text(text="CARTESIAN", x=-112, y=44.2, font="13p,Helvetica-Bold,red", fill="white")
@@ -54,9 +54,8 @@ NYC = [-74.0060, 40.7128]
 CHI = [-87.6298, 41.8781]
 SEA = [-122.3321, 47.6062]
 NO = [-90.0715, 29.9511]
-# '=' means geographic vectors.
-# With the modifier '+s', the input data should contain coordinates of start
-# and end points
+# '=' means geographic vectors. With the modifier '+s', the input data should contain
+# coordinates of start and end points
 style = "=0.5c+s+e+a30+gblue+h0.5+p1p,blue"
 data = np.array([NYC + CHI, NYC + SEA, NYC + NO])
 fig.plot(data=data, style=style, pen="1.0p,blue")
