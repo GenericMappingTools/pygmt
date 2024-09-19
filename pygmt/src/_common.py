258e58d0502ec9319b306e2ca079ad240621fd03
Dongdong Tian <seisman.info@gmail.com>
2024-09-19 16:40:45 +0800

    Figure.plot & Figure.plot3d: Use the private function to determine if data contains points

---
+++ pygmt/src/_common.py
@@ -0,0 +1,39 @@
+"""
+Common functions shared by multiple PyGMT functions/methods.
+"""
+
+from pathlib import Path
+from typing import Any
+
+from pygmt.src.which import which
+
+
+def _data_geometry_is_point(data: Any, kind: str) -> bool:
+    """
+    Check if the geometry of the GeoJSON object or the OGR_GMT file is Point/MultiPoint.
+
+    This private function is used in ``Figure.plot`` and ``Figure.plot3d``.
+
+    Parameters
+    ----------
+    data
+        The data being plotted.
+    kind
+        The data kind.
+
+    Returns
+    -------
+    bool
+        ``True`` if the geometry is Point/MultiPoint, ``False`` otherwise.
+    """
+    if kind == "geojson" and data.geom_type.isin(["Point", "MultiPoint"]).all():
+        return True
+    if kind == "file" and str(data).endswith(".gmt"):  # OGR_GMT file
+        try:
+            with Path(which(data)).open(encoding="utf-8") as file:
+                line = file.readline()
+            if "@GMULTIPOINT" in line or "@GPOINT" in line:
+                return True
+        except FileNotFoundError:
+            pass
+    return False
