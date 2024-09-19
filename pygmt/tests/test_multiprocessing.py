a392ee078fb1245d695db1cf45e5282f261a7d3f
Dongdong Tian <seisman.info@gmail.com>
2024-09-19 08:32:14 +0800

    Add the old test back

--- pygmt/tests/test_multiprocessing.py
+++ pygmt/tests/test_multiprocessing.py
@@ -3,6 +3,7 @@ Test multiprocessing support.
 """
 
 import multiprocessing as mp
+from importlib import reload
 from pathlib import Path
 
 from pygmt import Figure
@@ -24,5 +25,35 @@ def test_multiprocessing():
     prefix = "test_session_multiprocessing"
     with mp.Pool(2) as p:
         p.map(_func, [f"{prefix}-1.png", f"{prefix}-2.png"])
+    Path(f"{prefix}-1.png").unlink()
+    Path(f"{prefix}-2.png").unlink()
+
+
+def _func_reload(figname):
+    """
+    A wrapper for running PyGMT scripts with multiprocessing.
+
+    Before the official multiprocessing support in PyGMT, we have to reload the
+    PyGMT library. Workaround from
+    https://github.com/GenericMappingTools/pygmt/issues/217#issuecomment-754774875.
+
+    This test makes sure that the old workaround still works.
+    """
+    import pygmt
+
+    reload(pygmt)
+    fig = pygmt.Figure()
+    fig.basemap(region=[10, 70, -3, 8], projection="X8c/6c", frame="afg")
+    fig.savefig(figname)
+
+
+def test_multiprocessing_reload():
+    """
+    Make sure that multiprocessing is supported if pygmt is re-imported.
+    """
+
+    prefix = "test_session_multiprocessing"
+    with mp.Pool(2) as p:
+        p.map(_func_reload, [f"{prefix}-1.png", f"{prefix}-2.png"])
     Path(f"{prefix}-1.png").unlink()
     Path(f"{prefix}-2.png").unlink()
