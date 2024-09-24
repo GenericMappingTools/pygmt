bd390eebd3117dbb35c059b0c50de805ae935b84
Dongdong Tian <seisman.info@gmail.com>
2024-09-24 18:15:42 +0800

    Minor fix docstring of Figure.timestamp

--- pygmt/src/timestamp.py
+++ pygmt/src/timestamp.py
@@ -53,7 +53,7 @@ def timestamp(
     font
         Font of the timestamp and the optional label. Since the GMT logo has a fixed
         height, the font sizes are fixed to be 8-point for the timestamp and 7-point for
-        the label. The parameter can't change the font color for GMT 6.4.0, only the
+        the label. The parameter can't change the font color for GMT<=6.4.0, only the
         font style.
     timefmt
         Format string for the UNIX timestamp. The format string is parsed by the C
