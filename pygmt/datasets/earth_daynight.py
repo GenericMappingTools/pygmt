e886408f993a89d1ba9700fdcd9a41007ab878d3
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-09-30 14:22:17 +1300

    Add note on registration/gtype
    Xref #2384.

--- pygmt/datasets/earth_daynight.py
+++ pygmt/datasets/earth_daynight.py
@@ -68,6 +68,16 @@ def load_blue_marble(
     image
         The NASA Blue Marble image. Coordinates are latitude and longitude in degrees.
 
+    Note
+    ----
+    The registration and coordinate system type of the returned
+    :class:`xarray.DataArray` image can be accessed via the GMT accessors (i.e.,
+    ``image.gmt.registration`` and ``image.gmt.gtype`` respectively). However, these
+    properties may be lost after specific image operations (such as slicing) and will
+    need to be manually set before passing the image to any PyGMT data processing or
+    plotting functions. Refer to :class:`pygmt.GMTDataArrayAccessor` for detailed
+    explanations and workarounds.
+
     Examples
     --------
 
