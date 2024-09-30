755275e215bdc84e230731bcb018586d903893de
Wei Ji <23487320+weiji14@users.noreply.github.com>
2024-09-30 14:36:16 +1300

    Remove test_blue_marble_01m_default_registration
    The earth_day images are always pixel-registered anyway.

--- pygmt/tests/test_datasets_earth_day.py
+++ pygmt/tests/test_datasets_earth_day.py
@@ -42,21 +42,3 @@ def test_blue_marble_01d_with_region():
     npt.assert_allclose(data.x, np.arange(-9.5, 10.5, 1))
     npt.assert_allclose(data.min(), 10, atol=1)
     npt.assert_allclose(data.max(), 77, atol=1)
-
-
-def test_blue_marble_01m_default_registration():
-    """
-    Test that the grid returned by default for the 1 arc-minute resolution has
-    a "pixel" registration.
-    """
-    data = load_blue_marble(resolution="01m", region=[-10, -9, 3, 5])
-    assert data.shape == (3, 120, 60)
-    assert data.dtype == "uint8"
-    assert data.gmt.registration == 1
-    assert data.gmt.gtype == 1
-    npt.assert_allclose(data.coords["y"].data.min(), 3.0083333333333333)
-    npt.assert_allclose(data.coords["y"].data.max(), 4.991666666666666)
-    npt.assert_allclose(data.coords["x"].data.min(), -9.991666666666667)
-    npt.assert_allclose(data.coords["x"].data.max(), -9.008333333333335)
-    npt.assert_allclose(data.min(), 10, atol=1)
-    npt.assert_allclose(data.max(), 79, atol=1)
