cc0632661ea81b4af38bab496cca6280b133a231
Dongdong Tian <seisman.info@gmail.com>
2024-10-20 22:56:59 +0800

    Use patch as a decorator instead

--- pygmt/tests/test_figure.py
+++ pygmt/tests/test_figure.py
@@ -6,7 +6,7 @@ Doesn't include the plotting commands which have their own test files.
 
 import importlib
 from pathlib import Path
-from unittest import mock
+from unittest.mock import Mock, patch
 
 import numpy as np
 import numpy.testing as npt
@@ -433,18 +433,16 @@ class TestGetDefaultDisplayMethod:
         assert _get_default_display_method() == "none"
 
     @pytest.mark.skipif(not _HAS_IPYTHON, reason="Run when IPython is installed")
+    # Mock IPython.get_ipython() to return an object with a config attribute, so PyGMT
+    # can detect that an IPython kernel is running.
+    @patch("IPython.get_ipython", return_value=Mock(config={"IPKernelApp": True}))
     def test_notebook_display(self):
         """
         Default display method is "notebook" when an IPython kernel is running.
         """
-        # Mock IPython.get_ipython() to return an object with a config attribute,
-        # so PyGMT can detect that an IPython kernel is running.
-        with mock.patch(
-            "IPython.get_ipython", return_value=mock.Mock(config={"IPKernelApp": True})
-        ):
-            # Display method should be "notebook" when an IPython kernel is running.
-            assert _get_default_display_method() == "notebook"
+        # Display method should be "notebook" when an IPython kernel is running.
+        assert _get_default_display_method() == "notebook"
 
-            # PYGMT_USE_EXTERNAL_DISPLAY should not affect notebook display.
-            with mock.patch.dict("os.environ", {"PYGMT_USE_EXTERNAL_DISPLAY": "false"}):
-                assert _get_default_display_method() == "notebook"
+        # PYGMT_USE_EXTERNAL_DISPLAY should not affect notebook display.
+        with patch.dict("os.environ", {"PYGMT_USE_EXTERNAL_DISPLAY": "false"}):
+            assert _get_default_display_method() == "notebook"
