53fd75c4084d7f368a3582970865a39c7f2e05e0
Dongdong Tian <seisman.info@gmail.com>
2024-10-22 23:18:00 +0800

    Fix a typo

--- pygmt/figure.py
+++ pygmt/figure.py
@@ -528,7 +528,7 @@ class Figure:
         html = '<img src="data:image/png;base64,{image}" width="{width}px">'
         return html.format(image=base64_png.decode("utf-8"), width=500)
 
-    from pygmt.src import (  # type: ignore [misc]
+    from pygmt.src import (  # type: ignore[misc]
         basemap,
         coast,
         colorbar,
