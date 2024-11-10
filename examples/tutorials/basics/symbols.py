530830c9b6510574bc5876ca5f898296378e4e6a
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-11-10 22:38:08 +0100

    Add seperate example for 'pen' and 'fill' parameters

--- examples/tutorials/basics/symbols.py
+++ examples/tutorials/basics/symbols.py
@@ -3,13 +3,13 @@ Plotting single-parameter symbols
 =================================
 
 The :meth:`pygmt.Figure.plot` method can plot symbols via the ``style``, ``size``, and
-``symbol`` parameters. Via the ``fill`` parameter the symbols can be filled with a color
-or pattern. The ``pen`` parameter can add an outline by providing a string argument in
-the form *width*,\ *color*,\ *style*. The defaults are no fill and a 0.25-points thick,
-black, solid outline. For the available patterns see the Technical Reference
-:doc:`Bit and hachure patterns </techref/patterns>`. For details on adjusting ``pen``
-see the Gallery example :doc:`Line styles </gallery/lines/linestyles>`. For the
-available single- and multi-parameter symbols see the Gallery examples
+``symbol`` parameters. The ``fill`` parameter can fill the symbols with a color or
+pattern. For the available patterns see the Technical Reference
+:doc:`Bit and hachure patterns </techref/patterns>`. Using the ``pen`` parameter a (the)
+outline can be added (adjusted) by providing a string argument in the form
+*width*,\ *color*,\ *style*. For details on adjusting ``pen`` see the Gallery example
+:doc:`Line styles </gallery/lines/linestyles>`. For the available single- and multi-
+parameter symbols see the Gallery examples
 :doc:`Single-parameter symbols </gallery/symbols/basic_symbols>` and
 :doc:`Multi-parameter symbols </gallery/symbols/multi_parameter_symbols>`, respectively.
 """
@@ -24,6 +24,9 @@ y = np.array([0, 0, 0, 0, 0])
 
 
 # %%
+# Plot single-parameter symbols
+# -----------------------------
+#
 # Use the ``style`` parameter of the :meth:`pygmt.Figure.plot` method to plot all data
 # points with the same symbol and size.
 
@@ -31,14 +34,36 @@ fig = pygmt.Figure()
 fig.basemap(region=[-5, 5, -2, 2], projection="X10c/4c", frame=True)
 
 # Plot circles (first "c") with a diameter of 0.5 centimeters (second "c")
+fig.plot(x=x, y=y, style="c0.5c")
+
+fig.show()
+
+# %%
+# Use the ``fill`` and ``pen`` parameters to add a fill color (or pattern) and an
+# outline, respectively. Note, that no outline is drawn by default when ``fill`` is
+# used.
+
+fig = pygmt.Figure()
+fig.basemap(region=[-5, 5, -2, 2], projection="X10c/4c", frame=True)
+
+# Add a color (or pattern) via the fill parameter
+fig.plot(x=x, y=y, style="c0.5c", fill="gray")
+
+fig.shift_origin(xshift="w+1c")
+fig.basemap(region=[-5, 5, -2, 2], projection="X10c/4c", frame=True)
+
+# Add (Adjust) an (the) outline via the pen parameter
 fig.plot(x=x, y=y, style="c0.5c", fill="gray", pen="1p,orange")
 
 fig.show()
 
 
 # %%
+# Use individual sizes
+# --------------------
+#
 # Use the ``size`` parameter to plot the data points with individual sizes. Provide
-# the different sizes as a list or array of floats.
+# the different sizes as a NumPy array or array-like object of integers or floats.
 
 fig = pygmt.Figure()
 fig.basemap(region=[-5, 5, -2, 2], projection="X10c/4c", frame=True)
@@ -49,7 +74,7 @@ fig.plot(
     # Plot circles (first "c") with a diameter in centimeters (second "c")
     style="cc",
     # Use individual sizes
-    size=[0.5, 0.2, 0.4, 0.6, 0.3],
+    size=np.array([0.5, 0.2, 0.4, 0.6, 0.3]),
     fill="gray",
     pen="1p,orange",
 )
@@ -58,6 +83,9 @@ fig.show()
 
 
 # %%
+# Use individual symbols
+# ----------------------
+#
 # Use the ``symbol`` parameter to plot the data points with individual symbols. Provide
 # the different symbols as a list of strings. Here, only single-parameter symbols can
 # be used.
@@ -80,10 +108,13 @@ fig.show()
 
 
 # %%
+# Use individual symbols and sizes
+# --------------------------------
+#
 # Use the ``symbol`` and ``size`` parameters together to plot the data points with
 # individual symbols and sizes. The arguments passed to ``symbol`` and ``size`` must
 # have the same length. The unit for ``size`` is now set via the GMT default parameter
-# ``PROJ_LENGTH_UNIT`` which can by adjusted using :class:`pygmt.config`[Default is
+# ``PROJ_LENGTH_UNIT`` which can by adjusted using :class:`pygmt.config` [Default is
 # centimeters].
 
 fig = pygmt.Figure()
@@ -93,11 +124,11 @@ fig.plot(
     x=x,
     y=y,
     symbol=["c", "s", "t", "i", "d"],
-    size=[0.5, 0.2, 0.4, 0.6, 0.3],
+    size=np.array([0.5, 0.2, 0.4, 0.6, 0.3]),
     fill="gray",
     pen="1p,orange",
 )
 
 fig.show()
 
-# sphinx_gallery_thumbnail_number = 4
+# sphinx_gallery_thumbnail_number = 5
