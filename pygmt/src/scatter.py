5ca92f7a04a7f4bb77a94a805871acf4779aa6a3
Dongdong Tian <seisman.info@gmail.com>
2024-11-11 18:13:18 +0800

    Add more parameters

--- pygmt/src/scatter.py
+++ pygmt/src/scatter.py
@@ -25,16 +25,24 @@ def _parse_symbol_size(symbol, size):
     return "".join(f"{arg}" for arg in [symbol, size] if not is_nonstr_iter(arg))
 
 
-def scatter(
+def scatter(  # noqa: PLR0913
     self,
     x,
     y,
     symbol: str | Sequence[str],
     size: float | str | Sequence[float | str],
-    **kwargs,
+    fill: str | Sequence[float] | None = None,
+    intensity: float | Sequence[float] | None = None,
+    transparency: float | Sequence[float] | None = None,
+    cmap: str | None = None,
+    no_clip: bool = False,
+    perspective=None,
 ):
     """
     Plot scatter points on a map.
+
+    This function can plot points with different symbols, sizes, and colors.
+
 
     Parameters
     ----------
@@ -60,8 +68,18 @@ def scatter(
         - ``y``: Y-dash (|)
     size
         The size of the points.
+    intensity
+        The intensity of the points.
+    transparency
+        The transparency of the points.
+    cmap
+        The colormap to use for the points.
+    no_clip
+        If True, do not clip the points to the viewport.
+    perspective
+        The perspective of the points.
     """
-    kwargs = self._preprocess(**kwargs)
+    self._preprocess()
 
     # style is a combination of symbol and size, but only if they are string-like
     style = _parse_symbol_size(symbol, size)
@@ -70,4 +88,16 @@ def scatter(
     if not is_nonstr_iter(size):
         size = None
 
-    self.plot(x=x, y=y, style=style, symbol=symbol, size=size, **kwargs)
+    self.plot(
+        x=x,
+        y=y,
+        style=style,
+        symbol=symbol,
+        size=size,
+        fill=fill,
+        intensity=intensity,
+        transparency=transparency,
+        cmap=cmap,
+        no_clip=no_clip,
+        perspective=perspective,
+    )
