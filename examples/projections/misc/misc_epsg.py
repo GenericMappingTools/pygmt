08c475818814b05c8a90d42d52bf9aa85cd3667f
Ben <bwwjohnson@gmail.com>
2024-11-16 19:13:20 +0000

    added misc_epsg

--- examples/projections/misc/misc_epsg.py
+++ examples/projections/misc/misc_epsg.py
@@ -2,7 +2,7 @@ r"""
 Using EPSG codes to specify projection
 ===============================
 
-The EPSG registry is a public registry of geodetic datums, spatial reference systems, 
+The EPSG registry is a public registry of geodetic datums, spatial reference systems,
 Earth ellipsoids, coordinate transformations and related units of measurement.
 
 These are used frequently in GIS software and data. If data are plotted using a linear
@@ -14,15 +14,18 @@ Some common EPSG codes are:
 EPSG:4326 - WGS 84 - latitude/longitude coordinate system based on the Earth's centre
 of mass, used by the Global Positioning System among others.
 
-EPSG:3857 - Web Mercator - used for display by many web-based mapping tools, including 
+EPSG:3857 - Web Mercator - used for display by many web-based mapping tools, including
 Google Maps and OpenStreetMap.
 
 
 **[EPSG:XXXX]**
 
 """
+
 import pygmt
 
 fig = pygmt.Figure()
-fig.coast(region=[-180, 180, -80, 80], projection="EPSG:3857", frame=True, shorelines="1p")
+fig.coast(
+    region=[-180, 180, -80, 80], projection="EPSG:3857", frame=True, shorelines="1p"
+)
 fig.show()
