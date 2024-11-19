f2ffad1a2da8c3f2a096b63e72df0d280c030b83
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2024-11-19 13:50:26 +0100

    Re-wrap really to 88 chars
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/gallery/lines/envelope.py
+++ examples/gallery/lines/envelope.py
@@ -2,10 +2,10 @@
 Envelope
 ========
 
-The ``close`` parameter of the :meth:`pygmt.Figure.plot` method can be used to build
-a symmetrical or an asymmetrical envelope. The user can give either the deviations
-or the bounds in y-direction. For the first case append ``"+d"`` or ``"+D"`` and for
-the latter case ``"+b"``.
+The ``close`` parameter of the :meth:`pygmt.Figure.plot` method can be used to build a
+symmetrical or an asymmetrical envelope. The user can give either the deviations or the
+bounds in y-direction. For the first case append ``"+d"`` or ``"+D"`` and for the latter
+case ``"+b"``.
 """
 
 # %%
