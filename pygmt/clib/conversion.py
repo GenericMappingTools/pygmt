56a266dcf2b6fb06a2794f9ae7f82f2f042ab8c1
Dongdong Tian <seisman.info@gmail.com>
2024-12-12 15:42:58 +0800

    Add workaround for pandas 2.0

--- pygmt/clib/conversion.py
+++ pygmt/clib/conversion.py
@@ -195,7 +195,12 @@ def _to_numpy(data: Any) -> np.ndarray:
 
     # Deal with timezone-aware datetime dtypes.
     if getattr(dtype, "tz", None):  # pandas.DatetimeTZDtype
-        numpy_dtype = getattr(dtype, "base", None)
+        if Version(pd.__version__) < Version("2.1"):
+            # Workaround for bug https://github.com/pandas-dev/pandas/issues/52705.
+            # Solution from https://github.com/pandas-dev/pandas/pull/52706.
+            numpy_dtype = np.dtype(f"M8[{dtype.unit}]")
+        else:
+            numpy_dtype = getattr(dtype, "base", None)
     elif getattr(getattr(dtype, "pyarrow_dtype", None), "tz", None):
         # pd.ArrayDtype[pa.Timestamp]
         numpy_dtype = getattr(dtype, "numpy_dtype", None)
