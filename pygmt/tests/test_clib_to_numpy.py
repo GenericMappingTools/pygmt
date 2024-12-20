d50cd3de49066b0e2f9ae1db751b42c5093530d4
Dongdong Tian <seisman.info@gmail.com>
2024-12-20 14:48:19 +0800

    Update pygmt/tests/test_clib_to_numpy.py

--- pygmt/tests/test_clib_to_numpy.py
+++ pygmt/tests/test_clib_to_numpy.py
@@ -284,7 +284,7 @@ def test_to_numpy_pandas_numeric_with_na(dtype, expected_dtype):
         "string[python]",
         pytest.param("string[pyarrow]", marks=skip_if_no(package="pyarrow")),
         pytest.param(
-            pd.StringDtype(storage="pyarrow"),
+            pd.StringDtype(storage="pyarrow", na_value=np.nan),
             marks=[
                 skip_if_no(package="pyarrow"),
                 pytest.mark.skipif(
