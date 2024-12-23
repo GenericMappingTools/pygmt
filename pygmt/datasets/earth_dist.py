c9990bb0405a33c058a50730f62f352839235689
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2024-12-23 22:49:50 +0100

    Fix typos
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/datasets/earth_dist.py
+++ pygmt/datasets/earth_dist.py
@@ -1,6 +1,6 @@
 """
 Function to download the GSHHG Earth distance to shoreline dataset from the GMT data
-server, and loadas :class:`xarray.DataArray`.
+server, and load as :class:`xarray.DataArray`.
 
 The grids are available in various resolutions.
 """
@@ -40,12 +40,12 @@ def load_earth_dist(
     method. *res* is the grid resolution (see below), and *reg* is the grid registration
     type (**p** for pixel registration or **g** for gridline registration).
 
-    The default color palette table (CPT) for this dataset is *earth_dist*. It's
+    The default color palette table (CPT) for this dataset is *@earth_dist.cpt*. It's
     implicitly used when passing in the file name of the dataset to any grid plotting
     method if no CPT is explicitly specified. When the dataset is loaded and plotted
     as an :class:`xarray.DataArray` object, the default CPT is ignored, and GMT's
     default CPT (*turbo*) is used. To use the dataset-specific CPT, you need to
-    explicitly set ``cmap="earth_dist"``.
+    explicitly set ``cmap="@earth_dist.cpt"``.
 
     Refer to :gmt-datasets:`earth-dist.html` for more details about available datasets,
     including version information and references.
