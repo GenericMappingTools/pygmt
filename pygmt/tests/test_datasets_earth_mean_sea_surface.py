14c34ce7b3098e141dc10e06b9b276ce992a80b5
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-12-25 14:22:47 +0100

    Adjust minimum values for tests | Remove execution persmission | Use 'meters' as unit

--- pygmt/tests/test_datasets_earth_mean_sea_surface.py
+++ pygmt/tests/test_datasets_earth_mean_sea_surface.py
@@ -14,13 +14,13 @@ def test_earth_mss_01d():
     data = load_earth_mean_sea_surface(resolution="01d")
     assert data.name == "z"
     assert data.attrs["description"] == "CNES Earth mean sea surface"
-    assert data.attrs["units"] == "m"
+    assert data.attrs["units"] == "meters"
     assert data.attrs["horizontal_datum"] == "WGS84"
     assert data.shape == (181, 361)
     assert data.gmt.registration == 0
     npt.assert_allclose(data.lat, np.arange(-90, 91, 1))
     npt.assert_allclose(data.lon, np.arange(-180, 181, 1))
-    npt.assert_allclose(data.min(), -2655.7, atol=0.01)
+    npt.assert_allclose(data.min(), -104.71, atol=0.01)
     npt.assert_allclose(data.max(), 2463.42, atol=0.01)
 
 
@@ -33,7 +33,7 @@ def test_earth_mss_01d_with_region():
     assert data.gmt.registration == 0
     npt.assert_allclose(data.lat, np.arange(-5, 6, 1))
     npt.assert_allclose(data.lon, np.arange(-10, 11, 1))
-    npt.assert_allclose(data.min(), -1081.94, atol=0.01)
+    npt.assert_allclose(data.min(), 6.53, atol=0.01)
     npt.assert_allclose(data.max(), 105.18, atol=0.01)
 
 
@@ -49,5 +49,5 @@ def test_earth_mss_01m_default_registration():
     assert data.coords["lat"].data.max() == 5.0
     assert data.coords["lon"].data.min() == -10.0
     assert data.coords["lon"].data.max() == -9.0
-    npt.assert_allclose(data.min(), -243.62, atol=0.01)
+    npt.assert_allclose(data.min(), 21.27, atol=0.01)
     npt.assert_allclose(data.max(), 2.94, atol=0.01)
