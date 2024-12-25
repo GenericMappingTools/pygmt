fb2abe40fe76635b714f1c6db0909f6a9b7e73b0
Dongdong Tian <seisman.info@gmail.com>
2024-12-25 21:40:55 +0800

    Revert "Use noqa"
    This reverts commit f9fe25f0f06244964245835874fd34a825d5b866.

--- examples/gallery/maps/choropleth_map.py
+++ examples/gallery/maps/choropleth_map.py
@@ -20,7 +20,7 @@ import pygmt
 
 # Read the example dataset provided by geodatasets.
 gdf = gpd.read_file(geodatasets.get_path("geoda airbnb"))
-gdf  # noqa: B018
+print(gdf)
 
 # %%
 fig = pygmt.Figure()
