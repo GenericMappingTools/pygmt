2e24b2dda3c6e0169f5bae2c1a1e61924a80c05f
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2024-12-27 12:02:17 +0100

    Shorten formulation
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/datasets/earth_free_air_anomaly.py
+++ pygmt/datasets/earth_free_air_anomaly.py
@@ -71,8 +71,8 @@ def load_earth_free_air_anomaly(
         ``"gridline"`` for gridline registration. Default is ``None``, means
         ``"gridline"`` for all resolutions except ``"01m"`` which is ``"pixel"`` only.
     uncertainty : bool
-        Set to ``uncertainty=True`` to download the uncertainties related to the
-        free-air anomaly values [Default is ``uncertainty=False`` and gives the
+        Set to ``True`` to download the uncertainties related to the
+        free-air anomaly values [Default is ``False`` and gives the
         altimetry-based marine free-air anomaly values.
 
     Returns
