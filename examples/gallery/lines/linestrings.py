573309c5e8c20d8171e2039d7e7f1431a08cc23a
Michael Grund <michael.grund@kit.edu>
2024-12-30 18:38:42 +0100

    rm trailing ws

--- examples/gallery/lines/linestrings.py
+++ examples/gallery/lines/linestrings.py
@@ -2,12 +2,12 @@
 GeoPandas: Plotting lines with LineString/MultiLineString geometry
 ==================================================================
 
-The :meth:`pygmt.Figure.plot` method allows us to plot geographical data 
-such as lines which are stored in a :class:`geopandas.GeoDataFrame` object. 
-Use :func:`geopandas.read_file` to load data from any supported OGR format 
-such as a shapefile (.shp), GeoJSON (.geojson), geopackage (.gpkg), etc. 
-Then, pass the :class:`geopandas.GeoDataFrame` as an argument to the 
-``data`` parameter of :meth:`pygmt.Figure.plot`, and style the geometry 
+The :meth:`pygmt.Figure.plot` method allows us to plot geographical data
+such as lines which are stored in a :class:`geopandas.GeoDataFrame` object.
+Use :func:`geopandas.read_file` to load data from any supported OGR format
+such as a shapefile (.shp), GeoJSON (.geojson), geopackage (.gpkg), etc.
+Then, pass the :class:`geopandas.GeoDataFrame` as an argument to the
+``data`` parameter of :meth:`pygmt.Figure.plot`, and style the geometry
 using the ``pen`` parameter.
 """
 
