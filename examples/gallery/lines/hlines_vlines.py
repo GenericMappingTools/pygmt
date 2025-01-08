c29ae862d37b0de39ff6b8d65b9abab9db542412
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2025-01-08 14:21:24 +0100

    Update hlines_vlines.py

--- examples/gallery/lines/hlines_vlines.py
+++ examples/gallery/lines/hlines_vlines.py
@@ -23,11 +23,11 @@ fig.hlines(y=9, pen="1.5p,red3", label="Line 1")
 # add a gray dashed horizontal line at y=8 with x limits at 2 and 8
 fig.hlines(y=8, xmin=2, xmax=8, pen="1.5p,gray30,-", label="Line 2")
 # add two salmon-colored horizontal lines at y=6 and y=7 both with x limits at 3 and 7
-fig.hlines(y=[6, 7], xmin=3, xmax=7, pen="1.5p,salmon", label="Line 3")
+fig.hlines(y=[6, 7], xmin=3, xmax=7, pen="1.5p,salmon", label="Lines 3 & 4")
 # add two black dotted horizontal lines at y=4 and y=5 both with x limits at 4 and 9
-fig.hlines(y=[4, 5], xmin=4, xmax=9, pen="1.5p,black,.", label="Line 4")
+fig.hlines(y=[4, 5], xmin=4, xmax=9, pen="1.5p,black,.", label="Lines 5 & 6")
 # add two blue horizontal lines at y=2 and y=3 with different x limits
-fig.hlines(y=[2, 3], xmin=[0, 1], xmax=[7, 7.5], pen="1.5p,dodgerblue3", label="Line 5")
+fig.hlines(y=[2, 3], xmin=[0, 1], xmax=[7, 7.5], pen="1.5p,dodgerblue3", label="Lines 7 & 8")
 fig.legend(position="JBR+jBR+o0.2c", box="+gwhite+p1p")
 
 fig.shift_origin(xshift="w+2c")
@@ -40,11 +40,11 @@ fig.vlines(x=1, pen="1.5p,red3", label="Line 1")
 # add a gray dashed vertical line at x=2 with y limits at 2 and 8
 fig.vlines(x=2, ymin=2, ymax=8, pen="1.5p,gray30,-", label="Line 2")
 # add two salmon-colored vertical lines at x=3 and x=4 both with y limits at 3 and 7
-fig.vlines(x=[3, 4], ymin=3, ymax=7, pen="1.5p,salmon", label="Line 3")
+fig.vlines(x=[3, 4], ymin=3, ymax=7, pen="1.5p,salmon", label="Lines 3 & 4")
 # add two black dotted vertical lines at x=5 and x=6 both with y limits at 4 and 9
-fig.vlines(x=[5, 6], ymin=4, ymax=9, pen="1.5p,black,.", label="Line 4")
+fig.vlines(x=[5, 6], ymin=4, ymax=9, pen="1.5p,black,.", label="Lines 5 & 6")
 # add two blue vertical lines at x=7 and x=8 with different y limits
-fig.vlines(x=[7, 8], ymin=[0, 1], ymax=[7, 7.5], pen="1.5p,dodgerblue3", label="Line 5")
+fig.vlines(x=[7, 8], ymin=[0, 1], ymax=[7, 7.5], pen="1.5p,dodgerblue3", label="Lines 7 & 8")
 fig.legend()
 
 fig.show()
@@ -58,24 +58,24 @@ fig = pygmt.Figure()
 
 fig.basemap(region="g", projection="R15c", frame=["+thlines Geographic", "af"])
 # add a red horizontal line at a latitude of 70°N without specifying longitude limits
-fig.hlines(y=70, xmin=0, xmax=360, pen="1.5p,red3", label="line1")
+fig.hlines(y=70, xmin=0, xmax=360, pen="1.5p,red3", label="Line 1")
 # add a blue line at a latitude of 50°N with longitude limits at 20°E and 160°E
-fig.hlines(y=50, xmin=20, xmax=160, pen="1.5p,dodgerblue3", label="line2")
+fig.hlines(y=50, xmin=20, xmax=160, pen="1.5p,dodgerblue3", label="Line 2")
 # add a gray dashed horizontal line at a latitude of 30°S with longitude limits at 60°E
 # and 270°E
-fig.hlines(y=-30, xmin=60, xmax=270, pen="1.5p,gray30,-", label="line3")
+fig.hlines(y=-30, xmin=60, xmax=270, pen="1.5p,gray30,-", label="Line 3")
 fig.legend()
 
 fig.shift_origin(xshift="w+2c")
 
 fig.basemap(region="g", projection="R15c", frame=["+tvlines Geographic", "af"])
 # add a red vertical line at a longitude of 70°E without specifying latitude limits
-fig.vlines(x=70, ymin=-90, ymax=90, pen="1.5p,red3", label="line1")
+fig.vlines(x=70, ymin=-90, ymax=90, pen="1.5p,red3", label="Line 1")
 # add a blue vertical line at a longitude of 120°E with latitude limits at 50°S and 70°N
-fig.vlines(x=120, ymin=-50, ymax=70, pen="1.5p,dodgerblue3", label="line2")
+fig.vlines(x=120, ymin=-50, ymax=70, pen="1.5p,dodgerblue3", label="Line 2")
 # add a gray dashed vertical line at a longitude of 230°E with latitude limits at 70°S
 # and 80°N
-fig.vlines(x=230, ymin=-70, ymax=80, pen="1.5p,gray30,-", label="line3")
+fig.vlines(x=230, ymin=-70, ymax=80, pen="1.5p,gray30,-", label="Line 3")
 fig.legend()
 
 fig.show()
@@ -89,24 +89,24 @@ fig = pygmt.Figure()
 
 fig.basemap(region=[0, 360, 0, 1], projection="P10c", frame=["+thlines Polar", "af"])
 # add a red horizontal line along a radius of 0.8 without specifying azimuth limits
-fig.hlines(y=0.8, pen="1.5p,red3", label="line1")
+fig.hlines(y=0.8, pen="1.5p,red3", label="Line 1")
 # add a blue horizontal line along a radius of 0.5 with azimuth limits at 30° and 160°
-fig.hlines(y=0.5, xmin=30, xmax=160, pen="1.5p,dodgerblue3", label="line2")
+fig.hlines(y=0.5, xmin=30, xmax=160, pen="1.5p,dodgerblue3", label="Line 2")
 # add a gray dashed horizontal line along a radius of 0.25 with azimuth limits at 60°
 # and 270°
-fig.hlines(y=0.25, xmin=60, xmax=270, pen="1.5p,gray30,-", label="line3")
+fig.hlines(y=0.25, xmin=60, xmax=270, pen="1.5p,gray30,-", label="Line 3")
 fig.legend()
 
 fig.shift_origin(xshift="w+2c")
 
 fig.basemap(region=[0, 360, 0, 1], projection="P10c", frame=["+tvlines Polar", "af"])
 # add a red vertical line along at an azimuth of 120° without specifying radius limits
-fig.vlines(x=120, pen="1.5p,red3", label="line1")
+fig.vlines(x=120, pen="1.5p,red3", label="Line 1")
 # add a blue vertical line along at an azimuth of 190° with radius limits at 0.2 and 0.8
-fig.vlines(x=190, ymin=0.2, ymax=0.8, pen="1.5p,dodgerblue3", label="line2")
+fig.vlines(x=190, ymin=0.2, ymax=0.8, pen="1.5p,dodgerblue3", label="Line 2")
 # add a gray dashed vertical line along at an azimuth of 320° with radius limits at 0.5
 # and 0.9
-fig.vlines(x=320, ymin=0.5, ymax=0.9, pen="1.5p,gray30,-", label="line3")
+fig.vlines(x=320, ymin=0.5, ymax=0.9, pen="1.5p,gray30,-", label="Line 3")
 fig.legend()
 
 fig.show()
