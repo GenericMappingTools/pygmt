c8cc6fc000529935dbdb728cde84882827c33051
Dongdong Tian <seisman.info@gmail.com>
2025-03-10 17:25:48 +0800

    Rephrase the documentation for dataset loading functions (#3838)
    Co-authored-by: Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>

--- pygmt/datasets/earth_mean_sea_surface.py
+++ pygmt/datasets/earth_mean_sea_surface.py
@@ -30,22 +30,26 @@ def load_earth_mean_sea_surface(
 
        CNES Earth mean sea surface dataset.
 
-    The grids are downloaded to a user data directory (usually
-    ``~/.gmt/server/earth/earth_mss/``) the first time you invoke this function.
-    Afterwards, it will load the grid from the data directory. So you'll need an
-    internet connection the first time around.
+    This function downloads the dataset from the GMT data server, caches it in a user
+    data directory (usually ``~/.gmt/server/earth/earth_mss/``), and load the dataset as
+    an :class:`xarray.DataArray`. An internet connection is required the first time
+    around, but subsequent calls will load the dataset from the local data directory.
 
-    These grids can also be accessed by passing in the file name
-    **@earth_mss**\_\ *res*\[_\ *reg*] to any grid processing function or plotting
-    method. *res* is the grid resolution (see below), and *reg* is the grid registration
-    type (**p** for pixel registration or **g** for gridline registration).
+    The dataset can also be accessed by specifying a file name in any grid processing
+    function or plotting method, using the following file name format:
+    **@earth_mss**\_\ *res*\_\ *reg*. *res* is the grid resolution; *reg* is the grid
+    registration type (**p** for pixel registration, **g** for gridline registration).
+    If *reg* is omitted (e.g., ``@earth_mss_01d``), the gridline-registered grid will be
+    loaded for grid proccessing functions and the pixel-registered grid will be loaded
+    for plotting functions. If *res* is also omitted (i.e., ``@earth_mss``), GMT
+    automatically selects a suitable resolution based on the current region and
+    projection settings.
 
-    The default color palette table (CPT) for this dataset is *@earth_mss.cpt*. It's
-    implicitly used when passing in the file name of the dataset to any grid plotting
-    method if no CPT is explicitly specified. When the dataset is loaded and plotted
-    as an :class:`xarray.DataArray` object, the default CPT is ignored, and GMT's
-    default CPT (*turbo*) is used. To use the dataset-specific CPT, you need to
-    explicitly set ``cmap="@earth_mss.cpt"``.
+    This dataset comes with a color palette table (CPT) file, ``@earth_mss.cpt``. To use
+    the dataset-specific CPT when plotting the dataset, explicitly set
+    ``cmap="@earth_mss.cpt"``, otherwise GMT's default CPT (*turbo*) will be used. If
+    the dataset is referenced by the file name in a grid plotting method, the
+    dataset-specific CPT file is used automatically unless another CPT is specified.
 
     Refer to :gmt-datasets:`earth-mss.html` for more details about available datasets,
     including version information and references.
@@ -83,11 +87,11 @@ def load_earth_mean_sea_surface(
     --------
 
     >>> from pygmt.datasets import load_earth_mean_sea_surface
-    >>> # load the default grid (gridline-registered 1 arc-degree grid)
+    >>> # Load the default grid (gridline-registered 1 arc-degree grid)
     >>> grid = load_earth_mean_sea_surface()
-    >>> # load the 30 arc-minutes grid with "gridline" registration
+    >>> # Load the 30 arc-minutes grid with "gridline" registration
     >>> grid = load_earth_mean_sea_surface(resolution="30m", registration="gridline")
-    >>> # load high-resolution (5 arc-minutes) grid for a specific region
+    >>> # Load high-resolution (5 arc-minutes) grid for a specific region
     >>> grid = load_earth_mean_sea_surface(
     ...     resolution="05m",
     ...     region=[120, 160, 30, 60],
