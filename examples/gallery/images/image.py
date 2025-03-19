01f6a32cb072821cf79cbccf7a8e758af8f526ad
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2025-03-19 15:11:11 +0100

    Use URL to GMT
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/gallery/images/image.py
+++ examples/gallery/images/image.py
@@ -20,7 +20,7 @@ fig.basemap(region=[0, 2, 0, 2], projection="X10c", frame=True)
 # Place and center ("+jCM") an image to the position ("+g") 1/1 on the current plot,
 # scale it to a width of 8 centimeters ("+w") and draw a rectangular border around it
 fig.image(
-    imagefile="https://www.emirates247.com/polopoly_fs/1.569712.1452551297!/image/image.jpg",
+    imagefile="https://oceania.generic-mapping-tools.org/cache/needle.jpg",
     position="g1/1+w8c+jCM",
     box=True,
 )
