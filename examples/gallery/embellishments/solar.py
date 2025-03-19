65a5f114ab6ea91274b4888cd50324c6e723f0f8
yvonnefroelich <yvonne.froehlich@kit.edu>
2025-03-19 09:59:18 +0100

    Use shorter variable name

--- examples/gallery/embellishments/solar.py
+++ examples/gallery/embellishments/solar.py
@@ -21,7 +21,7 @@ fig.basemap(region="d", projection="W15c", frame=True)
 fig.coast(land="darkgreen", water="lightblue")
 
 # Set a time for the day-night terminator and twilights to 17:00 UTC on January 1, 2000
-terminator_datetime = datetime.datetime(
+reference_time = datetime.datetime(
     year=2000, month=1, day=1, hour=17, minute=0, second=0
 )
 
@@ -29,7 +29,7 @@ terminator_datetime = datetime.datetime(
 for terminator in ["day_night", "civil", "nautical", "astronomical"]:
     fig.solar(
         terminator=terminator,
-        terminator_datetime=terminator_datetime,
+        terminator_datetime=reference_time,
         # Set the fill for the night area to navy blue with 85 % transparency
         fill="navyblue@85",
         pen="0.5p",  # Set the outline to be 0.5 points thick
