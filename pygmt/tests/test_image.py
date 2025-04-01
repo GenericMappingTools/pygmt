86fb3af9f7ec5474fd0006925dd034c8cda71f47
Dongdong Tian <seisman.info@gmail.com>
2025-04-01 12:56:09 +0800

    Update Figure.image

--- pygmt/tests/test_image.py
+++ pygmt/tests/test_image.py
@@ -13,5 +13,11 @@ def test_image():
     Place images on map.
     """
     fig = Figure()
-    fig.image(imagefile="@circuit.png", position="x0/0+w2c", box=Box(pen="thin,blue"))
+    fig.image(
+        imagefile="@circuit.png",
+        position=(0, 0),
+        position_type="x",
+        dimension="2c",
+        box=Box(pen="thin,blue"),
+    )
     return fig
