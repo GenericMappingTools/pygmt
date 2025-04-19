c2011d76fb1de8c0612ceab02bbfc07ac7cfe455
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-04-20 09:03:28 +1200

    Switch from pygmt.io.load_dataarray to xr.load_dataarray
    Specifically with arguments (..., engine="gmt", decode_kind="grid").

--- pygmt/datasets/samples.py
+++ pygmt/datasets/samples.py
@@ -8,7 +8,6 @@ from typing import Literal, NamedTuple
 import pandas as pd
 import xarray as xr
 from pygmt.exceptions import GMTInvalidInput
-from pygmt.io import load_dataarray
 from pygmt.src import which
 
 
@@ -204,7 +203,7 @@ def _load_earth_relief_holes() -> xr.DataArray:
         is in meters.
     """
     fname = which("@earth_relief_20m_holes.grd", download="c")
-    return load_dataarray(fname, engine="netcdf4")
+    return xr.load_dataarray(fname, engine="gmt", decode_kind="grid")
 
 
 class GMTSampleData(NamedTuple):
