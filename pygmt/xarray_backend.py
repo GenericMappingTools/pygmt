0c6b8a3f225058dbf401daf365b815307760d9ab
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-04-22 14:42:34 +1200

    Add example usage to docstring of GMTBackendEntrypoint

--- pygmt/xarray_backend.py
+++ pygmt/xarray_backend.py
@@ -20,6 +20,51 @@ class GMTBackendEntrypoint(BackendEntrypoint):
 
     Internally, GMT uses the NetCDF C library to read NetCDF files, and GDAL for GeoTIFF
     files.
+
+    When using :py:func:`xarray.open_dataarray` or :py:func:`xarray.load_dataarray` with
+    ``engine='gmt'``, pass the ``decode_kind`` parameter that can be either:
+
+    - ``grid`` - for reading single-band raster grids
+    - ``image`` - for reading multi-band raster images
+
+    Examples
+    --------
+    Read a single-band NetCDF file using ``decode_kind="grid"``
+
+    >>> import pygmt
+    >>> import xarray as xr
+    >>>
+    >>> da_grid: xr.Dataset = xr.open_dataarray(
+    ...     "@static_earth_relief.nc", engine="gmt", decode_kind="grid"
+    ... )
+    >>> da_grid
+    <xarray.DataArray 'z' (lat: 14, lon: 8)> Size: 448B
+    [112 values with dtype=float32]
+    Coordinates:
+      * lat      (lat) float64 112B -23.5 -22.5 -21.5 -20.5 ... -12.5 -11.5 -10.5
+      * lon      (lon) float64 64B -54.5 -53.5 -52.5 -51.5 -50.5 -49.5 -48.5 -47.5
+    Attributes:
+        Conventions:   CF-1.7
+        title:         Produced by grdcut
+        history:       grdcut @earth_relief_01d_p -R-55/-47/-24/-10 -Gstatic_eart...
+        description:   Reduced by Gaussian Cartesian filtering (111.2 km fullwidt...
+        actual_range:  [190. 981.]
+        long_name:     elevation (m)
+
+    Read a multi-band GeoTIFF file using ``decode_kind="image"``
+
+    >>> da_image: xr.Dataset = xr.open_dataarray(
+    ...     "@earth_night_01d", engine="gmt", decode_kind="image"
+    ... )
+    >>> da_image
+    <xarray.DataArray 'z' (band: 3, y: 180, x: 360)> Size: 194kB
+    [194400 values with dtype=uint8]
+    Coordinates:
+      * y        (y) float64 1kB 89.5 88.5 87.5 86.5 ... -86.5 -87.5 -88.5 -89.5
+      * x        (x) float64 3kB -179.5 -178.5 -177.5 -176.5 ... 177.5 178.5 179.5
+      * band     (band) uint8 3B 1 2 3
+    Attributes:
+        long_name:  z
     """
 
     description = "Open raster (.grd, .nc or .tif) files in Xarray via GMT."
