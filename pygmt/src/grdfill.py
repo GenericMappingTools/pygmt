8cd4d3c72e4c57eb59b00733b3679d5b08548531
Dongdong Tian <seisman.info@gmail.com>
2025-05-22 10:57:17 +0800

    Shorter version

--- pygmt/src/grdfill.py
+++ pygmt/src/grdfill.py
@@ -131,7 +131,7 @@ def grdfill(
     replace the hole values. If no holes are found the original unchanged grid is
     returned.
 
-    Wraps GMT module ``grdfill``, with full docs at :gmt-docs:`grdfill.html`.
+    Full GMT docs at :gmt-docs:`grdfill.html`.
 
     {aliases}
 
