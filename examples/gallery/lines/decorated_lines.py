0784c62803495ac6f614333885cfe142d803b86c
Michael Grund <23025878+michaelgrund@users.noreply.github.com>
2025-05-28 09:58:57 +0200

    Update decorated_lines.py

--- examples/gallery/lines/decorated_lines.py
+++ examples/gallery/lines/decorated_lines.py
@@ -47,7 +47,8 @@ for decoline in [
     # Line with squares ("s") with a size of 0.7 centimeters in a distance of
     # 1 centimeter
     "~d1c:+ss0.7c+gtan+p1p,black",
-    # Shift symbols using "+n" in x- and y-directions relative to the main decorated line
+    # Shift symbols using "+n" in x- and y-directions relative to the main decorated
+    # line
     "~d1c:+sd0.5c+gtan+p1p,black+n-0.2c/0.1c",
     # Give the number of equally spaced symbols by using "n" instead of "d"
     "~n6:+sn0.5c+gtan+p1p,black",
