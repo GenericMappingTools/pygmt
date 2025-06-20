5de77a17044ea48911306bb4355eddb42429c95b
Dongdong Tian <seisman.info@gmail.com>
2025-06-21 00:22:56 +0800

    Add custom exception GMTValueError

--- pygmt/exceptions.py
+++ pygmt/exceptions.py
@@ -4,6 +4,9 @@ Custom exception types used throughout the library.
 All exceptions derive from GMTError.
 """
 
+from collections.abc import Sequence
+from typing import Any
+
 
 class GMTError(Exception):
     """
@@ -51,3 +54,28 @@ class GMTImageComparisonFailure(AssertionError):  # noqa: N818
     """
     Raised when a comparison between two images fails.
     """
+
+
+class GMTValueError(GMTError):
+    """
+    Raised when an invalid value is passed to a function/method.
+
+    >>> raise GMTValueError("x", 1)
+    Traceback (most recent call last):
+    ...
+    pygmt...GMTValueError: Invalid value for parameter 'x': 1.
+    >>> raise GMTValueError("x", 1, choices=["a", "b", 1, 2])
+    Traceback (most recent call last):
+        ...
+    pygmt...GMTValueError: ... 'x': 1. Expected one of: 'a', 'b', 1, 2.
+    >>> raise GMTValueError("x", 1, choices=["a", 0, True, False, None])
+    Traceback (most recent call last):
+        ...
+    pygmt...GMTValueError: ... 'x': 1. Expected one of: 'a', 0, True, False, None.
+    """
+
+    def __init__(self, name: str, value: Any, choices: Sequence[Any] | None = None):
+        msg = f"Invalid value for parameter {name!r}: {value!r}."
+        if choices is not None:
+            msg += f" Expected one of: {', '.join(repr(c) for c in choices)}."
+        super().__init__(msg)
