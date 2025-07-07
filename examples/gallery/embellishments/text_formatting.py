2462e992132a0ccab2858fb9749bde18ebd86c53
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-07-07 20:35:01 +0200

    Update docs

--- examples/gallery/embellishments/text_formatting.py
+++ examples/gallery/embellishments/text_formatting.py
@@ -3,11 +3,11 @@ Text formatting
 ===============
 
 There are various options to format text contained in a plot, e.g., text added via
-:meth:`pygmt.Figure.text`, title of a plot, labels of colorbars and Cartesian axes,
-and legend entries. It's also possible to change the font as well as its color and
-size only for specific characters of a longer text. The supported fonts are listed
-at :doc:`Supported Fonts  </techref/fonts>`. For an complete overview see
-:doc:`Text Formatting </techref/text_formatting>`.
+:meth:`pygmt.Figure.text`, the title of the plot, labels of colorbars as well as
+Cartesian axes, and legend entries. It's also possible to change the font as well
+as its color and size only for specific characters of a longer text. The supported
+fonts are listed at :doc:`Supported Fonts  </techref/fonts>`. For an complete
+overview see :doc:`Text Formatting </techref/text_formatting>`.
 """
 
 # %%
