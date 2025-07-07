15b2b8e71a9ded00119708109c8e716bc5d51bfc
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-07-07 20:33:22 +0200

    Update docs

--- examples/gallery/embellishments/text_formatting.py
+++ examples/gallery/embellishments/text_formatting.py
@@ -2,12 +2,12 @@
 Text formatting
 ===============
 
-There are various options to format text added to a plot as well as the title of the
-plot and labels of colorbars, Cartesian axes, and legend entries, including,
-superscripts, subscripts, underlining and small caps (for an overview see
-(:doc:`Text Formatting </techref/text_formatting>`). It's also possible to change the
-font as well as its color and size only for specific characters of a longer text. The
-supported fonts are listed at :doc:`Supported Fonts  </techref/fonts>`.
+There are various options to format text contained in a plot, e.g., text added via
+:meth:`pygmt.Figure.text`, title of a plot, labels of colorbars and Cartesian axes,
+and legend entries. It's also possible to change the font as well as its color and
+size only for specific characters of a longer text. The supported fonts are listed
+at :doc:`Supported Fonts  </techref/fonts>`. For an complete overview see
+:doc:`Text Formatting </techref/text_formatting>`.
 """
 
 # %%
@@ -20,13 +20,13 @@ fig.basemap(region=[-1, 1, -4, 4], projection="X4c/5c", frame=0)
 # blue for "P", yellow for "y", and red for "GMT"
 fig.text(x=0, y=3, text="@;63/124/173;P@;;@;255/212/59;y@;;@;238/86/52;GMT@;;")
 
-# Change font size and style for a single character, respectively
+# Change font size and style for one single character, respectively
 fig.text(x=0, y=2, text="te@:15:x@::t     tex@%Courier-Oblique%t@%%")
 
-# Superscript
+# Use superscript
 fig.text(x=0, y=1, text="E = mc@+2@+")
 
-# Subscripts and Greek letters
+# Use subscripts and Greek letters
 fig.text(x=0, y=0, text="@~s@~@-ij@- = c@-ijkl@- @~e@~@-kl@-")
 
 # Combine two characters above each other
