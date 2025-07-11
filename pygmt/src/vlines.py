a8e31256a26aedf2fab62bffe2af47996814aabf
Dongdong Tian <seisman.info@gmail.com>
2025-07-11 19:35:49 +0800

    Use GMTValueError in pygmt/src/vlines.py

--- pygmt/src/vlines.py
+++ pygmt/src/vlines.py
@@ -5,7 +5,7 @@ vlines - Plot vertical lines.
 from collections.abc import Sequence
 
 import numpy as np
-from pygmt.exceptions import GMTInvalidInput
+from pygmt.exceptions import GMTValueError
 
 __doctest_skip__ = ["vlines"]
 
@@ -99,11 +99,15 @@ def vlines(
 
     # Check if ymin/ymax are scalars or have the expected length.
     if _ymin.size not in {1, nlines} or _ymax.size not in {1, nlines}:
-        msg = (
-            f"'ymin' and 'ymax' are expected to be scalars or have lengths '{nlines}', "
-            f"but lengths '{_ymin.size}' and '{_ymax.size}' are given."
+        _value = f"{ymin=}, {ymax=}"
+        raise GMTValueError(
+            _value,
+            description="size for 'ymin' or 'ymax'",
+            reason=(
+                "'ymin' and 'ymax' are expected to be scalars or have lengths "
+                f"{nlines!r} but lengths {_ymin.size!r} and {_ymax.size!r} are given."
+            ),
         )
-        raise GMTInvalidInput(msg)
 
     # Repeat ymin/ymax to match the length of x if they are scalars.
     if nlines != 1:
