3764dec90d198ce0977b225eec7ab973141d4cbf
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-07-19 22:56:33 +0200

    Fix line length

--- examples/gallery/embellishments/text_formatting.py
+++ examples/gallery/embellishments/text_formatting.py
@@ -2,12 +2,12 @@
 Text formatting
 ===============
 
-There are various options for formatting text contained in a plot, e.g., text added
-via :meth:`pygmt.Figure.text`, the plot title, labels of colorbars as well as
-Cartesian axes, and legend entries. It's also possible to change the font as well
-as its color and size only for specific characters of a longer text. The supported
-fonts are listed at :doc:`Supported Fonts </techref/fonts>`. For an complete
-overview see :doc:`Text Formatting </techref/text_formatting>`.
+There are various options for formatting text contained in a plot, e.g., text added via
+:meth:`pygmt.Figure.text`, the plot title, labels of colorbars as well as Cartesian
+axes, and legend entries. It's also possible to change the font as well as its color
+and size only for specific characters of a longer text. The supported fonts are listed
+at :doc:`Supported Fonts </techref/fonts>`. For an complete overview see
+:doc:`Text Formatting </techref/text_formatting>`.
 """
 
 # %%
@@ -16,7 +16,7 @@ import pygmt
 fig = pygmt.Figure()
 fig.basemap(region=[-1, 1, -4, 4], projection="X4c/5c", frame=0)
 
-# Change font color for specific characters of the word "PyGMT"
+# Change font color for specific characters of the word "PyGMT":
 # blue for "P", yellow for "y", and red for "GMT"
 fig.text(x=0, y=3, text="@;63/124/173;P@;;@;255/212/59;y@;;@;238/86/52;GMT@;;")
 
