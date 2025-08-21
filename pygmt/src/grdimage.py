c096ca91fd71067b1c37c8f10116b1fc5e459f7f
Dongdong Tian <seisman.info@gmail.com>
2025-08-21 21:24:53 +0800

    Figure.grdimage: Raise NotImplementedError instead of GMTInvalidInput for -A option

--- pygmt/src/grdimage.py
+++ pygmt/src/grdimage.py
@@ -6,13 +6,7 @@ import xarray as xr
 from pygmt._typing import PathLike
 from pygmt.alias import Alias, AliasSystem
 from pygmt.clib import Session
-from pygmt.exceptions import GMTInvalidInput
-from pygmt.helpers import (
-    build_arg_list,
-    fmt_docstring,
-    kwargs_to_strings,
-    use_alias,
-)
+from pygmt.helpers import build_arg_list, fmt_docstring, kwargs_to_strings, use_alias
 
 __doctest_skip__ = ["grdimage"]
 
@@ -165,7 +159,7 @@ def grdimage(self, grid: PathLike | xr.DataArray, projection=None, **kwargs):
             "Parameter 'img_out'/'A' is not implemented. "
             "Please consider submitting a feature request to us."
         )
-        raise GMTInvalidInput(msg)
+        raise NotImplementedError(msg)
 
     aliasdict = AliasSystem(
         J=Alias(projection, name="projection"),
