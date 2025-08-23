4ea896712a58b7cff2e41c95a2ad393a29508889
Dongdong Tian <seisman.info@gmail.com>
2025-08-23 18:20:44 +0800

    Fix a typo

--- pygmt/tests/test_alias_system.py
+++ pygmt/tests/test_alias_system.py
@@ -113,7 +113,7 @@ def test_alias_system_common_parameter_verbose():
     assert func(verbose="warning") == ["-Vw"]
     assert func(verbose="timing") == ["-Vt"]
     assert func(verbose="info") == ["-Vi"]
-    assert func(verbose="compati") == ["-Vc"]
+    assert func(verbose="compat") == ["-Vc"]
     assert func(verbose=True) == ["-V"]
     assert func(verbose="debug") == ["-Vd"]
 
