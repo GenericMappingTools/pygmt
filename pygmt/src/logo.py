5f981c5cef4533a9cfa6b5c5a26837cf3314a7aa
Dongdong Tian <seisman.info@gmail.com>
2025-09-04 18:29:41 +0800

    Fix site to website
    Co-authored-by: Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>

--- pygmt/src/logo.py
+++ pygmt/src/logo.py
@@ -54,7 +54,7 @@ def logo(
         Control what is written beneath the map portion of the logo.
 
         - ``"standard"``: The text label "The Generic Mapping Tools".
-        - ``"url"``: The URL to the GMT site.
+        - ``"url"``: The URL to the GMT website.
         - ``"no_label"``: Skip the text label.
     {verbose}
     {panel}
