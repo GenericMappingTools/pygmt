fb9547881b00f3c475963f8451d857bd2796fa45
Dongdong Tian <seisman.info@gmail.com>
2025-10-18 16:10:45 +0800

    Fix offset to anchor_offset in the Figure.inset doctest

--- pygmt/src/inset.py
+++ pygmt/src/inset.py
@@ -127,7 +127,7 @@ def inset(
     ...     )
     ...
     >>> # Map elements outside the "with" statement are plotted in the main figure
-    >>> fig.logo(position="BR", position_type="inside", offset=0.2, width="3c")
+    >>> fig.logo(position="BR", position_type="inside", anchor_offset=0.2, width="3c")
     >>> fig.show()
     """
     self._activate_figure()
