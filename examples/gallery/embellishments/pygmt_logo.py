8b1fd3433f978f99e4470e518b73390d33cb0c41
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-21 21:17:41 +0100

    Reduce examples

--- examples/gallery/embellishments/pygmt_logo.py
+++ examples/gallery/embellishments/pygmt_logo.py
@@ -10,84 +10,6 @@ Python (blue and yellow) and GMT (red) or in black and white as well as in light
 dark mode. The wordmark can be added at the right side or bottom of the visual.
 """
 
-import pygmt
-
-fig = pygmt.Figure()
-fig.pygmtlogo()
-fig.show()
-
-# %%
-
-fig = pygmt.Figure()
-fig.pygmtlogo(theme="dark", box="+ggray20")
-fig.show()
-
-# %%
-
-fig = pygmt.Figure()
-fig.basemap(region=[-5, 5, -5, 5], projection="X10c", frame=[1, "+gtan"])
-
-fig.pygmtlogo(position="jTL+o0.2c+w4c", box="+gwhite+p1p,gray")
-fig.pygmtlogo(shape="hexagon", position="jTR+o0.2c+w4c")
-
-fig.pygmtlogo(color=False, wordmark=False, position="jTL+o0.5c/2c+w1.5c", box=False)
-fig.pygmtlogo(
-    color=False,
-    theme="dark",
-    shape="hexagon",
-    wordmark=False,
-    position="jTR+o0.5c/2c+w1.5c",
-    box=False,
-)
-fig.pygmtlogo(wordmark="vertical", position="jMC+w2c", box="+gwhite")
-
-fig.show()
-
-# %%
-# All combinations
-
-i_plot = 0
-
-fig = pygmt.Figure()
-
-for color in [True, False]:
-    for theme in ["light", "dark"]:
-        for shape in ["circle", "hexagon"]:
-            for wordmark in [False, True, "horizontal", "vertical"]:
-                for box in [False, True]:
-                    if not box:
-                        box_used = False
-                    elif box:
-                        if theme == "light":
-                            box_used = "+gwhite"
-                        elif theme == "dark":
-                            box_used = "+ggray20"
-                    # fig = pygmt.Figure()
-                    fig.basemap(
-                        region=[-1, 1, -1, 1], projection="X2.5c/3.5c", frame="+gtan"
-                    )
-                    # fig.image("@needle.png", position="jMC+w2c", box=box_used)
-                    fig.pygmtlogo(
-                        color=color,
-                        theme=theme,
-                        shape=shape,
-                        wordmark=wordmark,
-                        position="jMC+w2c",
-                        box=box_used,
-                    )
-
-                    fig.shift_origin(xshift="+w+0.5c")
-                    n_hor = 8
-                    if i_plot in range(n_hor - 1, 100, n_hor):
-                        fig.shift_origin(
-                            xshift=f"-{(n_hor * 2.5 + n_hor * 0.5)}c",
-                            yshift="-h-0.5c",
-                        )  # n_hor*width + n_hor*xshift
-
-                    i_plot = i_plot + 1
-fig.show()
-
-
 # %%
 # All versions
 # modified from
@@ -157,4 +79,4 @@ for x, y, theme in [(1, 3, "light"), (11, 3, "dark")]:
 fig.show(width=1000)
 
 
-# sphinx_gallery_thumbnail_number = 3
+# sphinx_gallery_thumbnail_number = 1
