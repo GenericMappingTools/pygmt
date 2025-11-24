fe18c87297892681543c460e0fb395a2e4612667
Dongdong Tian <seisman.info@gmail.com>
2025-11-24 18:50:13 +0800

    Improve docstrings

--- pygmt/params/position.py
+++ pygmt/params/position.py
@@ -29,21 +29,21 @@ class Position(BaseParam):
     >>> fig.show()
     """
 
-    #: Specify the reference point on the plot. The method of defining the reference
-    #: point is controlled by ``type``, and the exact location is set by ``position``.
+    #: Location of the reference point on the plot. Its meaning depends on the value of
+    #: ``type``.
     location: Sequence[float | str] | AnchorCode
 
-    #: Specify the type of coordinates used to define the reference point. It can be
-    #: one of the following values:
+    #: The coordinates used to define the reference point. Valid values and meanings for
+    #: corresponding ``location`` are:
     #:
-    #: - ``"mapcoords"``: ``position`` is specified as (*longitude*, *latitude*) in map
+    #: - ``"mapcoords"``: ``location`` is specified as (*longitude*, *latitude*) in map
     #:   coordinates.
-    #: - ``"boxcoords"``: ``position`` is specified as (*nx*, *ny*) in normalized
+    #: - ``"boxcoords"``: ``location`` is specified as (*nx*, *ny*) in normalized
     #:   coordinates, i.e., fractional values between 0 and 1 along the x- and y-axes.
-    #: - ``"plotcoords"``: ``position`` is specified as (*x*, *y*) in plot coordinates,
+    #: - ``"plotcoords"``: ``location`` is specified as (*x*, *y*) in plot coordinates,
     #:   i.e., distances from the lower-left plot origin given in inches, centimeters,
     #:   or points.
-    #: - ``"inside"`` or ``"outside"``: ``position`` is one of the nine
+    #: - ``"inside"`` or ``"outside"``: ``location`` is one of the nine
     #:   :doc:`two-character justification codes </techref/justification_codes>`,
     #:   indicating a specific location relative to the plot bounding box.
     #:
@@ -52,19 +52,19 @@ class Position(BaseParam):
         "plotcoords"
     )
 
-    #: Specify the anchor point of the GMT logo, using one of the
+    #: Anchor point of the embellishment, using one of the
     #: :doc:`2-character justification codes </techref/justification_codes>`. The
-    #: default value depends on ``position_type``.
+    #: default value depends on ``type``.
     #:
-    #: - ``position_type="inside"``: ``anchor`` defaults to the same as ``position``.
-    #: - ``position_type="outside"``: ``anchor`` defaults to the mirror opposite of
-    #:   ``position``.
+    #: - ``type="inside"``: ``anchor`` defaults to the same as ``location``.
+    #: - ``type="outside"``: ``anchor`` defaults to the mirror opposite of ``location``.
     #: - Otherwise, ``anchor`` defaults to ``"MC"`` (middle center).
     anchor: AnchorCode | None = None
 
-    #: Specifies an offset for the anchor point as *offset* or (*offset_x*, *offset_y*).
-    #: If a single value *offset* is given, both *offset_x* and *offset_y* are set to
-    #: *offset*.
+    #: Offset for the anchor point. It can be either a single value *offset* or a pair
+    #: (*offset_x*, *offset_y*), where *offset_x* and *offset_y* are the offsets in the
+    #: x- and y-directions, respectively. If a single value *offset* is given, both
+    #: *offset_x* and *offset_y* are set to *offset*.
     offset: Sequence[float | str] | None = None
 
     @property
