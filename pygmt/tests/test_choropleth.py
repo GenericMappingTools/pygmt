a1b967ed60b33cdf3651231b76ea007683753673
Dongdong Tian <seisman.info@gmail.com>
2025-11-24 17:49:43 +0800

    Update baseline image

--- pygmt/tests/test_choropleth.py
+++ pygmt/tests/test_choropleth.py
@@ -27,7 +27,7 @@ def test_choropleth(world):
 
     fig = Figure()
     fig.basemap(region=[-19.5, 53, -38, 37.5], projection="M15c", frame=True)
-    makecpt(cmap="bilbao", series=(0, 270, 10))
+    makecpt(cmap="bilbao", series=(0, 270, 10), reverse=True)
     fig.choropleth(world, column="POP_EST", pen="0.3p,gray10")
     fig.colorbar(frame=True)
     return fig
