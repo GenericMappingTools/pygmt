d9d047887344facac3df3c546253680c96a632a3
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-27 11:26:07 +0100

    Move comment

--- examples/gallery/maps/choropleth_map.py
+++ examples/gallery/maps/choropleth_map.py
@@ -19,13 +19,13 @@ import pygmt
 
 provider = "https://naciscdn.org/naturalearth"
 world = gpd.read_file(f"{provider}/50m/cultural/ne_50m_admin_0_countries.zip")
+
+# The dataset contains different attributes, here we focus on the population within
+# the different countries (column "POP_EST").
 world["POP_EST"] *= 1e-6
 
 fig = pygmt.Figure()
 fig.basemap(region=[-19.5, 53, -37.5, 38], projection="M15c", frame="+n")
-
-# The dataset contains different attributes, here we focus on the population within
-# the different countries (column "POP_EST").
 
 # First, we define the colormap to fill the polygons based on the "POP_EST" column.
 pygmt.makecpt(cmap="acton", series=(0, 100), reverse=True)
