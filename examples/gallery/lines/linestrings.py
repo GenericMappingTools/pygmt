15500c43928b18ee90212272815d01980e8fe606
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-27 21:12:43 +0100

    Adjust comments | Add code for South America

--- examples/gallery/lines/linestrings.py
+++ examples/gallery/lines/linestrings.py
@@ -17,11 +17,19 @@ shapefile (.shp), GeoJSON (.geojson), geopackage (.gpkg), etc. Then, pass the
 import geopandas as gpd
 import pygmt
 
-# Read a sample dataset provided by Natural Earth. The dataset contains large rivers
-# in Europe, stored as LineString/MultiLineString geometry types.
+# Read a sample dataset provided by Natural Earth. The dataset contains rivers stored
+# as LineString/MultiLineString geometry types. Here will focus on South America.
 provider = "https://naciscdn.org/naturalearth"
 rivers = gpd.read_file(f"{provider}/10m/physical/ne_10m_rivers_lake_centerlines.zip")
 
+fig = pygmt.Figure()
+fig.basemap(region=[-84.5, -33, -56.5, 13], projection="M10c", frame=True)
+fig.coast(land="gray95", shorelines="1/0.3p,gray50", borders="1/0.2p,black")
+
+fig.plot(data=rivers["geometry"], pen="1p,steelblue")
+fig.show()
+
+# %%
 fig = pygmt.Figure()
 fig.basemap(region=[-10, 30, 35, 57], projection="M10c", frame=True)
 fig.coast(land="gray95", shorelines="1/0.3p,gray50", borders="1/0.1p,black")
