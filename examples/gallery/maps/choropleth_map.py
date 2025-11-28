f21bf4b31a6efe614b5653f7afb5c041c37790b6
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2025-11-28 08:29:23 +0100

    Change to 110 m
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/gallery/maps/choropleth_map.py
+++ examples/gallery/maps/choropleth_map.py
@@ -18,7 +18,7 @@ import geopandas as gpd
 import pygmt
 
 provider = "https://naciscdn.org/naturalearth"
-world = gpd.read_file(f"{provider}/50m/cultural/ne_50m_admin_0_countries.zip")
+world = gpd.read_file(f"{provider}/110m/cultural/ne_110m_admin_0_countries.zip")
 
 # The dataset contains different attributes, here we focus on the population within
 # the different countries (column "POP_EST") for the continent "Africa".
