b2c98771175cdb1176ef68bb0f4c6c054175f327
Dongdong Tian <seisman.info@gmail.com>
2025-12-06 01:06:02 +0800

    Improve Axis/Axes/Frame and add more tests

--- pygmt/tests/test_params_frame.py
+++ pygmt/tests/test_params_frame.py
@@ -5,26 +5,66 @@ Test the Frame/Axes/Axis classes.
 from pygmt.params import Axes, Axis
 
 
-def test_params_axis():
+def test_params_axis_intervals():
     """
-    Test the Axis class.
+    Test the annotation, tick, and grid parameters of the Axis class.
     """
-    assert str(Axis(interval="a1f0.5")) == "a1f0.5"
-    assert str(Axis(interval="a1f0.5", angle=30)) == "a1f0.5+a30"
-    assert str(Axis(interval="a1f0.5", angle=30, skip_edge="left")) == "a1f0.5+a30+el"
-    assert str(Axis(interval="a1f0.5", fancy=True)) == "a1f0.5+f"
-    assert str(Axis(interval="a1f0.5", label="My Label")) == "a1f0.5+lMy Label"
-    assert str(Axis(interval="a1f0.5", hlabel="My HLabel")) == "a1f0.5+LMy HLabel"
-    assert str(Axis(interval="a1f0.5", alt_label="Alt Label")) == "a1f0.5+sAlt Label"
-    assert str(Axis(interval="a1f0.5", alt_hlabel="Alt HLabel")) == "a1f0.5+SAlt HLabel"
-    assert str(Axis(interval="a1f0.5", unit="km")) == "a1f0.5+ukm"
+    assert str(Axis(annotation=1)) == "a1"
+    assert str(Axis(tick=2)) == "f2"
+    assert str(Axis(grid=3)) == "g3"
+    assert str(Axis(annotation=1, tick=2, grid=3)) == "a1f2g3"
+
+    assert str(Axis(annotation=True)) == "a"
+    assert str(Axis(tick=True)) == "f"
+    assert str(Axis(grid=True)) == "g"
+    assert str(Axis(annotation=True, tick=True)) == "af"
+    assert str(Axis(annotation=True, grid=True)) == "ag"
+    assert str(Axis(tick=True, grid=True)) == "fg"
+    assert str(Axis(annotation=True, tick=True, grid=True)) == "afg"
+
+
+def test_params_axis_modifiers():
+    """
+    Test the modifiers of the Axis class.
+    """
+    assert str(Axis(annotation=True, angle=30)) == "a+a30"
+    assert str(Axis(annotation=True, angle="normal")) == "a+an"
+    assert str(Axis(annotation=True, angle="parallel")) == "a+ap"
+
+    assert str(Axis(annotation=True, skip_edge=True)) == "a+e"
+    assert str(Axis(annotation=True, skip_edge="lower")) == "a+el"
+    assert str(Axis(annotation=True, skip_edge="upper")) == "a+eu"
+
+    assert str(Axis(annotation=True, fancy=True)) == "a+f"
+
+    assert str(Axis(annotation=True, label="My Label")) == "a+lMy Label"
+    assert str(Axis(annotation=True, hlabel="My HLabel")) == "a+LMy HLabel"
+    assert str(Axis(annotation=True, alt_label="Alt Label")) == "a+sAlt Label"
+    assert str(Axis(annotation=True, alt_hlabel="Alt HLabel")) == "a+SAlt HLabel"
+
+    axis = Axis(annotation=True, label="My Label", alt_label="My HLabel")
+    assert str(axis) == "a+lMy Label+sMy HLabel"
+
+    assert str(Axis(annotation=True, prefix="$")) == "a+p$"
+
+    assert str(Axis(annotation=True, unit="km")) == "a+ukm"
 
 
 def test_params_axes():
     """
     Test the Axes class.
     """
-    assert (
-        str(Axes("WSen", title="My Plot Title", fill="lightred"))
-        == "WSen+glightred+tMy Plot Title"
-    )
+
+    assert str(Axes(axes="WSen")) == "WSen"
+    assert str(Axes(fill="lightred")) == "+glightred"
+    assert str(Axes(title="My Plot Title")) == "+tMy Plot Title"
+    assert str(Axes(subtitle="My Subtitle")) == "+sMy Subtitle"
+
+    axes = Axes(axes="WSen", fill="lightred", title="My Plot Title")
+    assert str(axes) == "WSen+glightred+tMy Plot Title"
+
+
+def test_params_frame():
+    """
+    Test the Frame class.
+    """
