d98c9d78fb24eb3a86e0e098e541f118ad248420
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-12-12 20:45:43 +0100

    Use maximum line length

--- examples/gallery/seismology/meca.py
+++ examples/gallery/seismology/meca.py
@@ -2,14 +2,13 @@
 Focal mechanisms
 ================
 
-The :meth:`pygmt.Figure.meca` method can plot focal mechanisms or beachballs.
-We can specify the focal mechanism nodal planes or moment tensor components
-as a dictionary using the ``spec`` parameter (or they can be specified as a
-1-D or 2-D array, or within a file). The size of the beachballs can be set
-using the ``scale`` parameter. The compressive and extensive quadrants can
-be filled either with a color or a pattern via the ``compression_fill`` and
-``extension_fill`` parameters, respectively. Use the ``pen`` parameter to
-adjust the outline of the beachballs.
+The :meth:`pygmt.Figure.meca` method can plot focal mechanisms or beachballs. We can
+specify the focal mechanism nodal planes or moment tensor components as a dictionary
+using the ``spec`` parameter (or they can be specified as a 1-D or 2-D array, or within
+a file). The size of the beachballs can be set using the ``scale`` parameter. The
+compressive and extensive quadrants can be filled either with a color or a pattern via
+the ``compression_fill`` and ``extension_fill`` parameters, respectively. Use the
+``pen`` parameter to adjust the outline of the beachballs.
 """
 
 # %%
