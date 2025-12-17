a398a28097db019a771e78b73ab68c58ab31bd99
Dongdong Tian <seisman.info@gmail.com>
2025-12-17 13:36:51 +0800

    Sort common parameters

--- pygmt/src/image.py
+++ pygmt/src/image.py
@@ -17,16 +17,16 @@ from pygmt.params import Box
 def image(  # noqa: PLR0913
     self,
     imagefile: PathLike,
-    projection: str | None = None,
-    region: Sequence[float | str] | str | None = None,
     box: Box | bool = False,
     monochrome: bool = False,
     invert: bool = False,
+    projection: str | None = None,
+    region: Sequence[float | str] | str | None = None,
     verbose: Literal["quiet", "error", "warning", "timing", "info", "compat", "debug"]
     | bool = False,
     panel: int | Sequence[int] | bool = False,
-    transparency: float | None = None,
     perspective: float | Sequence[float] | str | bool = False,
+    transparency: float | None = None,
     **kwargs,
 ):
     r"""
@@ -64,8 +64,6 @@ def image(  # noqa: PLR0913
         An Encapsulated PostScript (EPS) file or a raster image file. An EPS file must
         contain an appropriate BoundingBox. A raster file can have a depth of 1, 8, 24,
         or 32 bits and is read via GDAL.
-    $projection
-    $region
     position : str
         [**g**\|\ **j**\|\ **J**\|\ **n**\|\ **x**]\ *refpoint*\ **+r**\ *dpi*\
         **+w**\ [**-**]\ *width*\ [/*height*]\ [**+j**\ *justify*]\
@@ -94,6 +92,8 @@ def image(  # noqa: PLR0913
         **Note**: There was an upstream GMT bug, so this feature may not work correctly
         for some 1-bit images for GMT<=6.6.0.
         See `PR #8837 <https://github.com/GenericMappingTools/gmt/pull/8837>`__.
+    $projection
+    $region
     $verbose
     $panel
     $perspective
