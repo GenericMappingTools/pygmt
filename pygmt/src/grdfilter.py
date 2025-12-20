8c975a7c7902ad081eceed4c8415274abe2d7239
Dongdong Tian <seisman.info@gmail.com>
2025-12-20 18:17:53 +0800

    v1

--- pygmt/src/grdfilter.py
+++ pygmt/src/grdfilter.py
@@ -14,9 +14,30 @@ from pygmt.helpers import build_arg_list, fmt_docstring, use_alias
 
 @fmt_docstring
 @use_alias(D="distance", F="filter", N="nans", f="coltypes")
-def grdfilter(
+def grdfilter(  # noqa: PLR0913
     grid: PathLike | xr.DataArray,
     outgrid: PathLike | None = None,
+    filter_type: Literal[
+        "boxcar",
+        "cosine_arch",
+        "gaussian",
+        "custom",
+        "operator",
+        "median",
+        "mlprob",
+        "histogram",
+        "minall",
+        "minpos",
+        "maxall",
+        "maxneg",
+    ]
+    | None = None,
+    filter_width: Sequence[float] | None = None,
+    highpass: bool = False,
+    median_quantile: float | None = None,
+    histogram_bin_width: float | None = None,
+    histogram_center_bins: bool = False,
+    mode_extreme: Literal["min", "max"] | None = None,
     toggle: bool = False,
     spacing: Sequence[float | str] | None = None,
     region: Sequence[float | str] | str | None = None,
@@ -66,6 +87,57 @@ def grdfilter(
         - **p**: Maximum Likelihood probability
         - **h**: Histogram
 
+    filter_type
+      The filter type. Choose among convolution and non-convolution filters.
+
+      Convolution filters are:
+
+      - ``"boxcar"``: All weights are equal.
+      - ``"cosine_arch"``: Weights follow a cosine arch curve.
+      - ``"gaussian"``: Weights are given by the Gaussian function, where filter width
+        is 6 times the conventional Gaussian sigma.
+      - ``"custom"``: Weights are given by the precomputed values in the filter weight
+        grid file *weight*, which must have odd dimensions; also requires ``distance=0``
+        and output spacing must match input spacing or be integer multiples.
+      - ``"operator"``: Weights are given by the precomputed values in the filter weight
+        grid file *weight*, which must have odd dimensions; also requires ``distance=0``
+        and output spacing must match input spacing or be integer multiples. Weights
+        are assumed to sum to zero so no accumulation of weight sums and normalization
+        will be done.
+
+      Non-convolution filters are:
+
+      - ``"median"``: Returns median value. To select another quantile, use the
+        parameter ``median_quantile`` in the 0-1 range [Default is 0.5, i.e., median].
+      - ``"mlprob"``: Maximum likelihood probability (a mode estimator). Return modal
+        value. If more than one mode is found we return their average value. Set
+        ``mode_extreme`` to ``"min"`` or ``"max"`` to return the lowermost or uppermost
+        of the modal values.
+      - ``"histogram"``: Histogram mode (another mode estimator). Return the modal value
+        as the center of the dominant peak in a histogram. Use parameter
+        ``histogram_center_bins`` to center the bins on multiples of bin width [Default
+        has bin edges that are multiples of bin width]. Use parameter
+        ``histogram_bin_width`` to set the bin width. If more than one mode is found we
+        return their average value. Set ``mode_extreme`` to ``"min"`` or ``"max"`` to
+        return the lowermost or uppermost of the modal values.
+
+      - ``"minall"``: Return minimum of all values.
+      - ``"minpos"``: Return minimum of all positive values only.
+      - ``"maxall"``: Return maximum of all values.
+      - ``"maxneg"``: Return maximum of all negative values only.
+    median_quantile
+        Quantile to use when ``filter_type="median"``. Must be a float in the range 0-1.
+        [Default is 0.5 (median)].
+    histogram_bin_width
+        Bin width to use when ``filter_type="histogram"``.
+    histogram_center_bins
+        Center the histogram bins on multiples of *histogram_bin_width* when
+        ``filter_type="histogram"``. By default, the bins are aligned such that
+        their edges are on multiples of *histogram_bin_width*.
+    mode_extreme
+        Choose which extreme to return when ``filter_type="mlprob"`` and multiple modes
+        are found. Options are: ``"min"`` to return the lowermost mode, or ``"max"`` to
+        return the uppermost mode. By default, the average of all modes is returned.
     distance : str
         State how the grid (x,y) relates to the filter *width*:
 
@@ -137,6 +209,34 @@ def grdfilter(
     >>> smooth_field = pygmt.grdfilter(grid=grid, filter="g600", distance="4")
     """
     aliasdict = AliasSystem(
+        F=[
+            Alias(
+                filter_type,
+                name="filter_type",
+                mapping={
+                    "boxcar": "b",
+                    "cosine_arch": "c",
+                    "gaussian": "g",
+                    "custom": "f",
+                    "operator": "o",
+                    "median": "m",
+                    "mlprob": "p",
+                    "histogram": "h",
+                    "minall": "l",
+                    "minpos": "L",
+                    "maxall": "u",
+                    "maxneg": "U",
+                },
+            ),
+            Alias(filter_width, name="filter_width", sep="/"),
+            Alias(histogram_bin_width, name="histogram_bin_width", prefix="/"),
+            Alias(highpass, name="highpass", prefix="+h"),
+            Alias(median_quantile, name="median_quantile", prefix="+q"),
+            Alias(histogram_center_bins, name="histogram_center_bins", prefix="+c"),
+            Alias(
+                mode_extreme, name="mode_extreme", mapping={"min": "+l", "max": "+u"}
+            ),
+        ],
         I=Alias(spacing, name="spacing", sep="/", size=2),
         T=Alias(toggle, name="toggle"),
     ).add_common(
