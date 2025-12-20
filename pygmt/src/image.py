bcb1ef919e8777f759bcdab6e5b1ec3e6be785ea
Dongdong Tian <seisman.info@gmail.com>
2025-12-20 12:19:32 +0800

    Fix typos

--- pygmt/src/image.py
+++ pygmt/src/image.py
@@ -78,12 +78,12 @@ def image(  # noqa: PLR0913
 
         - A :class:`pygmt.params.Position` object to fully control the reference point,
           anchor point, and offset.
-        - A sequence of two values representing the x and y coordinates in plot
+        - A sequence of two values representing the x- and y-coordinates in plot
           coordinates, e.g., ``(1, 2)`` or ``("1c", "2c")``.
         - A :doc:`2-character justification code </techref/justification_codes>` for a
           position inside the plot, e.g., ``"TL"`` for Top Left corner inside the plot.
 
-        If not specified, defaults to the bottom-left corner of the plot (position
+        If not specified, defaults to the Bottom Left corner of the plot (position
         ``(0, 0)`` with anchor ``"BL"``).
     width
     height
