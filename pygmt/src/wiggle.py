d74a6eb2157123a48c593baeb254a60076d6ad43
Dongdong Tian <seisman.info@gmail.com>
2025-12-20 12:17:03 +0800

    Fix typos

--- pygmt/src/wiggle.py
+++ pygmt/src/wiggle.py
@@ -120,12 +120,12 @@ def wiggle(  # noqa: PLR0913
 
         - A :class:`pygmt.params.Position` object to fully control the reference point,
           anchor point, and offset.
-        - A sequence of two values representing the x and y coordinates in plot
+        - A sequence of two values representing the x- and y- coordinates in plot
           coordinates, e.g., ``(1, 2)`` or ``("1c", "2c")``.
         - A :doc:`2-character justification code </techref/justification_codes>` for a
           position inside the plot, e.g., ``"TL"`` for Top Left corner inside the plot.
 
-        If not specified, defaults to the bottom-left corner of the plot with a 0.2-cm
+        If not specified, defaults to the Bottom Left corner of the plot with a 0.2-cm
         offset.
     length
         Length of the vertical scale bar in data (z) units.
