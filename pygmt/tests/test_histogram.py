ad19b4b65a4dc0cfbb9856a7da85b3b71a1af083
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-12-21 14:03:29 +0100

    Add tests

--- pygmt/tests/test_histogram.py
+++ pygmt/tests/test_histogram.py
@@ -5,6 +5,7 @@ Test Figure.histogram.
 import pandas as pd
 import pytest
 from pygmt import Figure
+from pygmt.exceptions import GMTInvalidInput
 
 
 @pytest.fixture(scope="module", name="data", params=[list, pd.Series])
@@ -32,3 +33,38 @@ def test_histogram(data):
         fill="green",
     )
     return fig
+
+
+def test_histogram_barwidth_baroffset(data):
+    """
+    Test plotting a histogram specifying bar_with and bar_offset.
+    """
+    fig = Figure()
+    fig.histogram(
+        data=data,
+        projection="X10c/10c",
+        region=[0, 9, 0, 6],
+        series=1,
+        frame="a",
+        fill="green",
+        bar_width=0.5,
+        bar_offset=0.25,
+    )
+    return fig
+
+
+def test_histogram_baroffset(data):
+    """
+    Test passing bar_offset requires bar_width.
+    """
+    fig = Figure()
+    with pytest.raises(GMTInvalidInput):
+        fig.histogram(
+            data=data,
+            projection="X10c/10c",
+            region=[0, 9, 0, 6],
+            series=1,
+            frame="a",
+            fill="green",
+            bar_offset=0.25,
+        )
