fe93f172f984ca6a83edf8d580b63c28fe5302e7
Xingchen He <chuanjun@stu.cdut.edu.cn>
2026-01-13 19:47:32 +0800

    Update pygmt/tests/test_config.py
    Co-authored-by: Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>

--- pygmt/tests/test_config.py
+++ pygmt/tests/test_config.py
@@ -210,7 +210,7 @@ def test_config_ps_convert():
     # Check that a warning is raised when PS_CONVERT is used in config
     msg = (
         "Parameter 'PS_CONVERT' is not supported. "
-        "To confiure conversion options, please pass parameters to "
+        "To configure conversion options, please pass parameters to "
         "pygmt.Figure.savefig or pygmt.Figure.show instead."
     )
     with pytest.warns(SyntaxWarning, match=msg):
