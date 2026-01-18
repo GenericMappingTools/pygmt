4adcc26d42f425e2c5a97523ec06a89623632fc7
Dongdong Tian <seisman.info@gmail.com>
2026-01-18 15:20:48 +0800

    Pass an empty dict to kwdict

--- pygmt/src/directional_rose.py
+++ pygmt/src/directional_rose.py
@@ -85,7 +85,7 @@ def directional_rose(
 
     position = _parse_position(
         position,
-        kwdict={"width": width, "fancy": fancy, "labels": labels},
+        kwdict={},  # No need to check conflicts since it's a new function.
         default=Position("BL", cstype="inside"),  # Default to BL.
     )
 
