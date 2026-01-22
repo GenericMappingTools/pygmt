2e9ed08a47ca6348a111eab73065fbc267182b6b
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-01-22 22:18:34 +0100

    Improve intorduction

--- examples/gallery/3d_plots/3d_bar.py
+++ examples/gallery/3d_plots/3d_bar.py
@@ -2,24 +2,15 @@
 3-D bar plot
 ============
 
-TODO improve introduction
+A 3-bar plot can by created from any collection of three-dimensional tabular data. The
+data points can lie on a regular grid or be irregular scattered.
 
-A 3-D bar plot of a grid can be created in two steps: (i) convert the grid into a table
-via :func:`pygmt.grd2xyz` and (ii) plot this table as bars in 3-D using
-:meth:`pygmt.Figure.plot3d`. The bars can be outlined, and the fill can be one color or
-based on a quantity using a colormap.
-
-We can create a 3-D bar plot for any collection of XYZ points, whether they lie on a
-regular grid or are irregularly scattered. Therefore, it's clearer to start with a
-general description of the 3-D bar plot itself and then mention that, as a special
-case, we can convert a grid into XYZ and visualize it with the same routine.
-
-The program plot3d allows us to plot three-dimensional symbols, including columnar plots.
-As a simple demonstration, we will convert a gridded netCDF of bathymetry into an ASCII
-xyz table and use the height information to draw a 2-D histogram in a 3-D perspective
-view. We also use the height information to set to color of each column via a CPT file.
-Our gridded bathymetry file is the 5 arc-minute global relief. Depth ranges from -5000
-meter to sea-level. We produce the Figure by running this script.
+A 3-D bar plot based on a grid can be created in two steps:
+(i) Convert the grid into table via :func:`pygmt.grd2xyz`.
+(ii) Plot this table as bars in 3-D using :meth:`pygmt.Figure.plot3d`.
+The bars can be outlined, and the fill can be one color or based on a quantity using a
+colormap. For the later, a fourth column needs to be added containing the values of
+the quantity for the color-coding.
 """
 
 # %%
@@ -32,8 +23,8 @@ region = [141, 147, 36, 43]
 # Download a grid for the Earth relief with a resolution of 10 arc-minutes
 grid = pygmt.datasets.load_earth_relief(resolution="10m", region=region)
 
-# Convert the grid into a pandas DataFrame, with columns for longitude ("x"),
-# latitude ("y") and elevation ("z")
+# Convert the grid into a pandas DataFrame, with columns for longitude ("x"), latitude
+# ("y") and elevation ("z")
 grd_df = pygmt.grd2xyz(grid=grid)
 zmin = grd_df["z"].min() - 50
 zmax = grd_df["z"].max() + 50
