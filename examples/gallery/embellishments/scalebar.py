0fdf35246bfba3320a3fc1a5cd00aed6409b0b90
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-02-02 20:29:18 +0100

    Remove old codes

--- examples/gallery/embellishments/scalebar.py
+++ examples/gallery/embellishments/scalebar.py
@@ -21,7 +21,6 @@ fig.basemap(region=[-45, -25, -15, 0], projection="M0/0/10c", frame=["WSne", "af
 # It is placed based on geographic coordinates 42° West and 1° South,
 # applies at the reference point (scale_loc is not used and by default False), and
 # represents a length of 500 kilometers
-# fig.basemap(map_scale="g-42/-1+w500k")
 fig.scalebar(length="500k", position=Position((-42, -1), cstype="mapcoords"))
 
 # -----------------------------------------------------------------------------
@@ -29,7 +28,6 @@ fig.scalebar(length="500k", position=Position((-42, -1), cstype="mapcoords"))
 # It is placed based on normalized bounding box coordinates
 # Use a fancy style to get a scale bar that looks like train tracks
 # Add the distance unit to the single distance values
-# fig.basemap(map_scale="n0.8/0.95+w500k+f+u")
 fig.scalebar(
     position=Position((0.8, 0.95), cstype="boxcoords"),
     length="500k",
@@ -44,7 +42,6 @@ fig.scalebar(
 # It applies at the middle of the map (scale_loc is set to True)
 # Without providing a text, the label parameter adds the distance unit as label
 with pygmt.config(MAP_SCALE_HEIGHT="10p"):
-    # fig.basemap(map_scale="n0.2/0.15+c+w500k+f+l")
     fig.scalebar(
         position=Position((0.2, 0.15), cstype="boxcoords"),
         scale_loc=True,
@@ -59,7 +56,6 @@ with pygmt.config(MAP_SCALE_HEIGHT="10p"):
 # of 1 centimeter in both x- and y-directions
 # It applies at -7° South
 # A customized label is added via the label parameter
-# fig.basemap(map_scale="jBR+jMR+o1c/1c+c-7+w500k+f+u+lvalid at 7° S")
 fig.scalebar(
     position=Position("BR", anchor="MR", offset=1),
     scale_loc=-7,
@@ -81,7 +77,6 @@ fig = pygmt.Figure()
 fig.basemap(region=[-45, -25, -15, 0], projection="M10c", frame=["WSne", "af"])
 fig.coast(land="tan", water="steelblue")
 fig.scalebar(
-    # map_scale="jBL+o1c/1c+c-7+w500k+f+lkm+ar",
     position=Position("BL", cstype="inside", offset=1),
     scale_loc=-7,
     length="500k",
