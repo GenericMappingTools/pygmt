224bf1878c9866a7d510bf3828e474da4393279b
Xingchen He <chuanjun@stu.cdut.edu.cn>
2026-02-02 20:18:57 +0800

    BREAKING: Raise GMTParameterError exception for at_least_one parameters. Previously raise GMTInvalidInput (#4380)

--- pygmt/src/dimfilter.py
+++ pygmt/src/dimfilter.py
@@ -9,7 +9,7 @@ import xarray as xr
 from pygmt._typing import PathLike
 from pygmt.alias import Alias, AliasSystem
 from pygmt.clib import Session
-from pygmt.exceptions import GMTInvalidInput
+from pygmt.exceptions import GMTParameterError
 from pygmt.helpers import build_arg_list, fmt_docstring, use_alias
 
 __doctest_skip__ = ["dimfilter"]
@@ -144,11 +144,7 @@ def dimfilter(
     ... )
     """
     if not all(arg in kwargs for arg in ["D", "F", "N"]) and "Q" not in kwargs:
-        msg = (
-            "At least one of the following parameters must be specified: "
-            "distance, filters, or sectors."
-        )
-        raise GMTInvalidInput(msg)
+        raise GMTParameterError(at_least_one={"distance", "filters", "sectors"})
 
     aliasdict = AliasSystem(
         I=Alias(spacing, name="spacing", sep="/", size=2),
