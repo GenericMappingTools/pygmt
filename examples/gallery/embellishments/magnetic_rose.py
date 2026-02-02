c539929757e946a0b33845314d419dda9a4b795e
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-02-02 21:01:53 +0100

    Add comments

--- examples/gallery/embellishments/magnetic_rose.py
+++ examples/gallery/embellishments/magnetic_rose.py
@@ -18,18 +18,29 @@ fig.magnetic_rose()
 
 # Add a magnetic rose with several adjustments
 with pygmt.config(
+    # Pen used for the ticks related the outer circle and the outline of the North star
     MAP_TICK_PEN_SECONDARY="orange",
+    # Pen used for the ticks related to the inner circle
     MAP_TICK_PEN_PRIMARY="cyan",
+    # Pen used for the stem of the declination arrow and the fill of the North star
     MAP_DEFAULT_PEN="brown",
+    # Font used for the labels for the geographic directions
     FONT_TITLE="purple",
 ):
     fig.magnetic_rose(
+        position="MC",  # placed at MiddleCenter
+        width=4.5,  # width of the rose
+        # If a declination value is given, a arrow showing the declination is plotted
+        # instead of the simple North arrow
         declination=14.3,
+        # Adjust the label added to the declination arrow
         # declination_label="14.3 N°E",  # white spaces are not supported
-        position="MC",
-        width="4.5c",
+        # Add labels for the geographic directions. Use a * to get a North star.
+        # Use "" to skip a label.
         labels=["W", "E", "South", "*"],
+        # Draw an outer circle with the provided pen
         outer_pen="1p,red",
+        # Draw an inner circle with the provided pen
         inner_pen="1p,blue",
     )
 
