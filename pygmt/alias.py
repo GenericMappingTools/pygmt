3fef364f6ce5e906559b76794665a2d40bbd2094
chuan <chuanjun1978@gmail.com>
2026-02-04 19:26:08 +0800

    simplify the error message

--- pygmt/alias.py
+++ pygmt/alias.py
@@ -418,16 +418,13 @@ class AliasSystem(UserDict):
             if long_param_given:
                 if not isinstance(aliases, Sequence):  # Single Alias object.
                     raise GMTParameterError(
-                        conflicts_with={aliases.name: {short_param}},
+                        conflicts_with={short_param: {aliases.name}},
                         reason=f"'{aliases.name}' is already specified using the long-form parameter.",
                     )
                 # Sequence of Alias objects.
                 long_params = {v.name for v in aliases if not v.prefix}
-                params_str = ", ".join(repr(p) for p in sorted(long_params))
-                verb = "is" if len(long_params) == 1 else "are"
                 raise GMTParameterError(
-                    conflicts_with={param: {short_param} for param in long_params},
-                    reason=f"{params_str} {verb} already specified using long-form parameters.",
+                    conflicts_with={short_param: long_params},
                 )
 
             # Long-form parameters are not specified.
