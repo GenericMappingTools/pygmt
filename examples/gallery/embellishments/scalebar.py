cd6ef47d39e65a08cacf4a0167c6d4f7095c3638
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-02-08 13:01:11 +0100

    Line lengths now 94 chars

--- examples/gallery/embellishments/scalebar.py
+++ examples/gallery/embellishments/scalebar.py
@@ -2,8 +2,8 @@
 Scale bar
 =========
 
-The :meth:`pygmt.Figure.scalebar` method can be used to add a scale bar to a map.
-This example shows how such a scale bar can be customized.
+The :meth:`pygmt.Figure.scalebar` method can be used to add a scale bar to a map. This
+example shows how such a scale bar can be customized.
 """
 
 # %%
@@ -16,18 +16,15 @@ fig = pygmt.Figure()
 # Mercator projection with 10 centimeters width
 fig.basemap(region=[-45, -25, -15, 0], projection="M0/0/10c", frame=["WSne", "af"])
 
-# -----------------------------------------------------------------------------
-# Top Left: Add a plain scale bar
-# It is placed based on geographic coordinates 42° West and 1° South,
-# applies at the reference point by default, and
-# represents a length of 500 kilometers
+# --- Top Left: Add a plain scale bar ---
+# It is placed based on geographic coordinates 42° West and 1° South, applies at the
+# reference point by default, and represents a length of 500 kilometers.
 fig.scalebar(length="500k", position=Position((-42, -1), cstype="mapcoords"))
 
-# -----------------------------------------------------------------------------
-# Top Right: Add a fancy scale bar
-# It is placed based on normalized bounding box coordinates
-# Use a fancy style to get a scale bar that looks like train tracks
-# Add the distance unit to the single distance values
+# --- Top Right: Add a fancy scale bar ---
+# It is placed based on normalized bounding box coordinates. Use a fancy style to get a
+# scale bar that looks like train tracks. Add the distance unit to the single distance
+# values.
 fig.scalebar(
     position=Position((0.8, 0.95), cstype="boxcoords"),
     length="500k",
@@ -35,10 +32,9 @@ fig.scalebar(
     unit=True,
 )
 
-# -----------------------------------------------------------------------------
-# Bottom Left: Add a thick scale bar
-# It applies at the middle of the map (scale_loc is set to True)
-# Without providing a text, the label parameter adds the distance unit as label
+# --- Bottom Left: Add a thick scale bar ---
+# It applies at the middle of the map (scale_loc is set to True). Use Without providing
+# text, the label parameter adds the distance unit as label.
 fig.scalebar(
     position=Position((0.2, 0.15), cstype="boxcoords"),
     scale_loc=True,
@@ -48,12 +44,10 @@ fig.scalebar(
     label=True,
 )
 
-# -----------------------------------------------------------------------------
-# Bottom Right: Add a scale bar valid for a specific location
-# It is placed at BottomRight using MiddleRight as anchor point with an offset
-# of 1 centimeter in both x- and y-directions
-# It applies at -7° South
-# A customized label is added via the label parameter
+# --- Bottom Right: Add a scale bar valid for a specific location ---
+# It is placed at BottomRight using MiddleRight as anchor point with an offset of 1
+# centimeter in both x- and y-directions. It applies at -7° South. A customized label
+# is added via the label parameter.
 fig.scalebar(
     position=Position("BR", anchor="MR", offset=1),
     scale_loc=-7,
@@ -67,8 +61,8 @@ fig.show()
 
 
 # %%
-# The ``box`` parameter allows surrounding the scale bar. This can be useful when
-# adding a scale bar to a colorful map to improve contrast and readability.
+# The ``box`` parameter allows surrounding the scale bar. This can be useful when adding
+# a scale bar to a colorful map to improve contrast and readability.
 
 fig = pygmt.Figure()
 
@@ -81,9 +75,9 @@ fig.scalebar(
     fancy=True,
     label="km",
     label_alignment="right",
-    # Fill the box in white with a transparency of 30 percent, add a solid
-    # outline in darkgray (gray30) with a thickness of 0.5 points, and use
-    # rounded edges with a radius of 3 points
+    # Fill the box in white with a transparency of 30 percent, add a solid outline in
+    # darkgray (gray30) with a thickness of 0.5 points, and use rounded edges with a
+    # radius of 3 points
     box=Box(fill="white@30", pen="0.5p,gray30,solid", radius="3p"),
 )
 
