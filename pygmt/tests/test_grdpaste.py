a4ff78520606be1cc1c57d6666a903f1ab8d0029
Xingchen He <chuanjun@stu.cdut.edu.cn>
2026-02-20 20:29:52 +0800

    Update pygmt/tests/test_grdpaste.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/tests/test_grdpaste.py
+++ pygmt/tests/test_grdpaste.py
@@ -60,7 +60,7 @@ def test_grdpaste_file_in_file_out(grid, grid_top, grid_bottom):
 
 # TODO(GMT>=6.7): Remove the xfail marker for GMT<6.7.
 @pytest.mark.xfail(
-    condition=Version(__gmt_version__) < Version("6.7"),
+    condition=Version(__gmt_version__) > Version("6.6.0"),
     reason="Upstream bug fixed in https://github.com/GenericMappingTools/gmt/pull/8901",
 )
 def test_grdpaste_file_in_xarray_out(grid):
