a1ba80073e96d4659720d11488c91d3294c80242
Xingchen He <chuanjun@stu.cdut.edu.cn>
2026-03-04 15:14:29 +0800

    Update pygmt/src/grdmask.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- pygmt/src/grdmask.py
+++ pygmt/src/grdmask.py
@@ -57,7 +57,7 @@ def _alias_option_N(  # noqa: N802
         mode_char = "z" if inside == "z" else "p"
         if edge == inside:
             mode_char = mode_char.upper()
-        mask_values = mode_char if outside in {None, 0} else [mode_char, outside]
+        mask_values = mode_char if outside is None else [mode_char, outside]
     else:
         mask_values = [outside, edge, inside]
     return Alias(mask_values, name="mask_values", sep="/", size=(2, 3))
