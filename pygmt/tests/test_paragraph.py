4da6010a8ddaf0e2699dea11d1eb98b1ccc5f8b6
Dongdong Tian <seisman.info@gmail.com>
2026-04-02 18:33:27 +0800

    Merge two tests into one

--- pygmt/tests/test_paragraph.py
+++ pygmt/tests/test_paragraph.py
@@ -24,44 +24,30 @@ def test_paragraph():
 
 
 @pytest.mark.mpl_image_compare
-def test_paragraph_multiple_paragraphs_list():
+@pytest.mark.parametrize("inputtype", ["list", "string"])
+def test_paragraph_multiple_paragraphs(inputtype):
     """
-    Test typesetting a single paragraph.
+    Test typesetting multiple paragraphs.
     """
+    if inputtype == "list":
+        text = [
+            "This is the first paragraph. " * 5,
+            "This is the second paragraph. " * 5,
+        ]
+    else:
+        text = (
+            "This is the first paragraph.\n" * 5
+            + "\n\n"
+            + "This is the second paragraph.\n" * 5
+        )
+
     fig = Figure()
     fig.basemap(region=[0, 10, 0, 10], projection="X10c/10c", frame=True)
     fig.paragraph(
         x=4,
         y=4,
-        text=[
-            "This is the first paragraph. " * 5,
-            "This is the second paragraph. " * 5,
-        ],
+        text=text,
         parwidth="5c",
         linespacing="12p",
     )
-    return fig
-
-
-@pytest.mark.mpl_image_compare
-def test_paragraph_multiple_paragraphs_blankline():
-    """
-    Test typesetting a single paragraph.
-    """
-    text = """
-This is the first paragraph.
-This is the first paragraph.
-This is the first paragraph.
-This is the first paragraph.
-This is the first paragraph.
-
-This is the second paragraph.
-This is the second paragraph.
-This is the second paragraph.
-This is the second paragraph.
-This is the second paragraph.
-"""
-    fig = Figure()
-    fig.basemap(region=[0, 10, 0, 10], projection="X10c/10c", frame=True)
-    fig.paragraph(x=4, y=4, text=text, parwidth="5c", linespacing="12p")
     return fig
