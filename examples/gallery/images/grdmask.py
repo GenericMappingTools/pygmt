31d1b74c505520515ef96eb8d2b7f1bfd873ea50
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-04-09 20:52:21 +0200

    Try extracting a circle

--- examples/gallery/images/grdmask.py
+++ examples/gallery/images/grdmask.py
@@ -7,8 +7,14 @@ Create grid mask from polygons
 """
 
 # %%
+import geopandas
 import numpy as np
 import pygmt
+from shapely.geometry import Point
+
+# %%
+# Polygons based on NumPy arrays
+# ------------------------------
 
 # Define a study region
 region = [125, 135, 25, 36]
@@ -60,3 +66,49 @@ fig.plot(data=polygon, pen="2p,darkorange")
 
 fig.colorbar(frame=True)
 fig.show()
+
+
+# %%
+# Circle based on GeoPandas polygon geometry
+# ------------------------------------------
+
+# Define a study region
+region = [125, 135, 25, 36]
+
+# Create a point and buffer it
+point = geopandas.GeoSeries([Point(126.5, 33.5)])
+circle = point.buffer(0.6)  # 10 is the radius
+
+# Download elevation grid
+grid = pygmt.datasets.load_earth_relief(region=region, resolution="30s")
+
+# Create a grid mask based on the two polygons defined above, set all values
+# outside the polygons to NaN
+mask = pygmt.grdmask(region=region, data=circle, spacing="30s", outside="NaN")
+mask_lonlat = mask.rename(new_name_or_name_dict={"x": "lon", "y": "lat"})
+
+# Apply the grid mask to the downloaded elevation grid by multiplying the two grids
+grid_mask = grid * mask_lonlat
+
+
+fig = pygmt.Figure()
+pygmt.makecpt(cmap="oleron", series=[-2000, 2000])
+
+# Plot the elevation grid
+fig.basemap(region=region, projection="M12c", frame=True)
+fig.grdimage(grid=grid, cmap=True)
+fig.basemap(frame="g1")
+fig.plot(data=circle, pen="2p,darkorange")
+
+fig.shift_origin(xshift="+w+2c")
+
+# Plot the masked elevation grid
+fig.basemap(region=region, projection="M12c", frame=True)
+fig.grdimage(grid=grid_mask, cmap=True)
+fig.basemap(frame="g1")
+fig.plot(data=circle, pen="2p,darkorange")
+
+fig.colorbar(frame=True)
+fig.show()
+
+# sphinx_gallery_thumbnail_number = 1
