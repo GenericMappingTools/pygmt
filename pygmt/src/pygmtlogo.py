9b4bd13e6e7975625df16f85c9480f2987e4b7e4
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-04-19 15:06:29 +0200

    Fix typo

--- pygmt/src/pygmtlogo.py
+++ pygmt/src/pygmtlogo.py
@@ -109,13 +109,13 @@ def create_logo(  # noqa: PLR0915
 
     def _letter_m_coords():
         """Coordinates for letter M."""
-        # X coordinates from left to right.
+        # X-coordinates from left to right.
         x1 = thick_gap  # Left edge of left vertical line of M.
         x5 = r4  # Right edge of right vertical line of M.
         x2 = x1 + thick_m  # Right edge of left vertical line of M.
         x3 = (x1 + x5) / 2  # The middle of M.
         x4 = x5 - thick_m  # Left edge of right vertical line of M.
-        # Y coordinates from bottom to top.
+        # Y-coordinates from bottom to top.
         y1 = thick_gt / 2 + thick_gap  # Bottom of the letter M.
         y2 = r5 - thick_gt  # Bottom of the middle peak of M.
         y3 = r5  # Top of the middle peak of M.
