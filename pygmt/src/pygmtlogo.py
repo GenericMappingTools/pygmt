7f563e149b48e6529e273c1f0027a2d31e94d869
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-04-19 19:46:01 +0200

    Add one more helping line

--- pygmt/src/pygmtlogo.py
+++ pygmt/src/pygmtlogo.py
@@ -244,6 +244,7 @@ def create_logo(  # noqa: PLR0915
 
     # Vertical compass line above letters G and M.
     fig.plot(data=compass_lines["vline2"], **args_compass)
+    # fig.show()
 
     # Outline around the shape for black and white color with dark theme
     if not color and theme == "dark":
@@ -264,16 +265,17 @@ def create_logo(  # noqa: PLR0915
         # fig.show()
 
     # # Helpful for implementing the logo; not included in the logo
-    # Gridlines
+    # # Gridlines
     # with pygmt.config(MAP_FRAME_TYPE="inside", MAP_GRID_PEN="0.1p,gray30"):
     #     fig.basemap(frame="g1")
     # # Circles for the different radii
     # for r in [r0, r1, r2, r3, r4, r5]:
-    #     fig.plot(x=0, y=0, style=f"c{2 * r}c", pen="0.3p,gray30,4_2")
+    #     fig.plot(x=0, y=0, style=f"c{2 * r}c", pen="0.3p,gray30")
     # fig.plot(x=0, y=0, style=f"c{2 * (r2 + (r3 - r4))}c", pen="0.3p,gray30,2_2")
     # # Lines for letter M
     # fig.hlines(y=[r4, r5], pen="0.3p,gray30,2_2")
     # fig.vlines(x=r4, pen="0.3p,gray30,2_2")
+    # fig.vlines(x=(thick_gap + r4) / 2, pen="0.3p,gray30,2_2")
 
     # fig.show()
     fig_name_logo = "pygmt_logo"
