73dc51a3970cb1fec2d7ea32597776a51a4ee861
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2026-04-20 13:05:04 +0200

    Adjust radii and use fractional representation
    Co-authored-by: Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>

--- pygmt/src/pygmtlogo.py
+++ pygmt/src/pygmtlogo.py
@@ -30,7 +30,9 @@ def create_logo(  # noqa: PLR0915
 
     # Radii (make sure that r4-r5 == r2-r3)
     r0, r1, r2, r3, r4, r5 = size * np.array(
-        [1, 0.875, 0.58125, 0.4625, 0.4125, 0.29375]
+       #  [1, 0.875, 0.58125, 0.4625, 0.4125, 0.29375]   # old
+       #  [1, 0.875, 0.58125, 0.4825, 0.4125, 0.31375]   # exact
+        [1, 224/256, 150/256, 122/256, 106/256, 78/256]
     )
     # Pen thicknesses
     thick_shape = r0 - r1  # for shape
