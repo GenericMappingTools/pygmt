ca5c4c6dfb9bb211aeb86d1c4567a6a570cbd80c
Dongdong Tian <seisman.info@gmail.com>
2026-04-25 15:23:57 +0800

    Update

--- examples/tutorials/basics/frames.py
+++ examples/tutorials/basics/frames.py
@@ -4,8 +4,8 @@ Frames, ticks, titles, and labels
 
 Setting frame, ticks, titles, and labels is handled by the ``frame`` parameter that
 many plotting methods of the :class:`pygmt.Figure` class accept. This tutorial focuses
-on the higher-level :class:`pygmt.params.Axis` and :class:`pygmt.params.Frame`
-classes.
+on setting the ``frame`` parameter using the :class:`pygmt.params.Axis` and
+:class:`pygmt.params.Frame` classes.
 """
 
 # %%
@@ -35,24 +35,20 @@ fig.show()
 
 
 # %%
-# Tick marks and grid lines
-# -------------------------
+# Annotations, tick marks and grid lines
+# --------------------------------------
 #
-# For more control, use :class:`pygmt.params.Axis` to describe annotations, tick marks,
-# and gridlines. In GMT, tick labels are called **a**\ nnotations.
+# The default frame style includes tick labels (called annotations in GMT) and tick
+# marks at intervals determined by GMT, but doesn't include grid lines. To control
+# the presence and intervals of annotations, tick marks, and grid lines, you can use the
+# :class:`pygmt.params.Axis` class. The ``annot``, ``tick``, and ``grid`` attributes of
+# :class:`pygmt.params.Axis` control the presence and intervals of annotations, tick
+# marks, and grid lines, respectively. Setting them to ``True`` adds them at intervals
+# determined by GMT:
 
 fig = pygmt.Figure()
 fig.coast(shorelines="1/0.5p", region=[-180, 180, -60, 60], projection="M25c")
-fig.basemap(frame=Axis(annot=True))
-fig.show()
-
-# %%
-# Here, ``annot=True`` asks GMT to choose annotation intervals automatically, and
-# ``grid=True`` adds automatic grid lines:
-
-fig = pygmt.Figure()
-fig.coast(shorelines="1/0.5p", region=[-180, 180, -60, 60], projection="M25c")
-fig.basemap(frame=Axis(annot=True, grid=True))
+fig.basemap(frame=Axis(annot=True, tick=True, grid=True))
 fig.show()
 
 # %%
@@ -84,24 +80,23 @@ fig.show()
 # -----------
 #
 # Axis labels, in GMT simply called labels, can be set through the ``xaxis`` and
-# ``yaxis`` parameters of :class:`Frame`. The map boundaries (or plot axes) are named
-# as West/west/left (**W**, **w**, **l**), South/south/bottom (**S**, **s**, **b**),
-# North/north/top (**N**, **n**, **t**), and East/east/right (**E**, **e**, **r**)
-# sides of a figure. Uppercase letters (**W**, **S**, **N**, **E**) draw axes with
-# annotations and tick marks, lowercase letters (**w**, **s**, **n**, **e**) draw
-# axes with tick marks only, and **l**, **b**, **t**, **r** draw plain axis lines
-# without ticks or annotations. A frame like ``Frame(axes="WS")`` draws annotated west
-# and south axes only.
+# ``yaxis`` parameters of :class:`Frame`. The map boundaries (or plot axes) are named as
+# West/west/left (**W**, **w**, **l**), South/south/bottom (**S**, **s**, **b**),
+# North/north/top (**N**, **n**, **t**), and East/east/right (**E**, **e**, **r**) sides
+# of a figure. Uppercase letters (**W**, **S**, **N**, **E**) draw axes with annotations
+# and tick marks, lowercase letters (**w**, **s**, **n**, **e**) draw axes with tick
+# marks only, and **l**, **b**, **t**, **r** draw plain axis lines without ticks or
+# annotations. A frame like ``Frame(axes="WS")`` draws annotated west and south axes
+# only.
 #
-# The example below uses a Cartesian projection, as GMT does not allow
-# labels to be set for geographic maps.
+# The example below uses a Cartesian projection, as GMT does not allow labels to be set
+# for geographic maps.
 
 fig = pygmt.Figure()
 fig.basemap(
     region=[0, 10, 0, 20],
     projection="X10c/8c",
-    # Plot axis with tick marks, annotations, and labels on the
-    # West and South axes
+    # Plot axis with tick marks, annotations, and labels on the West and South axes.
     # Plot axis with tick marks on the north and east axes
     frame=Frame(
         axes="WSne",
