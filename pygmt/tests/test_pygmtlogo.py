be6c052274e130365fc6496c40bbf759e167a42e
Dongdong Tian <seisman.info@gmail.com>
2026-05-13 17:46:40 +0800

    Add a test and baseline image for circular logo with horizontal wordmark

--- pygmt/tests/test_pygmtlogo.py
+++ pygmt/tests/test_pygmtlogo.py
@@ -4,7 +4,7 @@ Test Figure.pygmtlogo.
 
 import pytest
 from pygmt import Figure
-from pygmt.params import Axis, Position
+from pygmt.params import Axis, Frame, Position
 from pygmt.src.pygmtlogo import _create_logo
 
 
@@ -47,5 +47,42 @@ def test_pygmtlogo_circle_no_wordmark():
         position=Position((3.5, 1), anchor="CM", cstype="mapcoords"),
         theme="dark",
         color=False,
+    )
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_pygmtlogo_circle_horizontal_wordmark():
+    """
+    Test the PyGMT circular logo with a horizontal wordmark, including both light/dark
+    themes, and colored/black-and-white versions.
+    """
+    fig = Figure()
+    fig.basemap(
+        region=[-0.5, 8.0, -0.5, 10.0],
+        projection="x1c",
+        frame=Frame(fill="gray", axis=Axis(grid=0.5)),
+    )
+    fig.pygmtlogo(
+        position=Position((0, 8.5), anchor="ML", cstype="mapcoords"),
+        theme="light",
+        wordmark="horizontal",
+    )
+    fig.pygmtlogo(
+        position=Position((0, 6), anchor="ML", cstype="mapcoords"),
+        theme="dark",
+        wordmark="horizontal",
+    )
+    fig.pygmtlogo(
+        position=Position((0, 3.5), anchor="ML", cstype="mapcoords"),
+        theme="light",
+        color=False,
+        wordmark="horizontal",
+    )
+    fig.pygmtlogo(
+        position=Position((0, 1), anchor="ML", cstype="mapcoords"),
+        theme="dark",
+        color=False,
+        wordmark="horizontal",
     )
     return fig
