41d4ba63ed0b51ea1d588035bf367a3d714dfc91
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-06-25 09:06:28 +0200

    Improve intro text

--- examples/gallery/lines/fill_areas_between_curves.py
+++ examples/gallery/lines/fill_areas_between_curves.py
@@ -3,17 +3,16 @@ Fill area between curves
 ========================
 The :meth:`pygmt.Figure.fill_between` method fills the area between two curves y1 and
 y2. Different fills (colors or patterns) can be used for the areas y1 > y2 and
-y1 < y2. Optionally, the curves can be drawn. The two curves can be co-registered or
-have different x-coordinates.
+y1 < y2. The two curves can be co-registered or have different x-coordinates. To plot
+an anomaly along a track use :meth:`pygmt.Figure.wiggle` and see the gallery example
+:doc:`Wiggle along tracks </gallery/lines/wiggle>`.
 
 For filling the areas between the two curves use the ``fill`` and ``fill2`` parameters
-to set different fills for areas with y1 > y2 and y1 < y2, respectively. Use the  ``label``
-and ``label2`` parameters to set the corresponding legend entries. In addition to filling
-the areas, we can draw the curves. Use the ``pen`` and  ``pen2`` parameters to set different
-lines for the two curves y1 and y2, respectively.
+to set different fills for the areas with y1 > y2 and y1 < y2, respectively. Use the
+``label`` and ``label2`` parameters to set the corresponding legend entries. In addition
+to filling the areas, we can draw the outline curves. Use the ``pen`` and  ``pen2``
+parameters to set different lines for the two curves y1 and y2, respectively.
 
-To plot an anomaly along a track use :meth:`pygmt.Figure.wiggle` and see the gallery
-example :doc:`Wiggle along tracks </gallery/lines/wiggle>`.
 """
 
 # %%
@@ -26,7 +25,7 @@ y1 = np.sin(3 * x)
 y2 = np.sin(x / 2)
 
 # Partly Segmentation fault problem (at least on Windows)
-x3 = np.sort((np.random.rand(20) - 0.5) * 20)
+x3 = np.sort((np.random.Generator(20) - 0.5) * 20)
 # Line limit still applies
 x3 = np.array([-10, -8.21, -7.4, -6.63, -5.89, -4.18, -3.45, -2.69, -1.96, 0.26, 1.61, 2.23, 3.49, 4.0, 5.28, 6.79, 7.12, 8.25, 9.13, 10])  # noqa: E501
 y3 = np.sin(x3 / 2)
@@ -51,8 +50,8 @@ fig.show()
 
 
 # %%
-# To compare two co-registered curves y1(x) and y2(x) pass a sequence with the same length
-# as the inputs for ``x`` and ``y`` to ``y2``.
+# To compare two co-registered curves y1(x) and y2(x) pass a sequence with the same
+# length as the inputs for ``x`` and ``y`` to ``y2``.
 
 fig = pygmt.Figure()
 fig.basemap(region=[-10, 10, -3.5, 3.5], projection="X15c/5c", frame=True)
@@ -69,7 +68,7 @@ fig.show()
 # x-coordinates. For providing the x-coordinates for the second curve, use
 # parameter ``x2``.  Via the ``legend_pen`` parameter the appearence in the legend
 # can be changed to draw the legend entries as colored lines (using the fill colors)
-# instead of filled boxes.
+# instead of filled and outlined boxes.
 
 fig = pygmt.Figure()
 fig.basemap(region=[-10, 10, -3.5, 3.5], projection="X15c/5c", frame=True)
