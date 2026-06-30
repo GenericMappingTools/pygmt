b372fce6dfdf61a1af824c330f4db959a9a07462
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-06-30 13:58:40 +0200

    Add note regarding not adding colorbars

--- examples/tutorials/advanced/create_grid_masks.py
+++ examples/tutorials/advanced/create_grid_masks.py
@@ -7,6 +7,8 @@ shapes given as closed polygons. These polygons can be provided as
 :class:`numpy.ndarray` or :class:`geopandas.GeoDataFrame`. For the nodes falling inside,
 outside, and on the edges, different values can be defined. The created mask can then be
 applied to a desired grid.
+As the focus of this tutorial is on creating a grid mask and a related masked grid, no
+colorbars for the elevation grids are added to the maps.
 
 To create a land-water mask based on the GMT built-in shoreline data you can directly
 use the function :func:`pygmt.grdlandmask` explained in the gallery example
