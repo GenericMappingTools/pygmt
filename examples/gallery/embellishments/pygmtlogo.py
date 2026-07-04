7a1880985fb2b012f6e2affa720c9b00a0edb705
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-07-04 17:01:47 +0200

    Add comments

--- examples/gallery/embellishments/pygmtlogo.py
+++ examples/gallery/embellishments/pygmtlogo.py
@@ -1,21 +1,21 @@
 """
 PyGMT logo
 ==========
-Beside the GMT logo, there is a separate PyGMT logo which can be plotted and added
-to a figure using :meth:`pygmt.Figure.pygmtlogo`. The design of the logo itself is
-kindly provided by `@sfrooti <https://github.com/sfrooti>`_ and consists of a visual
-and the wordmark "PyGMT".
+Beside the GMT logo, there is a separate PyGMT logo which can be plotted and added to a
+figure using :meth:`pygmt.Figure.pygmtlogo`. The design of the logo itself is kindly
+provided by `@sfrooti <https://github.com/sfrooti>`_ and consists of a visual and the
+wordmark "PyGMT".
 
 The PyGMT logo is available in circle and hexagon shapes. It can be plotted using colors
 of Python (blue and yellow) and GMT (red) or in black and white as well as in light or
-dark mode. The wordmark "PyGMT" can be added on the right or bottom sides of the visual.
+dark mode. The wordmark can be added on the right side or at the bottom of the visual.
 """
 
 # %%
 import pygmt
 
 # %%
-# By default
+# Plot the PyGMT logo without any arguments:
 
 fig = pygmt.Figure()
 fig.pygmtlogo()
@@ -23,16 +23,17 @@ fig.show()
 
 
 # %%
-# ``theme``, ``color``, ``shape``
+# Via the ``color``, ``theme``, ``shape`` parameters the appereance of the logo can be
+# changed:
 
 fig = pygmt.Figure()
 fig.basemap(region=[-5, 5, -5, 5], projection="10c/10c", frame=True)
 
-fig.pygmtlogo(theme="dark", position="TL")
+fig.pygmtlogo(color=False, position="TL")
 
-fig.pygmtlogo(color=False, position="TR")
+fig.pygmtlogo(theme="dark", position="TR")
 
-fig.pygmtlogo(theme="dark", color=False, position="BL")
+fig.pygmtlogo(color=False, theme="dark", position="BL")
 
 fig.pygmtlogo(shape="hexagon", position="BR")
 
@@ -40,7 +41,8 @@ fig.show()
 
 
 # %%
-# ``wordamrk``
+# Via the ``wordamrk`` parameter the text "PyGMT" can be added on the right side
+# or at the bottom of the visual:
 
 fig = pygmt.Figure()
 fig.basemap(region=[-5, 5, -5, 5], projection="10c/10c", frame=True)
