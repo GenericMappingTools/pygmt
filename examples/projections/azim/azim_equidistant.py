768a54b2d199136e9c51b1badd1ecf50183e2eb0
Xingchen He <chuanjun1978@gmail.com>
2026-07-21 16:53:33 +0800

    Update examples/projections/azim/azim_equidistant.py
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- examples/projections/azim/azim_equidistant.py
+++ examples/projections/azim/azim_equidistant.py
@@ -15,7 +15,7 @@ locations relative to the projection center.
 - *lon0/lat0*: Sets the projection center.
 - *horizon*: The maximum distance from the projection center to the edge of the
   map (i.e. the visible portion of the rest of the world map) in degrees
-  (<= 180°) [default is 180°]. [Optional]
+  (<= 180°) [Optional, default is 180°].
 - *scale* or *width*: Sets the figure size.
 """
 
