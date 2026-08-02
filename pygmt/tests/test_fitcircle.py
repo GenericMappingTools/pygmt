ea1646db239726492f1a9c419bd089b99d0aebdb
Will Schlitzer <schlitzer90@gmail.com>
2026-08-01 22:08:39 -0400

    Updates to fitcircle and test_fitcircle

--- pygmt/tests/test_fitcircle.py
+++ pygmt/tests/test_fitcircle.py
@@ -1,15 +1,15 @@
 """
-Tests for fitcircle.
+Test pygmt.fitcircle.
 """
 
-import os
+from pathlib import Path
 
 import numpy as np
 import numpy.testing as npt
 import pandas as pd
 import pytest
 from pygmt import fitcircle
-from pygmt.exceptions import GMTInvalidInput
+from pygmt.exceptions import GMTParameterError, GMTValueError
 from pygmt.helpers import GMTTempFile
 from pygmt.src import which
 
@@ -17,35 +17,28 @@ from pygmt.src import which
 @pytest.fixture(scope="module", name="data")
 def fixture_data():
     """
-    Load the sample data from the sat_03 remote file.
+    Load the sample data from the @sat_03 remote file.
     """
     fname = which("@sat_03.txt", download="c")
-    data = pd.read_csv(
-        fname,
-        header=None,
-        skiprows=1,
-        sep="\t",
-        names=["longitutde", "latitude", "z"],
+    return pd.read_csv(
+        fname, header=None, skiprows=1, sep="\t", names=["longitude", "latitude", "z"]
     )
-    return data
 
 
+@pytest.mark.benchmark
 def test_fitcircle_no_outfile(data):
     """
     Test fitcircle with no set outfile.
     """
     result = fitcircle(data=data, norm=True)
+    assert isinstance(result, pd.DataFrame)
     assert result.shape == (7, 3)
     # Test longitude results
     npt.assert_allclose(result.longitude.min(), 52.7434273422)
     npt.assert_allclose(result.longitude.max(), 330.243649573)
-    npt.assert_allclose(result.longitude.mean(), 223.078116476)
-    npt.assert_allclose(result.longitude.median(), 232.7449849)
     # Test latitude results
     npt.assert_allclose(result.latitude.min(), -21.2085369093)
     npt.assert_allclose(result.latitude.max(), 21.2085369093)
-    npt.assert_allclose(result.latitude.mean(), -7.8863683297)
-    npt.assert_allclose(result.latitude.median(), -18.406777)
 
 
 def test_fitcircle_file_output(data):
@@ -57,46 +50,47 @@ def test_fitcircle_file_output(data):
             data=data, norm=True, outfile=tmpfile.name, output_type="file"
         )
         assert result is None  # return value is None
-        assert os.path.exists(path=tmpfile.name)  # check that outfile exists
+        assert Path(tmpfile.name).stat().st_size > 0  # check that outfile exists
 
 
 def test_fitcircle_invalid_format(data):
     """
     Test that fitcircle fails with an incorrect format for output_type.
     """
-    with pytest.raises(GMTInvalidInput):
+    with pytest.raises(GMTValueError):
         fitcircle(data=data, norm=True, output_type="a")
 
 
-def test_fitcircle_no_normalize(data):
+def test_fitcircle_no_norm(data):
     """
-    Test that fitcircle fails with an argument is missing for normalize.
+    Test that fitcircle fails when the required "norm" parameter is missing.
     """
-    with pytest.raises(GMTInvalidInput):
+    with pytest.raises(GMTParameterError):
         fitcircle(data=data)
 
 
 def test_fitcircle_no_outfile_specified(data):
     """
-    Test that fitcircle fails when outpput_type is set to 'file' but no output
-    file name is specified.
+    Test that fitcircle fails when output_type is set to "file" but no outfile
+    is specified.
     """
-    with pytest.raises(GMTInvalidInput):
+    with pytest.raises(GMTParameterError):
         fitcircle(data=data, norm=True, output_type="file")
 
 
-def test_filter1d_outfile_incorrect_output_type(data):
+def test_fitcircle_outfile_incorrect_output_type(data):
     """
-    Test that filter1d raises a warning when an outfile filename is set but the
-    output_type is not set to 'file'.
+    Test that fitcircle raises a warning when an outfile filename is set but the
+    output_type is not set to "file".
     """
-    with pytest.warns(RuntimeWarning):
-        with GMTTempFile(suffix=".txt") as tmpfile:
+    with GMTTempFile(suffix=".txt") as tmpfile:
+        with pytest.warns(RuntimeWarning) as record:
             result = fitcircle(
                 data=data, norm=True, outfile=tmpfile.name, output_type="numpy"
             )
-            assert result is None  # return value is None
-            assert os.path.exists(path=tmpfile.name)  # check that outfile exists
+        assert len(record) == 1  # check that only one warning was raised
+        assert result is None  # return value is None
+        assert Path(tmpfile.name).stat().st_size > 0  # check that outfile exists
 
 
 def test_fitcircle_format(data):
@@ -109,3 +103,13 @@ def test_fitcircle_format(data):
     assert isinstance(circle_array, np.ndarray)
     circle_df = fitcircle(data=data, norm=True, output_type="pandas")
     assert isinstance(circle_df, pd.DataFrame)
+
+
+def test_fitcircle_small_circle(data):
+    """
+    Test that fitcircle can fit a small circle instead of a great circle.
+    """
+    result = fitcircle(data=data, norm=2, small_circle=True)
+    assert isinstance(result, pd.DataFrame)
+    assert result.shape == (5, 3)
+    assert "Small Circle Pole" in result.method.iloc[-1]
