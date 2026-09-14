19e882c37b76587f2f658f0fd0b2eaf51110552f
Dongdong Tian <seisman.info@gmail.com>
2026-09-14 10:58:51 +0800

    Figure.histogram: Migrate parameter 'stairs' to the new alias system (#4884)

--- pygmt/src/histogram.py
+++ pygmt/src/histogram.py
@@ -24,7 +24,6 @@ from pygmt.params import Axis, Frame
     F="center",
     L="extreme",
     N="distribution",
-    S="stairs",
     T="series",
     Z="histtype",
     b="binary",
@@ -44,6 +43,7 @@ def histogram(
     pen: str | None = None,
     fill: str | None = None,
     horizontal: bool = False,
+    stairs: bool = False,
     cumulative: bool | Literal["reverse"] = False,
     projection: str | None = None,
     region: Sequence[float | str] | str | None = None,
@@ -70,6 +70,7 @@ def histogram(
        - J = projection
        - Q = cumulative
        - R = region
+       - S = stairs
        - V = verbose
        - W = pen
        - c = panel
@@ -125,9 +126,9 @@ def histogram(
         bins. To only include extreme values below first bin into the first
         bin, use **l**, and to only include extreme values above the last bin
         into that last bin, use **h**.
-    stairs : bool
-        Draw a stairs-step diagram which does not include the internal bars
-        of the default histogram.
+    stairs
+        Draw a stairs-step diagram which does not include the internal bars of the
+        default histogram.
     horizontal
         Plot the histogram horizontally from x = 0 [Default is vertically from y = 0].
         The plot dimensions remain the same, but the two axes are flipped, i.e., the
@@ -177,6 +178,7 @@ def histogram(
         ],
         G=Alias(fill, name="fill"),
         Q=Alias(cumulative, name="cumulative", mapping={"reverse": "r"}),
+        S=Alias(stairs, name="stairs"),
         W=Alias(pen, name="pen"),
     ).add_common(
         B=frame,
